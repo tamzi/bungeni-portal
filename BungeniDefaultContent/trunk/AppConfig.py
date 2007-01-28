dbdc8fbd0ac967737feb34e4c67569a73ac6de7e
jean.jordaan <jean.jordaan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2007-01-28 20:10:39 +0000

    r4698@klippie:  jean | 2007-01-28 22:03:05 +0200  Fleshing out the content structure according to the layout doc. Still need to decide on content types (docs vs topics vs new custom types, e.g. for committee?)

--- BungeniDefaultContent/trunk/AppConfig.py
+++ BungeniDefaultContent/trunk/AppConfig.py
@@ -2,16 +2,68 @@
 DEPENDENCIES = ['Bungeni', ]
 
 DEFAULT_SITE_CONTENT = (
-            {'type': 'Folder', 'id': 'about', 'children': (
-                {'type': 'Document', 'id': 'index_html', 'text': 'Nothing much', 'children': ()},
-                )},
-            {'type': 'Folder', 'id': 'people', 'children': (
-                {'type': 'Document', 'id': 'index_html', 'text': 'Nothing much', 'children': ()},
-                )},
-            {'type': 'Folder', 'id': 'business', 'children': (
-                {'type': 'Document', 'id': 'index_html', 'text': 'Nothing much', 'children': ()},
-                )},
-            {'type': 'Folder', 'id': 'publications', 'children': (
-                {'type': 'Document', 'id': 'index_html', 'text': 'Nothing much', 'children': ()},
+        {'id': 'business', 'title': 'Business', 'type': 'Folder', 
+        'children': (
+            {'id': 'index_html', 'title': 'Business', 'type': 'Document',
+            'text': 'Documents about parliamentary business', 
+            'children': ()},
+            {'id': 'chamber', 'title': 'Chamber', 'type': 'Folder', 'children': (
+                {'title': 'Business Bulletin', 'type': '«»', 'children': ()}, 
+                {'title': 'Debate Report', 'type': '«»', 'children': ()}, 
+                {'title': 'Minutes of Proceedings', 'type': '«»', 'children': ()}, 
+                {'title': 'Tabled Documents', 'type': '«»', 'children': ()}, 
+                {'title': 'Voting Records', 'type': '«»', 'children': ()}, 
+                {'title': 'Attendance List', 'type': '«»', 'children': ()}, 
+                ),},
+            {'id': 'committees', 'title': 'Committees', 'type': 'Topic',  # TODO add criteria for smart folder to select committees
+            'children': (),},
+            {'id': 'bills', 'title': 'Bills', 'type': '«»', 'children': (),},
+            {'id': 'motions', 'title': 'Motions', 'type': '«»', 'children': (),},
+            {'id': 'questions', 'title': 'Questions', 'type': '«»', 'children': (),},
+            {'id': 'events', 'title': 'Events', 'type': '«»', 'children': (),},
+            ),
+        },
+        {'id': 'people', 'type': 'Folder', 'children': (
+            {'id': 'index_html', 'type': 'Document', 
+            'text': 'Lists the MPs and staff. Use RichTopic?',
+            'children': ()},
+            )
+        },
+        {'id': 'about', 'type': 'Folder', 'children': (
+            {'id': 'index_html', 'type': 'Document', 
+            'text': 'Background information about the parliament',
+            'children': (
+                {'title': 'How it works', 'type': '«»', 'children': (
+                    {'title': 'Parliamentary Procedure', 'type': 'Document', 'children': ()}, 
+                    {'title': 'Standing Orders', 'type': 'Document', 'children': ()}, 
+                    {'title': 'The Legislative Process', 'type': 'Document', 'children': ()}, 
+                    {'title': 'Oversight', 'type': 'Document', 'children': ()}, 
+                    {'title': 'Guides', 'type': 'Document', 'children': ()}, 
+                    {'title': 'Guide on Public Bills', 'type': 'Document', 'children': ()}, 
+                    {'title': 'Guide on Private Bills', 'type': 'Document', 'children': ()}, 
+                    {'title': 'Guide for Motions', 'type': 'Document', 'children': ()}, 
+                    {'title': 'Guide for Parliamentary Questions', 'type': 'Document', 'children': ()}, 
+                    {'title': 'Guide for Committee Conveners', 'type': 'Document', 'children': ()}, 
+                    {'title': 'Guide on Committees', 'type': 'Document', 'children': ()}, 
+                    )}, 
+                {'title': 'History', 'type': '«»', 'children': ()}, 
+                {'title': 'How to visit it', 'type': '«»', 'children': ()}, 
+                {'title': 'How to participate', 'type': '«»', 'children': ()}, 
+                {'title': 'Learning resources', 'type': '«»', 'children': ()}, 
+                {'title': 'Pictures', 'type': '«»', 'children': ()}, 
                 )},
             )
+        },
+        {'id': 'publications', 'type': 'Folder', 'children': (
+            {'id': 'index_html', 'type': 'Document', 
+            'text': 'Publications include legislation and hansard.',
+            'children': ()},
+            )
+        },
+        {'id': 'organisation', 'type': 'Folder', 'children': (
+            {'id': 'index_html', 'type': 'Document', 
+            'text': 'The parliament is structured as follows ...',
+            'children': ()},
+            )
+        },
+        )

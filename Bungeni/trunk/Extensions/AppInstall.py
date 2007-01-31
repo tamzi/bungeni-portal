909744c5d6554f5c8bd670d743f433b6b7272929
jean.jordaan <jean.jordaan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2007-01-31 06:25:51 +0000

    r4723@klippie:  jean | 2007-01-31 08:23:03 +0200  Reconfigure member data container to accept Bungeni member types

--- Bungeni/trunk/Extensions/AppInstall.py
+++ Bungeni/trunk/Extensions/AppInstall.py
@@ -1,6 +1,6 @@
 from StringIO import StringIO
 from Products.CMFCore.utils import getToolByName
-from Products.remember.utils import getAdderUtility
+from Products.remember.utils import getAdderUtility, getRememberTypes
 
 def install(self):
     """ Do stuff that GS will do for us soon ..
@@ -15,11 +15,19 @@ def install(self):
         result = setup_tool.runAllImportSteps()
         print >>out,  'Steps run: %s \n' % ', '.join(result['steps'])
 
-    # Change default Member type. Bungeni prefers MPs & clerks.
-    wft = getToolByName(self, 'portal_workflow')
-    wft.setChainForPortalTypes( ['Member'], "member_approval_workflow")
+    # Change default member to MemberOfPublic
     plone = getToolByName(self, 'portal_url').getPortalObject()
     adder = getAdderUtility(plone)
     adder.default_member_type = 'MemberOfPublic'
+
+    # Require approval for the adding of plain old members
+    wft = getToolByName(self, 'portal_workflow')
+    wft.setChainForPortalTypes( ['Member'], "member_approval_workflow")
+
+    # Allow member data container to store the new member types
+    types_tool = getToolByName(self, 'portal_types')
+    memberdata_tool = getToolByName(self, 'portal_memberdata')
+    fti = types_tool.getTypeInfo(memberdata_tool)
+    fti.manage_changeProperties(allowed_content_types=getRememberTypes(self))
 
     return out.getvalue()

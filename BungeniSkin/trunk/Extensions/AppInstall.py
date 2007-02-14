1ff543252ec632258908885262f65637c35d510c
jean.jordaan <jean.jordaan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2007-02-14 23:48:44 +0000

    r5146@klippie:  jean | 2007-02-15 01:41:15 +0200  Tweak actions as part of the skin

--- BungeniSkin/trunk/Extensions/AppInstall.py
+++ BungeniSkin/trunk/Extensions/AppInstall.py
@@ -8,6 +8,9 @@ def install(self):
 
     plone = getToolByName(self, 'portal_url').getPortalObject()
 
+    #
+    # Tweak portlets
+    #
     # Leave only the theme slot on the left; move all the others to the
     # right.
     left_slots = list(plone.getProperty('left_slots'))
@@ -21,5 +24,22 @@ def install(self):
             })
 
     print >>out, 'Moved all the left slots but the theme(s) to the right.'
+
+    #
+    # Tweak actions
+    #
+    actions_tool = getToolByName(portal, 'portal_actions', None)
+    changed_actions = {
+        'rss': {'category': 'site_actions',}
+        }
+    cloned_actions = actions_tool._cloneActions()
+    for action in cloned_actions:
+        changes = changed_actions.get(action.getId(), None)
+        if changes:
+            action.edit(**changes)
+            print >>out, 'Tweaked %s action.' % action.getId()
+    actions_tool._actions = cloned_actions
+
+    print >>out, 'Tweaked actions.'
 
     return out.getvalue()

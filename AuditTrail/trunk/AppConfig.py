3a1c8c28b3760250695e7e83c25c78c212e5bbf4
jean.jordaan <jean.jordaan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2007-03-20 12:18:09 +0000

    r5846@klippie:  jean | 2007-03-20 14:06:04 +0200  Leave Marshall dependency in, but commented. Integrate it in future.

--- AuditTrail/trunk/AppConfig.py
+++ AuditTrail/trunk/AppConfig.py
@@ -2,3 +2,10 @@ import os
 from Globals import INSTANCE_HOME 
 
 LOG_PATH = os.path.join(INSTANCE_HOME, 'log', 'audit.log')
+
+# # We depend on marshall because we want to register one consistent
+# # marshaller to use for the auditing.
+# DEPENDENCIES = [
+#     'Marshall',
+#     ]
+

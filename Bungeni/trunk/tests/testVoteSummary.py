a9767401f6eb9f1a45a412ffdf50e743653c6247
jean.jordaan <jean.jordaan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2007-06-20 13:23:01 +0000

    r9700@klippie:  jean | 2007-06-20 15:28:28 +0300  Test new classes

---
+++ Bungeni/trunk/tests/testVoteSummary.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+#
+# File: testVoteSummary.py
+#
+# Copyright (c) 2007 by []
+# Generator: ArchGenXML Version 1.6.0-beta-svn
+#            http://plone.org/products/archgenxml
+#
+# GNU General Public License (GPL)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+
+__author__ = """Jean Jordaan <jean.jordaan@gmail.com>"""
+__docformat__ = 'plaintext'
+
+import os, sys
+if __name__ == '__main__':
+    execfile(os.path.join(sys.path[0], 'framework.py'))
+
+##code-section module-header #fill in your manual code here
+##/code-section module-header
+
+#
+# Test-cases for class(es) VoteSummary
+#
+
+from Testing import ZopeTestCase
+from Products.Bungeni.config import *
+from Products.Bungeni.tests.testBungeni import testBungeni
+
+# Import the tested classes
+from Products.Bungeni.votes.VoteSummary import VoteSummary
+
+##code-section module-beforeclass #fill in your manual code here
+##/code-section module-beforeclass
+
+
+class testVoteSummary(testBungeni):
+    """Test-cases for class(es) ."""
+
+    ##code-section class-header_testVoteSummary #fill in your manual code here
+    ##/code-section class-header_testVoteSummary
+
+    def afterSetUp(self):
+        pass
+
+    # Manually created methods
+
+
+def test_suite():
+    from unittest import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(testVoteSummary))
+    return suite
+
+##code-section module-footer #fill in your manual code here
+##/code-section module-footer
+
+if __name__ == '__main__':
+    framework()
+
+

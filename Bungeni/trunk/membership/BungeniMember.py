d346c5f521816bfe820fc0811a35e448fa2dcf6b
jean.jordaan <jean.jordaan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2007-08-03 14:58:42 +0000

    Latest version of model.

--- Bungeni/trunk/membership/BungeniMember.py
+++ Bungeni/trunk/membership/BungeniMember.py
@@ -192,7 +192,7 @@ class BungeniMember(BaseContent):
 
     security.declarePublic('setExpirationDate')
     def setExpirationDate(self):
-        """ 
+        """
         # TODO: why is this needed again?
         # (comment is here because of
         # http://plone.org/products/archgenxml/issues/128)

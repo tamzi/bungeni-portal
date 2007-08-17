6f047ba4d842b4b490d49f2f0f9bd9b9162f8ec3
jean.jordaan <jean.jordaan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2007-08-17 10:06:26 +0000

    A reference class to store information about someone's tenure as (assistant) minister.

---
+++ Bungeni/trunk/content/Minister.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+#
+# File: Minister.py
+#
+# Copyright (c) 2007 by []
+# Generator: ArchGenXML Version 1.6.0-beta-svn
+#            http://plone.org/products/archgenxml
+#
+# GNU General Public License (GPL)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+
+__author__ = """Jean Jordaan <jean.jordaan@gmail.com>"""
+__docformat__ = 'plaintext'
+
+from AccessControl import ClassSecurityInfo
+from Products.Archetypes.atapi import *
+from zope import interface
+from Products.Bungeni.config import *
+
+##code-section module-header #fill in your manual code here
+##/code-section module-header
+
+schema = Schema((
+
+),
+)
+
+##code-section after-local-schema #fill in your manual code here
+##/code-section after-local-schema
+
+Minister_schema = BaseSchema.copy() + \
+    schema.copy()
+
+##code-section after-schema #fill in your manual code here
+##/code-section after-schema
+
+class Minister(BaseContent):
+    """
+    """
+    security = ClassSecurityInfo()
+    __implements__ = (getattr(BaseContent,'__implements__',()),)
+
+    # This name appears in the 'add' box
+    archetype_name = 'Minister'
+
+    meta_type = 'Minister'
+    portal_type = 'Minister'
+    allowed_content_types = []
+    filter_content_types = 0
+    global_allow = 1
+    #content_icon = 'Minister.gif'
+    immediate_view = 'base_view'
+    default_view = 'base_view'
+    suppl_views = ()
+    typeDescription = "Minister"
+    typeDescMsgId = 'description_edit_minister'
+
+    _at_rename_after_creation = True
+
+    schema = Minister_schema
+
+    ##code-section class-header #fill in your manual code here
+    ##/code-section class-header
+
+    # Methods
+
+
+registerType(Minister, PROJECTNAME)
+# end of class Minister
+
+##code-section module-footer #fill in your manual code here
+##/code-section module-footer
+
+
+

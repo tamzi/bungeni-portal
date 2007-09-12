22b22ff2b588ffe25ece780090ea11c320ba4b0f
jean.jordaan <jean.jordaan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2007-09-12 14:18:39 +0000

    Added a marker interface for an IObjectAddedEvent.

--- Bungeni/trunk/interfaces/IDebateRecordFolder.py
+++ Bungeni/trunk/interfaces/IDebateRecordFolder.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# File: interfaces.py
+# File: IDebateRecordFolder.py
 #
 # Copyright (c) 2007 by []
 # Generator: ArchGenXML Version 1.6.0-beta-svn
@@ -28,23 +28,25 @@ __author__ = """Jean Jordaan <jean.jordaan@gmail.com>"""
 __docformat__ = 'plaintext'
 
 
-##code-section init-module-header #fill in your manual code here
-##/code-section init-module-header
+##code-section module-header #fill in your manual code here
+##/code-section module-header
 
 
-# Subpackages
-# Additional
 
-# Classes
-import IBungeniMember
-import IMemberOfParliament
-import IClerk
-import IBill
-import IRotaFolder
-import IRotaItem
-import ITake
-import IDebateRecordFolder
 
-##code-section init-module-footer #fill in your manual code here
-##/code-section init-module-footer
+from zope import interface
+
+class IDebateRecordFolder(interface.Interface):
+    ''' '''
+
+    ##code-section class-header_IDebateRecordFolder #fill in your manual code here
+    ##/code-section class-header_IDebateRecordFolder
+
+
+
+
+##code-section module-footer #fill in your manual code here
+##/code-section module-footer
+
+
 

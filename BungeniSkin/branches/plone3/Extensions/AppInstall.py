b657a6e5425685f87e0eb1e98ef51adf8fcc407c
kapilt <kapilt@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2007-11-14 05:32:02 +0000

    make a branch for updating to plone3

---
+++ BungeniSkin/branches/plone3/Extensions/AppInstall.py
@@ -0,0 +1,45 @@
+from StringIO import StringIO
+from Products.CMFCore.utils import getToolByName
+
+def install(self):
+    """ Tweak settings .. 
+    """
+    out = StringIO()
+
+    plone = getToolByName(self, 'portal_url').getPortalObject()
+
+    #
+    # Tweak portlets
+    #
+    # Leave only the theme slot on the left; move all the others to the
+    # right.
+    left_slots = list(plone.getProperty('left_slots'))
+    right_slots = list(plone.getProperty('right_slots'))
+    theme_slots = [slot for slot in left_slots if 'theme' in slot]
+    for s in theme_slots:
+        left_slots.remove(s)
+    plone.manage_changeProperties({
+            'left_slots': theme_slots,
+            'right_slots': left_slots + right_slots,
+            })
+
+    print >>out, 'Moved all the left slots but the theme(s) to the right.'
+
+    #
+    # Tweak actions
+    #
+    actions_tool = getToolByName(self, 'portal_actions', None)
+    changed_actions = {
+        'rss': {'category': 'site_actions',}
+        }
+    cloned_actions = actions_tool._cloneActions()
+    for action in cloned_actions:
+        changes = changed_actions.get(action.getId(), None)
+        if changes:
+            action.edit(**changes)
+            print >>out, 'Tweaked %s action.' % action.getId()
+    actions_tool._actions = cloned_actions
+
+    print >>out, 'Tweaked actions.'
+
+    return out.getvalue()
---
+++ BungeniSkin/branches/plone3/LookAndFeel.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+#
+# File: LookAndFeel.py
+#
+# Copyright (c) 2007 by []
+# Generator: ArchGenXML Version 1.5.1-svn
+#            http://plone.org/products/archgenxml
+#
+# GNU General Public License (GPL)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+
+__author__ = """unknown <unknown>"""
+__docformat__ = 'plaintext'
+
+from AccessControl import ClassSecurityInfo
+from Products.Archetypes.atapi import *
+from Products.BungeniSkin.config import *
+
+##code-section module-header #fill in your manual code here
+##/code-section module-header
+
+schema = Schema((
+
+),
+)
+
+##code-section after-local-schema #fill in your manual code here
+##/code-section after-local-schema
+
+LookAndFeel_schema = schema.copy()
+
+##code-section after-schema #fill in your manual code here
+##/code-section after-schema
+
+class LookAndFeel(BaseContent):
+    """
+    """
+    security = ClassSecurityInfo()
+    __implements__ = (getattr(BaseContent,'__implements__',()),)
+
+    allowed_content_types = []
+
+    actions =  (
+
+
+       {'action': "string:${object_url}/main_template",
+        'category': "object",
+        'id': 'main_template',
+        'name': 'main_template',
+        'permissions': ("View",),
+        'condition': 'python:1'
+       },
+
+
+       {'action': "string:${object_url}/global_logo",
+        'category': "object",
+        'id': 'global_logo',
+        'name': 'global_logo',
+        'permissions': ("View",),
+        'condition': 'python:1'
+       },
+
+
+       {'action': "string:${object_url}/global_sections",
+        'category': "object",
+        'id': 'global_sections',
+        'name': 'global_sections',
+        'permissions': ("View",),
+        'condition': 'python:1'
+       },
+
+
+       {'action': "string:${object_url}/global_siteactions",
+        'category': "object",
+        'id': 'global_siteactions',
+        'name': 'global_siteactions',
+        'permissions': ("View",),
+        'condition': 'python:1'
+       },
+
+
+    )
+
+    _at_rename_after_creation = True
+
+    schema = LookAndFeel_schema
+
+    ##code-section class-header #fill in your manual code here
+    ##/code-section class-header
+
+    # Methods
+
+# end of class LookAndFeel
+
+##code-section module-footer #fill in your manual code here
+##/code-section module-footer
+
+
+

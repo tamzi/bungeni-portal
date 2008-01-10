d7414bc990735396744ca2e95d256c4327036c7e
kapilt <kapilt@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-01-10 07:13:45 +0000

    fix issue with forward property on backref property

--- bungeni.core/trunk/bungeni/core/orm.py
+++ bungeni.core/trunk/bungeni/core/orm.py
@@ -1,7 +1,7 @@
 
 
 
-
+import sqlalchemy as rdb
 from sqlalchemy.orm import mapper, relation
 
 import schema
@@ -28,11 +28,12 @@ mapper( domain.Group, schema.groups,
 mapper( domain.GroupMembership, schema.user_group_memberships,
         properties={
             'user': relation( domain.User,
-                              primaryjoin=schema.user_group_memberships.c.user_id==schema.users.c.user_id,
+                              primaryjoin=rdb.and_(schema.user_group_memberships.c.user_id==schema.users.c.user_id, 
+                                                   schema.user_group_memberships.c.group_id==schema.groups.c.group_id ),
                               lazy=False ),
-            'group': relation( domain.Group,
-                               primaryjoin=schema.user_group_memberships.c.user_id==schema.users.c.user_id,
-                               lazy=False ),                              
+#            'group': relation( domain.Group,
+#                               primaryjoin=schema.user_group_memberships.c.user_id==schema.users.c.user_id,
+#                               lazy=False ),                              
             'replaced': relation( domain.User,
                                   primaryjoin=schema.user_group_memberships.c.replaced_id==schema.users.c.user_id,
                                   lazy=True ),

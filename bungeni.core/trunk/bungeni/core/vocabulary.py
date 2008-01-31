2a34ecb179cb2c0fc44389b46862351b187b8b3f
kapilt <kapilt@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-01-31 19:07:32 +0000

    add session vocabulary, fix for member vocab

--- bungeni.core/trunk/bungeni/core/vocabulary.py
+++ bungeni.core/trunk/bungeni/core/vocabulary.py
@@ -8,8 +8,9 @@ from bungeni import core
 #ModelTypeSource = ObjectSource( model.DataModelType, 'short_name', 'id')
 #SecurityLevelSource = DatabaseSource( model.SecurityLevel, 'short_name', 'id' )
 
-ParliamentMembers = ObjectSource( core.ParliamentMember, 'name', 'id' )
+ParliamentMembers = ObjectSource( core.ParliamentMember, 'name', 'member_id' )
 PoliticalParties  = ObjectSource( core.PoliticalParty, 'full_name', "id")
+ParliamentSessions = ObjectSource( core.ParliamentSession, 'short_name', 'session_id')
 
 
                              

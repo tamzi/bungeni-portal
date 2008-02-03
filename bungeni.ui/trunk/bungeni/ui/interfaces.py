123ecab858ec8a91ba56f700bbde5fe9af263d6f
kapilt <kapilt@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-02-03 14:01:21 +0000

    ripping out plone3 ui into separate package

--- bungeni.ui/trunk/bungeni/ui/interfaces.py
+++ bungeni.ui/trunk/bungeni/ui/interfaces.py
@@ -2,39 +2,8 @@
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 from zope.viewlet.interfaces import IViewletManager
 
-class IBungeniSkin( IDefaultBrowserLayer ):
+from ploned.ui.interfaces import IPlonedSkin
+
+class IBungeniSkin( IPlonedSkin ):
     """ skin for bungeni """
 
-class ICSSManager( IViewletManager ):
-    """ viewlet manager for css """
-
-class IJavaScriptManger( IViewletManager ):
-    """ viewlet manager for js """
-
-
-class ILeftColumnManager( IViewletManager ):
-    """ """
-
-class IRightColumnManager( IViewletManager ):
-    """ """
-
-class IAboveContentManager( IViewletManager ):
-    """ """
-
-class IBelowContentManager( IViewletManager ):
-    """ """
-
-class IContentViewsManager( IViewletManager ):
-    """ """
-
-class IPortalHeaderManager( IViewletManager ):
-    """ """
-    
-class IPortalFooterManager( IViewletManager ):
-    """ """
-
-class IContentViews( IViewletManager ):
-    """ """
-
-class IDocumentActions( IViewletManager ):
-    """ """
--- bungeni.ui/trunk/bungeni/ui/macros.py
+++ bungeni.ui/trunk/bungeni/ui/macros.py
@@ -6,14 +6,7 @@ from zope.publisher.browser import BrowserView
 from zope.app.basicskin.standardmacros import StandardMacros as BaseMacros
 from zope.app.pagetemplate import ViewPageTemplateFile
 
-class BungeniLayout( BrowserView ):
-
-    template = ViewPageTemplateFile('templates/bungeni-template.pt')
-
-    def __getitem__(self, key):
-        return self.template.macros[key]
-
 class StandardMacros( BaseMacros ):
 
-    macro_pages = ['bungeni-layout', 'alchemist-form']
+    macro_pages = ['ploned-layout', 'alchemist-form']
 

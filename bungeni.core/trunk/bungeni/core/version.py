241608959cc4be0fdee4e4a45fcb980ed4198098
kapilt <kapilt@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-02-06 21:36:16 +0000

    implement versioned interface (create,revert), discard traversal mechanism, will use views for interacting with versions

--- bungeni.core/trunk/bungeni/core/version.py
+++ bungeni.core/trunk/bungeni/core/version.py
@@ -1,41 +1,56 @@
 """
 $Id: $
 """
-from zope import interface
+from zope import interface, schema
 
 from sqlalchemy import orm
-from ore.alchemist import container
+from ore.alchemist import container, Session
 from zope.security.proxy import removeSecurityProxy
 from z3c.traverser.interfaces import ITraverserPlugin
 
 import domain
 import interfaces
-
-class VersionTraversal( object ):
-    
-    interface.implements( ITraverserPlugin )
-
-    def __init__( self, context, request ):
-        self.context = context
-        self.request = request
-        
-    def publishTraverse(self, request, name):
-        if name != 'versions':
-            return 
-        return interfaces.IVersioned( self.context )
+# 
+# class VersionTraversal( object ):
+#     
+#     interface.implements( ITraverserPlugin )
+# 
+#     def __init__( self, context, request ):
+#         self.context = context
+#         self.request = request
+#         
+#     def publishTraverse(self, request, name):
+#         if name != 'versions':
+#             return 
+#         return interfaces.IVersioned( self.context )
         
 class Versioned( container.PartialContainer ):
+    
+    interface.implements( interfaces.IVersioned )
     
     def create( self ):
         """
         store the existing state of the adapted context as a new version
         """
+        version = self.domain_class()
+        for field in schema.getFields( IModelDescriptor( self.domain_class ).domain_interface ).values():
+            field.set( version, field.query( self.context ) )
+        session = Session()
+        session.save( version )
         
     def revert( self, version ):
         """
         revert the current state of the adapted object to the values specified
-        in version.
+        in version, and create a new version with reverted state. 
         """
+        for field in schema.getFields( IModelDescriptor( self.domain_class ).domain_interface ).values():
+            value = field.query( self.context )
+            if value:
+                field.set( version, value )
+                
+        self.create()
+        
+        
 def ContextVersioned( ob ):
     versionedFactory = globals()['Versioned%s'%(ob.__class__.__name__)]
     return versionedFactory( ob )

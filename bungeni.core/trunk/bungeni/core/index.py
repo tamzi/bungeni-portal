25886de1b77a38c551b5f7d32e37faecbfcfde9c
kapilt <kapilt@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-02-06 16:50:59 +0000

    enable indexing across bungeni content

--- bungeni.core/trunk/bungeni/core/index.py
+++ bungeni.core/trunk/bungeni/core/index.py
@@ -1,19 +1,58 @@
 """
 xapian indexing adapters
 """
-
 from zope import interface
-from ore.xapian import interfaces
+from zope.dottedname import resolve
+import xappy, os, os.path as path
 
-class MotionIndexer( object ):
+from ore.alchemist import container, Session
+from ore.alchemist.interfaces import IModelDescriptor
+from ore.xapian import queue, interfaces as iindex
 
-    interface.implements( interfaces.IIndexer )
-
-    def __init__(self, context):
-        self.context = context
-        
-    def index( self, connection):
-        pass
-        
+# we store indexes in buildout/parts/index
+# 
+def setupStorageDirectory( ):
+    # we start in buildout/src/bungeni.core/bungeni/core
+    # we end in buildout/parts/index    
+    store_dir = __file__
+    x = 0
+    while x < 5:
+        x += 1
+        store_dir = path.split( store_dir )[0]
+    store_dir = path.join( store_dir, 'parts', 'index')
+    if path.exists( store_dir ):
+        assert path.isdir( store_dir )
+    else:
+        os.mkdir( store_dir )
     
+    return store_dir
+
+class ContentResolver( object ):
+
+    interface.implements( iindex.IResolver )
+    scheme = u'bungeni'
     
+    def id( self, object ): 
+        return "%s.%s-%s"%( object.__class__.__module,
+                            object.__class__.__name__,
+                            container.stringKey( object ) )
+        
+    def resolve( self, id ): 
+        class_path, oid = id.split('-')
+        domain_class = resolve.resolve( class_path )
+        session = Session()
+        return session.query( domain_class ).get( oid )
+
+
+store_dir = setupStorageDirectory() 
+
+indexer = xappy.IndexerConnection( store_dir )
+indexer.add_field_action('resolver', xappy.FieldActions.INDEX_EXACT )
+indexer.add_field_action('resolver', xappy.FieldActions.STORE_CONTENT )
+
+# fields for parliamentary items
+indexer.add_field_action('title', xappy.FieldActions.INDEX_FREETEXT )
+indexer.add_field_action('title', xappy.FieldActions.STORE_CONTENT )
+indexer.add_field_action('description', xappy.FieldActions.INDEX_FREETEXT )
+
+queue.QueueProcessor.start( indexer )

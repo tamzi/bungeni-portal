c383134d35b864d0776e0312eb60af3da4a4a868
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-03-07 14:26:34 +0000

    validator actions

---
+++ BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/actions/IBungeniActionValidator.java
@@ -0,0 +1,25 @@
+/*
+ * IBungeniActionValidator.java
+ *
+ * Created on March 3, 2008, 8:15 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.bungeni.editor.actions;
+
+import org.bungeni.ooo.OOComponentHelper;
+
+/**
+ *All the validators for an action
+ * @author Administrator
+ */
+public interface IBungeniActionValidator {
+
+    public org.bungeni.error.BungeniValidatorState check_DocumentLevelAction(toolbarAction action, toolbarSubAction subAction, OOComponentHelper ooDocument) ;
+    public org.bungeni.error.BungeniValidatorState check_TextSelectedInsert(toolbarAction action, toolbarSubAction subAction, OOComponentHelper ooDocument) ;
+    public org.bungeni.error.BungeniValidatorState check_TextSelectedEdit(toolbarAction action, toolbarSubAction subAction, OOComponentHelper ooDocument) ;
+    public org.bungeni.error.BungeniValidatorState check_FullInsert(toolbarAction action, toolbarSubAction subAction, OOComponentHelper ooDocument) ;
+    public org.bungeni.error.BungeniValidatorState check_FullEdit(toolbarAction action, toolbarSubAction subAction, OOComponentHelper ooDocument) ;
+}

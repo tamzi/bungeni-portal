a94f7e9219c7b1e5c8a4a8b22461f45d925f83fe
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-03-09 21:57:50 +0000

    router interfaces

---
+++ BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/actions/routers/defaultRouter.java
@@ -0,0 +1,22 @@
+/*
+ * defaultRouter.java
+ *
+ * Created on March 10, 2008, 12:26 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.bungeni.editor.actions.routers;
+
+/**
+ *
+ * @author Administrator
+ */
+public class defaultRouter {
+    
+    /** Creates a new instance of defaultRouter */
+    public defaultRouter() {
+    }
+    
+}

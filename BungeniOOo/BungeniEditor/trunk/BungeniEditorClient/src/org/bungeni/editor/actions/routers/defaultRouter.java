c8a4810a9c991c39519bb57fc937f807ee0c0b1f
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-03-09 22:28:24 +0000

    updated router class

--- BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/actions/routers/defaultRouter.java
+++ BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/actions/routers/defaultRouter.java
@@ -9,14 +9,57 @@
 
 package org.bungeni.editor.actions.routers;
 
+import org.bungeni.editor.actions.toolbarAction;
+import org.bungeni.editor.actions.toolbarSubAction;
+import org.bungeni.error.BungeniMsg;
+import org.bungeni.error.BungeniValidatorState;
+import org.bungeni.ooo.OOComponentHelper;
+
 /**
  *
  * @author Administrator
  */
-public class defaultRouter {
+public class defaultRouter implements IBungeniActionRouter {
     
     /** Creates a new instance of defaultRouter */
     public defaultRouter() {
+    }
+
+    public BungeniValidatorState route(toolbarAction action, toolbarSubAction subAction, OOComponentHelper ooDoc) {
+      switch(subAction.getSelectorDialogMode()) {
+            case TEXT_INSERTION:
+                return route_FullInsert(action, subAction, ooDoc);
+            case DOCUMENT_LEVEL_ACTION:
+                return route_DocumentLevelAction(action, subAction, ooDoc);
+            case TEXT_EDIT:
+                return route_FullEdit(action, subAction, ooDoc);
+            case TEXT_SELECTED_EDIT:
+                return route_TextSelectedEdit(action, subAction, ooDoc);
+            case TEXT_SELECTED_INSERT:
+                return route_TextSelectedInsert(action, subAction, ooDoc);
+            default:
+                return new BungeniValidatorState(false, new BungeniMsg("DIALOG_MODE_MISSING"));
+        }
+    }
+
+    public BungeniValidatorState route_DocumentLevelAction(toolbarAction action, toolbarSubAction subAction, OOComponentHelper ooDocument) {
+       return new BungeniValidatorState(true, new BungeniMsg("SUCCESS")); 
+    }
+
+    public BungeniValidatorState route_TextSelectedInsert(toolbarAction action, toolbarSubAction subAction, OOComponentHelper ooDocument) {
+       return new BungeniValidatorState(true, new BungeniMsg("SUCCESS")); 
+    }
+
+    public BungeniValidatorState route_TextSelectedEdit(toolbarAction action, toolbarSubAction subAction, OOComponentHelper ooDocument) {
+       return new BungeniValidatorState(true, new BungeniMsg("SUCCESS")); 
+    }
+
+    public BungeniValidatorState route_FullInsert(toolbarAction action, toolbarSubAction subAction, OOComponentHelper ooDocument) {
+       return new BungeniValidatorState(true, new BungeniMsg("SUCCESS")); 
+    }
+
+    public BungeniValidatorState route_FullEdit(toolbarAction action, toolbarSubAction subAction, OOComponentHelper ooDocument) {
+       return new BungeniValidatorState(true, new BungeniMsg("SUCCESS")); 
     }
     
 }

154f5ebf67cc4dc269db7c420b23b942b97bab44
kapilt <kapilt@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-03-09 04:03:34 +0000

    switch personal bar to new menu infrastructure

--- ploned.ui/trunk/ploned/ui/interfaces.py
+++ ploned.ui/trunk/ploned/ui/interfaces.py
@@ -1,6 +1,7 @@
-
+from zope import     schema
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 from zope.viewlet.interfaces import IViewletManager
+from z3c.menu.ready2go import interfaces as imenu
 
 class IPlonedSkin( IDefaultBrowserLayer ):
     """ plone skin for zope3  """
@@ -32,6 +33,17 @@ class IPortalHeaderManager( IViewletManager ):
     
 class IPortalFooterManager( IViewletManager ):
     """ """
+
+class IPersonalBarMenu( imenu.IMenuManager ):
+    """ personalized site actions, login, logout, dashboard"""
+    
+class IPersonalTasksMenu( imenu.IMenuManager ):
+    """ personalized site actions, login, logout, dashboard"""
+
+class IPersonalActionItem( imenu.ISiteMenuItem ):
+    """ we register various role required """
+    groups = schema.List( value_type=schema.Int() )
+    roles = schema.List( value_type=schema.Int() )
 
 class IContentViews( IViewletManager ):
     """ """

ceae3702636c973517f7b1423368ad76db646562
kapilt <kapilt@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-03-22 17:01:15 +0000

    delete package from svn, going to reimport clean

--- bungeni.theme.private/trunk/browser/viewlets.py
+++
@@ -1,7 +0,0 @@
-from zope.component import getMultiAdapter
-from zope.pagetemplate import ViewPageTemplateFile
-from zope.viewlet.viewlet import ViewletBase
-
-class LogoViewlet(ViewletBase):
-    render = ViewPageTemplateFile('publiclogo.pt')
-

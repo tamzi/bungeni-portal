8dbd2f65aa0ab9e25001fec691beef7434e53f5d
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-03-24 20:55:15 +0000

    changed base class to abstracttablemodel instead of implementing treemodel directly.

--- BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/toolbar/BungeniToolbarXMLModelAdapter.java
+++ BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/toolbar/BungeniToolbarXMLModelAdapter.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import javax.swing.event.TreeModelEvent;
 import javax.swing.event.TreeModelListener;
+import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreePath;
 import org.jdom.Attribute;
@@ -22,11 +23,10 @@ import org.jdom.Document;
  *
  * @author Administrator
  */
-public class BungeniToolbarXMLModelAdapter implements TreeModel {
+public class BungeniToolbarXMLModelAdapter extends AbstractTreeModel {
     private Document document;
-    //listeners for changes, not used in this example
-    private ArrayList<TreeModelListener> listenerList = new ArrayList<TreeModelListener>();
-   
+      private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(BungeniToolbarXMLModelAdapter.class.getName());
+ 
     /** Creates a new instance of BungeniToolbarXMLModelAdapter */
     public BungeniToolbarXMLModelAdapter() {
     }
@@ -62,73 +62,4 @@ public class BungeniToolbarXMLModelAdapter implements TreeModel {
         BungeniToolbarXMLAdapterNode jdomNode = (BungeniToolbarXMLAdapterNode)node;
         return (jdomNode.node.getTextTrim().length() > 0);
     }
-
-    //override from TreeModel
-    public void valueForPathChanged(TreePath path, Object newValue) {
-        // Null. We won't be making changes in the GUI
-        // If we did, we would ensure the new value was really new,
-        // adjust the model, and then fire a TreeNodesChanged event.
-    }
-    
-    
-    /*
-     * Use these methods to add and remove event listeners.
-     * (Needed to satisfy TreeModel interface, but not used.)
-     */
-    
-    // override from TreeModel
-	public void addTreeModelListener(TreeModelListener listener) {
-		if (listener != null && !listenerList.contains(listener)) {
-			listenerList.add(listener);
-		}
-	}
-    // override from TreeModel
-	public void removeTreeModelListener(TreeModelListener listener) {
-		if (listener != null) {
-			listenerList.remove(listener);
-		}
-	}
-
-    /* * Invoke these methods to inform listeners of changes. (Not needed for this
-	 * example.) Methods taken from TreeModelSupport class described at
-	 * http://java.sun.com/products/jfc/tsc/articles/jtree/index.html That
-	 * architecture (produced by Tom Santos and Steve Wilson) is more elegant. I
-	 * just hacked 'em in here so they are immediately at hand.
-	 */
-    public void fireTreeNodesChanged(TreeModelEvent e) {
-       
-		Iterator<TreeModelListener> listeners = listenerList.iterator();
-		while (listeners.hasNext()) {
-			TreeModelListener listener = (TreeModelListener) listeners.next();
-			listener.treeNodesChanged(e);
-		}
-              
-         } 
-    public void fireTreeNodesInserted(TreeModelEvent e) {
-        
-		Iterator<TreeModelListener> listeners = listenerList.iterator();
-		while (listeners.hasNext()) {
-			TreeModelListener listener = (TreeModelListener) listeners.next();
-			listener.treeNodesInserted(e);
-		}
-	
-         }   
-    public void fireTreeNodesRemoved(TreeModelEvent e) {
-	
-        Iterator<TreeModelListener> listeners = listenerList.iterator();
-		while (listeners.hasNext()) {
-			TreeModelListener listener = (TreeModelListener) listeners.next();
-			listener.treeNodesRemoved(e);
-		}
-	
-         }   
-    public void fireTreeStructureChanged(TreeModelEvent e) {
-	
-        Iterator<TreeModelListener> listeners = listenerList.iterator();
-		while (listeners.hasNext()) {
-			TreeModelListener listener = (TreeModelListener) listeners.next();
-			listener.treeStructureChanged(e);
-		}
-	
-         }
-}
+ }

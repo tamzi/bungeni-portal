b08063ee26b1a6954a3b32dc39710ef752886cf6
kapilt <kapilt@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-03-24 23:40:21 +0000

    create a stable personal feature dev branch, to avoid automatically getting undiscussed questionable changes on trunk, from rev 1364

---
+++ bungeni.ui/branches/kapilt-dev/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bungeni.ui",
+    version="0.2",
+    author='Bungeni Developers',
+    author_email='bungeni-dev@googlegroups.com',
+    description='UI for Bungeni, based on Plone',
+    license="GPL",
+    keywords = "zope3 bungeni",
+      classifiers = [
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],    
+    packages=find_packages(),
+    package_data = { '': ['*.txt', '*.zcml', '*.pt'] },
+    namespace_packages=['bungeni'],
+    install_requires = [ 'setuptools',
+                         'bungeni.core',
+                         'simplejson',
+                         'alchemist.ui',
+                         'zope.formlib'],
+    zip_safe = False,
+    )
+

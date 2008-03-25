e76804fb59e5e0b19803aae4a96385d2bb70e7ed
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-03-25 21:24:30 +0000

    added methods for form processing

--- BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/selectors/IBungeniForm.java
+++ BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/selectors/IBungeniForm.java
@@ -18,6 +18,20 @@ import org.bungeni.editor.fragments.FragmentsFactory;
  */
 public interface IBungeniForm {
   public String getClassName();
+ 
+  public boolean preFullEdit();
+  public boolean postFullEdit();
+  public boolean processFullEdit();
+
+  
+  public boolean preFullInsert();
+  public boolean postFullInsert();
+  public boolean processFullInsert();
+  
+  public boolean preSelectInsert();
+  public boolean postSelectInsert();
+  public boolean processSelectInsert();
+  
   /*  
   public HashMap<String,String> getPreInsertMap();
   public HashMap<String,Object> getControlDataMap();

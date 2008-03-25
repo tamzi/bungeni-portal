fe7ee1d2d5cc70a74c55a7f003541a9fedec224e
kapilt <kapilt@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-03-25 06:42:37 +0000

    rename dtml css to add .dtml extension to map new resource class, minor modifications to remove some z2/plone dependent dtml calls

--- bungeni.theme.private/trunk/bungeni/theme/private/css/__init__.py
+++ bungeni.theme.private/trunk/bungeni/theme/private/css/__init__.py
@@ -2,5 +2,5 @@
 
 from zope.viewlet.viewlet import CSSViewlet
 
-Main = CSSViewlet('css/main.css')
-Public = CSSViewlet('css/public.css')
+Main = CSSViewlet('css/main.css.dtml')
+Public = CSSViewlet('css/public.css.dtml')

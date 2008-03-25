8b86820ed0804fa5f2a00d5c39617dec8d8be8b3
kapilt <kapilt@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-03-25 04:23:39 +0000

    change to using skin directories for theme, and qualify css resources with skin dir name

--- bungeni.theme.private/trunk/bungeni/theme/private/css/__init__.py
+++ bungeni.theme.private/trunk/bungeni/theme/private/css/__init__.py
@@ -2,5 +2,5 @@
 
 from zope.viewlet.viewlet import CSSViewlet
 
-Main = CSSViewlet('main.css')
-Public = CSSViewlet('public.css')
+Main = CSSViewlet('css/main.css')
+Public = CSSViewlet('css/public.css')

934ac7ad2f7a0c72caf6b64f8414173df461961a
crazylunjay <crazylunjay@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-03-31 12:30:30 +0000

    updated to include references,numbering and headings

--- BungeniOOo/OfficeNumberingApp/src/org/numbering/dialogs/sectionNumbererPanel.java
+++ BungeniOOo/OfficeNumberingApp/src/org/numbering/dialogs/sectionNumbererPanel.java
@@ -6,12 +6,28 @@
 
 package org.numbering.dialogs;
 
+import com.sun.star.beans.UnknownPropertyException;
+import com.sun.star.beans.XPropertySet;
+import com.sun.star.beans.XPropertySetInfo;
 import com.sun.star.container.NoSuchElementException;
+import com.sun.star.container.XEnumeration;
+import com.sun.star.container.XEnumerationAccess;
 import com.sun.star.container.XNamed;
 import com.sun.star.lang.WrappedTargetException;
 import com.sun.star.lang.XComponent;
+import com.sun.star.lang.XServiceInfo;
+import com.sun.star.text.XSimpleText;
+import com.sun.star.text.XText;
+import com.sun.star.text.XTextContent;
+import com.sun.star.text.XTextCursor;
+import com.sun.star.text.XTextRange;
 import com.sun.star.text.XTextSection;
+import com.sun.star.uno.AnyConverter;
+import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XComponentContext;
+import com.sun.star.util.XNumberFormatTypes;
+import com.sun.star.util.XNumberFormats;
+import com.sun.star.util.XNumberFormatsSupplier;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -23,6 +39,7 @@ import org.bungeni.ooo.BungenioOoHelper;
 import org.bungeni.ooo.OOComponentHelper;
 import org.apache.log4j.Logger;
 import org.bungeni.ooo.ooQueryInterface;
+import org.bungeni.ooo.ooUserDefinedAttributes;
 
 /**
  *
@@ -35,6 +52,8 @@ public class sectionNumbererPanel extends javax.swing.JPanel {
     private BungenioOoHelper openofficeObject;
     HashMap<String, String> sectionMetadataMap=new HashMap<String, String>();
     private static org.apache.log4j.Logger log = Logger.getLogger(sectionNumbererPanel.class.getName());
+
+   
     
     /** Creates new form sectionNumbererPanel */
     public sectionNumbererPanel() {
@@ -50,7 +69,7 @@ public class sectionNumbererPanel extends javax.swing.JPanel {
     private void init(){
         initSectionsArray();
         listSectionTypes.addListSelectionListener(new NumberingSchemeListener());
-        panelNumberingScheme.setVisible(false);
+        //panelNumberingScheme.setVisible(false);
     }
     
     
@@ -58,7 +77,7 @@ public class sectionNumbererPanel extends javax.swing.JPanel {
   
         openofficeObject = new org.bungeni.ooo.BungenioOoHelper(xContext);
         openofficeObject.initoOo();
-        xComponent = openofficeObject.openDocument("/home/undesa/downloads/setSectionMetadata(2).odt");
+        xComponent = openofficeObject.openDocument("/home/undesa/downloads/sections1.odt");
         ooDocument = new OOComponentHelper(xComponent, xContext);
         try{
             if (!ooDocument.isXComponentValid()) return;
@@ -102,9 +121,10 @@ public class sectionNumbererPanel extends javax.swing.JPanel {
                                while(metaIterator.hasNext()){
                                         for(int i=0; i< sectionMetadataMap.size(); i++) {
                                             String metaName = (String) metaIterator.next();
-                                            if(metaName.startsWith("BungeniSectionType")){
-                                                model.addElement(metaName + " " + "-" + sectionMetadataMap.get(metaName));
-                                            }
+                                         //  if(metaName.startsWith("BungeniSectionType")){
+                                                //model.addElement(metaName + " " + "-" + sectionMetadataMap.get(metaName));
+                                                model.addElement(sectionMetadataMap.get(metaName));
+                                          // }
                                             
                                            
                                         }
@@ -135,6 +155,153 @@ public class sectionNumbererPanel extends javax.swing.JPanel {
         
     }
     
+    private void applyNumberingFormat(){
+        HashMap<String,String> metadata = new HashMap();
+        metadata.put("NumberingScheme",cboNumberingScheme.getSelectedItem().toString());
+          
+        try{
+            Object sectionName = ooDocument.getTextSections().getByName("part2");
+            log.debug("set numbering scheme attribute " + cboNumberingScheme.getSelectedItem().toString());
+            ooDocument.setSectionMetadataAttributes("part2",metadata);
+          
+       
+        
+        }catch (NoSuchElementException ex) {
+            log.error(ex.getMessage());
+        } catch (WrappedTargetException ex) {
+            log.error(ex.getMessage());
+        }
+        
+       
+       
+       
+        
+    }
+    //method to insert reference mark over heading
+    private void insertReferenceMark(XTextRange aTextRange, String refName){
+        
+    }
+    
+    private void insertNumber(XTextRange aTextRange){
+        //get numbering scheme from drop down
+        //setup start and end figures
+       String numberingScheme =cboNumberingScheme.getSelectedItem().toString();
+        XText xRangeText=aTextRange.getText();
+        XTextCursor xTextCursor = xRangeText.createTextCursorByRange(aTextRange.getStart());
+                                xTextCursor.gotoRange(aTextRange.getEnd(), true);
+        xRangeText.insertString(xTextCursor,"1" + ") ",false);
+    }
+    
+    //nethod to get reference mark from heading
+    private void getReferenceMark(XTextRange aTextRange, Object elem){
+        XText xRangeText=aTextRange.getText();
+        XEnumerationAccess xRangeAccess = (XEnumerationAccess)UnoRuntime.queryInterface(com.sun.star.container.XEnumerationAccess.class,elem);
+         XEnumeration portionEnum =  xRangeAccess.createEnumeration();
+         while (portionEnum.hasMoreElements()){
+             try{
+                 Object textPortion =  portionEnum.nextElement();  
+                  XServiceInfo xTextPortionService= ooDocument.getServiceInfo(textPortion);
+                   if (xTextPortionService.supportsService( "com.sun.star.text.TextPortion")){
+                      XPropertySet xTextPortionProps = (XPropertySet)UnoRuntime.queryInterface(XPropertySet.class, textPortion);
+                      String textPortionType="";
+                      
+                        try {
+                            textPortionType = AnyConverter.toString(xTextPortionProps.getPropertyValue("TextPortionType"));
+                            if (textPortionType.equals("ReferenceMark")){
+                                return;
+                          } else{
+                                log.debug("no reference mark found");
+                                //insert reference mark
+                               // XText xText = aTextRange.getText();
+                                XTextCursor xTextCursor = xRangeText.createTextCursorByRange(aTextRange.getStart());
+                                xTextCursor.gotoRange(aTextRange.getEnd(), true);
+                                XNamed xRefMark = (XNamed) UnoRuntime.queryInterface(XNamed.class,
+                                        ooDocument.createInstance("com.sun.star.text.ReferenceMark"));
+                                
+                                 xRefMark.setName("refText");
+                                 XTextContent xContent = (XTextContent) UnoRuntime.queryInterface(XTextContent.class, xRefMark);
+                                 xRangeText.insertTextContent(xTextCursor,xContent,true);
+                                 
+                                 insertNumber(aTextRange);
+                                
+                                
+                          }
+                        } catch (com.sun.star.lang.IllegalArgumentException ex) {
+                            ex.printStackTrace();
+                        } catch (WrappedTargetException ex) {
+                            ex.printStackTrace();
+                        } catch (UnknownPropertyException ex) {
+                            ex.printStackTrace();
+                        }
+                          
+                     
+                      
+                      
+                      
+                   }
+                  
+             }catch(NoSuchElementException ex){
+                 log.error(ex.getMessage());
+             }catch (WrappedTargetException ex) {
+                log.error(ex.getMessage());
+             }
+             
+         }
+    }
+    //method to get heading from section
+    private void getHeading() {
+        
+        try{
+            Object sectionName = ooDocument.getTextSections().getByName("part2");
+            XTextSection theSection = ooQueryInterface.XTextSection(sectionName);
+            XTextRange range = theSection.getAnchor();
+            XText xText = range.getText();
+             XEnumerationAccess enumAccess = (XEnumerationAccess) UnoRuntime.queryInterface(XEnumerationAccess.class, xText);
+             XEnumeration enumeration = enumAccess.createEnumeration();
+            log.debug("InitSectionsArray: starting Enumeration ");
+             while (enumeration.hasMoreElements()) {
+               Object elem = enumeration.nextElement();
+                
+                 
+               XServiceInfo xInfo = (XServiceInfo)UnoRuntime.queryInterface(XServiceInfo.class, elem);
+               if(xInfo.supportsService("com.sun.star.text.Paragraph")){
+                    XPropertySet objProps = ooQueryInterface.XPropertySet(xInfo);
+                    
+                    short nLevel = -1;
+                    try {
+                        nLevel = com.sun.star.uno.AnyConverter.toShort(objProps.getPropertyValue("ParaChapterNumberingLevel"));
+                     if(nLevel>=0){
+                        XTextContent xContent = ooDocument.getTextContent(elem);
+                        XTextRange aTextRange =   xContent.getAnchor();
+                        String strHeading = aTextRange.getString();
+                        
+                       //insert reference mark
+                         getReferenceMark(aTextRange,elem);
+                        //apply numbering scheme
+                         log.debug("heading found " + strHeading);
+                      }
+                  }catch (com.sun.star.lang.IllegalArgumentException ex){
+                      log.error(ex.getMessage());
+                  }
+               }
+ 
+                
+                  
+                 
+             }
+       
+        
+        }catch (NoSuchElementException ex) {
+            log.error(ex.getMessage());
+        } catch (WrappedTargetException ex) {
+            log.error(ex.getMessage());
+        }catch(UnknownPropertyException ex){
+            log.error(ex.getMessage());
+        }catch(IllegalArgumentException ex){
+             log.error(ex.getMessage());
+        }
+        
+    }
     /** This method is called from within the constructor to
      * initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is
@@ -148,6 +315,7 @@ public class sectionNumbererPanel extends javax.swing.JPanel {
         txtSectionType = new javax.swing.JTextField();
         cboNumberingScheme = new javax.swing.JComboBox();
         btnApplyNumberingScheme = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         listSectionTypes.setModel(new javax.swing.AbstractListModel() {
             String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
@@ -161,6 +329,11 @@ public class sectionNumbererPanel extends javax.swing.JPanel {
         cboNumberingScheme.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Base Numbering", "Point Numbering", "Roman Numerals", "Alphabet" }));
 
         btnApplyNumberingScheme.setText("Apply Numbering Scheme");
+        btnApplyNumberingScheme.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnApplyNumberingSchemeActionPerformed(evt);
+            }
+        });
 
         org.jdesktop.layout.GroupLayout panelNumberingSchemeLayout = new org.jdesktop.layout.GroupLayout(panelNumberingScheme);
         panelNumberingScheme.setLayout(panelNumberingSchemeLayout);
@@ -186,6 +359,8 @@ public class sectionNumbererPanel extends javax.swing.JPanel {
                 .addContainerGap(83, Short.MAX_VALUE))
         );
 
+        jLabel1.setText("Bungeni Section Types");
+
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -193,25 +368,36 @@ public class sectionNumbererPanel extends javax.swing.JPanel {
             .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING)
-                    .add(org.jdesktop.layout.GroupLayout.LEADING, panelNumberingScheme, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .add(org.jdesktop.layout.GroupLayout.LEADING, jLabel1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 218, Short.MAX_VALUE)
+                    .add(panelNumberingScheme, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .add(org.jdesktop.layout.GroupLayout.LEADING, jScrollPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 218, Short.MAX_VALUE))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(layout.createSequentialGroup()
-                .addContainerGap()
+                .add(19, 19, 19)
+                .add(jLabel1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 15, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
                 .add(jScrollPane1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                .add(34, 34, 34)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
                 .add(panelNumberingScheme, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnApplyNumberingSchemeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnApplyNumberingSchemeActionPerformed
+       
+       // applyNumberingFormat();
+        getHeading();
+        
+    }//GEN-LAST:event_btnApplyNumberingSchemeActionPerformed
     
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnApplyNumberingScheme;
     private javax.swing.JComboBox cboNumberingScheme;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JList listSectionTypes;
     private javax.swing.JPanel panelNumberingScheme;

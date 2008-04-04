d708937ae817fd774abc897cd953b80833e83937
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-04-04 09:43:06 +0000

    Use <xapian:queue>-directive to set up indexing queue.

--- bungeni.core/trunk/bungeni/core/index.py
+++ bungeni.core/trunk/bungeni/core/index.py
@@ -63,7 +63,7 @@ class ContentIndexer( object ):
         doc.fields.append( 
              xappy.Field( "object_type", self.context.__class__.__name__ )
              )
-        
+
         # schema fields one by one
         for iface in interface.providedBy( self.context ):
             for field in schema.getFields( iface ).values():
@@ -79,6 +79,33 @@ class ContentIndexer( object ):
         return doc
 
 
+####################
+## Field Definitions
+#
+
+def setupFieldDefinitions(indexer):
+    # resolution utility type
+    indexer.add_field_action('resolver', xappy.FieldActions.INDEX_EXACT )
+    indexer.add_field_action('resolver', xappy.FieldActions.STORE_CONTENT )
+
+    # content type
+    indexer.add_field_action('object_type', xappy.FieldActions.INDEX_FREETEXT )
+    indexer.add_field_action('object_type', xappy.FieldActions.STORE_CONTENT )
+
+    # workflow status
+    indexer.add_field_action('status', xappy.FieldActions.INDEX_EXACT )
+    indexer.add_field_action('status', xappy.FieldActions.STORE_CONTENT )
+
+    # site relative path
+    indexer.add_field_action('path', xappy.FieldActions.STORE_CONTENT )
+
+    # fields for parliamentary items
+    # XXX xapian stores language codes at the field level, need to
+    # find a solution for this
+    indexer.add_field_action('title', xappy.FieldActions.INDEX_FREETEXT, weight=5, language='en' )
+    indexer.add_field_action('title', xappy.FieldActions.STORE_CONTENT )
+
+# storage directory
 store_dir = setupStorageDirectory() 
 
 # search connection hub
@@ -87,31 +114,5 @@ searcher = search.IndexSearch( store_dir )
 # async indexer 
 indexer = xappy.IndexerConnection( store_dir )
 
-####################
-## Field Definitions
-# 
-# resolution utility type
-indexer.add_field_action('resolver', xappy.FieldActions.INDEX_EXACT )
-indexer.add_field_action('resolver', xappy.FieldActions.STORE_CONTENT )
-
-# content type
-indexer.add_field_action('object_type', xappy.FieldActions.INDEX_FREETEXT )
-indexer.add_field_action('object_type', xappy.FieldActions.STORE_CONTENT )
-
-# workflow status
-indexer.add_field_action('status', xappy.FieldActions.INDEX_EXACT )
-indexer.add_field_action('status', xappy.FieldActions.STORE_CONTENT )
-
-# fields for parliamentary items
-# XXX xapian stores language codes at the field level, need to find a solution for this
-indexer.add_field_action('title', xappy.FieldActions.INDEX_FREETEXT, weight=5, language='en' )
-indexer.add_field_action('title', xappy.FieldActions.STORE_CONTENT )
-
-## End Field Definitions
-####################
-
-# start the processing thread
-queue.QueueProcessor.start( indexer )
-
-
-
+# field definitions
+setupFieldDefinitions(indexer)

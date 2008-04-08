a69bc6b2d6df67c642a09406a1f684795786c4a3
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-04-08 15:48:56 +0000

    Initial import

---
+++ bungeni.messaging/trunk/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bungeni.messaging",
+    version="0.1",
+    author='Bungeni Developers',
+    author_email='bungeni-dev@googlegroups.com',
+    description='Messaging capabilities for Bungeni',
+    keywords = "zope3 bungeni",
+      classifiers = [
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],    
+    packages=find_packages(),
+    package_data = { '': ['*.txt', '*.zcml'] },
+    namespace_packages=['bungeni'],
+    install_requires = [ 'setuptools',
+                         'zope.schema',
+                         'zope.interface',
+                         'zope.sendmail',
+                         'plone.smtp',]
+    zip_safe = False,
+    )
+

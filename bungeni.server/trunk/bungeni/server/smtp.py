f093e12d633eb6140004ebd71e13790ac58aded1
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-04-11 15:35:44 +0000

    Removed bungeni.messaging from externals and merged package contents into bungeni.server.

---
+++ bungeni.server/trunk/bungeni/server/smtp.py
@@ -0,0 +1,23 @@
+import smtplib
+
+import zope.interface
+import zope.sendmail.interfaces
+
+class SMTPMailer(object):
+    """A direct mailer for use with zope.sendmail."""
+
+    zope.interface.implements(zope.sendmail.interfaces.ISMTPMailer)
+
+    def send(self, fromaddr, toaddrs, message):
+        hostname = 'localhost'
+        port = 25
+        username = password = None
+
+        connection = smtp.SMTP(hostname, port)
+
+        # authenticate if needed
+        if username is not None and password is not None:
+            connection.login(username, password)
+
+        connection.sendmail(fromaddr, toaddrs, message)
+        connection.quit()

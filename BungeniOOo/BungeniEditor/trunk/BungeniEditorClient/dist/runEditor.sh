18cc11b02dcfba3d74bbe2cc3a14a02ade4233b4
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-05-07 16:28:02 +0000

    added log4j.ignoreTCL to suppress loading of external log4j in Ubuntu

--- BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/dist/runEditor.sh
+++ BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/dist/runEditor.sh
@@ -1,2 +1,4 @@
 export JAVA_HOME=/usr/java/jdk1.5.0_11
-$JAVA_HOME/bin/java -jar BungeniEditorClient.jar
+export BUNGENI_EDITOR_HOME=.
+export LOG4J_HOME=$BUNGENI_EDITOR_HOME/lib
+$JAVA_HOME/bin/java -Dlog4j.ignoreTCL=true -jar BungeniEditorClient.jar

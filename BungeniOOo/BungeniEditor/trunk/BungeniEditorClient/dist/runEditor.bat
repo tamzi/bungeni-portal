cc793d41d742d2409220f5f3b1333749d21a4be4
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-05-08 16:51:29 +0000

    updated batch file to invoke... jre 1.6 appears substantially faster !

--- BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/dist/runEditor.bat
+++ BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/dist/runEditor.bat
@@ -1,2 +1,3 @@
-LOG4J_PROPS=%CD%\settings\log4j.properties
-java -Dlog4j.ignoreTCL -Dlog4j.configuration=%LOG4J_PROPS% -jar BungeniEditorClient.jar
+set JAVA_HOME=e:\Java\jdk1.6.0
+set LOG4J_PROPS="file:///e:/projects/workingprojects/bungenieditor/trunk/BungeniEditorClient/dist/settings/log4j.properties"
+%JAVA_HOME%\bin\java -Dlog4j.ignoreTCL -Dlog4j.configuration=%LOG4J_PROPS% -jar BungeniEditorClient.jar

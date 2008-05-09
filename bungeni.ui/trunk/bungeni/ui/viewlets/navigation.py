9cbe4c3c595d9c423e6479d38ad6e2cfabab2713
christian.ledermann <christian.ledermann@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-05-09 09:07:17 +0000

    crude impementation of breadcrumbs

---
+++ bungeni.ui/trunk/bungeni/ui/viewlets/navigation.py
@@ -0,0 +1,53 @@
+# encoding: utf-8
+
+from zope.viewlet import viewlet
+from zope.app.pagetemplate import ViewPageTemplateFile
+from zope.interface.verify import verifyObject
+from zope.security import proxy
+
+from ore.alchemist.interfaces import IAlchemistContainer, IAlchemistContent
+from ore.alchemist.model import queryModelDescriptor
+
+class BreadCrumbsViewlet( viewlet.ViewletBase ):
+    """
+    render the Breadcrumbs to show a user his current context
+    """
+    def __init__( self,  context, request, view, manager ):        
+        self.context = context
+        self.request = request
+        self.__parent__= view
+        self.manager = manager
+        self.path = []
+
+
+    def _get_path( self, context): 
+        """
+        Return the current path as a list
+        """
+        path = []
+        context = proxy.removeSecurityProxy( context )
+        if context.__parent__ is not None:
+            path = path + self._get_path(context.__parent__)        
+        if  IAlchemistContent.providedBy(context):
+            path.append( getattr(context, 'short_name', None ))
+        if IAlchemistContainer.providedBy(context):                        
+            domain_model = context._class 
+            descriptor = queryModelDescriptor( domain_model )
+            if descriptor:
+                name = getattr( descriptor, 'display_name', None)
+            if not name:
+                name = getattr( context, '__name__', None)  
+            path.append(name)                         
+        return path
+        
+        
+        
+    def update( self ):
+        """
+        prepare the data needed to render the viewlet.        
+        """
+        self.path = self._get_path(self.context)       
+        
+    render = ViewPageTemplateFile( 'breadcrumbs.pt' )        
+    
+    

9a19090bf19f144e8e9fc96e94f80274d0f90ef4
christian.ledermann <christian.ledermann@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-05-14 11:29:24 +0000

    navigation for container as well as for content, breadcrumbs at the right place

--- bungeni.ui/trunk/bungeni/ui/viewlets/navigation.py
+++ bungeni.ui/trunk/bungeni/ui/viewlets/navigation.py
@@ -7,6 +7,9 @@ from zope.security import proxy
 
 from ore.alchemist.interfaces import IAlchemistContainer, IAlchemistContent
 from ore.alchemist.model import queryModelDescriptor
+from alchemist.traversal.managed import ManagedContainerDescriptor
+
+import pdb
 
 class BreadCrumbsViewlet( viewlet.ViewletBase ):
     """
@@ -18,7 +21,7 @@ class BreadCrumbsViewlet( viewlet.ViewletBase ):
         self.__parent__= view
         self.manager = manager
         self.path = []
-
+        self.navigation_root_url ='/'
 
     def _get_path( self, context, url = '' ): 
         """
@@ -51,4 +54,62 @@ class BreadCrumbsViewlet( viewlet.ViewletBase ):
         
     render = ViewPageTemplateFile( 'breadcrumbs.pt' )        
     
+class NavigationTreeViewlet( viewlet.ViewletBase ):
+    """
+    render a navigation tree
     
+    """ 
+    template = ViewPageTemplateFile('contained-constraint-navigation.pt')
+        
+    def __init__( self,  context, request, view, manager ):        
+        self.context = context
+        self.request = request
+        self.__parent__= view
+        self.manager = manager
+        self.path = []
+        self.name  =''
+        
+    def _get_menu ( self, context ):
+        """
+        Return the navigation tree as a list of dictionaries:
+        name, url, node       
+        """
+        items = []
+        context = proxy.removeSecurityProxy( context )
+        
+        if context.__parent__ is not None:                    
+            url = '../'
+            if IAlchemistContent.providedBy(context.__parent__):             
+                context_class = context.__parent__.__class__
+                context_class = proxy.removeSecurityProxy( context_class )                
+                #pdb.set_trace()
+                for k, v in context_class.__dict__.items():
+                    if isinstance( v, ManagedContainerDescriptor ):                    
+                        domain_model = v.domain_container._class 
+                        descriptor = queryModelDescriptor( domain_model )
+                        if descriptor:
+                            name = getattr( descriptor, 'display_name', None)
+                        if not name:
+                            name = domain_model.__name__
+                        current='odd'
+                        if domain_model == context._class:
+                            current='even'   
+                        i = { 'name' : name,
+                              'url'  : url + k,
+                              'current': current }
+                        items.append( i ) 
+            self.name = getattr(context.__parent__, 'short_name', None )                                                                              
+        return items       
+   
+   
+    def update( self ):
+        """
+        prepare the data needed to render the viewlet.        
+        """
+        self.menu = self._get_menu(self.context)    
+          
+    
+    def render( self ):
+        return self.template()
+        
+        

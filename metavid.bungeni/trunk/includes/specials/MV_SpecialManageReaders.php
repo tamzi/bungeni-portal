eeda1e376e2cc71e39cfae518a716280b51b8cec
mianonjoka <mianonjoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-05-15 14:16:02 +0000

    editors, readers, reports interface(work in progress)

--- metavid.bungeni/trunk/includes/specials/MV_SpecialManageReaders.php
+++ metavid.bungeni/trunk/includes/specials/MV_SpecialManageReaders.php
@@ -1,7 +1,47 @@
 <?php
 
-class MV_SpecialManageReaders
-{
+if (!defined('MEDIAWIKI')) die();
+ 
+global $IP;
+require_once( "$IP/includes/SpecialPage.php" );
+
+function doSpecialManageReaders() {
+	$MV_SpecialManageReaders = new MV_SpecialManageReaders();
+	$MV_SpecialManageReaders->execute();
 }
 
+SpecialPage::addPage( new SpecialPage('Mv_manage_readers','',true,'doSpecialManageReaders',false) );
+
+class MV_SpecialManageReaders
+{
+
+	function execute()
+	{
+		global $wgOut;
+		$html='';
+		$html.='<form name=test>';
+		$html.='<table>';
+		$html.='<tr><td>';
+		$html.='<select name="editor" size=20 onchange=load(this.value)>';
+		$html.='</select>';
+		$html.='</td>';
+		$html.='<td>-></td>';
+		$html.='<td>';
+		$html.='<select name="assigned" size=20 width=20>';
+		$html.='</select>';
+		$html.='</td>';
+		$html.='<td><table><tr><td> <a onclick="add()"> <== </a></td></tr><tr><td><a onclick="remove()"> ==> </a></td></tr></table></td>';
+		$html.='<td>';
+		$html.='<select name="unassigned" size=20 width=20>';
+		$html.='</select>';
+		$html.='</td>';
+		$html.='</tr>';
+		$html.='<tr>';
+		$html.='<td><input type=submit value="Save Changes"></input><td>';
+		$html.='</tr>';
+		$html.='</table>';
+		$html.='</form>';
+		$wgOut->addHTML($html);
+	}
+}
 ?>

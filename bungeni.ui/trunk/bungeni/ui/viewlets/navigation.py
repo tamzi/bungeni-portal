7b45effff09edc9ea8df993c4dab5055a15b608d
christian.ledermann <christian.ledermann@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-05-19 15:02:34 +0000

    kairo navigation

--- bungeni.ui/trunk/bungeni/ui/viewlets/navigation.py
+++ bungeni.ui/trunk/bungeni/ui/viewlets/navigation.py
@@ -8,6 +8,7 @@ from zope.security import proxy
 from ore.alchemist.interfaces import IAlchemistContainer, IAlchemistContent
 from ore.alchemist.model import queryModelDescriptor
 from alchemist.traversal.managed import ManagedContainerDescriptor
+from bungeni.core.app import BungeniApp
 
 import pdb
 
@@ -68,48 +69,141 @@ class NavigationTreeViewlet( viewlet.ViewletBase ):
         self.manager = manager
         self.path = []
         self.name  =''
+        self.tree = []
         
-    def _get_menu ( self, context ):
+
+   
+    def _get_object_path(self, context, url=''):
         """
-        Return the navigation tree as a list of dictionaries:
-        name, url, node       
+        traverse up the tree 
+        """
+        path = []
+        context = proxy.removeSecurityProxy( context )
+        if context.__parent__ is not None:            
+            path = path + self._get_object_path(context.__parent__, '../' + url )     
+        else:            
+            return []
+        path.append({'obj':context, 'url':url})            
+        return path
+   
+   
+        
+        
+    def _append_child( self, path):
+        """
+        build the navigation tree
         """
         items = []
-        context = proxy.removeSecurityProxy( context )
-        
-        if context.__parent__ is not None:                    
-            url = '../'
-            if IAlchemistContent.providedBy(context.__parent__):             
-                context_class = context.__parent__.__class__
-                context_class = proxy.removeSecurityProxy( context_class )                
-                #pdb.set_trace()
+        url = path[0]['url']
+        if IAlchemistContent.providedBy(path[0]['obj']):                     
+            item = {'name' : getattr(path[0]['obj'], 'short_name', None ), 'url' : url, 'current': 'navTreeCurrentNode'}
+            if len(path) > 1:
+                item['node'] = self._append_child(path[1:])
+            else: 
+                # leaf => append actions
+                item['current'] = 'navTreeCurrentItem'                                        
+                context_class = path[0]['obj'].__class__
+                context_class = proxy.removeSecurityProxy( context_class )    
+                
+                citems =[]
                 for k, v in context_class.__dict__.items():
-                    if isinstance( v, ManagedContainerDescriptor ):                    
+                    if isinstance( v, ManagedContainerDescriptor ):
                         domain_model = v.domain_container._class 
                         descriptor = queryModelDescriptor( domain_model )
                         if descriptor:
                             name = getattr( descriptor, 'display_name', None)
                         if not name:
                             name = domain_model.__name__
-                        current='odd'
-                        if domain_model == context._class:
-                            current='even'   
+                            
                         i = { 'name' : name,
-                              'url'  : url + k,
-                              'current': current }
-                        items.append( i ) 
-            self.name = getattr(context.__parent__, 'short_name', None )                                                                              
-        return items       
-   
-   
+                              'current' : '',
+                              'url'  :  k, 
+                              'node' : None}
+                        citems.append( i )
+                item['node'] = citems
+            items.append(item)
+        if IAlchemistContainer.providedBy(path[0]['obj']):                        
+            if (path[0]['obj'].__parent__ is None) or (path[0]['obj'].__parent__.__class__ == BungeniApp): 
+                #Navigation Root               
+                domain_model = path[0]['obj']._class 
+                descriptor = queryModelDescriptor( domain_model )
+                if descriptor:
+                    name = getattr( descriptor, 'display_name', None)
+                if not name:
+                    name = getattr( domain_model, '__name__', None) 
+                item = { 'name' : name,
+                            'url'  : url ,
+                            'current': 'navTreeCurrentNode', 
+                             } 
+                if len(path) > 1:
+                    item['node'] = self._append_child(path[1:])
+                else:
+                    item['node'] = None
+                items.append(item)                     
+                return items
+
+                         
+            context_class = path[0]['obj'].__parent__.__class__
+            context_class = proxy.removeSecurityProxy( context_class )                
+            for k, v in context_class.__dict__.items():
+                if isinstance( v, ManagedContainerDescriptor ):
+                    domain_model = v.domain_container._class 
+                    descriptor = queryModelDescriptor( domain_model )                    
+                    if descriptor:
+                        name = getattr( descriptor, 'display_name', None)
+                    if not name:
+                        name = domain_model.__name__
+                    if domain_model == path[0]['obj']._class:   
+                        item = { 'name' : name,
+                            'url'  : url + '../' + k,
+                            'current': 'navTreeCurrentNode', 
+                             }
+                        if len(path) > 1:
+                            item['node'] = self._append_child(path[1:])
+                        else:
+                            item['node'] = None
+                            #item['current'] = 'navTreeCurrentItem' 
+                    else:                            
+                        item = { 'name' : name,
+                              'current' : '',
+                              'node' : None,
+                              'url'  : url + '../' + k }
+                    items.append( item )  
+        return items                                          
+            
+    def _tree2html(self, items):
+        htmlstr = '<ul class="navTree">'
+        for item in items:
+            htmlstr = htmlstr + '<li class="navTreeItem ' + item['current'] +'" >'
+            htmlstr = htmlstr + '<div><a href="' + item['url'] + '"' + ' class="'  + item['current'] +'" >'
+            htmlstr = htmlstr + str( item['name'] )   
+            htmlstr = htmlstr + '</a></div>'
+           
+            if item['node']:
+                htmlstr = htmlstr + self._tree2html(item['node']) 
+            htmlstr = htmlstr +  '</li>'           
+        htmlstr = htmlstr +  '</ul>'
+        return htmlstr
+                    
+    def _get_tree ( self, context, url = ''):
+        items = []
+        path = self._get_object_path(context)
+        items = self._append_child( path)
+        return self._tree2html(items)
+        
+       
+        
+        
+    
     def update( self ):
         """
         prepare the data needed to render the viewlet.        
-        """
-        self.menu = self._get_menu(self.context)    
-          
+        """       
+        self.tree = self._get_tree(self.context)  
+    
     
     def render( self ):
-        return self.template()
+        return str(self.tree)
         
+
         

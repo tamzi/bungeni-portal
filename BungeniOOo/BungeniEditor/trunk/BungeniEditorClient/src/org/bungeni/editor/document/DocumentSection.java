38762e522704c41730cf4943d45424df9d09e490
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-05-22 16:21:58 +0000

    documentsection object class

---
+++ BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/document/DocumentSection.java
@@ -0,0 +1,32 @@
+/*
+ * DocumentSection.java
+ *
+ * Created on May 22, 2008, 6:13 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.bungeni.editor.document;
+
+/**
+ *
+ * @author Administrator
+ */
+public class DocumentSection {
+    
+    String documentType;
+    String sectionType;
+    String sectionNamePrefix;
+    String sectionNumberingStyle;
+    int sectionBackground;
+    long sectionLeftMargin;
+    long sectionRightMargin ;
+    boolean isProtected;
+    
+    
+    /** Creates a new instance of DocumentSection */
+    public DocumentSection() {
+    }
+    
+}

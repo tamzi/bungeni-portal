e7f26fefa190cc7d154e27a2183a92987e81bd36
christian.ledermann <christian.ledermann@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-05-22 13:04:01 +0000

    sort on container

--- bungeni.ui/trunk/bungeni/ui/browser/container.py
+++ bungeni.ui/trunk/bungeni/ui/browser/container.py
@@ -1,23 +1,13 @@
 # encoding: utf-8
-#from alchemist.ui.container import ContainerListing
+
 import alchemist.ui.container
 from zope.security import proxy
 from zc.table import  table
 
-import pdb
 
 class ContainerListing( alchemist.ui.container.ContainerListing ):
     
-    def update(self):
-        # order_by
-        order_by = self.request.get('order_by', None)
-        #start, limit = self.request.get('start',0), self.request.get('limit', 0)
-        #self.batch = super( ContainerListing, self ).batch(order_by)  
-        #self.context.batch =  
-        #context = proxy.removeSecurityProxy( self.context )
-        #query = context.query
-        #query = query.order_by( order_by )
-        #context.query = context._query.order_by ( order_by )              
+    def update(self):           
         super( ContainerListing, self ).update()
         
     @property
@@ -28,7 +18,6 @@ class ContainerListing( alchemist.ui.container.ContainerListing ):
         if order_by:
             if order_by in context._class.c._data._list:
                 query=query.order_by(order_by)            
-            #pdb.set_trace()
             
         formatter = table.AlternatingRowFormatter( context,
                                                    self.request,

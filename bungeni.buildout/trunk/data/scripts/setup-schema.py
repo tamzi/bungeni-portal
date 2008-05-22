2d60f8eaa222fb01ff0447fb2e14aae76e03ee7d
franz.baresi <franz.baresi@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-05-22 08:11:50 +0000

    Ensuring that the annotation schema is created along with bungeni schema.

--- bungeni.buildout/trunk/data/scripts/setup-schema.py
+++ bungeni.buildout/trunk/data/scripts/setup-schema.py
@@ -7,6 +7,7 @@ import transaction
 from bungeni import core
 from ore.alchemist import Session
 from alchemist import security
+from marginalia import schema as marginalia_schema
 from sqlalchemy import create_engine
 
 core.metadata.bind = db = create_engine('postgres://localhost/bungeni')
@@ -16,6 +17,10 @@ core.metadata.create_all()
 security.metadata.bind = db
 security.metadata.drop_all() 
 security.metadata.create_all() 
+
+marginalia_schema.metadata.bind = db
+marginalia_schema.metadata.drop_all()
+marginalia_schema.metadata.create_all() 
 
 db.dispose()
 

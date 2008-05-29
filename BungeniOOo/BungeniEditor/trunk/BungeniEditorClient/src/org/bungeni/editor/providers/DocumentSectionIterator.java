5c5cb59f887651ccececf5a093cfa24c21c84c1f
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-05-29 16:51:05 +0000

    standardized Document section iterator class that allows adding a callback function via the interface class....

---
+++ BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/providers/DocumentSectionIterator.java
@@ -0,0 +1,50 @@
+/*
+ * DocumentSectionIterator.java
+ *
+ * Created on May 29, 2008, 3:06 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.bungeni.editor.providers;
+
+import java.util.Iterator;
+import java.util.TreeMap;
+import org.bungeni.utils.BungeniBNode;
+
+/**
+ * A recursive section iterator class that allows callbacks via an interface for every section found by the iterator. Makes use of the DocumentSectionProvider class, and the BungeniBNode tree classes to implement a callback interface via the IBungeniSectionIteratorListener class which has a single callback function
+ * 
+ * @author Administrator
+ */
+public  class DocumentSectionIterator {
+    BungeniBNode rootNode;
+    IBungeniSectionIteratorListener callback;
+    /** Creates a new instance of DocumentSectionIterator */
+    public DocumentSectionIterator(IBungeniSectionIteratorListener callback) {
+        this.rootNode = DocumentSectionProvider.getTreeRoot();
+        this.callback = callback;
+    }
+    
+    public void startIterator() {
+        callback.iteratorCallback(rootNode);
+        recurseAllNodes(rootNode);
+    }
+    
+    
+    
+    private void recurseAllNodes(BungeniBNode theBNode) {
+       // BungeniBNode theBNode = (BungeniBNode) theNode.getUserObject();
+        if (theBNode.hasChildren()) {
+            TreeMap<Integer, BungeniBNode> children = theBNode.getChildrenByOrder();
+            Iterator<Integer> childIterator = children.keySet().iterator();
+            while (childIterator.hasNext()) {
+                Integer nodeKey = childIterator.next();
+                BungeniBNode newBNode = children.get(nodeKey);
+                callback.iteratorCallback(newBNode);
+                recurseAllNodes(newBNode);
+            }
+        }
+    }   
+}

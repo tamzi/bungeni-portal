18e4fcc7130ac5427a50f5c32071781e20cb9dcf
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-06-24 15:03:36 +0000

    add javadoc

--- BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/utils/compare/DifferenceChain.java
+++ BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/utils/compare/DifferenceChain.java
@@ -1,8 +1,20 @@
 package org.bungeni.utils.compare;
-
+/** 
+ * DifferenceChain - stores a chain of related BungeniNodeDifference objects
+ * @author Ashok
+ */
  public class DifferenceChain {
+        /**
+         * the node difference object
+         */
         public BungeniNodeDifference diff;
+        /**
+         * next difference in chain
+         */
         public DifferenceChain nextDifference = null;
+        /**
+         * previous difference in chain
+         */
         public DifferenceChain prevDifference = null;
         
         @Override
@@ -19,6 +31,10 @@ package org.bungeni.utils.compare;
             return output;
         }
         
+/**
+ * Returns the chainSize of the current chain
+ * @return int
+ */
         public int chainSize(){
             int n=1;
             DifferenceChain nextDiff = nextDifference;

bab809ee58f57edc0dca9da08410a21d74bb1d84
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-07-11 15:17:26 +0000

    retrieve filtered doc metadata api

--- BungeniOOo/OOoBungeniLib/src/org/bungeni/ooo/ooDocMetadata.java
+++ BungeniOOo/OOoBungeniLib/src/org/bungeni/ooo/ooDocMetadata.java
@@ -9,7 +9,9 @@
 
 package org.bungeni.ooo;
 
+import com.sun.star.beans.Property;
 import com.sun.star.beans.UnknownPropertyException;
+import java.util.ArrayList;
 
 /**
  *
@@ -24,6 +26,9 @@ public class ooDocMetadata {
         ooDocument  = docHandle;
     }
     
+    public void setOODocument(OOComponentHelper ooDoc) {
+        this.ooDocument = ooDoc;
+    }
     public void AddProperty (String propertyName, String propertyValue ){
         if (!PropertyExists(propertyName)) {
         ooDocument.addProperty(propertyName, propertyValue);
@@ -52,4 +57,28 @@ public class ooDocMetadata {
        return propertyValue;
        }
     }
+    /**
+     * Returns document metadata properties by type
+     * Properties are 'typed' based on a prefix e.g. rf: is an internal reference prefix
+     * @param ooDoc OOComponentHelper object for the document whose properties need to be retreived
+     * @param docMetadataType Type filter prefix (without ":" )
+     * @return
+     */
+    public static ArrayList<ooDocMetadataFieldSet> getMetadataObjectsByType (OOComponentHelper ooDoc, String docMetadataType) {
+        ArrayList<ooDocMetadataFieldSet> matchedProperties = new ArrayList<ooDocMetadataFieldSet>(0);
+        Property[] docProps = ooDoc.getDocumentProperties();
+        for (Property prop : docProps) {
+            if (prop.Name.startsWith(docMetadataType)){
+                try {
+                    String value = ooDoc.getPropertyValue(prop.Name);
+                    ooDocMetadataFieldSet fld = new ooDocMetadataFieldSet(prop.Name, value);
+                    matchedProperties.add(fld);
+                } catch (UnknownPropertyException ex) {
+                    log.error("getMetadataObjectsByType : " + ex.getMessage());
+                }
+            }
+        }
+        return matchedProperties;
+    }
+    
 }

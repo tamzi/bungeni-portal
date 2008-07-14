9270f373ee035f33c2760cb644ca20fce1111c59
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-07-14 10:09:10 +0000

    exception handlers and fixes....

--- BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/providers/DocumentSectionFriendlyTreeModelProvider.java
+++ BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/providers/DocumentSectionFriendlyTreeModelProvider.java
@@ -9,10 +9,9 @@
 
 package org.bungeni.editor.providers;
 
-import com.sun.star.text.XTextSection;
-import java.util.HashMap;
+import java.util.Iterator;
+import java.util.TreeMap;
 import javax.swing.tree.DefaultMutableTreeNode;
-import org.bungeni.ooo.OOComponentHelper;
 import org.bungeni.utils.BungeniBNode;
 
 /**
@@ -28,20 +27,29 @@ public class DocumentSectionFriendlyTreeModelProvider {
     /**
      * returns a non-static instance of the section TreeModel which is updated directly by the documentsectionProvider
      */
+     /*
     public static DocumentSectionFriendlyAdapterDefaultTreeModel create_without_subscription(){
         BungeniBNode bRootNode = DocumentSectionProvider.getTreeRoot();
         DefaultMutableTreeNode dmtRootNode = provideRootNode(bRootNode);
         DocumentSectionFriendlyAdapterDefaultTreeModel model = new DocumentSectionFriendlyAdapterDefaultTreeModel(dmtRootNode, false);
         return model;
-    }
+    }*/
     
     public static DocumentSectionFriendlyAdapterDefaultTreeModel create(){
-        BungeniBNode bRootNode = DocumentSectionProvider.getNewFriendlyTree().getFirstRoot();
-        DefaultMutableTreeNode dmtRootNode = provideRootNode(bRootNode);
-        DocumentSectionFriendlyAdapterDefaultTreeModel model = new DocumentSectionFriendlyAdapterDefaultTreeModel(dmtRootNode, false);
-        DocumentSectionProvider.subscribeModel(model);
-        return model;
+        DocumentSectionFriendlyAdapterDefaultTreeModel model = null;
+        try {
+            BungeniBNode bRootNode = DocumentSectionProvider.getNewFriendlyTree().getFirstRoot();
+            DefaultMutableTreeNode dmtRootNode = provideRootNode(bRootNode);
+            model = new DocumentSectionFriendlyAdapterDefaultTreeModel(dmtRootNode, false);
+            DocumentSectionProvider.subscribeModel(model);
+        } catch (Exception ex) {
+            log.error("exception create() :" + ex.getMessage());
+        } finally {
+            return model;
+        }
+     
     }
+    /*
     public static DocumentSectionFriendlyAdapterDefaultTreeModel create_static(){
        if (theSectionTreeModel == null ) {
             BungeniBNode bRootNode = DocumentSectionProvider.getTreeRoot();
@@ -52,7 +60,7 @@ public class DocumentSectionFriendlyTreeModelProvider {
             }
        return theSectionTreeModel;    
     }
-    
+    */
 
     /**
      * generates a newrootnode for the section model
@@ -91,15 +99,36 @@ public class DocumentSectionFriendlyTreeModelProvider {
     
     private static DefaultMutableTreeNode provideRootNode(BungeniBNode rootNode) {
         //walk nodes and build tree
-        DefaultMutableTreeNode theRootNode = new DefaultMutableTreeNode(rootNode);
-        rootNode.setNodeObject(theRootNode);
-       // OOComponentHelper ooDoc = DocumentSectionProvider.getOOoDocument();
-       // recurseNodes(theRootNode, ooDoc);
-        DocumentSectionIterator docIterate = new DocumentSectionIterator(rootNode, new DefaultSectionIteratorListener());
-        docIterate.startIterator();
+        DefaultMutableTreeNode theRootNode = null;
+        try {
+            theRootNode = new DefaultMutableTreeNode(rootNode);
+            rootNode.setNodeObject(theRootNode);
+//            DocumentSectionIterator docIterate = new DocumentSectionIterator(rootNode, new DefaultSectionIteratorListener());
+//            docIterate.startIterator();
+            recurseNodes(theRootNode);
+            //return theRootNode;            
+        } catch (Exception ex) {
+           log.error("provideRootNode() : " + ex.getMessage()); 
+        } finally {
         return theRootNode;
+        }
     }
     
+        private static void recurseNodes(DefaultMutableTreeNode theNode) {
+        BungeniBNode theBNode = (BungeniBNode) theNode.getUserObject();
+        if (theBNode.hasChildren()) {
+            TreeMap<Integer, BungeniBNode> children = theBNode.getChildrenByOrder();
+            Iterator<Integer> childIterator = children.keySet().iterator();
+            while (childIterator.hasNext()) {
+                Integer nodeKey = childIterator.next();
+                BungeniBNode childNode  = children.get(nodeKey);
+                DefaultMutableTreeNode dmtChildNode = new DefaultMutableTreeNode( childNode);
+                childNode.setNodeObject(dmtChildNode);
+                recurseNodes(dmtChildNode);
+                theNode.add(dmtChildNode );
+            }
+        }
+    }
     /*
     private static void recurseNodes(DefaultMutableTreeNode theNode, OOComponentHelper ooDoc) {
         BungeniBNode theBNode = (BungeniBNode) theNode.getUserObject();

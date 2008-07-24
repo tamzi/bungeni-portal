f614546417a6f4c04463dd0f50ece64cda562d34
christian.ledermann <christian.ledermann@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-07-24 09:59:07 +0000

    + vieewlet.py

---
+++ bungeni.ui/trunk/bungeni/ui/browser/viewlet.py
@@ -0,0 +1,31 @@
+# encoding: utf-8
+from zope import interface
+from zope.viewlet import viewlet, manager
+from zope.formlib import form
+from zope.formlib.namedtemplate import NamedTemplate
+from interfaces import ISubFormViewletManager
+
+from bungeni.core.i18n import _
+import alchemist.ui.core
+import alchemist.ui.viewlet
+
+class AttributesEditViewlet( alchemist.ui.core.DynamicFields, alchemist.ui.viewlet.EditFormViewlet ):
+
+    mode = "edit"
+    template = NamedTemplate('alchemist.subform')
+    form_name = _(u"General")
+    
+class AttributesViewViewlet( alchemist.ui.core.DynamicFields, alchemist.ui.viewlet.DisplayFormViewlet ):
+
+    mode = "view"
+    template = NamedTemplate('alchemist.subform')    
+    form_name = _(u"General")
+    
+class SubFormViewletManager( manager.WeightOrderedViewletManager ):
+    """
+    display subforms
+    """
+    interface.implements(ISubFormViewletManager)     
+    
+    
+    

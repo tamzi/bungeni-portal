cd436a6e61c794b9e24c5e1be2ab9a05948da5f1
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-08-12 09:58:55 +0000

    refactored base dialog impelementations

---
+++ BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/selectors/IMetadataPanel.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.bungeni.editor.selectors;
+
+import org.bungeni.editor.selectors.debaterecord.masthead.*;
+import java.awt.Component;
+import javax.swing.JFrame;
+import org.bungeni.editor.actions.toolbarAction;
+import org.bungeni.editor.actions.toolbarSubAction;
+import org.bungeni.editor.selectors.SelectorDialogModes;
+import org.bungeni.ooo.OOComponentHelper;
+
+/**
+ *
+ * @author undesa
+ */
+public interface IMetadataPanel {
+    public String getPanelName();
+    public Component getPanelComponent();
+    public void initVariables(OOComponentHelper ooDoc, JFrame pFrame, toolbarAction tAction, toolbarSubAction tSubAction, SelectorDialogModes smode) ;
+
+}

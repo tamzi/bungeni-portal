726dd235b472c6fa3e155b8a80d7b9a45b83ee40
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-08-12 21:54:02 +0000

    debate record speech

---
+++ BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/selectors/debaterecord/speech/Main.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.bungeni.editor.selectors.debaterecord.speech;
+
+import java.util.ArrayList;
+import javax.swing.JFrame;
+import org.bungeni.editor.selectors.BaseMetadataContainerPanel;
+
+/**
+ *
+ * @author undesa
+ */
+public class Main extends BaseMetadataContainerPanel {
+    public Main(){
+        super();
+    }
+    
+      public static void main(String[] args){
+        Main m = new Main();
+        JFrame f = new JFrame("MastHead title");
+        f.add(m);
+        f.pack();
+        f.setVisible(true);
+    }
+      
+    @Override
+    protected void setupPanels() {
+       m_allPanels = new ArrayList<panelInfo>(){
+                {
+                    add(new panelInfo("PersonSelector","org.bungeni.editor.selectors.debaterecord.speech.PersonSelector"));
+                    add(new panelInfo("PersonURI", "org.bungeni.editor.selectors.debaterecord.speech.PersonURI"));
+                    add(new panelInfo("SpeechBy", "org.bungeni.editor.selectors.debaterecord.speech.SpeechBy"));
+                }
+        };
+    
+       m_activePanels = new ArrayList<panelInfo>(){
+            {
+                    add(new panelInfo("PersonSelector","org.bungeni.editor.selectors.debaterecord.speech.PersonSelector"));
+                    add(new panelInfo("PersonURI", "org.bungeni.editor.selectors.debaterecord.speech.PersonURI"));
+                    add(new panelInfo("SpeechBy", "org.bungeni.editor.selectors.debaterecord.speech.SpeechBy"));
+            }
+         };
+    }
+
+}

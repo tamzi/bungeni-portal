759e29d4f98c88db9f80f02ae5e3189c92648bff
cervoneluca <cervoneluca@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-09-17 14:22:43 +0000

    translator

---
+++ DocTransforms/trunk/ODTtoAKN/src/org/un/bungeni/translators/odttoakn/translator/Translator.java
@@ -0,0 +1,18 @@
+package org.un.bungeni.translators.odttoakn.translator;
+
+import org.w3c.dom.Document;
+
+public class Translator implements TranslatorInterface
+{
+	/**
+	 * Transforms the document at the given path using the configuration at the given path 
+	 * @param aDocumentPath the path of the document to translate
+	 * @param aConfigurationPath the path of the configuration to use for the translation 
+	 * @return the translated document
+	 */
+	public Document translate(String aDocumentPath, String aConfigurationPath)
+	{
+		return null;
+	}
+
+}

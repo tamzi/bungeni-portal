d1bd2c3c35ac663cfcd6adb348505e6d09367e14
christian.ledermann <christian.ledermann@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-09-17 10:54:55 +0000

    oops question workflow transitions must not be  AUTOMATIC but SYSTEM. clarified tests

--- bungeni.core/trunk/bungeni/core/workflows/utils.py
+++ bungeni.core/trunk/bungeni/core/workflows/utils.py
@@ -29,14 +29,7 @@ def submitResponse( info, context ):
     A Response to a question is submitted to the clerks office,
     the questions status is set to responded
     """
-    ## this stuff is only to make the tests work
-    component.provideAdapter( bungeni.core.workflows.WorkflowState,
-                             (bungeni.core.interfaces.IBungeniContent,))
 
-    component.provideAdapter( bungeni.core.workflows.question.QuestionWorkflowAdapter,
-                            (domain.Question,))
-    component.provideAdapter(ore.workflow.workflow.WorkflowInfo,(domain.Question,))
-    ### end of test setup
     instance = removeSecurityProxy(context)
     question = dbutils.getQuestion(instance.question_id)
     IWorkflowInfo(question).fireTransition('respond-writing')
@@ -46,14 +39,6 @@ def publishResponse( info, context ):
     The Response was reviewed by the clerks office, the questions
     status is set to answered
     """
-    # make the tests work
-    component.provideAdapter( bungeni.core.workflows.WorkflowState,
-                             (bungeni.core.interfaces.IBungeniContent,))
-
-    component.provideAdapter( bungeni.core.workflows.question.QuestionWorkflowAdapter,
-                            (domain.Question,))
-    component.provideAdapter(ore.workflow.workflow.WorkflowInfo,(domain.Question,))
-    # end test setup
     instance = removeSecurityProxy(context)
     question = dbutils.getQuestion(instance.question_id)
     IWorkflowInfo(question).fireTransition('answer')

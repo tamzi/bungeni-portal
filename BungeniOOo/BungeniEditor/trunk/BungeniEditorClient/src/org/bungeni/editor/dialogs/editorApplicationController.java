2a1695c30c066559411e27c6d7b1f150933275e3
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-12-05 10:19:43 +0000

    ui changes for section dispaly

--- BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/dialogs/editorApplicationController.java
+++ BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/dialogs/editorApplicationController.java
@@ -855,7 +855,7 @@ public static int OPENOFFICE_HEIGHT_OFFSET =60;
 private void initFrame(XComponent component){
             BungeniFrame frame = new BungeniFrame("BungeniEditor Control Panel");
             //set the dimensions for the frame;
-            frame.setSize(270, 540);
+            frame.setSize(270, 655);
             //frame position information
             //position frame
             Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();

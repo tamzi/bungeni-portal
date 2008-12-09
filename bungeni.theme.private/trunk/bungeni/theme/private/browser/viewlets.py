ba467f32d3d06230eb1996dcb024ac989747d5ae
christian.ledermann <christian.ledermann@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-12-09 14:14:28 +0000

    after login a diffrent portalheader is presented "

--- bungeni.theme.private/trunk/bungeni/theme/private/browser/viewlets.py
+++ bungeni.theme.private/trunk/bungeni/theme/private/browser/viewlets.py
@@ -4,4 +4,15 @@ from zope.viewlet.viewlet import ViewletBase
 
 class LogoViewlet(ViewletBase):
     render = ViewPageTemplateFile('publiclogo.pt')
+    
+class AdminLogoViewlet(ViewletBase):   
+    render = ViewPageTemplateFile('adminlogo.pt')
 
+class MPLogoViewlet(ViewletBase):   
+    render = ViewPageTemplateFile('mplogo.pt')
+
+class ClerkLogoViewlet(ViewletBase):   
+    render = ViewPageTemplateFile('clerklogo.pt')
+ 
+class SpeakerLogoViewlet(ViewletBase):   
+    render = ViewPageTemplateFile('speakerlogo.pt')

a46511eced6376c9888c19826052626ab3e7ee1a
christian.ledermann <christian.ledermann@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-12-16 17:37:00 +0000

    jua kali implementation of task menues

--- bungeni.ui/trunk/bungeni/ui/portlets/taskmenu.py
+++ bungeni.ui/trunk/bungeni/ui/portlets/taskmenu.py
@@ -2,6 +2,8 @@ from zope import component
 
 from zope.app.publisher.interfaces.browser import IBrowserMenu
 from zope.app.pagetemplate import ViewPageTemplateFile
+from zope.viewlet import viewlet
+
 
 class TaskMenuViewlet(object):
     render = ViewPageTemplateFile("templates/taskmenu.pt")
@@ -15,3 +17,14 @@ class TaskMenuViewlet(object):
     def available(self):
         items = self.items()
         return bool(items)
+        
+        
+class ClerksTaskMenuViewlet( viewlet.ViewletBase ):  
+    render = ViewPageTemplateFile("templates/clerks-task-menu.pt")      
+    
+    
+class MpsTaskMenuViewlet( viewlet.ViewletBase ):  
+    render = ViewPageTemplateFile("templates/mps-task-menu.pt")          
+    
+class SpeakersTaskMenuViewlet( viewlet.ViewletBase ):  
+    render = ViewPageTemplateFile("templates/speakers-task-menu.pt")        

d7d0679a2145b28897851ee8885f601eda49df61
christian.ledermann <christian.ledermann@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2008-12-18 10:22:53 +0000

    interact menu

--- bungeni.ui/trunk/bungeni/ui/portlets/taskmenu.py
+++ bungeni.ui/trunk/bungeni/ui/portlets/taskmenu.py
@@ -3,6 +3,7 @@ from zope import component
 from zope.app.publisher.interfaces.browser import IBrowserMenu
 from zope.app.pagetemplate import ViewPageTemplateFile
 from zope.viewlet import viewlet
+import bungeni.core.globalsettings as prefs
 
 
 class TaskMenuViewlet(object):
@@ -28,3 +29,23 @@ class MpsTaskMenuViewlet( viewlet.ViewletBase ):
     
 class SpeakersTaskMenuViewlet( viewlet.ViewletBase ):  
     render = ViewPageTemplateFile("templates/speakers-task-menu.pt")        
+    
+    
+class InteractMenuViewlet( viewlet.ViewletBase ):  
+    render = ViewPageTemplateFile("templates/interact-menu.pt")       
+    
+    def getRootUrl(self):
+        m_url = prefs.getPloneMenuUrl()
+        r_url = '/'.join(m_url.split('/')[:-1])    
+        r_url = r_url + '/Members/' + self.user_name 
+        return r_url
+        
+    def update( self ):
+        """
+        prepare the data needed to render the viewlet.        
+        """
+        try:
+            self.user_name = self.request.principal.login          
+        except:
+            pass
+            

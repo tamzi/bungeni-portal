e63ccd8366aebbdd6d9f2deef01cfe206076a30c
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-01-06 07:01:14 +0000

    updating dist itmes

--- BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/dist/runEditor.bat
+++ BungeniOOo/BungeniEditor/trunk/BungeniEditorClient/dist/runEditor.bat
@@ -1,3 +1,4 @@
 set JAVA_HOME=e:\Java\jdk1.6.0
 set LOG4J_PROPS="file:///e:/projects/workingprojects/bungenieditor/trunk/BungeniEditorClient/dist/settings/log4j.properties"
-%JAVA_HOME%\bin\java -Dlog4j.ignoreTCL -Dlog4j.configuration=%LOG4J_PROPS% -jar BungeniEditorClient.jar
+set EDITOR_MEM=128m
+%JAVA_HOME%\bin\java -Dlog4j.ignoreTCL -Dlog4j.configuration=%LOG4J_PROPS% -Xmx%EDITOR_MEM% -jar BungeniEditorClient.jar

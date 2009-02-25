924b65e0dd1fb4726497b152b39dc4f647497e1d
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-02-25 17:49:13 +0000

    Refactored YUI tabbing.

--- bungeni.ui/trunk/bungeni/ui/js/__init__.py
+++ bungeni.ui/trunk/bungeni/ui/js/__init__.py
@@ -1,3 +1,4 @@
 from zope.viewlet.viewlet import JavaScriptViewlet as JSViewlet
 
 Plugins = JSViewlet('bungeni-js/plugins.js')
+Behavior = JSViewlet('bungeni-js/behavior.js')
---
+++ bungeni.ui/trunk/bungeni/ui/js/behavior.js
@@ -0,0 +1,6 @@
+(function($) {
+  $(document).ready(function() {
+      var manager = $(".alchemist-content-manager");
+      manager.yuiTabView(manager.find("div.listing"));
+    });
+ })(jQuery);

b3bb56551856bb408f1ff78cbd67e5f05dc666e7
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-02-26 08:21:13 +0000

    Reconfigured workspace to use view marker interfaces, e.g. 'IClerkWorkspace' instead of asking for a 'bungeni.Clerk' permission (now removed).

--- bungeni.ui/trunk/bungeni/ui/workspace.py
+++ bungeni.ui/trunk/bungeni/ui/workspace.py
@@ -1,8 +1,28 @@
+from zope import interface
+from zope import component
 from zope.publisher.browser import BrowserView
-from zope.app.pagetemplate import ViewPageTemplateFile
+from zope.securitypolicy.interfaces import IRole
 
-class Workspace(BrowserView):
-    __call__ = ViewPageTemplateFile("templates/workspace.pt")
+import interfaces
 
-    
+def getRoles(context=None):
+    return [role_id for role_id, role in \
+            component.getUtilitiesFor(IRole, context)]
 
+role_interface_mapping = {
+    u'bungeni.Admin': interfaces.IAdministratorWorkspace,
+    u'bungeni.Minister': interfaces.IMinisterWorkspace,
+    u'bungeni.MP': interfaces.IMPWorkspace,
+    u'bungeni.Speaker': interfaces.ISpeakerWorkspace,
+    u'bungeni.Clerk': interfaces.IClerkWorkspace
+    }
+
+class WorkspaceView(BrowserView):
+
+    def __init__(self, context, request):
+        super(WorkspaceView, self).__init__(context, request)
+
+        for role_id in getRoles():
+            iface = role_interface_mapping.get(role_id)
+            if iface is not None:
+                interface.alsoProvides(self, iface)

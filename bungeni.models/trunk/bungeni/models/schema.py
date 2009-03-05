6e674298cc7da9e6aa37645fe2eb1e32f80efc0c
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-03-05 08:54:31 +0000

    Make users change table; this is expected in the tests.

--- bungeni.models/trunk/bungeni/models/schema.py
+++ bungeni.models/trunk/bungeni/models/schema.py
@@ -1,21 +1,78 @@
 #!/usr/bin/env python
 # encoding: utf-8
-"""
-
-
-"""
-
 import sqlalchemy as rdb
 from datetime import datetime
+
 metadata = rdb.MetaData()
 
-ItemSequence = rdb.Sequence('item_sequence') # used for Bills, motions, questions
-PrincipalSequence = rdb.Sequence('principal_sequence') # for users and groups because of the zope users and groups
+# bills, motions, questions
+ItemSequence = rdb.Sequence('item_sequence')
+
+# users and groups because of the zope users and groups
+PrincipalSequence = rdb.Sequence('principal_sequence') 
+
+def make_changes_table( table, metadata ):
+    """Create an object log table for an object."""
+    
+    table_name = table.name
+    entity_name =  table_name.endswith('s') and table_name[:-1] or table_name
+    
+    changes_name = "%s_changes"%( entity_name )
+    fk_id = "%s_id"%( entity_name )
+    #print entity_name, fk_id
+    
+    changes_table = rdb.Table(
+            changes_name,
+            metadata,
+            rdb.Column( "change_id", rdb.Integer, primary_key=True ),
+            rdb.Column( "content_id", rdb.Integer, rdb.ForeignKey( table.c[ fk_id ] ) ),
+            rdb.Column( "action", rdb.Unicode(16) ),
+            rdb.Column( "date",  rdb.DateTime( timezone=False ), default=datetime.now),
+            rdb.Column( "description", rdb.UnicodeText),
+            rdb.Column( "notes", rdb.UnicodeText),
+            rdb.Column( "user_id", rdb.Unicode(32) ) # Integer, rdb.ForeignKey('users.user_id') ),
+    )
+    
+    return changes_table
+ 
+def make_versions_table( table, metadata ):
+    """Create a versions table, requires change log table for which
+    some version metadata information will be stored."""
+    
+    table_name = table.name
+    entity_name =  table_name.endswith('s') and table_name[:-1] or table_name
+    
+    versions_name = "%s_versions"%( entity_name )
+    fk_id = "%s_id"%( entity_name )
+    
+    columns = [
+        rdb.Column( "version_id", rdb.Integer, primary_key=True ),
+        #rdb.Column( "version_created", rdb.DateTime, default=rdb.PassiveDefault('now') ),
+        rdb.Column( "content_id", rdb.Integer, rdb.ForeignKey( table.c[ fk_id ] ) ),
+        rdb.Column( "change_id", rdb.Integer, rdb.ForeignKey('%s_changes.change_id'%entity_name)),
+        rdb.Column( "manual", rdb.Boolean, nullable=False, default=False),
+    ]
+    
+    columns.extend( [ c.copy() for c in table.columns if not c.primary_key ] )
+    
+    #primary = [ c.copy() for c in table.columns if c.primary_key ][0]
+    #primary.primary_key = False
+    #columns.insert( 2, primary )
+    
+    versions_table = rdb.Table(
+            versions_name,
+            metadata,
+            *columns 
+            )
+            
+    return versions_table
 
 #######################
 # Users 
 #######################
 # 
+
+
 
 users = rdb.Table(
    "users",
@@ -63,6 +120,7 @@ users = rdb.Table(
 #    rdb.Column( "type", rdb.Unicode(16) ),   
 #    )
 
+users_changes = make_changes_table(users, metadata)
 
    
 
@@ -491,63 +549,6 @@ resourcebookings = rdb.Table(
 
 # Parliamentary Items
 # 
-def make_changes_table( table, metadata ):
-    """ create an object log table for an object """
-    table_name = table.name
-    entity_name =  table_name.endswith('s') and table_name[:-1] or table_name
-    
-    changes_name = "%s_changes"%( entity_name )
-    fk_id = "%s_id"%( entity_name )
-    #print entity_name, fk_id
-    
-    changes_table = rdb.Table(
-            changes_name,
-            metadata,
-            rdb.Column( "change_id", rdb.Integer, primary_key=True ),
-            rdb.Column( "content_id", rdb.Integer, rdb.ForeignKey( table.c[ fk_id ] ) ),
-            rdb.Column( "action", rdb.Unicode(16) ),
-            rdb.Column( "date",  rdb.DateTime( timezone=False ), default=datetime.now),
-            rdb.Column( "description", rdb.UnicodeText),
-            rdb.Column( "notes", rdb.UnicodeText),
-            rdb.Column( "user_id", rdb.Unicode(32) ) # Integer, rdb.ForeignKey('users.user_id') ),
-    )
-    
-    return changes_table
- 
-def make_versions_table( table, metadata ):
-    """
-    create a versions table, requires change log table for which some version metadata information will be stored.
-    """
-    table_name = table.name
-    entity_name =  table_name.endswith('s') and table_name[:-1] or table_name
-    
-    versions_name = "%s_versions"%( entity_name )
-    fk_id = "%s_id"%( entity_name )
-    
-    columns = [
-        rdb.Column( "version_id", rdb.Integer, primary_key=True ),
-        #rdb.Column( "version_created", rdb.DateTime, default=rdb.PassiveDefault('now') ),
-        rdb.Column( "content_id", rdb.Integer, rdb.ForeignKey( table.c[ fk_id ] ) ),
-        rdb.Column( "change_id", rdb.Integer, rdb.ForeignKey('%s_changes.change_id'%entity_name)),
-        rdb.Column( "manual", rdb.Boolean, nullable=False, default=False),
-    ]
-    
-    columns.extend( [ c.copy() for c in table.columns if not c.primary_key ] )
-    
-    #primary = [ c.copy() for c in table.columns if c.primary_key ][0]
-    #primary.primary_key = False
-    #columns.insert( 2, primary )
-    
-    versions_table = rdb.Table(
-            versions_name,
-            metadata,
-            *columns 
-            )
-            
-    return versions_table
-    
-
-
 item_votes = rdb.Table( 
    'item_votes',
    metadata,

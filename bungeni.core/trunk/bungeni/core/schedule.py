59acbcac645626b9f7143f00b6dd6c0efad388e4
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-03-10 16:42:31 +0000

    Added options 'start_date' and 'end_date' to scheduling context's 'get_sittings' method.

--- bungeni.core/trunk/bungeni/core/schedule.py
+++ bungeni.core/trunk/bungeni/core/schedule.py
@@ -1,9 +1,18 @@
+import time
+
 from zope import interface
 from zope import component
 
 from bungeni.core.interfaces import ISchedulingContext
+from bungeni.core.globalsettings import getCurrentParliamentId
 from bungeni.models.interfaces import IBungeniApplication
+from bungeni.models.domain import GroupSitting
 
+from ore.alchemist import Session
+
+def format_date(date):
+    return time.strftime("%Y-%m-%d %H:%M:%S", date.timetuple())
+                  
 class PrincipalGroupSchedulingContext(object):
     interface.implements(ISchedulingContext)
 
@@ -12,11 +21,29 @@ class PrincipalGroupSchedulingContext(object):
     def __init__(self, context):
         self.__parent__ = context
 
-    def get_sittings(self):
-        pass
+    def get_sittings(self, start_date=None, end_date=None):
+        session = Session()
 
+        if start_date is None and end_date is None:
+            return session.query(GroupSitting).filter_by(
+                group_id=self.group_id)
+            
+        assert start_date and end_date
+
+        query = session.query(GroupSitting).filter(
+            "group_id=:group_id and start_date>:start_date and end_date<:end_date")
+
+        return query.params(
+            group_id=self.group_id,
+            start_date=format_date(start_date),
+            end_date=format_date(end_date))
+    
 class PlenarySchedulingContext(PrincipalGroupSchedulingContext):
     component.adapts(IBungeniApplication)
 
-    group_id = "bungeni.Plenary"    
+    @property
+    def group_id(self):
+        """Return current parliament's group id."""
+
+        return getCurrentParliamentId()
         

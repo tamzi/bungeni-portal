3ab71ab709df5d973e01ef962db363bd71ca9e05
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-03-16 12:08:35 +0000

    Provide location information for returned sittings.

--- bungeni.core/trunk/bungeni/core/schedule.py
+++ bungeni.core/trunk/bungeni/core/schedule.py
@@ -10,6 +10,7 @@ from bungeni.models.domain import GroupSitting
 from bungeni.models.domain import Group
 
 from ore.alchemist import Session
+from ore.alchemist.container import stringKey
 
 def format_date(date):
     return time.strftime("%Y-%m-%d %H:%M:%S", date.timetuple())
@@ -22,28 +23,42 @@ class PrincipalGroupSchedulingContext(object):
     def __init__(self, context):
         self.__parent__ = context
 
-    def get_group(self):
+    def get_group(self, name="group"):
         session = Session()
 
-        return session.query(Group).filter_by(
+        group = session.query(Group).filter_by(
             group_id=self.group_id)[0]
+
+        group.__name__ = name
+        group.__parent__ = self
+
+        return group
 
     def get_sittings(self, start_date=None, end_date=None):
         session = Session()
 
         if start_date is None and end_date is None:
-            return session.query(GroupSitting).filter_by(
+            sesions = session.query(GroupSitting).filter_by(
                 group_id=self.group_id)
-            
-        assert start_date and end_date
 
-        query = session.query(GroupSitting).filter(
-            "group_id=:group_id and start_date>:start_date and end_date<:end_date")
+        else:
+            assert start_date and end_date
 
-        return query.params(
-            group_id=self.group_id,
-            start_date=format_date(start_date),
-            end_date=format_date(end_date))
+            query = session.query(GroupSitting).filter(
+                "group_id=:group_id and start_date>:start_date and end_date<:end_date")
+
+            sittings = query.params(
+                group_id=self.group_id,
+                start_date=format_date(start_date),
+                end_date=format_date(end_date))
+
+        sittings = tuple(sittings)
+        
+        for sitting in sittings:
+            sitting.__name__ = stringKey(sitting)
+            sitting.__parent__ = self.get_group().sittings
+
+        return sittings
     
 class PlenarySchedulingContext(PrincipalGroupSchedulingContext):
     component.adapts(IBungeniApplication)

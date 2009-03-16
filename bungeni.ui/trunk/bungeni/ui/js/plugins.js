3a0948fb73c7740a27c0ce93b733a5ce27f447f5
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-03-16 07:21:43 +0000

    Move logic into plugin for clean separation.

--- bungeni.ui/trunk/bungeni/ui/js/plugins.js
+++ bungeni.ui/trunk/bungeni/ui/js/plugins.js
@@ -1,4 +1,60 @@
 (function($) {
+  var re_time_range = /(.*) \((\d+):(\d+):\d+-(\d+):(\d+):\d+\)/;
+  
+  // when selecting an option on the format "Label
+  // (start_time-end_time)", listen to the ``change`` event and
+  // update corresponding start- and end time options
+  $.fn.bungeniTimeRangeSelect = function() {
+    $.each($(this), function(i, o) {
+      var options = $(o).children();
+      var form = $(o).parents("form").eq(0);
+
+      var start_hour = form.find("select[name$=start_date__hour]").get(0);
+      if (!start_hour) return;
+      var start_minute = form.find("select[name$=start_date__minute]").get(0);
+      if (!start_minute) return;
+      var end_hour = form.find("select[name$=end_date__hour]").get(0);
+      if (!end_hour) return;
+      var end_minute = form.find("select[name$=end_date__minute]").get(0);
+      if (!end_minute) return;
+
+      var option_matches = [];
+      $.each(options, function(j, p) {
+          var option = $(p);
+          var matches = re_time_range.exec(option.text());
+          option_matches.push(matches);
+          if (matches)
+            option.text(matches[1]);
+        });
+
+      function handle_change() {
+        var matches = option_matches[o.selectedIndex];
+
+        if (!matches) return;
+
+        // convert matches to integers
+        for (var k=1; k < 5; k++) {
+          var v = matches[k];
+          if (v[0] == '0') v = v[1];
+          matches[k] = parseInt(v);
+        }
+
+        // for each dropdown, change selection
+        start_hour.selectedIndex = matches[2]
+        start_minute.selectedIndex = matches[3];
+        end_hour.selectedIndex = matches[4];
+        end_minute.selectedIndex = matches[5];
+      };
+
+      // setup event handler
+      $(o).change(handle_change);
+
+      // initialize
+      handle_change();
+    });
+  };
+    
+    
   $.fn.yuiTabView = function(elements) {
     if (!YAHOO.widget.TabView) {
       return console.log("Warning: YAHOO.widget.TabView module not loaded.")

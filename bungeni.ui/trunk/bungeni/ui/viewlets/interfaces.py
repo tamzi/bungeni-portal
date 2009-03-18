1795ec50b29acec9da96b86fb8b3b26fe7a174f7
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-03-18 09:10:18 +0000

    Adding scheduling viewlets.

--- bungeni.ui/trunk/bungeni/ui/viewlets/interfaces.py
+++ bungeni.ui/trunk/bungeni/ui/viewlets/interfaces.py
@@ -1,4 +1,22 @@
+from zope.viewlet.interfaces import IViewlet
 from zope.viewlet.interfaces import IViewletManager
 
 class IWorkspaceManager(IViewletManager):
     """Workspace viewlet manager."""
+
+class ISchedulingManager(IViewletManager):
+    """Scheduling viewlet manager."""
+
+class ISchedulingViewlet(IViewlet):
+    """A scheduling viewlet renders table rows corresponding to
+    schedulable items.
+
+    Must render columns in order:
+
+    - Type name
+    - Title
+    - Workflow state
+    - Modification date
+
+    The ``title`` should be linked to the URL of the item.
+    """

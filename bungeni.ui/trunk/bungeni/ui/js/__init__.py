e55d9f23545ccf7836e62e55b3c8cc2695885fdd
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-03-20 07:27:00 +0000

    Adding sortable plugin.

--- bungeni.ui/trunk/bungeni/ui/js/__init__.py
+++ bungeni.ui/trunk/bungeni/ui/js/__init__.py
@@ -7,5 +7,6 @@ Core = JSViewlet('bungeni-js/jquery/ui.core.js')
 Resizable = JSViewlet('bungeni-js/jquery/ui.resizable.js')
 Droppable = JSViewlet('bungeni-js/jquery/ui.droppable.js')
 Draggable = JSViewlet('bungeni-js/jquery/ui.draggable.js')
+Sortable = JSViewlet('bungeni-js/jquery/ui.sortable.js')
 
 Form = JSViewlet('bungeni-js/jquery/jquery.form-2.12.js')

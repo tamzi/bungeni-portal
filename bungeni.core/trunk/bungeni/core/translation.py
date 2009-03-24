4a74d6c7677b1daa4c662e7578d4f085da1242a7
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-03-24 09:39:54 +0000

    Added methods to return available languages and default language; these should be pulled from some global configuration later on.

--- bungeni.core/trunk/bungeni/core/translation.py
+++ bungeni.core/trunk/bungeni/core/translation.py
@@ -1,5 +1,17 @@
+from bungeni.core.i18n import  _
+
+def get_default_language():
+    return "en"
+
 def get_language(context):
     return "en"
 
 def has_language(context, name):
     return False
+
+def available_languages():
+    return (
+        ('en', _(u"English")),
+        ('fr', _(u"French")),
+        ('sw', _(u"Swahili")),
+        )

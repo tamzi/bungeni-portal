1be5773b294428d54be8efc2fc9bee90cbf0c7f8
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-03-25 08:11:37 +0000

    To view a version, instead of using a form action and response, just provide a link to the version (e.g. versions/obj-%(id)s).

--- bungeni.ui/trunk/bungeni/ui/versions.py
+++ bungeni.ui/trunk/bungeni/ui/versions.py
@@ -7,14 +7,17 @@ from zope.app.pagetemplate import ViewPageTemplateFile
 from zope.app.publisher.browser import queryDefaultViewName
 from zope.formlib import form
 from zope.security.proxy import removeSecurityProxy
+from zope.traversing.browser import absoluteURL
 
 from ore.alchemist.interfaces import IIModelInterface
 
 from bungeni.ui.i18n import MessageFactory as _
+from bungeni.ui.table import TableFormatter
 from bungeni.core.interfaces import IVersioned
 
 from alchemist.ui.core import BaseForm, getSelected
-from zc.table import  column, batching
+from zc.table import  column
+
 import z3c.schemadiff.browser
 
 class VersionsView(BrowserView):
@@ -49,41 +52,46 @@ class VersionsView(BrowserView):
 
         return view()                
 
-class IVersionEntry( interface.Interface ):
-    commit_message = schema.Text(title=_(u"Change Message"))
-
 class VersionLogView(BaseForm):
-    form_fields = form.Fields( IVersionEntry )
-    formatter_factory = batching.Formatter
+    class IVersionEntry(interface.Interface):
+        commit_message = schema.Text(title=_(u"Change Message"))
+
+    form_fields = form.Fields(IVersionEntry)
+    formatter_factory = TableFormatter
     
     render = ViewPageTemplateFile('templates/version.pt')
-
     extra = None
     
     columns = [
-        column.SelectionColumn( lambda item: str(item.version_id), name="selection"),
-        column.GetterColumn( title=_(u"version"), getter=lambda i,f:i.version_id ),  
-        column.GetterColumn( title=_(u"manual"), getter=lambda i,f:i.manual ),          
-        column.GetterColumn( title=_(u"modified"), getter=lambda i,f: i.change.date ),
-        column.GetterColumn( title=_(u"by"), getter=lambda i,f:i.change.user_id ),
-        column.GetterColumn( title=_(u"message"), getter=lambda i,f:i.change.description ),
-        ]    
+        column.SelectionColumn(lambda item: str(item.version_id), name="selection"),
+        column.GetterColumn(
+            title=_(u"version"),
+            getter=lambda i,f: '<a href="%s">%d</a>' % (
+                "%s/versions/obj-%d" % (f.url, i.version_id), i.version_id)),
+        column.GetterColumn(title=_(u"manual"), getter=lambda i,f:i.manual),
+        column.GetterColumn(title=_(u"modified"), getter=lambda i,f: i.change.date),
+        column.GetterColumn(title=_(u"by"), getter=lambda i,f:i.change.user_id),
+        column.GetterColumn(title=_(u"message"), getter=lambda i,f:i.change.description ),
+        ]
     
     selection_column = columns[0]
 
     def __init__(self, context, request):
         super(VersionLogView, self).__init__(context.__parent__, request)
         
-    def listing( self ):
-        columns = self.columns
-        formatter = self.formatter_factory( self.context,
-                                            self.request,
-                                            self._versions.values(),
-                                            prefix="results",
-                                            visible_column_names = [c.name for c in columns],
-                                            #sort_on = ('name', False)
-                                            columns = columns )
-        formatter.cssClasses['table'] = 'listing'
+    def listing(self):
+        # set up table
+        formatter = self.formatter_factory(
+            self.context, self.request,
+            self._versions.values(),
+            prefix="results",
+            visible_column_names = [c.name for c in self.columns],
+            columns = self.columns)
+
+        # the column getter methods expect an ``url`` attribute
+        formatter.url = absoluteURL(self.context, self.request)
+
+        # update and render
         formatter.updateBatching()
         return formatter()
             
@@ -91,9 +99,6 @@ class VersionLogView(BaseForm):
     def handle_new_version( self, action, data ):
         self._versions.create( message = data['commit_message'], manual=True )        
         self.status = _(u"New Version Created")
-
-    #def validate_diff_version( self, ):
-    #def validate_revert_version( self )
 
     @form.action(label=_("Revert To") )
     def handle_revert_version( self, action, data):
@@ -104,22 +109,8 @@ class VersionLogView(BaseForm):
         self.status = (_(u"Reverted to Previous Version %s") %(version.version_id))
 
     @form.action(
-        label=_("View"), name="view", validator=lambda form, action, data: ())
-    def handle_view_version( self, action, data):
-        selected = getSelected(self.selection_column, self.request)
-        if len(selected) != 1:
-            self.status = _(u"Select one item to display")
-            return
-            
-        context = self._versions.get( selected[0] )
-
-        view = component.getMultiAdapter((context, self.request),
-                                         name=queryDefaultViewName(context, self.request))
-
-        self.extra = view()
-        
-    @form.action(
-        label=_("Show Differences"), name="diff", validator=lambda form, action, data: ())
+        label=_("Show Differences"), name="diff",
+        validator=lambda form, action, data: ())
     def handle_diff_version( self, action, data):
         self.status = _("Displaying differences.")
 

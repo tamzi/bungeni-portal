502ead00273bef825aeab0d43cc3ed7490f9b84e
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-03-26 12:31:47 +0000

    Added versions workflow.

--- bungeni.core/trunk/bungeni/core/workflows/adapters.py
+++ bungeni.core/trunk/bungeni/core/workflows/adapters.py
@@ -1,12 +1,16 @@
 import os
+import events
+import xmlimport
+
 import bill
 import question
 import motion
 import response
-import events
-import xmlimport
+import version
 
 from bungeni.models import domain
+from bungeni.models.interfaces import IVersion
+
 from ore.workflow import workflow
 
 path = os.path.split(os.path.abspath(__file__))[0]
@@ -27,4 +31,6 @@ BillWorkflowAdapter = workflow.AdaptedWorkflow(
     load_workflow(bill, domain.Bill))
 ResponseWorkflowAdapter = workflow.AdaptedWorkflow(
     load_workflow(response, domain.Response))
+VersionWorkflowAdapter = workflow.AdaptedWorkflow(
+    load_workflow(version, IVersion))
 

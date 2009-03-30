cd02ae4098de7686979160a158b2e24bb2d05f57
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-03-30 07:15:38 +0000

    Use Plone's form tabbing script and styling to move closer to a homogenous look and feel for the portal.

--- bungeni.ui/trunk/bungeni/ui/js/behavior.js
+++ bungeni.ui/trunk/bungeni/ui/js/behavior.js
@@ -1,9 +1,5 @@
 (function($) {
   $(document).ready(function() {
-      // prepare alchemist content views
-      var manager = $(".alchemist-content-manager");
-      manager.yuiTabView(manager.find("div.listing"));
-
       // wire workflow dropdown menu to use POST actions
       var menu_links = $('#plone-contentmenu-workflow dd.actionMenuContent a');
       menu_links.bungeniPostWorkflowActionMenuItem();

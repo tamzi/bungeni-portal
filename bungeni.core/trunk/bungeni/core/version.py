e757b8719586d35f42723ac2d5ea1621a5183a69
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-03-30 15:45:08 +0000

    Reworked versions utility such that it does not rely on 'zope.schema'. Instead, it copies any column that is not a primary key.

--- bungeni.core/trunk/bungeni/core/version.py
+++ bungeni.core/trunk/bungeni/core/version.py
@@ -1,89 +1,101 @@
 """
 $Id: $
 """
-from zope import interface, schema, event
+from zope import interface
+from zope import event
 
 from sqlalchemy import orm
-from ore.alchemist import container, model, Session
+from ore.alchemist import container
+from ore.alchemist import Session
 from zope.security.proxy import removeSecurityProxy
 
 from i18n import _
 
 from bungeni.models import domain
+from bungeni.core import interfaces
 
-import interfaces
-
-
-class Versioned( container.PartialContainer ):
+class Versioned(container.PartialContainer):
+    interface.implements(interfaces.IVersioned)
     
-    interface.implements( interfaces.IVersioned )
-    
-    def _copyFields( self, source, dest, iface ):
-        for field in schema.getFields( iface ).values():
-            #import pdb; pdb.set_trace()
-            #print field.__name__
-            value = field.query( source ) 
-            if value is None:
+    def _copyFields( self, source, dest):
+        kls = source.__class__
+        table = orm.class_mapper(kls).mapped_table
+
+        for column in table.columns:
+            if column.primary_key:
                 continue
-            field.set( dest, value )
-            
+
+            value = getattr(source, column.name)
+            setattr(dest, column.name, value)
+
     def create( self, message, manual = False ):
-        """
-        store the existing state of the adapted context as a new version
-        """
+        """Store the existing state of the adapted context as a new
+        version."""
+
         version = self.domain_model()
-        trusted_ctx = removeSecurityProxy( self.__parent__ )
-        ctx_class = trusted_ctx.__class__
+        context = self.__parent__
+        trusted = removeSecurityProxy(context)
         
         # set values on version from context
-        self._copyFields(  trusted_ctx, version,
-                           model.queryModelInterface( ctx_class ) )
+        self._copyFields(trusted, version)
         
         # content domain ids are typically not in the interfaces
         # manually inspect and look for one, by hand to save on the new version
-        mapper = orm.object_mapper( trusted_ctx )
-        version.content_id = mapper.primary_key_from_instance( trusted_ctx )[0]
+        mapper = orm.object_mapper(trusted)
+        version.content_id = mapper.primary_key_from_instance(trusted)[0]
         
         version.manual = manual
         
         # we rely on change handler to attach the change object to the version
-        event.notify( interfaces.VersionCreated( self.__parent__, self, version, message  ) )
+        event.notify(
+            interfaces.VersionCreated(context, self, version, message))
         
         # save our new version to the db
         session = Session()
-        session.add( version )
+        session.add(version)
 
         return version
         
     def revert( self, version, message ):
-        """
-        revert the current state of the adapted object to the values specified
-        in version, and create a new version with reverted state. 
-        """
-        trusted_ctx = removeSecurityProxy( self.__parent__ )
-        ctx_class = trusted_ctx.__class__        
-        has_wf_status = getattr(trusted_ctx,'status',None)
+        """Revert the current state of the adapted object to the
+        values specified in version, and create a new version with
+        reverted state."""
+
+        context = self.__parent__
+        trusted = removeSecurityProxy(context)
+        ctx_class = trusted.__class__
+        
+        has_wf_status = hasattr(context, 'status')
         if has_wf_status:
-            wf_status = trusted_ctx.status
+            wf_status = context.status
+            
         # set values on version from context
-        self._copyFields(  version, self.__parent__,
-                           model.queryModelInterface( ctx_class ) )                  
+        self._copyFields(version, trusted)
+
         if has_wf_status:
-            trusted_ctx.status = wf_status                                       
-        msg = (_(u"reverted to previous version %s") %(version.version_id)) + u" - " + message        
-        event.notify( interfaces.VersionReverted( self.__parent__, self, version, msg  ) )
-        self.create( message=msg )
+            context.status = wf_status
+            
+        msg = _(u"Reverted to previous version $version.",
+                mapping={'version': version.version_id})
+
+        event.notify(
+            interfaces.VersionReverted(context, self, version, msg))
+        
+        self.create(message=msg)
 
 class ContentVersionsFactory( object ):
 
     def __call__( self, context ):
-        content_version_class = getattr( domain, context.__class__.__name__ + "Version" )
+        content_version_class = getattr(
+            domain, context.__class__.__name__ + "Version")
+        
         versions = Versioned()
         versions._class = content_version_class 
         trusted_ctx = removeSecurityProxy( context )
         mapper = orm.object_mapper( trusted_ctx )
         pk_value = mapper.primary_key_from_instance( trusted_ctx )[0]
-        versions.setQueryModifier( content_version_class.c[ "content_id" ] == pk_value )
+        versions.setQueryModifier(
+            content_version_class.c[ "content_id" ] == pk_value)
         versions.__parent__ = context
         versions.__name__ = 'versions'
         return versions

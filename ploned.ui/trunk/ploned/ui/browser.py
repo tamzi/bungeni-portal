a5b253ecf4782585265a6b02e9a9f0f8a46890ee
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-03-30 07:56:05 +0000

    Use PEP 8 method name for 'show_editable_border' method; added 'have_viewlets' method that allows templates to query a viewlet manager for viewlets. Also ensured that 'Plone' view is not callable.

--- ploned.ui/trunk/ploned/ui/browser.py
+++ ploned.ui/trunk/ploned/ui/browser.py
@@ -1,6 +1,30 @@
+from zope import interface
+from zope import component
 from zope.publisher.browser import BrowserView
 from zope.security import checkPermission
+from zope.viewlet.interfaces import IViewlet
+from zope.viewlet.interfaces import IViewletManager
 
 class PloneView(BrowserView):
-    def showEditableBorder(self):
+    def show_editable_border(self):
         return checkPermission("zope.ManageContent", self.context)  
+
+    def have_viewlets(self, view, name):
+        gsm = component.getSiteManager()
+
+        specification = map(interface.providedBy, (self.context, self.request, view))
+        manager = gsm.adapters.lookup(specification, IViewletManager, name=name)
+
+        iface = tuple(interface.implementedBy(manager))[0]
+        specification.append(iface)
+
+        for viewlet in  gsm.adapters.lookupAll(specification, IViewlet):
+            return True
+
+        return False
+
+    def __call__(self):
+        """Return ``self`` such that templates don't need an explicit
+        ``nocall`` pragma."""
+        
+        return self

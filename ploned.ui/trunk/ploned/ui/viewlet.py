1f71fbaedd735708724191d74703de50d04d8a13
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-04-06 07:56:22 +0000

    Split out 'is_selected' method for reuse outside of classes; made the personal tools viewlet structure-aware.

--- ploned.ui/trunk/ploned/ui/viewlet.py
+++ ploned.ui/trunk/ploned/ui/viewlet.py
@@ -5,15 +5,15 @@ $Id:
 from zope.viewlet.manager import WeightOrderedViewletManager
 from ploned.ui.interfaces import IStructuralView
 
+class StructureAwareViewlet(object):
+    def __init__(self, context, request, view, manager):
+        if IStructuralView.providedBy(view):
+            context = context.__parent__
+        super(StructureAwareViewlet, self).__init__(
+            context, request, view, manager)
+
 class WeightBasedSorter( WeightOrderedViewletManager ):
     """
     No need to do a custom sort implementation - it did not work anyway
     """
- 
-class ContentTabsViewlet(object):
-    def __init__(self, context, request, view, manager):
-        if IStructuralView.providedBy(view):
-            context = context.__parent__
-        super(ContentTabsViewlet, self).__init__(
-            context, request, view, manager)
 

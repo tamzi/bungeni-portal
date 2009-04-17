4e5806ec85666638a6644b13756d69ed0eb8d175
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-04-17 06:51:51 +0000

    Abandoned mail-merge idea; instead, use Chameleon engine to process content as ZPT.

--- bungeni.core/trunk/bungeni/core/odf.py
+++ bungeni.core/trunk/bungeni/core/odf.py
@@ -1,6 +1,7 @@
 import copy
 import zipfile
-import lxml.etree
+
+from z3c.pt.pagetemplate import ViewPageTemplate
 
 namespaces = {'text': 'urn:oasis:names:tc:opendocument:xmlns:text:1.0'}
 
@@ -25,6 +26,13 @@ class OpenDocument(object):
         info = self.zipfile.getinfo("content.xml")
         return self.get(info)
 
+    def process(self, filename, view):
+        info = self.zipfile.getinfo(filename)
+        bytes = self.get(info)
+        template = ViewPageTemplate(bytes)
+        bound_template = template.bind(view)
+        self.contents[info] = bound_template()
+        
     def save(self, filename):
         outfile = zipfile.ZipFile(filename, 'w')
         try:
@@ -33,83 +41,3 @@ class OpenDocument(object):
                 outfile.writestr(info, bytes)
         finally:
             outfile.close()
-
-    def merge(self, databases):
-        """Mail-merge document using ``databases``.
-
-        If a field appears outside a section, a single row is fetched
-        immediately and consecutive rows only when a 'next marker'
-        appears*.
-
-        Sections that contain fields are repeated until the table of
-        the field is exhausted. If fields from multiple tables are
-        used, the tables must contain the same number of rows.
-
-        Only the first section in a branch will be repeated; nested
-        sections have no semantic value.
-
-        *) To-do: Add support for 'next marker'.
-        """
-        
-        info = self.zipfile.getinfo("content.xml")
-        content = self.get(info)
-        tree = lxml.etree.fromstring(content)
-
-        # initialize table pointer directory
-        tp = {}
-        for db_name, database in databases.items():
-            for tb_name in database:
-                tp[db_name, tb_name] = 0
-
-        # process elements that appear outside a section
-        elements = []
-        for element in tree.xpath(".//text:database-display", namespaces=namespaces):
-            if element.xpath('ancestor::text:section', namespaces=namespaces):
-                continue
-            elements.append(element)
-        process_elements(elements, databases, tp)
-
-        # process sections
-        for element in tree.xpath(".//text:section", namespaces=namespaces):
-            if element.xpath('ancestor::text:section', namespaces=namespaces):
-                continue
-
-            while True:
-                section = copy.deepcopy(element)
-                tp_keys = process_elements(
-                    section.xpath(".//text:database-display", namespaces=namespaces),
-                    databases, tp)
-
-                if tp_keys is None:
-                    break
-                else:
-                    for key in tp_keys:
-                        tp[key] = tp[key] + 1
-
-                # add section
-                element.addnext(section)
-            
-            # remove original section
-            parent = element.getparent()
-            parent.remove(element)
-
-        content = lxml.etree.tostring(tree)
-        self.contents[info] = content
-
-def process_elements(elements, databases, tp):
-    tp_keys = set()
-    for element in elements:
-        db_name = element.attrib["{%s}database-name" % namespaces['text']]
-        tb_name = element.attrib["{%s}table-name" % namespaces['text']]
-        cl_name = element.attrib["{%s}column-name" % namespaces['text']]
-
-        key = db_name, tb_name
-        tp_keys.add(key)
-        pointer = tp[key]
-        
-        table = databases[db_name][tb_name]
-        if pointer > len(table) - 1:
-            return None
-        element.text = table[pointer][cl_name]
-        
-    return tp_keys

2e8b2431144a6371894c5ad3f7b3b0694ec600f3
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-04-20 12:40:58 +0000

    Reverse query.

--- bungeni.models/trunk/bungeni/models/queries.py
+++ bungeni.models/trunk/bungeni/models/queries.py
@@ -1,7 +1,8 @@
 from ore.alchemist import Session
 from bungeni.models import domain
+from sqlalchemy import desc
 
 def get_current_parliament(context):
     session = Session()
     return session.query(domain.Parliament).order_by(
-        'election_date').first()
+        desc(domain.Parliament.election_date)).first()

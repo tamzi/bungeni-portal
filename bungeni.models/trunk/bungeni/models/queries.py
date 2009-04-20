27a81599465a20274eae2851aeecf3ce160638b2
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-04-20 13:57:41 +0000

    Allow open-ended parliament and session objects.

--- bungeni.models/trunk/bungeni/models/queries.py
+++ bungeni.models/trunk/bungeni/models/queries.py
@@ -1,6 +1,6 @@
 from ore.alchemist import Session
 from bungeni.models import domain
-from sqlalchemy import desc, between
+from sqlalchemy import desc
 
 def get_current_parliament(context):
     session = Session()
@@ -11,11 +11,13 @@ def get_parliament_by_date_range(context, start_date, end_date):
     session = Session()
     return session.query(domain.Parliament).filter(
         (domain.Parliament.start_date < start_date) & \
-        (domain.Parliament.end_date > end_date)).\
+        ((domain.Parliament.end_date == None) | \
+         (domain.Parliament.end_date > end_date))).\
         order_by(desc(domain.Parliament.election_date)).first()
 
 def get_session_by_date_range(context, start_date, end_date):
     session = Session()
     return session.query(domain.ParliamentSession).filter(
         (domain.ParliamentSession.start_date < start_date) & \
-        (domain.ParliamentSession.end_date > end_date)).first()
+        ((domain.ParliamentSession.end_date == None) | \
+         (domain.ParliamentSession.end_date > end_date))).first()

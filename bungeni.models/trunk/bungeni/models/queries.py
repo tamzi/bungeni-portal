2dc72d36593d5ee74ccc39ff831c03d49f6c8833
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-04-20 13:37:39 +0000

    Adding utility-methods to get parliament and session by range (they must contain this range).

--- bungeni.models/trunk/bungeni/models/queries.py
+++ bungeni.models/trunk/bungeni/models/queries.py
@@ -1,8 +1,21 @@
 from ore.alchemist import Session
 from bungeni.models import domain
-from sqlalchemy import desc
+from sqlalchemy import desc, between
 
 def get_current_parliament(context):
     session = Session()
     return session.query(domain.Parliament).order_by(
         desc(domain.Parliament.election_date)).first()
+
+def get_parliament_by_date_range(context, start_date, end_date):
+    session = Session()
+    return session.query(domain.Parliament).filter(
+        (domain.Parliament.start_date < start_date) & \
+        (domain.Parliament.end_date > end_date)).\
+        order_by(desc(domain.Parliament.election_date)).first()
+
+def get_session_by_date_range(context, start_date, end_date):
+    session = Session()
+    return session.query(domain.ParliamentSession).filter(
+        (domain.ParliamentSession.start_date < start_date) & \
+        (domain.ParliamentSession.end_date > end_date)).first()

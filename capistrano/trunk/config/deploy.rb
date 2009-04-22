a26cbec78cc09bf8cb5c1900d074bd9d75f6b288
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-04-22 14:31:38 +0000

    Added namespaces for doing bungeni updates with & without db updates

--- capistrano/trunk/config/deploy.rb
+++ capistrano/trunk/config/deploy.rb
@@ -41,7 +41,7 @@ set :adm_python_home, "#{user_python_home}"
 set :adm_python, "#{adm_python_home}/bin/python"
 
 
-##generate supervisord config files
+## generate supervisord config files
 ## config file for supervisord is generated using a ERB template
 ## supervisord is installed using ez_setup
 
@@ -51,10 +51,10 @@ set :supervisorctl, "#{adm_python_home}/bin/supervisorctl"
 # erb template to supervisord.conf
 set :supervisord_config_file, "supervisord.conf.erb"
 
-##force prompt if any unknown prompts pop up
+## force prompt if any unknown prompts pop up
 default_run_options[:pty] = true
 
-##set :deploy_to_root, Proc.new { Capistrano::CLI.password_prompt('Deploy within this folder: ') }
+## set :deploy_to_root, Proc.new { Capistrano::CLI.password_prompt('Deploy within this folder: ') }
 set :deploy_to, "#{deploy_to_root}/#{application}"
 #was set :deploy_to, "/home/bungeni/bungeni_deploy/#{application}"
 set :buildout_dir, "#{deploy_to}/current"
@@ -75,120 +75,132 @@ role :app, "#{bungeni_username}@#{app_host}"
 #
 
 
-namespace :bungeni do
-## generate supervisord.conf using a ERB template found in config/templates
-desc "write supervisor config file"
-task :supervisord_config, :roles => [:app] do
-	file = File.join(File.dirname(__FILE__), "templates", supervisord_config_file)
-	template = File.read(file)
-	buffer = ERB.new(template).result(binding)
-	put buffer, "#{buildout_dir}/supervisord.conf", :mode => 0644
-end
+namespace :libbungeni do
+    ## generate supervisord.conf using a ERB template found in config/templates
+    desc "write supervisor config file"
+    task :supervisord_config, :roles => [:app] do
+        file = File.join(File.dirname(__FILE__), "templates", supervisord_config_file)
+        template = File.read(file)
+        buffer = ERB.new(template).result(binding)
+        put buffer, "#{buildout_dir}/supervisord.conf", :mode => 0644
+    end
 
-## setup easy_install for the python and then install supervisord 
-task :python_setup, :roles => [:app] do 
-	run "cd #{user_python_home} && [ -f ./ez_setup.py ] && echo 'ez_setup.py exists' || wget http://peak.telecommunity.com/dist/ez_setup.py"
-	run "cd #{user_python_home} && #{user_python} ./ez_setup.py"
-	run "#{user_python_home}/bin/easy_install supervisor"
-end
-
-
-
-desc "bootstrap"
-task :bootstrap_bo, :roles=> :app do
-	run  "cd #{buildout_dir} && #{user_python} ./bootstrap.py"
-end
-
-
-desc "full buildout"
-task :buildout_full, :roles=> :app do
-	run "cd #{buildout_dir} && PYTHON=#{user_python} ./bin/buildout"
-end
-
-desc "optimisitic builout" 
-task :buildout_opt, :roles=> :app do
-	run "cd #{buildout_dir} && PYTHON=#{user_python} ./bin/buildout -N"
-end
-
-desc "update source" 
-task :bungeni_upd, :roles=> :app do
-	run "cd #{buildout_dir} && svn up"
-	run "cd #{buildout_dir}/src && svn up"
-end
+    ## setup easy_install for the python and then install supervisord
+    task :python_setup, :roles => [:app] do
+      run "cd #{user_python_home} && [ -f ./ez_setup.py ] && echo 'ez_setup.py exists' || wget http://peak.telecommunity.com/dist/ez_setup.py"
+      run "cd #{user_python_home} && #{user_python} ./ez_setup.py"
+      run "#{user_python_home}/bin/easy_install supervisor"
+    end
 
 
 
-
-desc "start postgres"
-task :postgres_start, :roles=> :app do
-	run "cd #{buildout_dir} && ./bin/pg_ctl start"
-end
-
-desc "stop postgres"
-task :postgres_stop, :roles=> :app do
-	run "cd #{buildout_dir} && ./bin/pg_ctl stop"
-end
-
-desc "setup database"
-task :setup_db, :roles=> :app do
-	run "cd #{buildout_dir} && ./bin/setup-database"
-end
-
-desc "reset database"
-task :reset_db, :roles=> :app do
-	run "cd #{buildout_dir} && ./bin/reset-db"
-end
-
-desc "load demo data"
-task :install_demo_data, :roles=> :app do
-	run "cd #{buildout_dir} && ./bin/load-demo-data"
-end
+    desc "bootstrap"
+    task :bootstrap_bo, :roles=> :app do
+      run  "cd #{buildout_dir} && #{user_python} ./bootstrap.py"
+    end
 
 
-desc "start supervisor"
-task :start_supervisor, :roles=> :app do
-	run "#{supervisord} -c #{buildout_dir}/supervisord.conf"
-end
+    desc "full buildout"
+    task :buildout_full, :roles=> :app do
+      run "cd #{buildout_dir} && PYTHON=#{user_python} ./bin/buildout"
+    end
 
-desc "stop supervisor"
-task :stop_supervisor, :roles=> :app do
-	run "#{supervisord} -c #{buildout_dir}/supervisord.conf"
-end
+    desc "optimisitic builout"
+    task :buildout_opt, :roles=> :app do
+      run "cd #{buildout_dir} && PYTHON=#{user_python} ./bin/buildout -N"
+    end
+
+    desc "update source"
+    task :bungeni_upd, :roles=> :app do
+      run "cd #{buildout_dir} && svn up"
+      run "cd #{buildout_dir}/src && svn up"
+    end
+
+    desc "start postgres"
+    task :postgres_start, :roles=> :app do
+      run "cd #{buildout_dir} && ./bin/pg_ctl start"
+    end
+
+    desc "stop postgres"
+    task :postgres_stop, :roles=> :app do
+      run "cd #{buildout_dir} && ./bin/pg_ctl stop"
+    end
+
+    desc "setup database"
+    task :setup_db, :roles=> :app do
+      run "cd #{buildout_dir} && ./bin/setup-database"
+    end
+
+    desc "reset database"
+    task :reset_db, :roles=> :app do
+      run "cd #{buildout_dir} && ./bin/reset-db"
+    end
+
+    desc "load demo data"
+    task :install_demo_data, :roles=> :app do
+      run "cd #{buildout_dir} && ./bin/load-demo-data"
+    end
+
+
+    desc "start supervisor"
+    task :start_supervisor, :roles=> :app do
+      run "#{supervisord} -c #{buildout_dir}/supervisord.conf"
+    end
+
+    desc "stop bungeni"
+    task :stop_bungeni, :roles=> :app do
+      run "#{supervisorctl} stop bungeni"
+    end
+
+    desc "start bungeni"
+    task :start_bungeni, :roles=> :app do
+      run "#{supervisorctl} start bungeni"
+    end
 
 
 
-
+    desc "stop supervisor"
+    task :stop_supervisor, :roles=> :app do
+      run "#{supervisorctl} shutdown"
+    end
 
 ## run python_setup automatically aftter deploy:setup
-after "deploy:setup", "bungeni:python_setup"
+#after "deploy:setup", "bungeni:python_setup"
 ## after python_setup automatically do a deploy:update to setup the svn repo
-after "bungeni:python_setup", "deploy:update"
+#after "bungeni:python_setup", "deploy:update"
 ## after deploy:update, setup supervisord config
-after "deploy:update", "bungeni:supervisord_config"
+#after "deploy:update", "bungeni:supervisord_config"
 
 end
 
-namespace :bungeniupdate do
 
-task :optimistic_update, :roles=> :app do
-	run "echo 'Running optimistic update of bungeni'"
+### Defines a sequence of tasks for updating bungeni ###
+namespace :bungeni_update do
+
+    task :quick_update, :roles=> :app do
+      run "echo 'Running optimistic update of bungeni'"
+    end
+
+
+    after ":bungeni_update:quick_update", "libbungeni:stop_bungeni", "libbungeni:bungeni_upd", "libbungeni:buildout_opt", "libbungeni:start_bungeni"
+
+    task :quick_update_with_db, :roles=> :app do
+      run "echo 'Runnning quick update with db update'"
+    end
+
+    after ":bungeni_update:quick_update_with_db",  "libbungeni:stop_bungeni", "libbungeni:bungeni_upd", "libbungeni:buildout_opt", "libbungeni:reset_db", "libbungeni:install_demo_data", "libbungeni:start_bungeni"
+
 end
 
 
-after "bungeniupdate:optimistic_update", "bungeni:bungeni_upd", "bungeni:buildout_opt"
+### Defines a sequence of tasks for installing bungeni from scratch ###
+namespace :bungeni_install do
 
+    task :full, :roles=> :app do
+      run "echo 'bootstraping and running a full buildout'"
+    end
 
-
-end
-
-namespace :bungeniinstall do
-
-task :full, :roles=> :app do
-	run "echo 'bootstraping and running a full buildout'"
-end
-
-after "bungeniinstall:full", "bungeni:bootstrap_bo", "bungeni:buildout_full"
-
+    after "bungeni_install:full", "bungeni:bootstrap_bo", "bungeni:buildout_full"
 
 end
   

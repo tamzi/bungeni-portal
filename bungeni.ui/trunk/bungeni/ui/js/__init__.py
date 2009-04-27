a5984b0133195ecedfcc8d269a53c21ce72c35a6
christian.ledermann <christian.ledermann@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-04-27 17:19:27 +0000

    jquery effects for recurring events form

--- bungeni.ui/trunk/bungeni/ui/js/__init__.py
+++ bungeni.ui/trunk/bungeni/ui/js/__init__.py
@@ -3,11 +3,13 @@ from zope.viewlet.viewlet import JavaScriptViewlet as JSViewlet
 Plugins = JSViewlet('bungeni-js/plugins.js')
 Behavior = JSViewlet('bungeni-js/behavior.js')
 FormTabbing = JSViewlet('bungeni-js/form_tabbing.js')
+RecurringForm = JSViewlet('bungeni-js/recurring_events.js')
 
 Core = JSViewlet('bungeni-js/jquery/ui.core.js')
 Resizable = JSViewlet('bungeni-js/jquery/ui.resizable.js')
 Droppable = JSViewlet('bungeni-js/jquery/ui.droppable.js')
 Draggable = JSViewlet('bungeni-js/jquery/ui.draggable.js')
 Sortable = JSViewlet('bungeni-js/jquery/ui.sortable.js')
+Datepicker = JSViewlet('bungeni-js/jquery/ui.datepicker.js')
 
 Form = JSViewlet('bungeni-js/jquery/jquery.form-2.12.js')

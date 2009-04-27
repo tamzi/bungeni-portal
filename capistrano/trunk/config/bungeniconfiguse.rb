a9cee1d4b9b0be2d27d7cef781abc786dc966070
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-04-27 07:32:18 +0000

    adding config file

---
+++ capistrano/trunk/config/bungeniconfiguse.rb
@@ -0,0 +1,58 @@
+#### CONFIG VARIABLE SETTING ####
+require "commonfunctions.rb"
+
+set :application, "bungeni"
+set :bungeni_username, "bungeni"
+set :repository,  "https://bungeni-portal.googlecode.com/svn/bungeni.buildout/trunk"
+
+## prompt for svn user names & passwords
+set :scm, :subversion
+
+## all prompts here
+set :scm_username, "ashok.hariharan"
+set :scm_password, "y8n5h7d9"
+set :user_python_home, "/home/bungeni/dev/bungeni/python/245"
+set :deploy_to_root, "/home/bungnei/dev/test_deploy"
+
+
+## user python is used to run bungeni in the user context -- this is a pre-requisite
+#was "/home/bungeni/apps/python"
+set :user_python, "#{user_python_home}/bin/python"
+
+## admin python is used to run supervisord can be same as user python or a different one
+set :adm_python_home, "/home/bungeni/dev/bungeni/python/adm"
+set :adm_python, "#{adm_python_home}/bin/python"
+
+
+## generate supervisord config files
+## config file for supervisord is generated using a ERB template
+## supervisord is installed using ez_setup
+
+set :supervisord, "#{adm_python_home}/bin/supervisord"
+set :supervisorctl, "#{adm_python_home}/bin/supervisorctl"
+
+# erb template to supervisord.conf
+set :supervisord_config_file, "supervisord.conf.erb"
+
+## force prompt if any unknown prompts pop up
+default_run_options[:pty] = true
+
+## set :deploy_to_root, Proc.new { Capistrano::CLI.password_prompt('Deploy within this folder: ') }
+set :deploy_to, "#{deploy_to_root}/#{application}"
+#was set :deploy_to, "/home/bungeni/bungeni_deploy/#{application}"
+set :buildout_dir, "#{deploy_to}/current"
+
+set :user, "#{bungeni_username}"
+set :use_sudo, false
+set :app_host, "localhost"
+
+#### ROLE SETTING ####
+#
+# db role is not required for capistrano 
+# for webistrano, a db role is mandatory. so we add the following line for webistrano
+# which adds the db role but never deploys or releases it
+# role :db, "demo.bungeni.org", {:no_release=>true, :primary=>true}
+#
+
+role :app, "#{bungeni_username}@#{app_host}"
+

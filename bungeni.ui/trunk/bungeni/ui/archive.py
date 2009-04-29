7ff775842876336b8c9e696a190a2159eaff5a3c
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-04-29 08:28:47 +0000

    Add structural view marker to archive.

--- bungeni.ui/trunk/bungeni/ui/archive.py
+++ bungeni.ui/trunk/bungeni/ui/archive.py
@@ -1,5 +1,11 @@
+from zope import interface
 from zope.publisher.browser import BrowserView
 from zope.app.pagetemplate import ViewPageTemplateFile
 
+from ploned.ui.interfaces import IViewView
+from ploned.ui.interfaces import IStructuralView
+
 class ArchiveBrowserView(BrowserView):
     __call__ = ViewPageTemplateFile("templates/archive.pt")
+
+    interface.implements(IViewView, IStructuralView)

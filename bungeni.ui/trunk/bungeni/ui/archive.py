5801c625d60f49b075733a1d39207fdb1c68a596
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-04-30 13:59:02 +0000

    Clean-up after simplifying default browser publishing patterns in 'bungeni.core'.

--- bungeni.ui/trunk/bungeni/ui/archive.py
+++ bungeni.ui/trunk/bungeni/ui/archive.py
@@ -3,9 +3,8 @@ from zope.publisher.browser import BrowserView
 from zope.app.pagetemplate import ViewPageTemplateFile
 
 from ploned.ui.interfaces import IViewView
-from ploned.ui.interfaces import IStructuralView
 
 class ArchiveBrowserView(BrowserView):
     __call__ = ViewPageTemplateFile("templates/archive.pt")
 
-    interface.implements(IViewView, IStructuralView)
+    interface.implements(IViewView)

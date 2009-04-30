461298cdc964062eba76b1de8cd9ed9cf0be16e1
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-04-30 12:43:29 +0000

    bungeni_presetup namespace installs python with the correct bindings for running bungeni

--- capistrano/trunk/config/bungeni_presetup.rb
+++ capistrano/trunk/config/bungeni_presetup.rb
@@ -5,21 +5,21 @@
 ####  - Installs all the required python libraries 
 ####  	- Python Imaging
 ####	- Python ssl
-#### 	- Python svn
+#### 	- Python svn bindings
+#####	Tested on Ubuntu 8.04 
 ##################################################################
 
 ### Common functions ####
-### add a starts with API to string ###
-class String
-  def startsWith str
-    return self[0...str.length] == str
-  end
+### add a string function to check if a string starts with a particular pattern
+def str_start_with (orig_string, little_string)
+   return !orig_string.match(/\A#{Regexp.escape(little_string)}/).nil?
 end
+
 
 ### use wget if its a url or use a local 'cp' command if its on a folder 
 ### on the same computer
-def get_python_download_command strUrl
-	if (strUrl.startsWith("http") or strUrl.startsWith("ftp"))
+def get_download_command strUrl
+	if (str_start_with(strUrl, "http") or str_start_with(strUrl, "ftp"))
 		return "wget " + strUrl
 	else
 		return "cp " + strUrl + " ."
@@ -35,31 +35,71 @@ end
 set :user_build_root, "/home/bungeni/cap_builds"
 set :user_install_root, "/home/bungeni/cap_installs"
 
+#### download URLs for components #####
+### can be set to a http / ftp url or an absolute path to the file on the computer
+set :python_download_url, "/home/bungeni/Python-2.5.4.tgz" # http://www.python.org/ftp/python/2.5.4/Python-2.5.4.tgz
+set :python_imaging_download_url, "http://effbot.org/media/downloads/Imaging-1.1.7b1.tar.gz"
+set :svn_download_url,  "/home/bungeni/Desktop/subversion-1.5.4.tar.gz" #"http://subversion.tigris.org/downloads/subversion-1.5.4.tar.gz"
+
+
+##### Component Specific Parameters ##### 
+
 #### Python build parameters #####
 
 set :user_python_build_path, "#{user_build_root}/python"
 set :user_python25_runtime, "#{user_install_root}/python25"
-set :python_download_url, "/home/bungeni/Python-2.5.4.tgz"
 set :python_download_file, File.basename(python_download_url)
 set :python_src_dir, File.basename(python_download_file, ".tgz")
-set :python_download_command, get_python_download_command(python_download_url)
+set :python_download_command, get_download_command(python_download_url)
 
-#### python imaging parameters ####
+#### Python imaging parameters ####
 
-set :python_imaging_download_url, "http://effbot.org/media/downloads/Imaging-1.1.7b1.tar.gz"
+set :python_imaging_build_path, "#{user_build_root}/imaging"
+set :python_imaging_download_command, get_download_command(python_imaging_download_url)
 set :python_imaging_download_file, File.basename(python_imaging_download_url)
 set :python_imaging_src_dir, File.basename(python_imaging_download_file, ".tar.gz")
 
+
 #### libneon parameters ####
-set :neon_src_dir, "#{user_build_root}/neon"
-set :neon_install_dir, "#{user_install_root}/neon"
+set :neon_build_path, "#{user_build_root}/neon"
+set :neon_src_dir, "#{neon_build_path}/src"
+set :neon_runtime, "#{user_install_root}/neon"
 set :neon_svn_source, "http://svn.webdav.org/repos/projects/neon/tags/0.28.3"
+
+#### svn build parameters ####
+set :svn_build_path, "#{user_build_root}/svn"
+set :svn_runtime, "#{user_install_root}/svn"
+set :svn_download_command, get_download_command(svn_download_url)
+set :svn_download_file, File.basename(svn_download_url)
+set :svn_src_dir, File.basename(svn_download_file, ".tar.gz")
+set :svn_neon_config, "#{neon_runtime}/bin/neon-config"
 
 
 namespace :bungeni_presetup do
 	
+    desc "Installs the OS prequisites for Bungeni"
     task :essentials, :roles=> [:app] do
-	sudo "apt-get install build-essential libjpeg62-dev libfreetype6-dev libpng12-dev openssl libssl-dev bison flex libreadline5-dev zlib1g-dev libtool automake autoconf libaprutil1-dev swig -y"
+	required_libs = [
+			"build-essential", # for building from source
+			"linux-headers-`uname -r`", # for building from source
+			"libjpeg62-dev", # for python
+			"libfreetype6-dev", # for python
+			"libpng12-dev", # for python
+			"openssl", # for python
+			"libssl-dev", # for python
+			"bison",  # for postgresql
+			"flex" , # for postgresql
+			"libreadline5-dev" , # for postgresql
+			"zlib1g-dev" , # for postgresql
+			"libtool" , # for svn
+			"automake" ,  # for svn
+			"autoconf" , # for svn
+			"libaprutil1-dev", # for svn
+			"swig", # for svn
+			"xmlto" # for libneon 
+			]
+	install_cmd = "apt-get install " + required_libs * " " 	+ " -y"
+	sudo install_cmd
     end
 
     task :build_python, :roles=> [:app] do
@@ -77,12 +117,43 @@ namespace :bungeni_presetup do
 
     task :build_imaging, :roles=> [:app] do
 	[
-	"cd #{user_python_build_path} && rm -rf #{python_imaging_download_file}",
-	"cd #{user_python_build_path} && wget #{python_imaging_download_url}",
-	"rm -rf #{user_python_build_path}/#{python_imaging_src_dir}",
- 	"cd #{user_python_build_path} && tar xvzf #{python_imaging_download_file}",
-	"cd #{user_python_build_path}/#{python_imaging_src_dir} && #{user_python25_runtime}/bin/python setup.py build_ext -i",
-	"cd #{user_python_build_path}/#{python_imaging_src_dir} && #{user_python25_runtime}/bin/python setup.py install"
+	"mkdir -p #{python_imaging_build_path}",
+	"cd #{python_imaging_build_path} && rm -rf #{python_imaging_download_file}",
+	"cd #{python_imaging_build_path} && wget #{python_imaging_download_url}",
+	"rm -rf #{python_imaging_build_path}/#{python_imaging_src_dir}",
+ 	"cd #{python_imaging_build_path} && tar xvzf #{python_imaging_download_file}",
+	"cd #{python_imaging_build_path}/#{python_imaging_src_dir} && #{user_python25_runtime}/bin/python setup.py build_ext -i",
+	"cd #{python_imaging_build_path}/#{python_imaging_src_dir} && #{user_python25_runtime}/bin/python setup.py install"
+	].each {|cmd| run cmd}
+    end
+
+    task :build_libneon, :roles=> [:app] do 
+	[
+	"mkdir -p #{neon_build_path}",
+	"mkdir -p #{neon_src_dir}",
+	"mkdir -p #{neon_runtime}",
+	"svn export #{neon_svn_source} #{neon_src_dir} --force",
+	"cd #{neon_src_dir} && ./autogen.sh",
+	"cd #{neon_src_dir} && ./configure --prefix=#{neon_runtime} --with-ssl=openssl", 
+	"cd #{neon_src_dir} && make ",
+	### the output exit 0 in the below line is required to prevent a 
+	### man installation error in neon which causes the capistrano task 
+	### to wrongly fail
+	"cd #{neon_src_dir} && make install ; exit 0"
+	].each {|cmd| run cmd}
+    end
+
+    task :build_svn, :roles=> [:app] do 
+	[
+	"mkdir -p #{svn_build_path}",
+	"rm -rf #{svn_build_path}/*.*",
+	"mkdir -p #{svn_runtime}",
+	"cd #{svn_build_path} && #{svn_download_command}",
+ 	"cd #{svn_build_path} && tar xvzf #{svn_download_file}",
+	"cd #{svn_build_path}/#{svn_src_dir} && neon_config=#{svn_neon_config} ./configure --prefix=#{svn_runtime} PYTHON=#{user_python25_runtime}/bin/python",
+	"cd #{svn_build_path}/#{svn_src_dir} && make && make install",
+	"cd #{svn_build_path}/#{svn_src_dir} && make swig-py && make check-swig-py && make install-swig-py",
+	"echo #{svn_runtime}/lib/svn-python > #{user_python25_runtime}/lib/python2.5/site-packages/subversion.pth"
 	].each {|cmd| run cmd}
     end
 

8a444f35dfc1489bd83fff64bba2becdb4cac8a3
mborch <mborch@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-05-07 13:01:17 +0000

    Adding proxies to wrap dublin core metadata and navigation structure around an object; this is used in the sections logic to apply descriptive properties and navigation to an inherited container.

--- bungeni.core/trunk/bungeni/core/proxy.py
+++ bungeni.core/trunk/bungeni/core/proxy.py
@@ -1,9 +1,12 @@
 import zope.interface
 
 from zope.proxy import ProxyBase, getProxiedObject, non_overridable
+from zope.proxy import removeAllProxies
 from zope.proxy.decorator import DecoratorSpecificationDescriptor
 from zope.security.checker import getCheckerForInstancesOf
 from zope.location.interfaces import ILocation
+from zope.container.interfaces import IReadContainer
+from zope.dublincore.interfaces import IDCDescriptiveProperties
 
 class ClassAndInstanceDescr(object):
 
@@ -15,6 +18,104 @@ class ClassAndInstanceDescr(object):
             return self.funcs[1](cls)
         return self.funcs[0](inst)
 
+class DublinCoreDescriptivePropertiesProxy(ProxyBase):
+    """This is a non-picklable proxy that can be put around objects to
+    add descriptive properties.
+    """
+
+    zope.interface.implements(IDCDescriptiveProperties)
+    
+    __slots__ = 'title', 'description', '__Security_checker__'
+    __safe_for_unpickling__ = True
+
+    def __new__(self, ob, title=None, description=None):
+        checker = getCheckerForInstancesOf(type(removeAllProxies(ob)))
+        ob = ProxyBase.__new__(self, ob)
+        ob.__Security_checker__ = checker
+        return ob
+    
+    def __init__(self, ob, title=None, description=None):
+        ProxyBase.__init__(self, ob)
+        self.title = title
+        self.description = description
+        
+    @non_overridable
+    def __reduce__(self, proto=None):
+        raise TypeError("Not picklable")
+
+    __doc__ = ClassAndInstanceDescr(
+        lambda inst: getProxiedObject(inst).__doc__,
+        lambda cls, __doc__ = __doc__: __doc__,
+        )
+
+    __reduce_ex__ = __reduce__
+
+    __providedBy__ = DecoratorSpecificationDescriptor()
+
+class NavigationProxy(ProxyBase):
+    """Navigation proxy.
+
+    Forwards the ``__getitem__`` attribute of the proxied object to
+    that of another.
+    """
+
+    zope.interface.implements(IReadContainer)
+    
+    __slots__ = '__target__', '__Security_checker__'
+    __safe_for_unpickling__ = True
+
+    def __new__(self, ob, target):
+        checker = getCheckerForInstancesOf(type(removeAllProxies(ob)))
+        ob = ProxyBase.__new__(self, ob)
+        ob.__Security_checker__ = checker
+        return ob
+
+    def __init__(self, ob, target):
+        ProxyBase.__init__(self, ob)
+        self.__target__ = target
+
+    def __getitem__(self, key):
+        return self.__target__[key]
+
+    def __setitem__(self, key, value):
+        self.__target__[key] = value
+
+    def __iter__(self):
+        return iter(self.__target__)
+
+    @property
+    def __name__(self):
+        return self.__target__.__name__
+
+    @property
+    def __parent__(self):
+        return self.__target__.__parent__
+
+    @property
+    def items(self):
+        return self.__target__.items
+
+    @property
+    def keys(self):
+        return self.__target__.keys
+
+    @property
+    def values(self):
+        return self.__target__.values
+
+    @non_overridable
+    def __reduce__(self, proto=None):
+        raise TypeError("Not picklable")
+
+    __doc__ = ClassAndInstanceDescr(
+        lambda inst: getProxiedObject(inst).__doc__,
+        lambda cls, __doc__ = __doc__: __doc__,
+        )
+
+    __reduce_ex__ = __reduce__
+
+    __providedBy__ = DecoratorSpecificationDescriptor()    
+    
 class LocationProxy(ProxyBase):
     """Location-object proxy
 
@@ -28,7 +129,7 @@ class LocationProxy(ProxyBase):
     __safe_for_unpickling__ = True
 
     def __new__(self, ob, container=None, name=None):
-        checker = getCheckerForInstancesOf(type(ob))
+        checker = getCheckerForInstancesOf(type(removeAllProxies(ob)))
         ob = ProxyBase.__new__(self, ob)
         ob.__Security_checker__ = checker
         return ob

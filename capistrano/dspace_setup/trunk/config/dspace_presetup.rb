ca2444b5a13b31d1d39d3647bbc3dafe561288dc
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-05-14 09:22:12 +0000

    Adding tasks for installing JVM, Tomcat, maven and ant

--- capistrano/dspace_setup/trunk/config/dspace_presetup.rb
+++ capistrano/dspace_setup/trunk/config/dspace_presetup.rb
@@ -23,21 +23,43 @@ set :user_build_root, "#{user_root}/cap_builds"
 set :user_install_root, "#{user_root}/cap_installs"
 
 #### download URLs for components #####
-### can be set to a http / ftp url or an absolute path to the file on the computer
+### Can be set to a http / ftp url or an absolute path to the file on the computer
+### Edit these as desired ####
 set :java6_download_url, "/home/undesa/Software/Software/jdk-6u14-ea-bin-b06-linux-i586-06_may_2009.bin"
-set :java6_install_binary, "jdk6.bin"
-set :java6_default_extract_dir, "jdk1.6.0_14"
-set :java6_install_dir_name, "java6"
-set :java6_download_command, get_download_command(java6_download_url, java6_install_binary)
-
-
+set :tomcat_download_url, "http://mirror.cinquix.com/pub/apache/tomcat/tomcat-5/v5.5.27/bin/apache-tomcat-5.5.27.tar.gz"
+set :maven_download_url, "http://www.apache.org/dist/maven/binaries/apache-maven-2.1.0-bin.tar.gz"
+set :ant_download_url, "http://archive.apache.org/dist/ant/binaries/apache-ant-1.7.1-bin.tar.gz"
 ##### Component Specific Parameters ##### 
 
-#### Java 6  #####
+### do not edit any of these parameters (unless you know what you are doing) #### 
 
+#### Java 6  #####
+set :java6_install_binary, "jdk6.bin"
+set :java6_install_dir_name, File.basename(java6_install_binary, ".bin")
+set :java6_download_command, get_download_command(java6_download_url, java6_install_binary)
 set :java6_home, "#{user_install_root}/#{java6_install_dir_name}"
 set :java6_download_dir, "#{user_build_root}/#{java6_install_dir_name}"
 
+#### Tomcat 5.5  #####
+set :tomcat_install_archive, "tomcat.tar.gz"
+set :tomcat_install_dirname, File.basename(tomcat_install_archive, ".tar.gz")
+set :tomcat_download_command, get_download_command(tomcat_download_url, tomcat_install_archive)
+set :tomcat_home, "#{user_install_root}/#{tomcat_install_dirname}"
+set :tomcat_download_dir, "#{user_build_root}/#{tomcat_install_dirname}"
+
+##### Maven #####
+set :maven_install_archive, "maven.tar.gz"
+set :maven_install_dirname, File.basename(maven_install_archive, ".tar.gz")
+set :maven_download_command, get_download_command(maven_download_url, maven_install_archive)
+set :maven_home, "#{user_install_root}/#{maven_install_dirname}"
+set :maven_download_dir, "#{user_build_root}/#{maven_install_dirname}"
+
+##### Ant #####
+set :ant_install_archive, "ant.tar.gz"
+set :ant_install_dirname, File.basename(ant_install_archive, ".tar.gz")
+set :ant_download_command, get_download_command(ant_download_url, ant_install_archive)
+set :ant_home, "#{user_install_root}/#{ant_install_dirname}"
+set :ant_download_dir, "#{user_build_root}/#{ant_install_dirname}"
 
 
 namespace :dspace_presetup do
@@ -59,7 +81,10 @@ namespace :dspace_presetup do
 	[
 	"mkdir -p #{user_build_root}",
 	"mkdir -p #{user_install_root}",
-	"mkdir -p #{java6_download_dir}"
+	"mkdir -p #{java6_download_dir}",
+	"mkdir -p #{tomcat_download_dir}",
+	"mkdir -p #{maven_download_dir}",
+	"mkdir -p #{ant_download_dir}"
 	].each {|cmd| run cmd}
     end
 
@@ -70,11 +95,39 @@ namespace :dspace_presetup do
 	"cd #{java6_download_dir} && #{java6_download_command} && chmod ug+x ./#{java6_install_binary}",
 	"cd #{java6_download_dir} && echo y > answer.txt",
 	"cd #{java6_download_dir}  && ./#{java6_install_binary} <answer.txt &>/dev/null",
-	"cd #{java6_download_dir}  &&  mv ./#{java6_default_extract_dir} #{java6_home}"
+	"cd #{java6_download_dir}  &&  mv ./jdk1.6* #{java6_home}"
 	].each {|cmd| run cmd}
     end
 
+    desc "Install Tomcat"
+    task :setup_tomcat, :roles=> [:app] do
+	[
+	"rm -rf #{tomcat_home}",
+	"cd #{tomcat_download_dir} && #{tomcat_download_command}",
+	"cd #{tomcat_download_dir} && mkdir ./#{tomcat_install_dirname} && tar xvzf #{tomcat_install_archive} -C ./#{tomcat_install_dirname} --strip-components=1",
+	"cd #{tomcat_download_dir} &&  mv ./#{tomcat_install_dirname} #{tomcat_home}"
+	].each {|cmd| run cmd}
+    end
 	
+    desc "Install Maven"
+    task :setup_maven, :roles=> [:app] do
+	[
+	"rm -rf #{maven_home}",
+	"cd #{maven_download_dir} && #{maven_download_command}",
+	"cd #{maven_download_dir} && mkdir ./#{maven_install_dirname} && tar xvzf #{maven_install_archive} -C ./#{maven_install_dirname} --strip-components=1",
+	"cd #{maven_download_dir} &&  mv ./#{maven_install_dirname} #{maven_home}"
+	].each {|cmd| run cmd}
+    end
+
+    desc "Install Ant"
+    task :setup_ant, :roles=> [:app] do
+	[
+	"rm -rf #{ant_home}",
+	"cd #{ant_download_dir} && #{ant_download_command}",
+	"cd #{ant_download_dir} && mkdir ./#{ant_install_dirname} && tar xvzf #{ant_install_archive} -C ./#{ant_install_dirname} --strip-components=1",
+	"cd #{ant_download_dir} &&  mv ./#{ant_install_dirname} #{ant_home}"
+	].each {|cmd| run cmd}
+    end
 	
    
 end

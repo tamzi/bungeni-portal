8e5c1930238aa71da976185026210a7ebbab8408
christian.ledermann <christian.ledermann@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-05-27 07:33:12 +0000

    use principal.id instead of userid for groups oops for getGroups too

--- bungeni.portal/trunk/bungeni/portal/auth.py
+++ bungeni.portal/trunk/bungeni/portal/auth.py
@@ -124,5 +124,5 @@ class GlobalAuthWhoPlugin(object):
             identity.update({
                 'title': principal.title,
                 #'groups': tuple(principal.groups) + (principal.id,),
-                'groups' : tuple( self.getGroups(userid)) + (principal.id,),                
+                'groups' : tuple( self.getGroups(principal.id)) + (principal.id,),                
                 })

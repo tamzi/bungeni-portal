f3a12bb782743173bbd6d830f9506ca5909ec3d4
christian.ledermann <christian.ledermann@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-05-28 16:04:10 +0000

    assign roles to groups upon creation, modification

--- bungeni.core/trunk/bungeni/core/events.py
+++ bungeni.core/trunk/bungeni/core/events.py
@@ -2,7 +2,11 @@
 # note that other events are handled in workflows
 # audit and files!
 
+import datetime
+
 from zope.security.proxy import removeSecurityProxy
+from zope.securitypolicy.interfaces import IPrincipalRoleMap
+
 from ore.alchemist import Session
 from bungeni.core import audit
 
@@ -39,3 +43,35 @@ def consignatory_modified(ob, event):
             title)
     if ob.item:                        
         audit.objectContained( ob.item, event)
+        
+
+def group_added_or_modified(context, group, role):
+    """ when a group is added we 
+    give the users of this group the local role
+    'role' when the group is closed we delete
+    the role (unsetRoleForPrincipal as oposed to 
+    removeRoleFromPrincipal)
+    """
+    if (not group.end_date) or (group.end_date >= datetime.date.today()):
+        IPrincipalRoleMap(context).assignRoleToPrincipal(
+            role, group.group_principal_id)
+    else:            
+        IPrincipalRoleMap(context).unsetRoleForPrincipal(
+            role, group.group_principal_id)
+        
+def parliament_added_modified(ob,event):
+    """when a parliament is added all members get the local role
+    bungeni.MP
+    """
+    trusted = removeSecurityProxy(ob)
+    group_added_or_modified(ob, trusted, 'bungeni.MP')
+    
+def ministry_added_modified(ob,event):  
+    trusted = removeSecurityProxy(ob)
+    group_added_or_modified(ob, trusted, 'bungeni.Minister')
+
+def committee_added_modified(ob,event):
+    trusted = removeSecurityProxy(ob)
+    group_added_or_modified(ob, trusted, 'bungeni.CommitteeMember')
+
+            

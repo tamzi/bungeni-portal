780fe1fe613be1d05e8fa19f13adf1ad40cbeaae
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-06-12 10:47:08 +0000

    New scripts for cap. local caching support

---
+++ capistrano/bungeni_setup/trunk/config/bungeniconfiglocalcache.rb
@@ -0,0 +1,9 @@
+#### These parameters are used only during the cached buildout #####
+
+set :local_buildout_index, "http://192.168.0.14:6543/bungeni"
+set :pg_download_url, "http://192.168.0.14:6543/cache/postgresql/postgresql-8.3.7.tar.gz"
+set :xapian_download_url, "http://192.168.0.14:6543/cache/xapian-core/xapian-core-11831.tgz"
+set :xapian_binding_download_url, "http://192.168.0.14:6543/cache/xapian-bindings/xapian-bindings-11831.tgz"
+set :exist_download_url, "http://192.168.0.14:6543/cache/eXist/eXist-setup-1.2.0-rev7233.jar"
+
+

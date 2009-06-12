db49dfd8c4e0179514212cfe1965a214cf85c587
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-06-12 11:41:27 +0000

    Added service controller for postgres

--- capistrano/bungeni_setup/trunk/config/bungeni_services.rb
+++ capistrano/bungeni_setup/trunk/config/bungeni_services.rb
@@ -2,6 +2,15 @@ namespace :bungeni_services do
 
 desc "Start Bungeni"
 
+    task :start_pg, :roles => [:app] do
+	run "echo 'Starting postgres'"
+    end
+
+    task :stop_pg, :roles => [:app] do
+	run "echo 'Stopping postgres'"
+    end
+
+
     task :start_bungeni, :roles => [:app] do
 	run "echo 'Starting Bungeni...'"
     end
@@ -18,6 +27,9 @@ desc "Start Bungeni"
     after "bungeni_services:stop_bungeni", "bungeni_tasks:stop_supervisor"
     after "bungeni_services:restart_bungeni", "bungeni_services:stop_bungeni", "bungeni_services:start_bungeni"
 
+    after "bungeni_services:start_pg", "bungeni_tasks:postgres_start"
+    after "bungeni_services:stop_pg", "bungeni_tasks:postgres_stop"
+    after "bungeni_services:restart_pg", "bungeni_services:stop_pg", "bungeni_services:start_pg"
 
 
 end

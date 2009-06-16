ffb1d88075a5074a1ad5301da8ad260d0cd19bf9
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-06-16 15:44:47 +0000

    Adding REST interface to translator

---
+++ DocTransforms/trunk/ODTtoAKN/src/org/bungeni/restlet/resources/OdtResource.java
@@ -0,0 +1,87 @@
+package org.bungeni.restlet.resources;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.bungeni.restlet.server.TransformerServer;
+import org.restlet.Context;
+import org.restlet.data.Form;
+import org.restlet.data.MediaType;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.resource.FileRepresentation;
+import org.restlet.resource.Representation;
+import org.restlet.resource.Variant;
+
+/**
+ *Accepts ODT submissions - processes the ODT file and returns a response
+ * @author Ashok Hariharan
+ */
+public class OdtResource  extends org.restlet.resource.Resource  {
+
+    private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(OdtResource.class.getName());
+
+    /**
+     * Add return Application xml as a variant
+     * @param context
+     * @param request
+     * @param response
+     */
+    public OdtResource(Context context, Request request,
+			Response response) {
+		super(context, request, response);
+        getVariants().add(new Variant(MediaType.APPLICATION_XML));
+        
+	}
+
+    /**
+     * Allow post for this resource
+     * @return
+     */
+	@Override
+	public boolean allowPost() {
+		return true;
+	}
+
+	/**
+	 * Recieve an ODT file to transform
+	 * -- transform the ODT to xml
+	 * -- return the result xml back in an Xml envelop
+	 * 
+	 */
+	@Override
+	public void acceptRepresentation(Representation entity) {
+		log.debug("acceptRepresentation :  media type = " + entity.getMediaType());
+        {
+            FileOutputStream os = null;
+            try {
+                //get the submission headers, for the name of the input file
+                Form requestHeaders = (Form) getRequest().getAttributes().get("org.restlet.http.headers");
+                log.debug("output form headers = " + requestHeaders);
+                String fileName = requestHeaders.getFirstValue("X-Odt-File");
+                //write the file to a folder path on the server
+                String folderPath = TransformerServer.getTempFileFolder();
+                File file = new File(folderPath + File.separator + fileName);
+                //overwrite the existing file
+                os = new FileOutputStream(file, false);
+                entity.write(os);
+                //for now return a dummy response
+                File outputXml = new File ("/home/undesa/Desktop/out.xml");
+                Representation returnResponse = new FileRepresentation(outputXml,
+				MediaType.APPLICATION_XML, 0);
+                getResponse().setEntity(returnResponse);
+
+            } catch (FileNotFoundException ex) {
+                log.error("acceptRepresentation : ", ex);
+            } catch (IOException e) {
+                log.error("acceptRepresentation : ", e);
+            } finally {
+                try {
+                    os.close();
+                } catch (IOException ex) {
+                    log.error("acceptRepresentation : ", ex);
+                }
+            }
+        }
+	}
+}
---
+++ DocTransforms/trunk/ODTtoAKN/src/org/bungeni/restlet/server/TransformerServer.java
@@ -0,0 +1,77 @@
+package org.bungeni.restlet.server;
+
+import org.bungeni.restlet.resources.OdtResource;
+import org.bungeni.restlet.restlets.TransformParamsRestlet;
+import org.restlet.Application;
+import org.restlet.Component;
+import org.restlet.Restlet;
+import org.restlet.Router;
+import org.restlet.data.Protocol;
+
+
+public class TransformerServer extends Application {
+   private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(TransformerServer.class.getName());
+   private static Component serverComponent = null;
+   private static int SERVER_PORT = 8182;
+   private static String SERVER_TMP_FOLDER = "/home/undesa/tmp";
+   private static Restlet setParamsRestlet;
+
+   static {
+       try {
+            setParamsRestlet = new TransformParamsRestlet();
+       } catch (Exception ex) {
+           log.error("TransformServer.setParamsRestlet : ", ex);
+       }
+
+   }
+
+   public static void setServerPort(int nPort) {
+       SERVER_PORT = nPort;
+   }
+
+   public static void setServerTempFolder(String sFolder) {
+       SERVER_TMP_FOLDER = sFolder;
+   }
+
+   public static TransformerServer startServer() {
+            TransformerServer ts = null;
+            try {
+            	//to do .. bootstrap stuff to read server settings from properties file
+                if (serverComponent == null) {
+                    serverComponent = new Component();
+                    serverComponent.getServers().add(Protocol.HTTP, SERVER_PORT);
+                    ts = new TransformerServer();
+                    serverComponent.getDefaultHost().attach("", ts);
+                    serverComponent.start();
+                } else {
+                    if (serverComponent.isStopped()) {
+                        serverComponent.start();
+                    } else {
+                        System.out.println("TransformServer is already running on port " + SERVER_PORT);
+                        log.info("TransformServer is already running on port " + SERVER_PORT);
+                    }
+                }
+
+            } catch (Exception ex) {
+                log.error("startServer : " , ex);
+            } finally {
+                return ts;
+            }
+     }
+
+    public static String getTempFileFolder() {
+        return SERVER_TMP_FOLDER;
+    }
+
+     @Override
+      public Restlet createRoot() {
+        Router router = new Router(getContext());
+        router.attach("/convert_to_anxml", OdtResource.class);
+        router.attach("/set_convert_params",setParamsRestlet);
+        return router;
+      }
+
+     public static void main(String[] args) {
+         TransformerServer trans =TransformerServer.startServer();
+     }
+}

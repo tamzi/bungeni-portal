898b5067f0bd26a86703b45e16944558e442c45b
christian.ledermann <christian.ledermann@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-06-18 10:48:24 +0000

    add properties from bungeni to plone

--- bungeni.plonepas/trunk/bungeni/plonepas/install.py
+++ bungeni.plonepas/trunk/bungeni/plonepas/install.py
@@ -1,10 +1,22 @@
 
 from StringIO import StringIO
 
-import group, user
+import group, user, property
 
 group_id = "bungeni_groups"
 user_id  = "bungeni_users"
+property_id = "bungeni.properties"
+
+def install_property_plugin(self):
+    pas = self.acl_users
+    if not property_id in  pas.objectIds():
+        manager = property.PropertyProvider(property_id, "Bungeni Property Provider")
+        pas._setObject( property_id, manager )
+        property_provider = pas[ property_id ]
+        property_provider.manage_activateInterfaces(
+            ['IPropertiesPlugin', 'IUpdatePlugin']
+            )
+    
 
 def install_pas_plugins( self ):
     pas = self.acl_users
@@ -47,4 +59,5 @@ def install( self ):
     out = StringIO()
     print >> out, "Installing and Activating PAS Property Plugin"
     install_pas_plugins( self )
+    install_property_plugin( self)
     return out.getvalue()

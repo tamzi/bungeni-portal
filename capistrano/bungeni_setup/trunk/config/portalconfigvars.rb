5b4552d03898a7d5b5bb29bb9d335218642a9eed
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-08-12 14:10:03 +0000

    Updating capistrano recipes

---
+++ capistrano/bungeni_setup/trunk/config/portalconfigvars.rb
@@ -0,0 +1,5 @@
+#### CONFIG VARIABLE SETTING ####
+#was set :deploy_to, "/home/bungeni/bungeni_deploy/#{application}"
+set :portal_buildout_dir, "#{buildout_dir}/portal"
+set :portal_local_buildout_config_file , "buildout_portal_local.cfg"
+set :portal_repository,  "https://bungeni-portal.googlecode.com/svn/portal.buildout/trunk"

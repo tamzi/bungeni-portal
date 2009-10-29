e3885f5645255a845514304f4155324ac851b1ff
christian.ledermann <christian.ledermann@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-10-29 11:39:38 +0000

    synchronize real order and planned order in transition from agenda to minutes

--- bungeni.core/trunk/bungeni/core/workflows/groupsitting.py
+++ bungeni.core/trunk/bungeni/core/workflows/groupsitting.py
@@ -1,2 +1,8 @@
+from bungeni.core.workflows import dbutils
+from zope.security.proxy import removeSecurityProxy
+
 class actions:
-    pass
+
+    @staticmethod
+    def draftminutes(info, context):
+        dbutils.set_real_order(removeSecurityProxy(context))

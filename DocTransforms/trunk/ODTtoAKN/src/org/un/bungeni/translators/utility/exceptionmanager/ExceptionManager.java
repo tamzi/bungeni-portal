2d12ddea5d32131edf74f2b3b7815e71414d9621
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-10-30 14:49:05 +0000

    Handle outlier case for exceptions

--- DocTransforms/trunk/ODTtoAKN/src/org/un/bungeni/translators/utility/exceptionmanager/ExceptionManager.java
+++ DocTransforms/trunk/ODTtoAKN/src/org/un/bungeni/translators/utility/exceptionmanager/ExceptionManager.java
@@ -262,31 +262,151 @@ public class ExceptionManager implements ErrorHandler
         }
 		else if (exceptionMessage.matches("(.*)The content of element '(.*)' is not complete(.*)"))
 		{
-			
-			System.err.println("At line " + ex.getLineNumber()  + " of " + ex.getSystemId() + ':');
-	        System.err.println(exceptionMessage);
-	        Node node = null;
+			//System.out.println("ALLLLOOOOOOORRRRRAAAAAAA");
+			//compile the regex
+			Pattern p = Pattern.compile("(.*)The content of element '(.*)' is not complete(.*)");
+        	//set the input
+			Matcher m = p.matcher(exceptionMessage);
+        	//the attribute name
+			String attribute = ""; 
+			//the elemet name
+			String element = ""; 
+        	while (m.find())
+        	{
+        	    //get the attribute name
+        		attribute = m.group(2).toString();
+        		validationError.setMissingAttribute(attribute);
+        		//get element name
+        	    element = m.group(3).toString();
+        	    validationError.setMissingAtributeFor(element);
+        	}
+        	
 
-    		try {
+        	//create the text of the exception
+    		String message = 	resourceBundle.getString("PROBLEM_DESCRIPTION_LEFT") +
+	     				     	resourceBundle.getString("MISSING_ATTRIBUTE_LEFT_TEXT") +
+    					     	attribute +
+    					     	resourceBundle.getString("MISSING_ATTRIBUTE_CENTER_TEXT") +
+    					     	element +
+    					     	resourceBundle.getString("MISSING_ATTRIBUTE_RIGHT_TEXT");
+            validationError.getFullErrorMessage().add(0, message);
+    		try
+            {
+            	
+            	/*Create the message for the section*/
+            	String sectionId = "";
+            	
+            	//this will store the section name
+            	String sectionName = ""; 
+            	
+            	//this is the message that shows the id the name and the starting words of the found problem
+            	String messageId = resourceBundle.getString("VALIDATION_FAILED_TEXT") + "\n";
+            	
+            	//get the line and column number
+            	String errorLocation = 	ex.getLineNumber() +
+            	                      						"-" +
+            							ex.getColumnNumber();
+            	validationError.setColNo(ex.getColumnNumber());
+            	validationError.setLineNo(ex.getLineNumber());
+            	
+            	
+            	//get the elements location string
+            	String elementsLocationString = LocationHandler.getInstance().getElementsLocation();
+            	
+            	//split the elements location string in lines
+            	String[] elementsLocationsLines = elementsLocationString.split("\n");
+            	
+            	//iterate the lines and find the one regarding the current element
+            	for (int i = 0; i < elementsLocationsLines.length; i++)
+            	{
+            		//get the location of the element
+            		String elementLocation = elementsLocationsLines[i].split(",")[0];
+            		
+            		//if it is not the line regarding the current element continue
+            		if(elementLocation.compareTo(errorLocation) != 0)
+            		{
+            			//continue
+            			continue;
+            		}
+            		//else get the section type and id
+            		else
+            		{
+            			//get the section type
+            			sectionName = elementsLocationsLines[i].split(",")[1].split(":")[0];
+            			validationError.setSectionType(sectionName);
+            			//get the section id
+            			sectionId = elementsLocationsLines[i].split(",")[1].split(":")[1];
+            			validationError.setSectionId(sectionId);
+            		}
+            	}
+                
+            	String startingWords = getStartingWords(sectionId);
+            	
+                //complete the message to show 
+                messageId = 	messageId + 
+                				resourceBundle.getString("SECTION_TYPE_LEFT") +
+                				sectionName +
+                				", " +
+                				resourceBundle.getString("SECTION_ID_LEFT") +
+                				sectionId + 
+                				", " +
+                				resourceBundle.getString("STARTING_WORD_TEXT_LEFT") +
+                    			startingWords;
+                
+                validationError.setStartingWords(startingWords);
+                validationError.getFullErrorMessage().add(0, messageId);
+                       			
+                /*Create the message for the parent section*/
+            	//this will store the section id
+            	String sectionIdParent = "";
+            	
+            	//this will store the section name
+            	String sectionNameParent = ""; 
+            	
+            	//this is the message that shows the id the name and the starting words of the found problem
+            	String messageIdParent = "";
+            	
+            	//get the current visited node
+                Node node =	(Node)this.parser.getProperty("http://apache.org/xml/properties/dom/current-element-node");
+                
+                //get the name of the node
+                sectionNameParent = node.getLocalName();
+                
+                if (node.getAttributes().getNamedItem("id") != null)
+                {
+                	 //get the section id 
+                	 sectionIdParent = node.getAttributes().getNamedItem("id").getNodeValue();
+                }
+                if (node.getAttributes().getNamedItem("name") != null)
+                {
+                	//get the section name
+                	sectionNameParent = node.getAttributes().getNamedItem("name").getNodeValue();
+                }
+                validationError.setParentSectionType(sectionNameParent);
+                validationError.setParentSection(sectionIdParent);
+                //complete the message to show 
+                messageIdParent = 	messageIdParent + 
+                					resourceBundle.getString("SECTION_PARENT_TYPE_LEFT") +
+                					sectionNameParent +
+                					", " +
+                					resourceBundle.getString("SECTION_ID_LEFT") +
+                					sectionIdParent; 
+                			//"\n" +
+                			//resourceBundle.getString("STARTING_WORD_TEXT_LEFT") +
+                			//getStartingWords(node.getAttributes().getNamedItem("id").getNodeValue());
     			
-    			System.err.println(parser.getProperty("http://apache.org/xml/properties/dom/current-element-node"));
-    	        // System.err.println(this.validator.getProperty("http://apache.org/xml/properties/dom/current-element-node"));
-    			// System.err.println(this.validator.getFeature("http://apache.org/xml/features/dom/defer-node-expansion"));
-     		    node =	(Node)this.parser.getProperty("http://apache.org/xml/properties/dom/current-element-node");
-    		}
-        	catch (SAXException e)
+                validationError.getFullErrorMessage().add(0, messageIdParent);
+                //print the messages 
+                System.err.println(messageId);
+                System.err.println(messageIdParent);
+                System.err.println(message);
+                this.validationErrors.add(validationError);
+            }
+            catch (SAXException e)
         	{
+            	//print the stack trace 
         		e.printStackTrace();
-        	}
-        	if (node != null)
-    		{
-    			System.err.println("node = " + node.getNodeName() + " id=" );
-    		}
-        	else
-        	{
-        		System.err.println("");
-        			
-        	}
+        	} 
        }
     }
 

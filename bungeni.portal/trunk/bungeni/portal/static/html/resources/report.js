9a87efbf83ecc3baf6e8a4d92fc63894f6b481ce
mianonjoka <mianonjoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-12-07 08:29:21 +0000

    Javascript for reports page

---
+++ bungeni.portal/trunk/bungeni/portal/static/html/resources/report.js
@@ -0,0 +1,92 @@
+(function($) { 
+    $(document).ready(function(){  
+        $("#groupsitting-form #actionsView").before('<div id="options" style="display:block"></div>');
+        $("#form\\.bill_options\\.0").parents(".widget-multicheckboxwidget").appendTo("#options").css('display', 'inline-block');
+        $("#form\\.bill_options\\.0").parents(".widget-multicheckboxwidget").hide();
+        $("#form\\.agenda_options\\.0").parents(".widget-multicheckboxwidget").appendTo("#options").css('display', 'inline-block');
+        $("#form\\.agenda_options\\.0").parents(".widget-multicheckboxwidget").hide();
+        $("#form\\.motion_options\\.0").parents(".widget-multicheckboxwidget").appendTo("#options").css('display', 'inline-block');
+        $("#form\\.motion_options\\.0").parents(".widget-multicheckboxwidget").hide();
+        $("#form\\.question_options\\.0").parents(".widget-multicheckboxwidget").appendTo("#options").css('display', 'inline-block');
+        $("#form\\.question_options\\.0").parents(".widget-multicheckboxwidget").hide();
+        $("#options span").css('padding','0 0 0 0');
+        $("#form\\.bill_options\\.0").attr('checked', true);
+        $("#form\\.bill_options\\.1").attr('checked', true);
+        $("#form\\.agenda_options\\.0").attr('checked', true);
+        $("#form\\.agenda_options\\.3").attr('checked', true);
+        $("#form\\.motion_options\\.0").attr('checked', true);
+        $("#form\\.motion_options\\.3").attr('checked', true);
+        $("#form\\.question_options\\.0").attr('checked', true);
+        $("#form\\.question_options\\.3").attr('checked', true);
+        $("input#form\\.items\\.0").change(function () {
+            if ($("#form\\.items\\.0").is(":checked"))
+            {
+                $("#form\\.bill_options\\.0").parents(".widget-multicheckboxwidget").show();
+            }
+            else
+            {
+                $("#form\\.bill_options\\.0").parents(".widget-multicheckboxwidget").hide();
+            }
+        });
+        $("input#form\\.items\\.1").change(function () {
+            if ($("#form\\.items\\.1").is(":checked"))
+            {
+                $("#form\\.agenda_options\\.0").parents(".widget-multicheckboxwidget").show();
+            }
+            else
+            {
+                $("#form\\.agenda_options\\.0").parents(".widget-multicheckboxwidget").hide();
+            }
+        });
+        $("input#form\\.items\\.2").change(function () {
+            if ($("#form\\.items\\.2").is(":checked"))
+            {
+                $("#form\\.motion_options\\.0").parents(".widget-multicheckboxwidget").show();
+            }
+            else
+            {
+                $("#form\\.motion_options\\.0").parents(".widget-multicheckboxwidget").hide();
+            }
+        });
+        $("input#form\\.items\\.3").change(function () {
+            if ($("#form\\.items\\.3").is(":checked"))
+            {
+                $("#form\\.question_options\\.0").parents(".widget-multicheckboxwidget").show();
+            }
+            else
+            {
+                $("#form\\.question_options\\.0").parents(".widget-multicheckboxwidget").hide();
+            }
+        });
+        
+        $("#form\\.doc_type").change(function() {
+            var choice = $("option:selected", this).val();
+            if (choice == "Order of the day")
+            {
+                $("#form\\.items\\.0").attr('checked', true);
+                $("#form\\.items\\.1").attr('checked', true);
+                $("#form\\.items\\.2").attr('checked', true);
+                $("#form\\.items\\.3").attr('checked', true);
+            }
+            else if (choice == "Weekly Business")
+            {
+                $("#form\\.items\\.0").attr('checked', true);
+                $("#form\\.items\\.1").attr('checked', true);
+                $("#form\\.items\\.2").attr('checked', true);
+                $("#form\\.items\\.3").attr('checked', false);
+            }
+            else if (choice == "Questions of the week")
+            {
+                $("#form\\.items\\.0").attr('checked', false);
+                $("#form\\.items\\.1").attr('checked', false);
+                $("#form\\.items\\.2").attr('checked', false);
+                $("#form\\.items\\.3").attr('checked', true);
+            }
+            $("#form\\.items\\.0").trigger('change');
+            $("#form\\.items\\.1").trigger('change');
+            $("#form\\.items\\.2").trigger('change');
+            $("#form\\.items\\.3").trigger('change');
+        }).change();
+
+    });
+ })(jQuery)

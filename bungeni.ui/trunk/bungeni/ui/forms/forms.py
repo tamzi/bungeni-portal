dc8406a75c2a6e6a9485b43e5f62229086c24185
mianonjoka <mianonjoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-12-14 15:37:27 +0000

    Reports - Items to include processing

--- bungeni.ui/trunk/bungeni/ui/forms/forms.py
+++ bungeni.ui/trunk/bungeni/ui/forms/forms.py
@@ -403,7 +403,9 @@ class GroupSittingAddForm(AddForm):
     def get_form_fields(self):
         fields = super(GroupSittingAddForm, self).get_form_fields()
         fields = form.Fields(self.IRecurringEvents) + fields
+        
         def MultiCheckBoxWidgetFactory(field, request):
+            import pdb; pdb.set_trace()
             return MultiCheckBoxWidget(
                 field, field.value_type.vocabulary, request)
         fields['weekdays'].custom_widget = MultiCheckBoxWidgetFactory

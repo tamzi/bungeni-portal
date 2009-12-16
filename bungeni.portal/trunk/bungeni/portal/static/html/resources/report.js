efce427693250ad711a80928f7825413979b62ee
mianonjoka <mianonjoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2009-12-16 07:17:33 +0000

    Added tabled documents

--- bungeni.portal/trunk/bungeni/portal/static/html/resources/report.js
+++ bungeni.portal/trunk/bungeni/portal/static/html/resources/report.js
@@ -9,6 +9,8 @@
         $("#form\\.motion_options\\.0").parents(".widget-multicheckboxwidget").hide();
         $("#form\\.question_options\\.0").parents(".widget-multicheckboxwidget").appendTo("#options").css('display', 'inline-block');
         $("#form\\.question_options\\.0").parents(".widget-multicheckboxwidget").hide();
+        $("#form\\.tabled_document_options\\.0").parents(".widget-multicheckboxwidget").appendTo("#options").css('display', 'inline-block');
+        $("#form\\.tabled_document_options\\.0").parents(".widget-multicheckboxwidget").hide();
         $("#options span").css('padding','0 0 0 0');
         $("#form\\.bill_options\\.0").attr('checked', true);
         $("#form\\.bill_options\\.1").attr('checked', true);
@@ -18,6 +20,8 @@
         $("#form\\.motion_options\\.3").attr('checked', true);
         $("#form\\.question_options\\.0").attr('checked', true);
         $("#form\\.question_options\\.3").attr('checked', true);
+        $("#form\\.tabled_document_options\\.0").attr('checked', true);
+        $("#form\\.tabled_document_options\\.3").attr('checked', true);
         $("input#form\\.item_types\\.0").change(function () {
             if ($("#form\\.item_types\\.0").is(":checked"))
             {
@@ -58,7 +62,16 @@
                 $("#form\\.question_options\\.0").parents(".widget-multicheckboxwidget").hide();
             }
         });
-        
+        $("input#form\\.item_types\\.4").change(function () {
+            if ($("#form\\.item_types\\.4").is(":checked"))
+            {
+                $("#form\\.tabled_document_options\\.0").parents(".widget-multicheckboxwidget").show();
+            }
+            else
+            {
+                $("#form\\.tabled_document_options\\.0").parents(".widget-multicheckboxwidget").hide();
+            }
+        });
         $("#form\\.doc_type").change(function() {
             var choice = $("option:selected", this).val();
             if (choice == "Order of the day")
@@ -67,6 +80,7 @@
                 $("#form\\.item_types\\.1").attr('checked', true);
                 $("#form\\.item_types\\.2").attr('checked', true);
                 $("#form\\.item_types\\.3").attr('checked', true);
+                $("#form\\.item_types\\.4").attr('checked', true);
             }
             else if (choice == "Weekly Business")
             {
@@ -74,6 +88,7 @@
                 $("#form\\.item_types\\.1").attr('checked', true);
                 $("#form\\.item_types\\.2").attr('checked', true);
                 $("#form\\.item_types\\.3").attr('checked', false);
+                $("#form\\.item_types\\.4").attr('checked', true);
             }
             else if (choice == "Questions of the week")
             {
@@ -81,11 +96,13 @@
                 $("#form\\.item_types\\.1").attr('checked', false);
                 $("#form\\.item_types\\.2").attr('checked', false);
                 $("#form\\.item_types\\.3").attr('checked', true);
+                $("#form\\.item_types\\.4").attr('checked', false);
             }
             $("#form\\.item_types\\.0").trigger('change');
             $("#form\\.item_types\\.1").trigger('change');
             $("#form\\.item_types\\.2").trigger('change');
             $("#form\\.item_types\\.3").trigger('change');
+            $("#form\\.item_types\\.4").trigger('change');
         }).change();
 
     });

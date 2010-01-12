9a2db6f057cdcd392de5cfa6748f8e62b0747c2b
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-01-12 15:22:43 +0000

    Creating branch for gandi.net setup scripts

---
+++ capistrano/bungeni_setup/branches/gandinet_setup/config/deploy.rb
@@ -0,0 +1,14 @@
+### Author : Ashok Hariharan
+### Description :
+### deploy.erb for setting up bungeni using capistrano
+### checks out svn source , sets up python correctly, sets up the config files (supervisord.conf) using erb templatss
+### 
+
+### commmon functions used by other scripts
+### This file does not load anything - all the loading happens in Capfile
+### this file is merely a placeholder
+
+
+
+  
+

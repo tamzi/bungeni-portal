5ddb260251c5c09dfb1fe33fd585e3110cb03707
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-01-15 12:08:27 +0000

    Updated install configs for gandi

--- capistrano/bungeni_setup/branches/gandinet_setup/config/bungeni_install.rb
+++ capistrano/bungeni_setup/branches/gandinet_setup/config/bungeni_install.rb
@@ -6,6 +6,10 @@ namespace :bungeni_install do
 	run "echo 'setting up bungeni installation'"
     end
 
+    task :setup_gandi, :roles=> [:app] do
+	run "echo 'setting up bungeni installation (gandi) '"
+    end
+
     task :setup_from_cache, :roles=> [:app] do
 	run "echo 'setting up bungeni installation (cached) '"
     end
@@ -18,6 +22,10 @@ namespace :bungeni_install do
       run "echo 'bootstraping and running a full buildout (cached)'"
     end
 
+    task :full_gandi, :roles=> [:app] do
+      run "echo 'bootstraping and running a full buildout (gandi)'"
+    end
+
     task :setup_db, :role=> [:app] do 
       run "echo 'setting up postgres'"
     end
@@ -27,7 +35,11 @@ namespace :bungeni_install do
 
     after "bungeni_install:setup_from_cache", "deploy:setup", "deploy:update", "bungeni_install:full_from_cache"
 
+    after "bungeni_install:setup_gandi", "deploy:setup", "deploy:update", "bungeni_install:full_gandi"
+
     after "bungeni_install:full", "bungeni_tasks:python_setup", "bungeni_tasks:bootstrap_bo", "bungeni_tasks:buildout_full", "bungeni_install:setup_db"
+
+    after "bungeni_install:full_gandi", "bungeni_tasks:python_setup", "bungeni_tasks:bootstrap_bo", "bungeni_tasks:buildout_full_gandi","bungeni_tasks:supervisord_config"
 
     after "bungeni_install:full_from_cache", "bungeni_tasks:python_setup", "bungeni_tasks:bootstrap_bo", "bungeni_tasks:localbuildout_config", "bungeni_tasks:buildout_full_local", "bungeni_install:setup_db"
 

318e697020d4fb34d5c22981be773f1497a16448
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-01-19 13:41:46 +0000

    Added country db creation scripts

--- capistrano/bungeni_setup/branches/gandinet_setup/support_scripts/db_setup/instpg.sh
+++ capistrano/bungeni_setup/branches/gandinet_setup/support_scripts/db_setup/instpg.sh
@@ -3,4 +3,18 @@ tar xvf postgresql-8.3.4.tar.bz2
 cd postgresql-8.3.4
 ./configure --prefix=/home/undesa/disk1/postgres --enable-thread-safety --with-readline
 make && make install
+/home/undesa/disk1/postgres/bin/initdb -D /home/undesa/disk1/postgres-data
+/home/undesa/disk1/postgres/bin/createdb angola
+/home/undesa/disk1/postgres/bin/createdb kenya 
+/home/undesa/disk1/postgres/bin/createdb uganda
+/home/undesa/disk1/postgres/bin/createdb tanzania
+/home/undesa/disk1/postgres/bin/createdb ghana   
+/home/undesa/disk1/postgres/bin/createdb cameroon
+/home/undesa/disk1/postgres/bin/createdb haiti   
+/home/undesa/disk1/postgres/bin/createdb nigeria
+/home/undesa/disk1/postgres/bin/createdb southafrica
+/home/undesa/disk1/postgres/bin/createdb pap        
+/home/undesa/disk1/postgres/bin/createdb mozambique
+/home/undesa/disk1/postgres/bin/createdb mauritius
+/home/undesa/disk1/postgres/bin/createdb rwanda
 

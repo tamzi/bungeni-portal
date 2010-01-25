dde77979032e47ee2f7f5de952f372b4553e5a58
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-01-25 08:00:08 +0000

    Adding script to setup configure.zcml

---
+++ capistrano/bungeni_setup/branches/gandinet_setup/support_scripts/bungeni_config/repldbconfig.sh
@@ -0,0 +1,21 @@
+# $1 = database name
+# $2 = bungeni installation folder path without trailing backslas e.g./home/undesa/cap_installs/bungeni_install/bungeni/current
+file_path="src/bungeni.portal/bungeni/portal"
+file_name="configure.zcml"
+full_path="$2/$file_path/$file_name"
+
+rr="postgres://demodb.bungeni.org/$1"
+echo $rr | sed  's/\:/\\\:/g' | sed 's/\//\\\//g' | sed 's/\./\\\./g' > out.txt
+export FILE_DATA=( $( /bin/cat out.txt ) )
+for I in $(/usr/bin/seq 0 $((${#FILE_DATA[@]} - 1)))
+	do
+		echo $I $FILE_DATA[$i]
+	done
+dname=`date +%Y%M%d-%s`
+bakname="configure-$dname.zcml"
+
+cp "$full_path" "$2/$file_path/$bakname"
+rm out.txt 
+sed -i 's/postgres\:\/\/localhost\/bungeni/'$FILE_DATA'/g' $full_path
+
+

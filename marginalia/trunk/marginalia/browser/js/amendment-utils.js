5cb02891bf9e5ec9d0247b80184f97cacdc21132
mngoka <mngoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-02-03 15:11:04 +0000

    Cleanup file naming structure, add custom js and css.

---
+++ marginalia/trunk/marginalia/browser/js/amendment-utils.js
@@ -0,0 +1,53 @@
+function load() {
+  var search_button = document.getElementById('search');
+  filterAnnotations(search_button);
+}
+
+function show_annotations_on_load()
+{
+  var serviceRoot = '${request/base}/${utool/getPortalPath}/portal_annotations';
+  var username = '${user/getUserName}';
+  var isadmin = '${isadmin}';
+  var superuser = false;
+  if (isadmin == 'True') {
+            superuser = true;
+  }
+  var url = '${annotatedUrl}';
+  var restService = '/amendment';
+  bungeniMarginaliaInit( username, superuser, url, serviceRoot, restService );
+  setTimeout( 'load();', 500);
+}
+
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      if (oldonload) {
+        oldonload();
+      }
+      func();
+    }
+  }
+}
+
+addLoadEvent(show_annotations_on_load);
+
+/*
+ * Called when the margin button is clicked to create an annotation.
+ * There are two choices for editor:
+ * 1. SelectAnnotationNoteEditor - select an edit action
+ * 2. BungeniNoteEditor - create a simple margin note
+ */
+function bungeniClickCreateAnnotation( event, id )
+{
+  var button = document.getElementById('clear');
+  flag = myAnnotations(button);
+  if (flag!=true) {
+    alert( getLocalized( 'add settings' ) );
+    return false
+      }
+  clickCreateAnnotation( event, id, new SelectActionNoteEditor( ) );
+  
+}

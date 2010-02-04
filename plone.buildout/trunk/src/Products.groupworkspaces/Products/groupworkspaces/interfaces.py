56b62bb75e31ef0981a897356193b23dbd4b3f34
mngoka <mngoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-02-04 14:44:37 +0000

    Add custom membership listing view.

--- plone.buildout/trunk/src/Products.groupworkspaces/Products/groupworkspaces/interfaces.py
+++ plone.buildout/trunk/src/Products.groupworkspaces/Products/groupworkspaces/interfaces.py
@@ -3,3 +3,7 @@ from zope.interface import Interface
 class IGroupWorkspacesView(Interface):
     """Marker interface identifying the Groups Workspace View'
     """
+
+class IMemberWorkspacesView(Interface):
+    """Marker interface identifying the Members Workspace View'
+    """

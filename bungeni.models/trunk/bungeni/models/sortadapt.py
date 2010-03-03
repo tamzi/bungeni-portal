8d1288c0cd96a241232d95d6bbde364d6f04eeb1
christian.ledermann <christian.ledermann@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-03-03 17:18:01 +0000

    test sorting

--- bungeni.models/trunk/bungeni/models/sortadapt.py
+++ bungeni.models/trunk/bungeni/models/sortadapt.py
@@ -2,7 +2,7 @@ from sqlalchemy import sql
 from bungeni.models import domain
 from bungeni.models import schema
 
-def sort_on_partymember(sort_key):
+def sort_on_partymember():
 #sort on ['last_name', 'first_name', 'middle_name']
     pass
 
@@ -10,11 +10,11 @@ def sort_replace_partymember(sort_key):
 #sort replace {'user_id': ['last_name', 'first_name']}
     pass
 
-def sort_on_parliamentsession(sort_key):
+def sort_on_parliamentsession():
 #sort on ['start_date']
     pass
 
-def sort_on_motion(sort_key):
+def sort_on_motion():
 #sort on ['motion_number', 'submission_date']
     pass
 
@@ -26,7 +26,7 @@ def sort_replace_parliamentaryitem(sort_key):
 #sort replace {'owner_id': ['last_name', 'first_name']}
     pass
 
-def sort_on_question(sort_key):
+def sort_on_question():
 #sort on ['question_number', 'submission_date']
     pass
 
@@ -34,7 +34,7 @@ def sort_replace_question(sort_key):
 #sort replace {'owner_id': ['last_name', 'first_name']}
     pass
 
-def sort_on_committeestaff(sort_key):
+def sort_on_committeestaff():
 #sort on ['last_name', 'first_name', 'middle_name']
     pass
 
@@ -42,7 +42,7 @@ def sort_replace_committeestaff(sort_key):
 #sort replace {'user_id': ['last_name', 'first_name']}
     pass
 
-def sort_on_committeemember(sort_key):
+def sort_on_committeemember():
 #sort on ['last_name', 'first_name', 'middle_name']
     pass
 
@@ -50,11 +50,11 @@ def sort_replace_committeemember(sort_key):
 #sort replace {'user_id': ['last_name', 'first_name']}
     pass
 
-def sort_on_government(sort_key):
+def sort_on_government():
 #sort on ['start_date']
     pass
 
-def sort_on_bill(sort_key):
+def sort_on_bill():
 #sort on ['submission_date']
     pass
 
@@ -62,7 +62,7 @@ def sort_replace_bill(sort_key):
 #sort replace {'owner_id': ['last_name', 'first_name']}
     pass
 
-def sort_on_groupmembership(sort_key):
+def sort_on_groupmembership():
 #sort on ['last_name', 'first_name', 'middle_name']
     pass
 
@@ -78,9 +78,12 @@ def sort_replace_committee(sort_key):
 #sort replace {'committee_type_id': ['committee_type']}
     pass
 
-def sort_on_memberofparliament(sort_key):
+def sort_on_memberofparliament():
 #sort on ['last_name', 'first_name', 'middle_name']
-    pass
+    return [ domain.User.last_name.desc(), 
+            domain.User.first_name.desc(), 
+            domain.User.middle_name.desc() ]
+        
 
 def sort_replace_memberofparliament(sort_key):
 #sort replace {'constituency_id': ['name'], 'user_id': ['last_name', 'first_name']}
@@ -94,7 +97,7 @@ def sort_replace_eventitem(sort_key):
 #sort replace {'owner_id': ['last_name', 'first_name']}
     pass
 
-def sort_on_user(sort_key):
+def sort_on_user():
 #sort on ['last_name', 'first_name', 'middle_name']
     pass
 
@@ -102,7 +105,7 @@ def sort_replace_user(sort_key):
 #sort replace {'user_id': ['last_name', 'first_name']}
     pass
 
-def sort_on_officemember(sort_key):
+def sort_on_officemember():
 #sort on ['last_name', 'first_name', 'middle_name']
     pass
 
@@ -114,7 +117,7 @@ def sort_replace_heading(sort_key):
 #sort replace {'owner_id': ['last_name', 'first_name']}
     pass
 
-def sort_on_parliament(sort_key):
+def sort_on_parliament():
 #sort on ['start_date']
     pass
 
@@ -122,7 +125,7 @@ def sort_replace_tableddocument(sort_key):
 #sort replace {'owner_id': ['last_name', 'first_name']}
     pass
 
-def sort_on_minister(sort_key):
+def sort_on_minister():
 #sort on ['last_name', 'first_name', 'middle_name']
     pass
 
@@ -130,7 +133,7 @@ def sort_replace_minister(sort_key):
 #sort replace {'user_id': ['last_name', 'first_name']}
     pass
 
-def sort_on_groupsittingattendance(sort_key):
+def sort_on_groupsittingattendance():
 #sort on ['last_name', 'first_name', 'middle_name']
     pass
 

1124482225a3082c0dc1ddcb90a27e8747f0e57d
christian.ledermann <christian.ledermann@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-03-03 15:13:01 +0000

    move sort_on and sort_replace out of domain into custom adapters

---
+++ bungeni.models/trunk/bungeni/models/sortadapt.py
@@ -0,0 +1,140 @@
+from sqlalchemy import sql
+from bungeni.models import domain
+from bungeni.models import schema
+
+def sort_on_partymember(sort_key):
+#sort on ['last_name', 'first_name', 'middle_name']
+    pass
+
+def sort_replace_partymember(sort_key):
+#sort replace {'user_id': ['last_name', 'first_name']}
+    pass
+
+def sort_on_parliamentsession(sort_key):
+#sort on ['start_date']
+    pass
+
+def sort_on_motion(sort_key):
+#sort on ['motion_number', 'submission_date']
+    pass
+
+def sort_replace_motion(sort_key):
+#sort replace {'owner_id': ['last_name', 'first_name']}
+    pass
+
+def sort_replace_parliamentaryitem(sort_key):
+#sort replace {'owner_id': ['last_name', 'first_name']}
+    pass
+
+def sort_on_question(sort_key):
+#sort on ['question_number', 'submission_date']
+    pass
+
+def sort_replace_question(sort_key):
+#sort replace {'owner_id': ['last_name', 'first_name']}
+    pass
+
+def sort_on_committeestaff(sort_key):
+#sort on ['last_name', 'first_name', 'middle_name']
+    pass
+
+def sort_replace_committeestaff(sort_key):
+#sort replace {'user_id': ['last_name', 'first_name']}
+    pass
+
+def sort_on_committeemember(sort_key):
+#sort on ['last_name', 'first_name', 'middle_name']
+    pass
+
+def sort_replace_committeemember(sort_key):
+#sort replace {'user_id': ['last_name', 'first_name']}
+    pass
+
+def sort_on_government(sort_key):
+#sort on ['start_date']
+    pass
+
+def sort_on_bill(sort_key):
+#sort on ['submission_date']
+    pass
+
+def sort_replace_bill(sort_key):
+#sort replace {'owner_id': ['last_name', 'first_name']}
+    pass
+
+def sort_on_groupmembership(sort_key):
+#sort on ['last_name', 'first_name', 'middle_name']
+    pass
+
+def sort_replace_groupmembership(sort_key):
+#sort replace {'user_id': ['last_name', 'first_name']}
+    pass
+
+def sort_replace_constituency(sort_key):
+#sort replace {'province_id': ['province'], 'region_id': ['region']}
+    pass
+
+def sort_replace_committee(sort_key):
+#sort replace {'committee_type_id': ['committee_type']}
+    pass
+
+def sort_on_memberofparliament(sort_key):
+#sort on ['last_name', 'first_name', 'middle_name']
+    pass
+
+def sort_replace_memberofparliament(sort_key):
+#sort replace {'constituency_id': ['name'], 'user_id': ['last_name', 'first_name']}
+    pass
+
+def sort_replace_agendaitem(sort_key):
+#sort replace {'owner_id': ['last_name', 'first_name']}
+    pass
+
+def sort_replace_eventitem(sort_key):
+#sort replace {'owner_id': ['last_name', 'first_name']}
+    pass
+
+def sort_on_user(sort_key):
+#sort on ['last_name', 'first_name', 'middle_name']
+    pass
+
+def sort_replace_user(sort_key):
+#sort replace {'user_id': ['last_name', 'first_name']}
+    pass
+
+def sort_on_officemember(sort_key):
+#sort on ['last_name', 'first_name', 'middle_name']
+    pass
+
+def sort_replace_officemember(sort_key):
+#sort replace {'user_id': ['last_name', 'first_name']}
+    pass
+
+def sort_replace_heading(sort_key):
+#sort replace {'owner_id': ['last_name', 'first_name']}
+    pass
+
+def sort_on_parliament(sort_key):
+#sort on ['start_date']
+    pass
+
+def sort_replace_tableddocument(sort_key):
+#sort replace {'owner_id': ['last_name', 'first_name']}
+    pass
+
+def sort_on_minister(sort_key):
+#sort on ['last_name', 'first_name', 'middle_name']
+    pass
+
+def sort_replace_minister(sort_key):
+#sort replace {'user_id': ['last_name', 'first_name']}
+    pass
+
+def sort_on_groupsittingattendance(sort_key):
+#sort on ['last_name', 'first_name', 'middle_name']
+    pass
+
+def sort_replace_groupsittingattendance(sort_key):
+#sort replace {'user_id': ['last_name', 'first_name']}
+    pass
+

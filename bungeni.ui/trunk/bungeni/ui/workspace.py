9641960101bb9a46b903a73a557ff0463e4a3ea1
mario.ruggier <mario.ruggier@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-03-16 14:43:24 +0000

    Generalized bungeni.ui.workspace.getRoles to not need request as input parameter; renamed it to get_roles() and moved it up to bungeni.models.utils for availability for other packages. Updated workspace-index and woprkspace-archive views to the switchover.

--- bungeni.ui/trunk/bungeni/ui/workspace.py
+++ bungeni.ui/trunk/bungeni/ui/workspace.py
@@ -1,8 +1,5 @@
 from zope import interface
-from zope import component
 from zope.publisher.browser import BrowserView
-from zope.securitypolicy.interfaces import IRole, IPrincipalRoleMap
-from zope.securitypolicy.settings import Allow, Deny
 from zope.security.proxy import ProxyFactory
 from ploned.ui.interfaces import IViewView
 
@@ -10,46 +7,14 @@ from ore.alchemist import Session
 import sqlalchemy.sql.expression as sql
 
 from bungeni.models import domain
+from bungeni.models.utils import get_principal
 from bungeni.models.utils import get_db_user_id
+from bungeni.models.utils import get_roles
 from bungeni.models.utils import get_group_ids_for_user_in_parliament 
 from bungeni.models.utils import get_ministry_ids_for_user_in_government
 from bungeni.core.globalsettings import getCurrentParliamentId
 
 import interfaces
-
-
-def add_roles(principal, prms, roles):
-    for prm in prms:
-        l_roles = prm.getRolesForPrincipal(principal)
-        for role in l_roles:
-            if role[1] == Allow:
-                if not(role[0] in roles):
-                    roles.append(role[0])
-            elif role[1] == Deny:
-                if role[0] in roles:
-                    roles.remove(role[0])
-    return roles
-
-
-def getRoles(context, request):
-    #return [role_id for role_id, role in \
-    #        component.getUtilitiesFor(IRole, context)]
-    # eeks we have to loop through all groups of the
-    # principal and al PrincipalRoleMaps to get all roles 
-    ctx = context
-    prms = []
-    while ctx:  
-        if component.queryAdapter(ctx, IPrincipalRoleMap):  
-            prms.append(IPrincipalRoleMap(ctx))
-        ctx = getattr(ctx,'__parent__', None)
-    pg = request.principal.groups.keys()
-    roles = []
-    prms.reverse()
-    for pn in pg:
-        roles = add_roles(pn, prms, roles)
-    pn = request.principal.id
-    roles = add_roles(pn, prms, roles)
-    return roles
 
 role_interface_mapping = {
     u'bungeni.Admin': interfaces.IAdministratorWorkspace,
@@ -91,7 +56,7 @@ class WorkspaceView(BrowserView):
             else:
                 self.government_id = None
         
-        roles = getRoles(self.context, self.request)
+        roles = get_roles(self.context)
         
         for role_id in roles:
             iface = role_interface_mapping.get(role_id)

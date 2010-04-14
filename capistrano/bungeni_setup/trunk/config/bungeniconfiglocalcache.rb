7e178b53584141633f55f24367f7234a74b08ccd
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-04-14 13:33:03 +0000

    Updating local bungeni plone package index

--- capistrano/bungeni_setup/trunk/config/bungeniconfiglocalcache.rb
+++ capistrano/bungeni_setup/trunk/config/bungeniconfiglocalcache.rb
@@ -1,7 +1,7 @@
 #### These parameters are used only during the cached buildout #####
 
 set :local_buildout_index, "http://192.168.0.14:6543/bungeni"
-set :plone_local_buildout_index, "http://192.168.0.14:6543/plone"
+set :plone_local_buildout_index, "http://192.168.0.14:6543/bungeniplone"
 set :portal_local_buildout_index, "http://192.168.0.14:6543/plone"
 set :pg_download_url, "http://192.168.0.14:6543/cache/postgresql/postgresql-8.3.7.tar.gz"
 set :xapian_download_url, "http://192.168.0.14:6543/cache/xapian-core/xapian-core-14157.tgz"

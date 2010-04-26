21bb0368abf109159201be19a4754efbb92db9cd
mngoka <mngoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-04-26 17:06:13 +0000

    Check if the user is logged in and then show the workspace ui.

--- bungeni.portal/trunk/bungeni/portal/static/themes/rules.py
+++ bungeni.portal/trunk/bungeni/portal/static/themes/rules.py
@@ -6,12 +6,8 @@ def add_section_links(content, theme, resource_fetcher, log):
     Add top level class links and logos for the different sections (workspace and portal).
     """
     host_url = urlsplit(log.theme_url)
-    if theme("#portaltab-").html() is not None:
-        home_tab_value = str(theme("#portaltab-").html())
-    else:
-        home_tab_value = str(content("#portaltab-").html())
+    if len(theme('#portal-personaltools li:nth-child(4)')) > 0:        
 
-    if 'workspace' in  home_tab_value:
         theme('#portal-logo img').attr('src', host_url[0] + '://' +  host_url[1] +'/++resource++portal/logo-workspace.png')        
         theme('#portal-logo img').attr('width', '803px')
         theme('#portal-logo img').attr('height', '60px')

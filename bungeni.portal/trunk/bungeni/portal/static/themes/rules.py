e07fba925af4cf62b4018057e67e8465d515a4b6
mngoka <mngoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-04-27 16:11:26 +0000

    Add top level menu tabs to represent Plone content.

--- bungeni.portal/trunk/bungeni/portal/static/themes/rules.py
+++ bungeni.portal/trunk/bungeni/portal/static/themes/rules.py
@@ -15,3 +15,10 @@ def add_section_links(content, theme, resource_fetcher, log):
     else:
         theme('body').addClass('template-portal')
 
+def add_plone_tabs(content, theme, resource_fetcher, log):
+    """
+    Add top level dummy menu tabs to represent plone content.
+    """
+    theme('.level0').append("<li class='plain'><a href='#'>have our say</a></li>")
+    theme('.level0').append("<li class='plain'><a href='#'>how we work</a></li>")
+    theme('.level0').append("<li class='plain'><a href='#'>reference material</a></li>")

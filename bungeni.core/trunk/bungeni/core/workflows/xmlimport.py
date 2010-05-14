4905e4e01df6dd7eec165713382496515e39e046
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-05-14 09:01:35 +0000

    Removing Amara dependency in bungeni.core -- see Issue 613 'migrate from amara to lxml'

--- bungeni.core/trunk/bungeni/core/workflows/xmlimport.py
+++ bungeni.core/trunk/bungeni/core/workflows/xmlimport.py
@@ -2,8 +2,7 @@
 Support for xml defined workflows, and security manipulations by state.
 """
 
-import amara
-
+from lxml import etree
 from zope.dottedname.resolve import resolve
 from zope.i18nmessageid import Message
 
@@ -16,64 +15,59 @@ from bungeni.core.workflows.states import StateWorkflow
 from ore.workflow import interfaces
 
 trigger_value_map = {
-    'manual':interfaces.MANUAL,
-    'automatic':interfaces.AUTOMATIC,
-    'system':interfaces.SYSTEM
-    }
+    'manual': interfaces.MANUAL,
+    'automatic': interfaces.AUTOMATIC,
+    'system': interfaces.SYSTEM}
 
-def load( uri ):
-    doc = parse( uri )
-    
-    return _load( doc.workflow )
 
-def parse( uri ):
-    doc = amara.parse( uri )
-    return doc
+def load(file_path):
+    doc = etree.fromstring(open(file_path).read())
+    return _load(doc)
 
-def _load( workflow ):
+
+def _load(workflow):
     transitions = []
     states = []
-    domain = getattr( workflow, 'domain', None )
+    domain = workflow.get('domain')
 
-    for s in getattr( workflow, 'state', ()):
+    for s in workflow.iterchildren('state'):
         permissions = []
-        for g in getattr( s, 'grant', () ):
+        for g in s.iterchildren('grant'):
             permissions.append(
-                ( GRANT, g.permission, g.role )
-                )
-        for d in getattr( s, 'deny', () ):
+                (GRANT, g.get('permission'), g.get('role')))
+        for d in s.iterchildren('deny'):
             permissions.append(
-                (DENY, d.permission, d.role )
-                )
-        state_id  = s.id 
-        states.append( State( state_id, Message(s.title, domain), permissions ) )
-    
-    for t in workflow.transition:
-        try:
-            t.source
-            t.destination
-            t.id
-            t.title
-        except AttributeError:
-            raise SyntaxError(t.toxml())
+                (DENY, d.get('permission'), d.get('role')))
+        state_id = s.get('id')
+        states.append(
+            State(state_id,
+                  Message(s.get('title', domain)),
+                  permissions))
+
+    for t in workflow.iterchildren('transition'):
+
+        for key in ('source', 'destination', 'id', 'title'):
+            if t.get(key) is None:
+                raise SyntaxError("%s not in %s"%(key, etree.tostring(t)))
 
         # XXX: There must be a better way to do this:
-        source = t.source
+        source = t.get('source')
         while '  ' in source:
             source = source.replace('  ', ' ')
         sources = [s or None for s in source.split(' ')]
 
         for source in sources:
             if len(sources) > 1:
-                tid = "%s-%s" % (t.id, source)
+                tid = "%s-%s" % (t.get('id'), source)
             else:
-                tid = t.id
-            args = (tid, Message(t.title, domain), source, t.destination)
+                tid = t.get('id')
+            args = (tid, Message(t.get('title'), domain),
+                    source, t.get('destination'))
             kw = {}
 
             # optionals
             for i in ('trigger', 'order', 'permission'):
-                val = getattr( t,i,None )
+                val = t.get(i)
                 if not val:
                     continue
                 kw[i] = val
@@ -84,18 +78,17 @@ def _load( workflow ):
 
             if 'trigger' in kw:
                 k = kw['trigger']
-                v = trigger_value_map[ k ]
+                v = trigger_value_map[k]
                 kw['trigger'] = v
 
             # optional python resolvables
             for i in('condition', 'action', 'event'):
-                val = getattr( t,i,None)
+                val = t.get(i)
                 if not val:
                     continue
-                val = resolve( val , 'bungeni.core.workflows' ) # raises importerror/nameerror
+                # raises importerror/nameerror
+                val = resolve(val, 'bungeni.core.workflows')
                 kw[i] = val
-            transitions.append( StateTransition( *args, **kw ) )
+            transitions.append(StateTransition(*args, **kw))
 
-    return StateWorkflow( transitions, states )
-
-
+    return StateWorkflow(transitions, states)

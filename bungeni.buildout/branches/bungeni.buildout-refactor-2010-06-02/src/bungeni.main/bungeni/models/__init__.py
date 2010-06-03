f5a3717168c36fb26010d0778c3cdd6d202600fa
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-06-03 08:31:22 +0000

    Adding updated __init__.py

--- bungeni.buildout/branches/bungeni.buildout-refactor-2010-06-02/src/bungeni.main/bungeni/models/__init__.py
+++ bungeni.buildout/branches/bungeni.buildout-refactor-2010-06-02/src/bungeni.main/bungeni/models/__init__.py
@@ -5,3 +5,22 @@ try:
 except ImportError:
     import pkgutil
     __path__ = pkgutil.extend_path(__path__, __name__)
+
+
+import orm
+
+from schema import metadata
+
+from domain import User, Minister
+from domain import GroupMembership, Group, Government, Parliament, PoliticalParty, Ministry, Committee
+from domain import GroupSitting, SittingType, GroupSittingAttendance, AttendanceType
+from domain import ParliamentSession, PoliticalGroup
+from domain import Question, QuestionVersion, QuestionChange
+from domain import Motion, MotionVersion, MotionChange
+from domain import Bill, BillVersion, BillChange, BillType
+from domain import Constituency, Parliament
+from domain import Country, Region, Province
+from domain import MemberOfParliament
+from domain import MemberTitle, MemberRoleTitle
+from domain import ItemSchedule
+

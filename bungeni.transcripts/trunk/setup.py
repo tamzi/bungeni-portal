60362fb49d39f7308b89c8df6a97fab8c6833e96
mario.ruggier <mario.ruggier@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-06-03 08:03:24 +0000

    Corrected IOError: [Errno 2] No translation file found for domain: 'bungeni.ui' that was being triggered when bungeni.transcipts package is added to the buildout (reported on bungeni-dev by Miano this past sunday).

--- bungeni.transcripts/trunk/setup.py
+++ bungeni.transcripts/trunk/setup.py
@@ -1,31 +1,32 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1'
+version = "0.1"
 
-setup(name='bungeni.transcripts',
-      version=version,
-      description="Transctipts functionality for Bungeni",
-      long_description=open("README.txt").read() + "\n" +
+setup(name="bungeni.transcripts",
+    version=version,
+    author='Bungeni Developers',
+    author_email="bungeni-dev@googlegroups.com",
+    description="Transctipts functionality for Bungeni",
+    long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+    keywords="bungeni zope3",
+    # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Programming Language :: Python",
-        ],
-      keywords='Bungeni Zope3',
-      author='Miano Njoka',
-      author_email='miano@parliaments.info',
-      url='www.bungeni.org/',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['bungeni'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+    ],
+    packages=find_packages(),
+    namespace_packages=["bungeni"],
+    url="www.bungeni.org/",
+    license="GPL",
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "setuptools",
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+# -*- Entry points: -*-
+""",
+)
+

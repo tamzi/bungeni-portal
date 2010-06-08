945428626383b16dd96d3e5006e50a2c8afa41a7
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-06-08 09:33:52 +0000

    Removing bungeni.core dependency from marginalia

--- marginalia/branches/marginalia-refactor-2010-06-02/setup.py
+++ marginalia/branches/marginalia-refactor-2010-06-02/setup.py
@@ -19,7 +19,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires = [ 'setuptools',
-                         'bungeni.core',
+                         'bungeni',
                          'zope.formlib'],
     zip_safe = False,
     )

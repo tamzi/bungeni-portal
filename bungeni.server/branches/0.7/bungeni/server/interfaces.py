176835eaa62bde6d96cd54b19525f13f6c5346f6
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-06-16 07:09:24 +0000

    Deleting bungeni.server - refactored into bungeni.main

--- bungeni.server/branches/0.7/bungeni/server/interfaces.py
+++
@@ -1,4 +0,0 @@
-from zope.interface import Interface
-
-class ISettings(Interface):
-    """WSGI global configuration settings."""
--- bungeni.server/branches/0.7/bungeni/server/startup.py
+++
@@ -1,8 +0,0 @@
-from zope import component
-
-from ore.wsgiapp import startup
-from interfaces import ISettings
-
-def application_factory(global_conf, **kwargs):
-    component.provideUtility(global_conf, ISettings)
-    return startup.application_factory(global_conf, **kwargs)

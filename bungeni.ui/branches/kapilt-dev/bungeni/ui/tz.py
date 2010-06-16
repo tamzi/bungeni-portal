380f4291bd5e79c8df43b18ef25a240d16ef6eed
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-06-16 07:20:54 +0000

    Deleting bungeni.ui - refactored into bungeni.main

--- bungeni.ui/branches/kapilt-dev/bungeni/ui/tz.py
+++
@@ -1,9 +0,0 @@
-import pytz
-from zope import interface, component
-from zope.interface.common.idatetime import ITZInfo
-from zope.publisher.interfaces.browser import IBrowserRequest
-
-@interface.implementer(ITZInfo)
-@component.adapter(IBrowserRequest)
-def tzinfo(request):
-     return pytz.timezone('Africa/Nairobi') # or whatever timezone you wish

0a3589cb6b3a0693bfe74cf89cd6fe208d541484
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-06-24 06:04:24 +0000

    Fabric script updated

--- capistrano/fabric/bungeni.py
+++ capistrano/fabric/bungeni.py
@@ -1,4 +1,4 @@
-from fabric.api import run,env
+from fabric.api import run,sudo, env
 
 class OsInfo:
 	def __init__(self):
@@ -13,6 +13,71 @@ class OsInfo:
 		return output
 
 
+class OsEssentials:
+        def __init__(self):
+                pass
+	
+	def get_reqd_libs(self, dist_id, dist_rel):
+		return self.requiredLibs[dist_id+"-"+dist_rel]
+
+
+        requiredLibs = {
+                        'Ubuntu-10.04' : [
+					"wget",
+					"zip",
+					"unzip",
+					"build-essential", # for building from source
+					"libjpeg62-dev", # for python
+					"libfreetype6-dev", # for python
+					"libbz2-dev", # for python bz2 processing
+					"libxslt1-dev", # for python lxml
+					"libxml2-dev", # for python lxml
+					"libpng12-dev", # for python
+					"openssl", # for python
+					"libssl-dev", # for python
+					"bison",  # for postgresql
+					"flex" , # for postgresql
+					"libreadline5-dev" , # for postgresql
+					"zlib1g-dev" , # for postgresql
+					"libtool" , # for svn
+					"automake" ,  # for svn
+					"autoconf" , # for svn
+					"libsqlite3-dev", #for python unit tests
+					"uuid-dev", # for ubuntu 9.04 xapian
+					"openoffice.org-headless", #for generating pdf reports
+					"python-uno", #for generating pdf reports
+					"libtidy-dev" #required by tidy
+					],
+                        'Ubuntu-8.04' : [
+					"wget",
+					"zip",
+					"unzip",
+					"build-essential", # for building from source
+					"libjpeg62-dev", # for python
+					"libfreetype6-dev", # for python
+					"libbz2-dev", # for python bz2 processing
+					"libxslt1-dev", # for python lxml
+					"libxml2-dev", # for python lxml
+					"libpng12-dev", # for python
+					"openssl", # for python
+					"libssl-dev", # for python
+					"bison",  # for postgresql
+					"flex" , # for postgresql
+					"libreadline5-dev" , # for postgresql
+					"zlib1g-dev" , # for postgresql
+					"libtool" , # for svn
+					"automake" ,  # for svn
+					"autoconf" , # for svn
+					"libsqlite3-dev", #for python unit tests
+					"uuid-dev", # for ubuntu 9.04 xapian
+					"openoffice.org-headless", #for generating pdf reports
+					"python-uno", #for generating pdf reports
+					"libtidy-dev" #required by tidy
+					],
+			}
+		
+		
+
 
 
 class BungeniPresetup:
@@ -22,6 +87,14 @@ class BungeniPresetup:
 
 	def presetup(self):
 		pass
+
+	def essentials(self):
+		osInfo = OsInfo()
+		osEssent = OsEssentials()
+		liLibs = osEssent.get_reqd_libs(osInfo.release_id(), osInfo.release_no())
+		sudo("apt-get install " + " ".join(liLibs))
+
+		
 
 
 class BungeniTasks:
@@ -44,6 +117,11 @@ class PloneTasks:
 
 def presetup():
 	osInfo = OsInfo()
+	osEssent = OsEssentials()
 	print "I am " + osInfo.release_id() + " " + osInfo.release_no()
+	print "Required = " + str(osEssent.get_reqd_libs(osInfo.release_id(), osInfo.release_no()))
+	bungenipre = BungeniPresetup()
+	bungenipre.essentials()
+
 
 

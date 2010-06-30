7819ad358337a304b3dcf9057ca2340564cb5679
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-06-30 14:26:10 +0000

    Updated fabric scripts ; added scm support

--- capistrano/fabric/fabfile.py
+++ capistrano/fabric/fabfile.py
@@ -4,6 +4,9 @@ import bungeni
 Build python 2.5
 """
 def build_python25():
+	"""
+	Builds Python 2.5 from source
+	"""
 	bungenipre = bungeni.Presetup()
 	bungenipre.build_py25()
 	
@@ -11,13 +14,24 @@ def build_python25():
 Build python 2.4
 """	
 def build_python24():
+	"""
+	Builds Python 2.4 from source
+	"""
 	bungenipre = bungeni.Presetup()
 	bungenipre.build_py24()
 
 
 def build_imaging():
+	"""
+	Builds python imaging extensions for Python 2.5 and 2.4
+	"""
 	bungenipre = bungeni.Presetup()
 	bungenipre.build_imaging()
 
 
-
+def setup_pylibs():
+	"""
+	Installs setuptools and supervisor for the built Pythons (2.5 and 2.4)
+	"""
+	bungenipre = bungeni.Presetup()
+	bungenipre.required_pylibs()

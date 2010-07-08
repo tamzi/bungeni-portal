463370d6104c4d607e41c3ce4d22bb0dee717942
mngoka <mngoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-07-08 15:03:25 +0000

    Append trailing slash to the each breadcrumb and to the workspace navigation viewlet links. This is necessary in order to maintain the right context especially for deliverance views.

--- bungeni.main/trunk/bungeni/ui/viewlets/navigation.py
+++ bungeni.main/trunk/bungeni/ui/viewlets/navigation.py
@@ -284,6 +284,13 @@ class BreadCrumbsViewlet(viewlet.ViewletBase):
             path.extend(self._get_path(context.__parent__))
         
         _url = url.absoluteURL(context, self.request)
+
+        # Append a trailing slash to each breadcrumb entry so that
+        # the right context is always maintained when the breadcrumbs
+        #are used for navigation.
+        if _url[-1:] != "/":
+            _url = _url + "/"
+
         title = get_title_from_context(context)
         
         if title is not None:

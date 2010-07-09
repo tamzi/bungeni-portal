cc8200b27d473c856df6a67248a015f9609059e0
mngoka <mngoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-07-09 14:33:45 +0000

    Fix image reference link, only if it exists, fix css for workspace tab.

--- bungeni.main/trunk/bungeni/portal/static/themes/rules.py
+++ bungeni.main/trunk/bungeni/portal/static/themes/rules.py
@@ -19,18 +19,19 @@ def image_links(content, theme, resource_fetcher, log):
     """
     Use absolute links to the members image and political groups logo.
     """
-    link_value  = str(content('#region-content').html().encode("utf-8"))
-    to_replace = 'file-image/image'
-    to_replace = ['file-image/image', 'file-image/logo_data']
-    for image in to_replace:
-        if image in link_value:
-            link_id= content('#portal-breadcrumbs a:last-child')
-            link_id = str(link_id).split('href="')
-            link_id = str(link_id[1]).split('">')[0]
-            link_node = content('#region-content')
-            new_value = link_value.replace('./'+image, 
-                                        str(link_id)+'/'+image)
-            link_node.replaceWith('<div id="region-content" class="documentContent">' + new_value + '</div>')
+    if content('#region-content').html() != None:
+        link_value  = str(content('#region-content').html().encode("utf-8"))
+        to_replace = 'file-image/image'
+        to_replace = ['file-image/image', 'file-image/logo_data']
+        for image in to_replace:
+            if image in link_value:
+                link_id= content('#portal-breadcrumbs a:last-child')
+                link_id = str(link_id).split('href="')
+                link_id = str(link_id[1]).split('">')[0]
+                link_node = content('#region-content')
+                new_value = link_value.replace('./'+image, 
+                                               str(link_id)+'/'+image)
+                link_node.replaceWith('<div id="region-content" class="documentContent">' + new_value + '</div>')
 
 
 def add_plone_tabs(content, theme, resource_fetcher, log):

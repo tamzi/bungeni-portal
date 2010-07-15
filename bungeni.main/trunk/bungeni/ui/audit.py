6d68b95e54ef9b88a670f9d547bd6e6fbf3a4d69
mario.ruggier <mario.ruggier@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-07-15 16:22:27 +0000

    Localized dates in the workflow/versions/audit-log views. Related code cleanup.

--- bungeni.main/trunk/bungeni/ui/audit.py
+++ bungeni.main/trunk/bungeni/ui/audit.py
@@ -7,58 +7,65 @@ from zc.table import batching, column
 import sqlalchemy as rdb
 
 from bungeni.core.i18n import _
+from bungeni.ui.utils import date
 
-
-class ChangeBaseView( BrowserView ):
-    """
-    base view for looking at changes to context
+class ChangeBaseView(BrowserView):
+    """Base view for looking at changes to context.
     """
     
     formatter_factory = batching.Formatter
     
-    columns = [
-        column.GetterColumn( title=_(u"action"), getter=lambda i,f:i['action'] ),
-        column.GetterColumn( title=_(u"date"), getter=lambda i,f: i['date'].strftime('%Y-%m-%d %H:%M') ),
-        column.GetterColumn( title=_(u"user"), getter=lambda i,f:i['user_id'] ),
-        column.GetterColumn( title=_(u"description"), getter=lambda i,f:i['description'] ),
+    def __init__(self, context, request):
+        super(ChangeBaseView, self).__init__(context, request)
+        # table to display the versions history
+        formatter = date.getLocaleFormatter(self.request, "dateTime", "short")
+        # !+ note this breaks the previous sort-dates-as-strings-hack of 
+        # formatting dates, for all locales, as date.strftime("%Y-%m-%d %H:%M")
+        # that, when sorted as a string, gives correct results.
+        self.columns = [
+            column.GetterColumn(title=_(u"action"), 
+                    getter=lambda i,f:i["action"]),
+            column.GetterColumn(title=_(u"date"), 
+                    getter=lambda i,f:formatter.format(i["date"])),
+            column.GetterColumn(title=_(u"user"), 
+                    getter=lambda i,f:i["user_id"]),
+            column.GetterColumn(title=_(u"description"), 
+                    getter=lambda i,f:i["description"]),
         ]
     
-    def listing( self ):
-        columns = self.columns
-        formatter = self.formatter_factory( self.context,
-                                            self.request,
-                                            self.getFeedEntries(),
-                                            prefix="results",
-                                            visible_column_names = [c.name for c in columns],
-                                            columns = columns )
-        formatter.cssClasses['table'] = 'listing'
+    def listing(self):
+        formatter = self.formatter_factory(self.context, self.request,
+                        self.getFeedEntries(),
+                        prefix="results",
+                        visible_column_names=[ c.name for c in self.columns ],
+                        columns=self.columns)
+        formatter.cssClasses["table"] = "listing"
         formatter.updateBatching()
         return formatter()
         
     @property
-    def _log_table( self ):
-        auditor = audit.getAuditor( self.context )
+    def _log_table(self):
+        auditor = audit.getAuditor(self.context)
         return auditor.change_table
         
-    def getFeedEntries( self ):
-        instance = removeSecurityProxy( self.context )
-        mapper = orm.object_mapper( instance )
+    def getFeedEntries(self):
+        instance = removeSecurityProxy(self.context)
+        mapper = orm.object_mapper(instance)
         
-        query = self._log_table.select().where(
-            rdb.and_( self._log_table.c.content_id == rdb.bindparam('content_id') )
-            ).order_by(self._log_table.c.change_id.desc())
+        query = self._log_table.select().where(rdb.and_(
+                    self._log_table.c.content_id==rdb.bindparam("content_id"))
+                ).order_by(self._log_table.c.change_id.desc())
+        
+        content_id = mapper.primary_key_from_instance(instance)[0] 
+        content_changes = query.execute(content_id=content_id)
+        return map(dict, content_changes)
 
-        content_id = mapper.primary_key_from_instance( instance )[0] 
-        content_changes = query.execute( content_id = content_id )
-        return map( dict, content_changes)
-
-class RSS2( ChangeBaseView ):
-    """
-    RSS Feed for an object
+class RSS2(ChangeBaseView):
+    """RSS Feed for an object
     """
 
-class ChangeLog( ChangeBaseView ):
-    """
-    Change Log View for an object
+class ChangeLog(ChangeBaseView):
+    """Change Log View for an object
     """
     form_name = "Change Log"
+

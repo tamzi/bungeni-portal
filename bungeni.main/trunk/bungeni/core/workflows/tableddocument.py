7efc6213bdd6bad76e5df63080147972f275c4fe
mianonjoka <mianonjoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-07-19 10:07:35 +0000

    workflow revision for tabled documents to add reschedule and revert to admissible actions

--- bungeni.main/trunk/bungeni/core/workflows/tableddocument.py
+++ bungeni.main/trunk/bungeni/core/workflows/tableddocument.py
@@ -72,9 +72,13 @@ class actions:
     @staticmethod
     def schedule(info, context):
         pass
-
+        
     @staticmethod
-    def schedule(info, context):
+    def reschedule(info, context):
+        pass
+    
+    @staticmethod
+    def revert_to_admissible(info, context):
         pass
 
     @staticmethod

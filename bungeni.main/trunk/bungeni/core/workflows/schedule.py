2dcb1bead640aa2769bbe099b21198a29a35424f
mianonjoka <mianonjoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-07-19 13:44:29 +0000

    items scheduled in a sitting are taken back to schedule_pending status when a sitting's agenda is redrafted

--- bungeni.main/trunk/bungeni/core/workflows/schedule.py
+++ bungeni.main/trunk/bungeni/core/workflows/schedule.py
@@ -3,9 +3,11 @@ from zope.security.proxy import removeSecurityProxy
 from bungeni.models import domain
 from bungeni.ui.tagged import get_states
 from ore.workflow.interfaces import IWorkflowInfo
-from sqlalchemy.orm import eagerload
+from sqlalchemy.orm import eagerload, object_mapper
+from bungeni.core import audit
+import sqlalchemy as rdb
 def handleSchedule( object, event):
-    """ move scheduled items from to be scheduled state to schedule when draft agenda is finalised"""
+    """ move scheduled items from to be scheduled state to schedule when draft agenda is finalised and vice versa"""
     session = Session()
     s = removeSecurityProxy(object)
     sitting = session.query(domain.GroupSitting).options(
@@ -18,7 +20,14 @@ def handleSchedule( object, event):
             if sch.item.type != "heading":
                 wf_info = IWorkflowInfo(sch.item)
                 transitions = wf_info.getManualTransitionIds()
-            
+                state = wf_info.state()
+                wf = wf_info.workflow()
+                for transition_id in transitions:
+                    t = wf.getTransition(state.getState(), transition_id)
+                    if t.destination in get_states(sch.item.type, tagged=["tobescheduled"]):
+                        wf_info.fireTransition(transition_id)
+                        break
+                        
     elif sitting.status == "published_agenda":
         for sch in schedulings:
             if sch.item.type != "heading":

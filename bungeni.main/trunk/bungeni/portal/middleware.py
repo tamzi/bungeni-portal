0f4869018e6986ea350f140f4eb8a7e7d21aa5c7
mngoka <mngoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-07-22 11:51:28 +0000

    Added key -'theme_uri' to allow theme configuration per site (NB: You need to rerun the portal buildout) - to change the theme that is loaded  change the value of the key in either deploy.ini or debug.ini.

--- bungeni.main/trunk/bungeni/portal/middleware.py
+++ bungeni.main/trunk/bungeni/portal/middleware.py
@@ -2,9 +2,9 @@ from pyquery import rules
 from deliverance import middleware
 from deliverance import security
 
-def make_deliverance_middleware(app, global_conf, rule_uri):
+def make_deliverance_middleware(app, global_conf, rule_uri, theme_uri):
     rule_getter = middleware.SubrequestRuleGetter(rule_uri)
-    app = middleware.DeliveranceMiddleware(app, rule_getter)
+    app = middleware.DeliveranceMiddleware(app, rule_getter, default_theme=theme_uri)
     return security.SecurityContext.middleware(app,
         display_local_files=True, display_logging=True)
         

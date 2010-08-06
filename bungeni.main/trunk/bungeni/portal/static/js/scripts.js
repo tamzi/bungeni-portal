569f535676346b13d4055c6c25f46fe7aa1da2dc
sp.brendan <sp.brendan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-08-06 09:32:05 +0000

    Latest updates to the CSS including bug fixes. Also modification of template file and script file.

--- bungeni.main/trunk/bungeni/portal/static/js/scripts.js
+++ bungeni.main/trunk/bungeni/portal/static/js/scripts.js
@@ -1,35 +1,39 @@
 function listTogglr(){
   var viewletDateToggle = $('#whats-on-overview .whats-on-viewlet dd ul li h2');
-  viewletDateToggle.css('background','url(/static/images/minus.png) 2px 4px no-repeat');
-  viewletDateToggle.css('padding-left','20px');
-  viewletDateToggle.css('cursor','pointer');
+  viewletDateToggle.css({
+  	background: '#fdcd8b url(/static/images/minus.png) 2px 4px no-repeat',
+	paddingLeft: '20px',
+	cursor: 'pointer'
+  });
   viewletDateToggle.click(function(){
       var state = $(this).siblings('ul').css('display');
       if (state == 'hidden' || state == 'none') {
         $(this).css('background-image', 'url(/static/images/minus.png)');
-        $(this).siblings('ul').slideDown();
+        $(this).siblings('ul').slideDown('fast');
       }
       else {
         $(this).css('background-image', 'url(/static/images/plus.png)');
-        $(this).siblings('ul').slideUp();
+        $(this).siblings('ul').slideUp('fast');
       }
     });
   
   var viewlet = $('#fieldset-upcoming-sittings ul li ul li');
-  viewlet.css('background','#fff url(/static/images/plus.png) 2px 4px no-repeat');
-  viewlet.css('padding-left','20px');
-  viewlet.css('cursor','pointer');
+  viewlet.css({
+  	background: '#fff url(/static/images/plus.png) 2px 4px no-repeat',
+	paddingLeft: '20px',
+	cursor: 'pointer'
+  });
   $('#fieldset-upcoming-sittings ul li ul li ul li').css({background: '#fff', padding: '0'});
   viewlet.children('ul').hide();
   viewlet.click(function(){
       var state = $(this).children('ul').css('display');
       if(state == 'hidden' || state == 'none'){
         $(this).css('background-image','url(/static/images/minus.png)');
-        $(this).children('ul').slideDown();
+        $(this).children('ul').slideDown('fast');
       }
       else {
         $(this).css('background-image','url(/static/images/plus.png)');
-        $(this).children('ul').slideUp();
+        $(this).children('ul').slideUp('fast');
       }
     });
   $('#fieldset-upcoming-sittings ul li ul li ul li').css('cursor','default');
@@ -38,32 +42,37 @@ function listTogglr(){
     });
 }
 function searchTogglr(){
-  var portletHeader = $('#portletArchiveDates .portletHeader');
-  portletHeader.css('cursor','pointer');
-  portletHeader.css('background','url(/static/images/minus.png) 2px 4px no-repeat');
-  portletHeader.css('padding-left','20px');
-  portletHeader.hover(function(){
-      portletHeader.css('text-decoration','underline');
-    },function(){
-      portletHeader.css('text-decoration','none');
-    });
-  var portletItem = $('#portletArchiveDates .portletItem');
-  portletItem.css('padding-left','10px');
-  if(!portletItem.hasClass('dates-filtered')){
-    portletItem.hide();
-    portletHeader.css('background-image','url(/static/images/plus.png)');
-  }
-  portletHeader.click(function(){
-      var state = portletItem.css('display');
-      if(state == 'hidden' || state == 'none'){
-        portletHeader.css('background-image','url(/static/images/minus.png)');
-        portletItem.slideDown('fast');
-      }
-      else {
-        portletHeader.css('background-image','url(/static/images/plus.png)');
-        portletItem.slideUp('fast');
-      }
-    });
+	var portletHeader = $('#portletArchiveDates .portletHeader');
+	var portletHeaderSpan = portletHeader.children('span');
+	portletHeaderSpan.css({
+		float:'right',
+		display:'block',
+		paddingLeft: '20px',
+		cursor: 'pointer',
+		background: 'url(/static/images/minus.png) 2px 4px no-repeat'
+	});
+	portletHeaderSpan.hover(function(){
+		portletHeaderSpan.css('text-decoration','underline');
+	},function(){
+		portletHeaderSpan.css('text-decoration','none');
+	});
+	var portletItem = $('#portletArchiveDates .portletItem');
+	portletItem.css('clear','right');
+	if(!portletItem.hasClass('dates-filtered')){
+		portletItem.hide();
+		portletHeaderSpan.css('background-image','url(/static/images/plus.png)');
+	}
+	portletHeader.click(function(){
+		var state = portletItem.css('display');
+		if(state == 'hidden' || state == 'none'){
+			portletHeaderSpan.css('background-image','url(/static/images/minus.png)');
+			portletItem.slideDown('fast');
+		}
+		else {
+			portletHeaderSpan.css('background-image','url(/static/images/plus.png)');
+			portletItem.slideUp('fast');
+		}
+	});
 }
 $(document).ready(function(){
     searchTogglr();

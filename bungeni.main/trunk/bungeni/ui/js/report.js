64c5ce1b3787f4d61de3964a450d6224366d3940
mianonjoka <mianonjoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-08-09 15:28:57 +0000

    updated reports page javascript

--- bungeni.main/trunk/bungeni/ui/js/report.js
+++ bungeni.main/trunk/bungeni/ui/js/report.js
@@ -1,27 +1,27 @@
 (function($) { 
     $(document).ready(function(){  
         $("#groupsitting-form #actionsView").before('<div id="options" style="display:block"></div>');
-        $("#form\\.bill_options\\.0").parents(".widget-multicheckboxwidget").appendTo("#options").css('display', 'inline-block');
-        $("#form\\.bill_options\\.0").attr('disabled', false);
-        $("#form\\.agenda_options\\.0").parents(".widget-multicheckboxwidget").appendTo("#options").css('display', 'inline-block');
-        $("#form\\.agenda_options\\.0").attr('disabled', false);
-        $("#form\\.motion_options\\.0").parents(".widget-multicheckboxwidget").appendTo("#options").css('display', 'inline-block');
-        $("#form\\.motion_options\\.0").attr('disabled', false);
-        $("#form\\.question_options\\.0").parents(".widget-multicheckboxwidget").appendTo("#options").css('display', 'inline-block');
-        $("#form\\.question_options\\.0").attr('disabled', false);
-        $("#form\\.tabled_document_options\\.0").parents(".widget-multicheckboxwidget").appendTo("#options").css('display', 'inline-block');
-        $("#form\\.tabled_document_options\\.0").attr('disabled', false);
+        $("#form\\.bills_options\\.0").parents(".widget-multicheckboxwidget").appendTo("#options").css('display', 'inline-block');
+        $("#form\\.bills_options\\.0").attr('disabled', false);
+        $("#form\\.agenda_items_options\\.0").parents(".widget-multicheckboxwidget").appendTo("#options").css('display', 'inline-block');
+        $("#form\\.agenda_items_options\\.0").attr('disabled', false);
+        $("#form\\.motions_options\\.0").parents(".widget-multicheckboxwidget").appendTo("#options").css('display', 'inline-block');
+        $("#form\\.motions_options\\.0").attr('disabled', false);
+        $("#form\\.questions_options\\.0").parents(".widget-multicheckboxwidget").appendTo("#options").css('display', 'inline-block');
+        $("#form\\.questions_options\\.0").attr('disabled', false);
+        $("#form\\.tabled_documents_options\\.0").parents(".widget-multicheckboxwidget").appendTo("#options").css('display', 'inline-block');
+        $("#form\\.tabled_documents_options\\.0").attr('disabled', false);
         $("#options span").css('padding','0 0 0 0');
-        $("#form\\.bill_options\\.0").attr('checked', true);
-        $("#form\\.bill_options\\.1").attr('checked', true);
-        $("#form\\.agenda_options\\.0").attr('checked', true);
-        $("#form\\.agenda_options\\.3").attr('checked', true);
-        $("#form\\.motion_options\\.0").attr('checked', true);
-        $("#form\\.motion_options\\.3").attr('checked', true);
-        $("#form\\.question_options\\.0").attr('checked', true);
-        $("#form\\.question_options\\.3").attr('checked', true);
-        $("#form\\.tabled_document_options\\.0").attr('checked', true);
-        $("#form\\.tabled_document_options\\.3").attr('checked', true);
+        $("#form\\.bills_options\\.0").attr('checked', true);
+        $("#form\\.bills_options\\.1").attr('checked', true);
+        $("#form\\.agenda_items_options\\.0").attr('checked', true);
+        $("#form\\.agenda_items_options\\.3").attr('checked', true);
+        $("#form\\.motions_options\\.0").attr('checked', true);
+        $("#form\\.motions_options\\.3").attr('checked', true);
+        $("#form\\.questions_options\\.0").attr('checked', true);
+        $("#form\\.questions_options\\.3").attr('checked', true);
+        $("#form\\.tabled_documents_options\\.0").attr('checked', true);
+        $("#form\\.tabled_documents_options\\.3").attr('checked', true);
         
         var pathname = window.location.pathname;
         var path = new Array();
@@ -30,65 +30,57 @@
         if (doc_type == "")
         {
             doc_type = path[path.length-2];
-        }
-        if (doc_type == 'agenda')
-        {
-            $("#form\\.doc_type option[value='Proceedings of the day']").remove();
-        }
-        else
-        {
-            $("#form\\.doc_type option[value='Order of the day']").remove();
-        }   
+        }  
         $("input#form\\.item_types\\.0").change(function () {
             if ($("#form\\.item_types\\.0").is(":checked"))
             {
-                $('input[name="form.bill_options"]').attr('disabled', false);
+                $('input[name="form.bills_options"]').attr('disabled', false);
                //alert( $('input[name="form.bill_options"]').val() );
             }
             else
             {
-                 $('input[name="form.bill_options"]').attr('disabled', true);
+                 $('input[name="form.bills_options"]').attr('disabled', true);
                 //alert( $('input[name="form.bill_options"]').val() );
             }
         });
         $("input#form\\.item_types\\.1").change(function () {
             if ($("#form\\.item_types\\.1").is(":checked"))
             {
-                $('input[name="form.agenda_options"]').attr('disabled', false);
+                $('input[name="form.agenda_items_options"]').attr('disabled', false);
             }
             else
             {
-                $('input[name="form.agenda_options"]').attr('disabled', true);
+                $('input[name="form.agenda_items_options"]').attr('disabled', true);
             }
         });
         $("input#form\\.item_types\\.2").change(function () {
             if ($("#form\\.item_types\\.2").is(":checked"))
             {
-                $('input[name="form.motion_options"]').attr('disabled', false);
+                $('input[name="form.motions_options"]').attr('disabled', false);
             }
             else
             {
-                $('input[name="form.motion_options"]').attr('disabled', true);
+                $('input[name="form.motions_options"]').attr('disabled', true);
             }
         });
         $("input#form\\.item_types\\.3").change(function () {
             if ($("#form\\.item_types\\.3").is(":checked"))
             {
-                $('input[name="form.question_options"]').attr('disabled', false);
+                $('input[name="form.questions_options"]').attr('disabled', false);
             }
             else
             {
-                $('input[name="form.question_options"]').attr('disabled', true);
+                $('input[name="form.questions_options"]').attr('disabled', true);
             }
         });
         $("input#form\\.item_types\\.4").change(function () {
             if ($("#form\\.item_types\\.4").is(":checked"))
             {
-                $('input[name="form.tabled_document_options"]').attr('disabled', false);
+                $('input[name="form.tabled_documents_options"]').attr('disabled', false);
             }
             else
             {
-                $('input[name="form.tabled_document_options"]').attr('disabled', true);
+                $('input[name="form.tabled_documents_options"]').attr('disabled', true);
             }
         });
         $("#form\\.doc_type").change(function() {

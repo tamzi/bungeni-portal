0dcebdd61bdeb0be4623dde152c79b6765754623
sp.brendan <sp.brendan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-08-26 15:39:35 +0000

    Remove workspace logo, it will be loaded via css, remove css files that were being loaded twice.

--- bungeni.main/trunk/bungeni/portal/static/themes/rules.py
+++ bungeni.main/trunk/bungeni/portal/static/themes/rules.py
@@ -7,10 +7,6 @@ def add_section_links(content, theme, resource_fetcher, log):
     """
     host_url = urlsplit(log.theme_url)
     if len(theme('#portal-personaltools li:nth-child(4)')) > 0:
-
-        theme('#portal-logo img').attr('src', host_url[0] + '://' +  host_url[1] +'/++resource++portal/logo-workspace.png')
-        theme('#portal-logo img').attr('width', '803px')
-        theme('#portal-logo img').attr('height', '60px')
         theme('body').addClass('template-workspace')
     else:
         theme('body').addClass('template-portal')
@@ -42,17 +38,6 @@ def add_plone_tabs(content, theme, resource_fetcher, log):
     theme('.level0').append("<li class='plain'><a href='#'>how we work</a></li>")
     theme('.level0').append("<li class='plain'><a href='#'>reference material</a></li>")
 
-def rewrite_links(content, theme, resource_fetcher, log):
-    """
-    Fix links for css imports.
-    """
-    head_styles = content("head style")
-    for stylevalue in head_styles:
-        stylevalue =  stylevalue.text.split('(')[1]
-        stylevalue = stylevalue.split(')')[0]
-        
-        content('head').append('<link rel="stylesheet" type="text/css" href=' \
-                               + stylevalue + ' />')
 
 def enable_text_editor(content, theme, resource_fetcher, log):
     """

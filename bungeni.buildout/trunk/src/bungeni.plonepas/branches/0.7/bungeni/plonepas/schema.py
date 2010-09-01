15ef5bfa6d02801eeada6779d62ff4cbe619234e
mngoka <mngoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-09-01 14:59:43 +0000

    Remove bungeni.plonepas from bungeni.main, no need to include pysqlite in the bungeni build any longer.

---
+++ bungeni.buildout/trunk/src/bungeni.plonepas/branches/0.7/bungeni/plonepas/schema.py
@@ -0,0 +1,3 @@
+from bungeni.models.schema import users
+from bungeni.models.schema import groups
+from bungeni.models.schema import user_group_memberships
--- bungeni.main/trunk/setup.py
+++ bungeni.main/trunk/setup.py
@@ -79,8 +79,6 @@ setup(name='bungeni',
                         'z3c.menu.ready2go',
                         'zc.displayname',
                         'zope.formlib',
-			# the following are used by bungeni.plonepas
-			'pysqlite',
 			],
       entry_points =
 	{

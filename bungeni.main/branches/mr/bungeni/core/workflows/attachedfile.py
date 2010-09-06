870fbe554138622bc228f4034ed0f14f8af84746
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-09-06 07:35:13 +0000

    Branch for mario for listing performance refactor

---
+++ bungeni.main/branches/mr/bungeni/core/workflows/attachedfile.py
@@ -0,0 +1,12 @@
+import zope.securitypolicy.interfaces
+from bungeni.core.workflows import dbutils
+
+class conditions:
+    pass
+
+    
+class actions:
+    @staticmethod
+    def create(info, context):
+       pass
+                
---
+++ bungeni.main/branches/mr/bungeni/models/events.py
@@ -0,0 +1,13 @@
+from zope import component
+from zope.lifecycleevent.interfaces import IObjectCreatedEvent
+
+import domain, utils
+
+@component.adapter(domain.ItemSchedule, IObjectCreatedEvent)
+def create_discussion_for_item_scheduling(context, event):
+    context.discussion = domain.ScheduledItemDiscussion()
+    
+@component.adapter(domain.AgendaItem, IObjectCreatedEvent)
+def set_parliament_id(context, event):
+    parliament =  utils.get_parliament_for_group_id(context.group_id)
+    context.parliament_id = parliament.group_id

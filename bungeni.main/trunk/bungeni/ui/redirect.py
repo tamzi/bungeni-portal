a35448bc1a8dd5744d837562eb5a3b873f6ae410
mario.ruggier <mario.ruggier@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-09-14 10:20:26 +0000

    Code cleanup: moved "redirect"-related code from ui/container.py to ui/redirect.py.

--- bungeni.main/trunk/bungeni/ui/redirect.py
+++ bungeni.main/trunk/bungeni/ui/redirect.py
@@ -1,11 +1,16 @@
+log = __import__("logging").getLogger("bungeni.ui.redirect")
+
 import datetime
 
 from zope.publisher.browser import BrowserView
-from zope.interface import implements
+from zope import interface
+from zope import component
 from zope.publisher.interfaces import IPublishTraverse
+from zope.annotation.interfaces import IAnnotations
 
 import bungeni.core.globalsettings as prefs
-from bungeni.ui.utils import url as ui_url
+from bungeni.ui.utils import url
+
 
 class RedirectToCurrent(BrowserView):
     """Redirect to current.
@@ -14,7 +19,7 @@ class RedirectToCurrent(BrowserView):
     and you will be redirected to the apropriate container
     
     """
-    implements(IPublishTraverse)
+    interface.implements(IPublishTraverse)
     
     def __init__(self, context, request):
         self.context = context
@@ -30,7 +35,7 @@ class RedirectToCurrent(BrowserView):
         """redirect to container"""
         #context = proxy.removeSecurityProxy( self.context )
         response = self.request.response
-        rooturl = ui_url.absoluteURL(self.context, self.request)
+        root_url = url.absoluteURL(self.context, self.request)
         #response.setHeader('Content-Type', 'application/octect-stream')
         #if len(self.traverse_subpath) != 1:
         #    return
@@ -39,14 +44,60 @@ class RedirectToCurrent(BrowserView):
         if 'date' not in self.request.form:
             qstr = "%s&date=%s" % (qstr,
                     datetime.date.strftime(datetime.date.today(),'%Y-%m-%d'))
-        url = rooturl + '/parliament/'
+        to_url = root_url + '/parliament/'
         if len(self.traverse_subpath) >= 1:
             # we have a traversal to redirect to
             if self.traverse_subpath[0] == 'parliament':
-                url = "%s/parliament/obj-%s/%s?%s" % (
-                        rooturl,
+                to_url = "%s/parliament/obj-%s/%s?%s" % (
+                        root_url,
                         self.currParliament,
                         '/'.join(self.traverse_subpath[1:]),
                         qstr)
-        return response.redirect(url)
+        return response.redirect(to_url)
+
+
+class WorkspaceRootRedirect(BrowserView):
+    """Redirect to the the "pi" view of the user's *first* workspace OR for the 
+    case on no workspaces, to "/workspace".
+    """
+    def __call__(self):
+        request = self.request
+        try: 
+            first_workspace = IAnnotations(request)["layer_data"].workspaces[0]
+            to_url = "/workspace/obj-%s/pi" % first_workspace.group_id
+        except:
+            to_url = "/workspace"
+        # !+TRAILING_SLASH(mr, sep-2010) this is still needed?
+        to_url = url.set_url_context(to_url)
+        if url.get_destination_url_path(request) != to_url:
+            # never redirect to same destination!
+            log.warn("WorkspaceRootRedirect %s -> %s" % (request.getURL(), to_url))
+            request.response.redirect(to_url)
+        else:
+            # !+
+            # user has no workspaces and is requesting /workspace view
+            # return the "no workspace" *rendered* view for /workspace
+            return component.getMultiAdapter(
+                        (self.context, request), name="no-workspace-index")()
+
+class _IndexRedirect(BrowserView):
+    """Redirect to the named "index" view."""
+    index_name = "index"
+    def __call__(self):
+        request = self.request
+        log.warn("%s: %s -> %s" % (
+            self.__class__.__name__, request.getURL(), self.index_name))
+        request.response.redirect(self.index_name)
+class WorkspaceContainerIndexRedirect(_IndexRedirect):
+    # !+TRAILING_SLASH(mr, sep-2010) this is still needed?
+    index_name = url.set_url_context("pi")
+class BusinessIndexRedirect(_IndexRedirect):
+    index_name = "whats-on"
+class MembersIndexRedirect(_IndexRedirect):
+    index_name = "current"
+class ArchiveIndexRedirect(_IndexRedirect):
+    index_name = "browse"
+class AdminIndexRedirect(_IndexRedirect):
+    index_name = "content"
+
 

3d8ae382f1a4474c504e255aa3c85e115238302a
mario.ruggier <mario.ruggier@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-09-15 10:13:03 +0000

    Some src code cleanup (in passing, while reviewing date filtering code).

--- bungeni.main/trunk/bungeni/ui/viewlets/archive.py
+++ bungeni.main/trunk/bungeni/ui/viewlets/archive.py
@@ -9,10 +9,9 @@ from zope.app.pagetemplate import ViewPageTemplateFile
 from bungeni.models.interfaces import IParliament
 
 from bungeni.ui.i18n import _
-from bungeni.ui.cookies import get_date_range
-from bungeni.ui.cookies import set_date_range
-from bungeni.ui.cookies import unset_date_range
+from bungeni.ui import cookies
 from bungeni.ui.widgets import TextDateWidget as DateWidget
+
 
 class ArchiveDatesForm(form.PageForm):
 
@@ -35,98 +34,98 @@ class ArchiveDatesForm(form.PageForm):
             vocabulary="bungeni.vocabulary.ParliamentSource",
             required=False)
 
-    template = NamedTemplate('alchemist.subform')
+    template = NamedTemplate("alchemist.subform")
     form_fields = form.Fields(IDateRangeSchema, render_context=True)
-    form_fields['range_start_date'].custom_widget = DateWidget
-    form_fields['range_end_date'].custom_widget = DateWidget
+    form_fields["range_start_date"].custom_widget = DateWidget
+    form_fields["range_end_date"].custom_widget = DateWidget
     form_description = _(u"Filter the archive by date range")
 
     def is_in_parliament(self, context):
         parent = context
         while not IParliament.providedBy(parent):
-            parent = getattr(parent, '__parent__', None)
+            parent = getattr(parent, "__parent__", None)
             if parent is None:
                 return False
         return True
             
     def get_start_end_restictions(self, context):
         parent = context
-        while not hasattr(parent,'start_date'):
-            parent = getattr(parent, '__parent__', None)
+        while not hasattr(parent, "start_date"):
+            parent = getattr(parent, "__parent__", None)
             if parent is None:
                 return None, None
-        return getattr(parent, 'start_date', None), getattr(parent, 'end_date', None)
+        return getattr(parent, "start_date", None), getattr(parent, "end_date", None)
 
     def setUpWidgets(self, ignore_request=False, cookie=None):
         if ignore_request is False:
-            start_date, end_date = get_date_range(self.request)
+            start_date, end_date = cookies.get_date_range(self.request)
         else:
             start_date = end_date = None
 
         context = type("context", (), {
-            'range_start_date': start_date,
-            'range_end_date': end_date,
-            'parliament': None})
+            "range_start_date": start_date,
+            "range_end_date": end_date,
+            "parliament": None})
 
         self.adapters = {
             self.IDateRangeSchema: context,
             }
         if self.is_in_parliament(self.context):
-            self.form_fields = self.form_fields.omit('parliament')
+            self.form_fields = self.form_fields.omit("parliament")
             
         self.widgets = form.setUpWidgets(
             self.form_fields, self.prefix, self.context, self.request,
             form=self, adapters=self.adapters, ignore_request=True)
         try:
-            self.widgets['parliament']._messageNoValue = _(
+            self.widgets["parliament"]._messageNoValue = _(
                 u"parliament...")
         except KeyError:
             pass
         start, end = self.get_start_end_restictions(self.context)
-        self.widgets['range_start_date'].set_min_date(start)
-        self.widgets['range_end_date'].set_min_date(start)
-        self.widgets['range_start_date'].set_max_date(end)
-        self.widgets['range_end_date'].set_max_date(end)
-        
-        
+        self.widgets["range_start_date"].set_min_date(start)
+        self.widgets["range_end_date"].set_min_date(start)
+        self.widgets["range_start_date"].set_max_date(end)
+        self.widgets["range_end_date"].set_max_date(end)
+    
     @form.action(_(u"Filter"))
     def handle_filter(self, action, data):
-        start_date = data.get('range_start_date')
-        end_date = data.get('range_end_date')
+        start_date = data.get("range_start_date")
+        end_date = data.get("range_end_date")
         params = {}
 
         if start_date and end_date:
             if start_date > end_date:
                 self.status = _("Invalid Date Range")
-                unset_date_range(self.request)
+                cookies.unset_date_range(self.request)
                 return
         start, end = self.get_start_end_restictions(self.context)
         if start_date and end:
             if start_date > end:
                   self.status = (_("Start date must be before %s") %
                         end.strftime("%d %B %Y"))
-                  unset_date_range(self.request)
+                  cookies.unset_date_range(self.request)
                   return
         if end_date and start:
             if end_date < start:
                   self.status = (_("End date must be after %s") %
                         start.strftime("%d %B %Y"))
-                  unset_date_range(self.request)
+                  cookies.unset_date_range(self.request)
                   return
                                 
-        set_date_range(self.request, start_date, end_date)
+        cookies.set_date_range(self.request, start_date, end_date)
         self.request.response.redirect(
             "?portal_status_message=%s" % translate(
                 _(u"Date range set")))
 
     @form.action(_(u"Clear"))
     def handle_clear(self, action, data):
-        unset_date_range(self.request)
+        cookies.unset_date_range(self.request)
         
         self.request.response.redirect(
             "?portal_status_message=%s" % translate(
                 _(u"Date range cleared")))
-        
+
+
 class ArchiveDatesViewlet(object):
     """Viewlet to allow users to choose start- and end-dates to frame
     a search into the archive.
@@ -134,15 +133,14 @@ class ArchiveDatesViewlet(object):
     In effect, parameters ``start_date`` and ``end_date`` will be
     set as a cookie.
     """
-    
     render = ViewPageTemplateFile("templates/archive-dates.pt")
 
     def update(self):
         self.form = ArchiveDatesForm(self.context, self.request)
-        start_date, end_date = get_date_range(self.request)
+        start_date, end_date = cookies.get_date_range(self.request)
         if start_date or end_date:
-            self.filter_on = 'dates-filtered'
+            self.filter_on = "dates-filtered"
         else:
-            self.filter_on = ''
+            self.filter_on = ""
 
             

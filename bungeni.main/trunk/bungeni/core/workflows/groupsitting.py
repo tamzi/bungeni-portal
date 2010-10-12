09095e2404ca5547f580ea0e90817e8ea5b517b8
mario.ruggier <mario.ruggier@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-10-12 13:24:53 +0000

    Added invalidation of caches of groupsitting json listings on workflow transition towards or away from a public state. Added "transition" as a valid action to invalidate caches. Renamed draft_minutes workflow action, to be consistent with other action names.

--- bungeni.main/trunk/bungeni/core/workflows/groupsitting.py
+++ bungeni.main/trunk/bungeni/core/workflows/groupsitting.py
@@ -1,16 +1,31 @@
-from bungeni.core.workflows import utils, dbutils
+
 from zope.security.proxy import removeSecurityProxy
 
+from bungeni.core.workflows import utils, dbutils
+
+
 class actions:
+
+    @staticmethod
+    def invalidate_caches(info, context):
+        """GroupSitting JSON Listing caches should be invalidated for 
+        any transition towards or away from a "public" state.
+        """
+        # we import here because module load time the setup of various
+        # GroupSitting-related classes (descriptor, ...) is not yet complete
+        from bungeni.ui import container
+        container.invalidate_caches_for("GroupSitting", "transition")
     
     @staticmethod
-    def draftminutes(info, context):
+    def draft_minutes(info, context):
         dbutils.set_real_order(removeSecurityProxy(context))
-    
+        actions.invalidate_caches(info, context)
+        
     @staticmethod
     def publish_agenda(info, context):
         utils.schedule_sitting_items(info, context)
-
+        actions.invalidate_caches(info, context)
+        
 class conditions:
     
     @staticmethod

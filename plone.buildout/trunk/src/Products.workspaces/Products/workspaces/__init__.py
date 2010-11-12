bae5903160cc736bb301d30deda9458434be6fbb
mngoka <mngoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-11-12 16:54:22 +0000

    Updates to member workspace functionality - member spaces are now created as Cynin ContentSpaces.

--- plone.buildout/trunk/src/Products.workspaces/Products/workspaces/__init__.py
+++ plone.buildout/trunk/src/Products.workspaces/Products/workspaces/__init__.py
@@ -1,7 +1,8 @@
 #register skins directory
 from Products.CMFCore.DirectoryView import registerDirectory
+from AccessControl import allow_module
 GLOBALS = globals()
 registerDirectory('skins', GLOBALS)
 
 def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
+    """Initializer called when used as a Zope 2 product."""   

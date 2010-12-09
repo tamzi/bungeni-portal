82c9e3bdba18e1a642210ad97a3f62723ec3476d
mario.ruggier <mario.ruggier@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2010-12-09 09:48:31 +0000

    Localization of Bungeni Forms: numerous refinements to integrity checking, error messages, memory usage. Minor corrections to wiki page.

--- bungeni.main/trunk/bungeni/alchemist/model.py
+++ bungeni.main/trunk/bungeni/alchemist/model.py
@@ -65,37 +65,67 @@ component.getGlobalSiteManager().registerAdapter(queryModelDescriptor,
 
 #
 
-def validated_set(key, allowed_values, str_or_seq):
-    """ (str_or_seq:either(str, sequence), allowed_values:sequence(str) 
-            ) -> tuple
+class interned(object):
+    """Re-use of same immutable instances."""
+    _tuples = {}
+    @classmethod
+    def tuple(cls, seq):
+        key = intern("".join([ str(s) for s in seq ]))
+        if not cls._tuples.has_key(key):
+           cls._tuples[key] = tuple(seq)
+        return cls._tuples[key]
+
+def validated_set(key, allowed_values, str_or_seq, nullable=False):
+    """ (key:str, # the kind of values e.g. "modes" or "roles"
+         allowed_values:sequence(str), # full list of valid values
+         str_or_seq:either(str, sequence), # whitespace separated str or seq
+         nullable:bool # whether to leave a None str_or_seq as is
+        ) -> tuple
     
-    We return a tuple becuase tuples are immutable AND python automatically
-    optimizes by interning and automatically re-using equivalent instances. !+?
+    We return a tuple becuase tuples are immutable and we can thus safely and
+    easily reuse same instances (to reduce application runtime memory usage 
+    for these numerous and repetitive memory-resident instances).
     """
     if str_or_seq is None:
+        if nullable:
+            # we keep None as value, letting the appropriate default value 
+            # to be determined downstream
+            return None
         str_or_seq = allowed_values
     if isinstance(str_or_seq, basestring):
-        # str, convert to a list removing any additional whitespace
-        values = sorted(set(str_or_seq.split()))
+        # convert to a list, removing any additional whitespace
+        seq = str_or_seq.split()
     else: 
-        # sequence (should be)
-        values = sorted(set(str_or_seq))
-    for value in values:
-        assert value in allowed_values, """Invalid "%s" [%s]""" % (key, value)
-    return tuple(values)
+        # assume we already have a sequence
+        seq = str_or_seq
+    # remove duplicates, and ensure a standard ordering
+    seq = sorted(set(seq))
+    for value in seq:
+        assert value in allowed_values, \
+            """Invalid "%s" item: %s. Must be one of: %s.""" % (
+                key, value, tuple(allowed_values))
+    return interned.tuple(seq)
 
 #
 
 class show(object):
+    __slots__ = ["modes", "roles", "_from_hide"]
     def __init__(self, modes=None, roles=None):
         self.modes = Field.validated_modes(modes)
         self.roles = Field.validated_roles(roles)
+        self._from_hide = False
     def __str__(self):
-        return "<show(modes=%s, roles=%s)>" % (self.modes, self.roles)
+        if not self._from_hide:
+            return "<show(modes=%s, roles=%s)>" % (self.modes, self.roles)
+        else:
+            return "<hide(modes=%s, roles=%s)>" % (self.modes, 
+                tuple(Field._roles.difference(self.roles)))
 
 def hide(modes=None, roles=None):
     roles = Field.validated_roles(roles)
-    return show(modes, list(Field._roles.difference(roles)))
+    s = show(modes, list(Field._roles.difference(roles)))
+    s._from_hide = True
+    return s
 
 #
 
@@ -154,8 +184,8 @@ class Field(object):
     # A field in a descriptor must be displayable in at least one of these modes
     _modes = set(["view", "edit", "add", "listing", "search"])
     @classmethod 
-    def validated_modes(cls, modes):
-        return validated_set("modes", cls._modes, modes)
+    def validated_modes(cls, modes, nullable=True):
+        return validated_set("modes", cls._modes, modes, nullable=nullable)
     
     # The set of roles exposed to localization
     _roles = set([
@@ -189,7 +219,7 @@ class Field(object):
     # the default list of show/hide localization directives -- by default
     # a field is NOT localizable in any mode and for any role.
     localizable = []
-    _localizable_modes = None # set, to cache list of localizable modes
+    _localizable_modes = None # to cache the set of localizable modes
     
     property = None # zope.schema.interfaces.IField
     
@@ -204,7 +234,7 @@ class Field(object):
     edit_permission = "zope.ManageContent"  # str
     # !+FIELD_PERMISSIONS(mr, nov-2010) these attributes are left here because 
     # alchemist.catalyst.domain.ApplySecurity accesses them directly (even if
-    # resulting permission is what is anyway what is specified in domain.zcml).
+    # resulting permission is anyway what is specified in domain.zcml).
     
     
     # OTHER Attributes (and Defaults)
@@ -241,9 +271,9 @@ class Field(object):
         # be useful for when a descriptor field does not correspond directly 
         # to a domain class attribute (to be determined). 
     ):
-        """The defaults of each init paremeter is set as a class attribute --
+        """The defaults of each init parameter is set as a class attribute --
         if not explicitly specified, then an attribute on the instance is
-        NOT set.
+        NOT set (falling back on the value held in the class attribute).
         
         CONVENTION: not specifying a parameter or specifying it as None
         are interpreted to be equivalent.
@@ -267,7 +297,7 @@ class Field(object):
         self.modes = self.validated_modes(self.modes)
         # Ensure that a field is included in a descriptor only when it is 
         # relevant to the UI i.e. it is displayed in at least one mode -- 
-        # this obsoletes/replaces the previous concept of descripor "omit". 
+        # this obsoletes/replaces the previous concept of descriptor "omit". 
         # Apparently there has been a security-related issue as motivation 
         # for the previous "omit" concept -- as per this discussion:
         # http://groups.google.com/group/bungeni-dev/browse_thread/thread/7f7831b32e798708
@@ -286,8 +316,12 @@ class Field(object):
         self.validate_localizable()
     
     def validate_localizable(self):
-        self._localizable_modes = set() # reset
+        self._localizable_modes = set() # reset cache of localizable modes
         for show_directive in self.localizable:
+            # if modes is still None, we default to instance's modes
+            if show_directive.modes is None:
+                show_directive.modes = self.modes
+            # build list of localizable modes
             count = len(self._localizable_modes)
             self._localizable_modes.update(show_directive.modes)
             assert (count + len(show_directive.modes) == 
@@ -295,9 +329,23 @@ class Field(object):
                 "Field [%s] duplicates mode in localizable directive: %s" % (
                     self.name, show_directive)
         for mode in self._localizable_modes:
-            assert mode in self._modes, \
-                "Field [%s] mode [%s] may only localize support modes [%s]" % (
-                    self.name, mode, self.modes)
+            assert mode in self.modes, \
+                "Field [%s] may only localize mode [%s] if mode is " \
+                "displayable i.e. one of: %s." % (self.name, mode, self.modes)
+    
+    def is_displayable(self, mode, user_roles):
+        """Does this field pass localization directives for this mode?
+        """
+        if mode not in self.modes:
+            return False
+        if mode not in self._localizable_modes:
+            return True
+        for show_directive in self.localizable:
+            for role in user_roles:
+                if role in show_directive.roles:
+                    return True
+        return False
+
     
     def get(self, k, default=None):
         return self.__dict__.get(k, default)
@@ -339,19 +387,20 @@ class ModelDescriptor(object):
         return self
     
     def __init__(self):
-        self._fields_by_name = {}
-        for f in self.__class__.fields:
-            self.sanity_check_field(f)
         log.info("Initializing ModelDescriptor: %s" % self)
+        self._fields_by_name = {}
+        self.sanity_check_fields()
     
-    def sanity_check_field(self, f):
-        """Do necessary checks on supplied Field instances.
+    def sanity_check_fields(self):
+        """Do necessary checks on all specified Field instances.
         """
-        name = f["name"]
-        assert name not in self._fields_by_name, \
-            "[%s] Can't have two fields with same name [%s]" % (
-                self.__class__.__name__, name)
-        self._fields_by_name[name] = f
+        self._fields_by_name.clear()
+        for f in self.__class__.fields:
+            name = f["name"]
+            assert name not in self._fields_by_name, \
+                "[%s] Can't have two fields with same name [%s]" % (
+                    self.__class__.__name__, name)
+            self._fields_by_name[name] = f
         # !+DESCRIPTOR_VALIDATION(mr, nov-2010) a descriptor may specify a 
         # field with a name that does not correspond to an attribute on the 
         # model -- this may be useful, but we do not use it, and any such
@@ -363,35 +412,24 @@ class ModelDescriptor(object):
     # they may be redefined in a much simpler way (as well as being faster).
     
     def get(self, name, default=None):
-        print '!+ModelDescriptor.get("%s")' % (name), self
+        #print '!+ModelDescriptor.get("%s")' % (name), self
         return self._fields_by_name.get(name, default)
     
     def keys(self):
-        print "!+ModelDescriptor.keys", self
+        #print "!+ModelDescriptor.keys", self
         return self._fields_by_name.keys()
     
     def values(self):
-        print "!+ModelDescriptor.values", self
+        #print "!+ModelDescriptor.values", self
         return self._fields_by_name.values()
     
     def __getitem__(self, name):
-        print "!+ModelDescriptor.__getitem__", self
+        #print "!+ModelDescriptor.__getitem__", self
         return self._fields_by_name[name]
     
     def __contains__(self, name):
-        print "!+ModelDescriptor.__contains__", self
+        #print "!+ModelDescriptor.__contains__", self
         return name in self._fields_by_name
-
-    def _is_displayable(self, field, mode, user_roles):
-        """Does this field pass localization directives?
-        """
-        if mode not in field._localizable_modes:
-            return True
-        for show_directive in field.localizable:
-            for role in user_roles:
-                if role in show_directive.roles:
-                    return True
-        return False
     
     def get_context_user_roles(self):
         """Get the list of user's roles (including whether admin or not)--this 
@@ -421,8 +459,7 @@ class ModelDescriptor(object):
     def _mode_columns(self, mode):
         user_roles = set(self.get_context_user_roles())
         return [ f for f in self.__class__.fields 
-            if mode in f.modes and 
-                self._is_displayable(f, mode, user_roles) ]
+            if f.is_displayable(mode, user_roles) ]
     
     @property
     def listing_columns(self): # !+listing_column_NAMES(mr, nov-2010) !

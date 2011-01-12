059009ef4f59ec0c16f10b5ddda023f1c2c0f695
Semargl89 <Semargl89@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2011-01-12 12:15:07 +0000

    file storage path in bungeni:fs utility configuration is now relative to the buildout directory

--- bungeni.main/trunk/bungeni/core/filestorage.py
+++ bungeni.main/trunk/bungeni/core/filestorage.py
@@ -13,7 +13,10 @@ class FileStorageUtility(object):
     implements(IFSUtility)
 
     def __init__(self, fs_path):
-        self.fs_path = fs_path
+        # Cobstructing path from the buildout directory
+        cur_dir_path = os.path.dirname(__file__)
+        base_path = os.path.join(cur_dir_path, '../../../../')
+        self.fs_path = os.path.join(base_path, fs_path)
         self.lock = RLock()
         if not os.path.exists(self.fs_path):
             raise ValueError("Unable to find storage path: %s" % self.fs_path)

eb90b26326bc8a9a1132a92ffef76ee691eb2249
mianonjoka <mianonjoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2011-02-22 12:46:01 +0000

    Rewrote workflow actions viewlet to properly use zope.formlib. Removed its dependency on workflow history viewlet that caused it to bomb when the history viewlet was not on a page. added comments to clarify the code. added check for history_viewlet in evoque template for workflow page

--- bungeni.main/trunk/bungeni/ui/workflow.py
+++ bungeni.main/trunk/bungeni/ui/workflow.py
@@ -24,6 +24,7 @@ from bungeni.core import audit
 from bungeni.core import globalsettings
 from bungeni.ui.forms.workflow import bindTransitions
 from bungeni.ui.forms.common import BaseForm
+from bungeni.ui.forms.common import set_widget_errors
 from bungeni.ui.widgets import TextDateTimeWidget
 from bungeni.ui.table import TableFormatter
 from bungeni.ui.menu import get_actions
@@ -99,15 +100,7 @@ class WorkflowHistoryViewlet(viewlet.ViewletBase):
         self.wf_status = wf_state
         self.has_status = has_wfstate
         self.entries = self.getFeedEntries()
-        # min_date_active
-        if len(self.entries):
-            # then use the "date_active" of the most recent entry
-            min_date_active = self.entries[0].date_active
-        else:
-            # then use the current parliament's atart_date
-            min_date_active = globalsettings.get_current_parliament().start_date
-        # remember "min_date_active" on the request
-        IAnnotations(self.request)["min_date_active"] = min_date_active
+        
     
     def render(self):
         columns = self.columns
@@ -129,55 +122,12 @@ class WorkflowHistoryViewlet(viewlet.ViewletBase):
 
     def getFeedEntries(self):
         instance = removeSecurityProxy(self.context)
-        session = Session()
-        mapper = orm.object_mapper(instance)
-        content_id = mapper.primary_key_from_instance(instance)[0]
-        changes = session.query(self._change_object) \
-                         .filter(
-                            rdb.and_(self._change_object.content_id==content_id,
-                            self._change_object.action=="workflow")) \
-                         .order_by(desc(self._change_object.change_id)) \
-                         .all()
-        return changes
-
+        return [change for change in instance.changes if change.action=='workflow']
+        
 
 class WorkflowActionViewlet(browser.BungeniBrowserView, 
         BaseForm, viewlet.ViewletBase):
     """Display workflow status and actions."""
-    
-    class IWorkflowComment(zope.interface.Interface):
-        note = zope.schema.Text(
-            title=_("Comment on workflow change"), required=False)
-        date_active = zope.schema.Datetime(
-            title=_("Active Date"), required=True)
-        
-        @zope.interface.invariant
-        def valid_date_active(comment):
-            request = common.get_request()
-            # recover min_date_active, and adjust it to be 59 secs earlier to
-            # avoid issues of doing 2 transitions in quick succession (within 
-            # the same minute) the 2nd of which could be taken to be too old...
-            min_date_active = (IAnnotations(request)["min_date_active"] - 
-                                                        timedelta(seconds=59))
-            if not hasattr(comment, "date_active"):
-                # !+ because of a BUG in the datetime widget (probably) :
-                # after a server restart, resubmitting a previously loaded 
-                # form -- that displays valid data_active value results in a
-                # form.NoDataInput("date_active") error... thus causing:
-                # (comment.date_active<min_date_active) to be False !
-                raise zope.interface.Invalid(_("NoDataInput for Active Date."))
-            elif comment.date_active < min_date_active:
-                raise zope.interface.Invalid(_("Active Date is too old."))
-            elif comment.date_active > datetime.now():
-                raise zope.interface.Invalid(_("Active Date is in the future."))
-    
-    class WorkflowComment(object):
-        note = u""
-        date_active = None
-    
-    form_name = "Workflow"
-    form_fields = form.Fields(IWorkflowComment)  # [form.FormField]
-    actions = ()
     
     # evoque
     render = z3evoque.ViewTemplateFile("form.html#form")
@@ -185,50 +135,63 @@ class WorkflowActionViewlet(browser.BungeniBrowserView,
     # zpt
     #render = ViewPageTemplateFile("templates/viewlet.pt")
     
-    def update(self, transition=None):
-        self.adapters = {
-            self.IWorkflowComment: self.WorkflowComment(),
-        }
-        wf = interfaces.IWorkflow(self.context) 
+    class IWorkflowForm(zope.interface.Interface):
+        note = zope.schema.Text(
+            title=_("Comment on workflow change"), required=False)
+        date_active = zope.schema.Datetime(title=_("Active Date"), 
+                                           required=True,)
+    form_name = "Workflow"
+    form_fields = form.Fields(IWorkflowForm)
+    note_widget = TextAreaWidget
+    note_widget.height = 1
+    form_fields["note"].custom_widget = note_widget
+    form_fields["date_active"].custom_widget = TextDateTimeWidget
+    actions = ()
+    
+    def validate(self, action, data):
+        min_date_active = None
+        errors = super(WorkflowActionViewlet, self).validate(action,data)
+        if 'date_active' in data.keys():
+            if IAuditable.providedBy(self.context):
+                instance = removeSecurityProxy(self.context)
+                changes = [change for change in instance.changes 
+                                                   if change.action=='workflow']
+                if changes:
+                    min_date_active = changes[-1].date_active
+                else:
+                    min_date_active = None
+            if min_date_active and (data.get('date_active') < min_date_active):
+                errors.append(zope.interface.Invalid(_(u"Active Date is too old.")))
+            elif min_date_active and (data.get('date_active') > datetime.now()):
+                errors.append(zope.interface.Invalid(_(u"Active Date is in the future.")))                
+        return errors
         
+    def setUpWidgets(self, ignore_request=False):
+        class WorkflowForm:
+            note = None
+            date_active = None
+        self.adapters = {
+            self.IWorkflowForm: WorkflowForm,
+        }
+        self.widgets = form.setUpDataWidgets(
+            self.form_fields, self.prefix, self.context, self.request,
+            ignore_request = ignore_request)     
+        
+    
+    def update(self, transition=None):
+        wf = interfaces.IWorkflow(self.context) 
         if transition is not None:
             state_transition = wf.getTransitionById(transition)
             self.status = _(
                 u"Confirmation required for workflow transition: '${title}'",
                 mapping={"title": _(state_transition.title)})
-        
         self.setupActions(transition)
+        if not self.actions: 
+            self.form_fields = self.form_fields.omit("note", "date_active")
+        elif not IAuditable.providedBy(self.context):
+            self.form_fields = self.form_fields.omit("note", "date_active")
         super(WorkflowActionViewlet, self).update()
         
-        # after we transition we have different actions
-        self.setupActions(transition)
-        # only display the notes field to comment if there is an action
-        # and a log table
-        auditor = audit.getAuditor(self.context)
-        if len(self.actions)==0: 
-            self.form_fields = self.form_fields.omit("note", "date_active")
-        elif auditor is None:
-            self.form_fields = self.form_fields.omit("note", "date_active")
-        else:
-            # note widget
-            note_widget = TextAreaWidget
-            note_widget.height = 1
-            self.form_fields["note"].custom_widget = note_widget
-            # date_active widget
-            self.form_fields["date_active"].custom_widget = TextDateTimeWidget
-            # !+ for "past data entry" mode, the default "date_active" value
-            # should be gotten from a "pseudo_current_date" service utility
-        self.setUpWidgets()
-        # update form status in case of any errors
-        # !+ follow the "bungeni descriptor schema_invariants" way of doing 
-        # this, i.e. displaying widget-specific errors next to each widget
-        if self.errors:
-            if self.status is None: 
-                self.status = _("Errors")
-            self.status = "%s: %s " % (self.status, 
-                            " / ".join([ e.message #or e.__class__.__name__ 
-                                         for e in self.errors ]))
-    
     def setupActions(self, transition):
         self.wf = interfaces.IWorkflowInfo(self.context)
         if transition is None:
@@ -238,14 +201,12 @@ class WorkflowActionViewlet(browser.BungeniBrowserView,
         self.actions = bindTransitions(
             self, transitions, None, interfaces.IWorkflow(self.context))
     
-    def setUpWidgets(self, ignore_request=False):
-        # setup widgets in data entry mode not bound to context
-        self.widgets = form.setUpDataWidgets(
-            self.form_fields, self.prefix, self.context, self.request,
-            ignore_request = ignore_request)
+    
     
 class WorkflowView(browser.BungeniBrowserView):
-    
+    """This view is linked to by the "workflow" context action and dislays the 
+    workflow history and the action viewlet with all possible transitions
+    """
     # evoque
     template = z3evoque.PageViewTemplateFile("workflow.html#main")
     
@@ -255,25 +216,18 @@ class WorkflowView(browser.BungeniBrowserView):
     _page_title = "Workflow"
     
     def update(self, transition=None):
+        # set up viewlets; the view is rendered from viewlets for
+        # historic reasons; this may be refactored anytime.
         if IAuditable.providedBy(self.context):
-            # set up viewlets; the view is rendered from viewlets for
-            # historic reasons; this may be refactored anytime.
             self.history_viewlet = WorkflowHistoryViewlet(
                 self.context, self.request, self, None)
             self.history_viewlet.update()
         self.action_viewlet = WorkflowActionViewlet(
             self.context, self.request, self, None)
-        # update viewlets
         self.action_viewlet.update(transition=transition)
     
     def __call__(self):
         self.update()
-        # NOTE: the success_handler for the parent form is at:
-        #   bungeni.ui.forms.workflow.TransitionHandler
-        # To get the form data:
-        #   av = self.action_viewlet
-        #   data = {}
-        #   form.getWidgetsData(av.widgets, av.prefix, data)
         template = self.template()
         return template
         

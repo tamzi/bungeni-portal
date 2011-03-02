97f71a8837f6866e3911d0a2b3730858d5657b94
mario.ruggier <mario.ruggier@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2011-03-02 14:42:18 +0000

    Implementation of form localization processing: - added placeholder package bungeni.ui.descriptor.localize that when loaded will trigger the reading and processing of the form localization file forms/ui.xml, under currently active bungeni_custom. - added module bungeni.ui.descriptor.locatization to handle both (a) the reading of forms/ui.xml and applying it back to bungeni descriptors and fields (requirements [C1] and [C2]) (b) the auto-generation of the default forms/ui.xml (requirement [X1]). - added automatically generated default localization file: bungeni_custom/forms/ui.xml - added import of bungeni.core.workflows.adapters to descriptor module, to allow this to be imported and used by standalone scripts; removed some unued imports. - extended bungeni.alchemist.model.Field.validate_localizable() to also handle validation of incoming localizable directives. - Added README contents, as a minimal documentatin of the process of localizing the UI Form Views of a bungeni deployment.

---
+++ bungeni.main/trunk/bungeni/ui/descriptor/localization.py
@@ -0,0 +1,254 @@
+# Bungeni Parliamentary Information System - http://www.bungeni.org/
+# Copyright (C) 2010 - Africa i-Parliaments - http://www.parliaments.info/
+# Licensed under GNU GPL v2 - http://www.gnu.org/licenses/gpl-2.0.txt
+
+"""Localization of Form Schema Descriptors
+
+$Id$
+"""
+log = __import__("logging").getLogger("bungeni.ui.descriptor.localization")
+
+import elementtree.ElementTree
+
+from bungeni.alchemist.model import (
+    #ModelDescriptor, 
+    IModelDescriptor,
+    #queryModelDescriptor,
+    Field,
+    show, hide,
+    norm_sorted, 
+)
+import bungeni.ui.descriptor.descriptor
+from bungeni.utils.capi import capi
+from bungeni.ui.utils import debug
+
+
+# constants 
+
+CUSTOM_PATH = capi.get_path_for("forms", "ui.xml")
+DESCRIPTOR_CLASSNAME_POSTFIX = "Descriptor"
+
+ROLES_DEFAULT = " ".join(Field._roles)
+INDENT = " " * 4
+
+
+# utils
+
+def read_custom():
+    try:
+        return open(CUSTOM_PATH, "r").read().decode("utf-8")
+    except IOError:
+        return '<NO-SUCH-FILE path="%s" />' % (CUSTOM_PATH)
+
+def write_custom(content):
+    open(CUSTOM_PATH, "w").write(content.encode("utf-8"))
+
+def is_descriptor(cls):
+    try:
+        return IModelDescriptor.implementedBy(cls)
+    except TypeError:
+        return False
+    '''
+    try:
+        return issubclass(cls, ModelDescriptor):
+    except TypeError:
+        return False
+    '''
+
+def localizable_model_descriptor_classes(module):
+    """(module:bungeni.ui.descriptor.descriptor) -> 
+        generator[ localizable descriptor class ]
+    """
+    for key in dir(module):
+        cls = getattr(module, key)
+        if is_descriptor(cls):
+            if cls.localizable:
+                yield cls
+
+
+####
+# Localize descriptors from {bungeni_custom}/forms/ui.xml.
+
+def get_localizable_descriptor_class(module, name):
+    descriptor_cls_name = "%s%s" % (name, DESCRIPTOR_CLASSNAME_POSTFIX)
+    assert hasattr(module, descriptor_cls_name), \
+        "Unknown descriptor [%s]" % (descriptor_cls_name)
+    cls = getattr(module, descriptor_cls_name) # raises AttributeError
+    assert is_descriptor(cls), \
+        "Invalid descriptor [%s]" % (descriptor_cls_name)
+    assert cls.localizable, \
+        "May not localize a non-localizable descriptor [%s]" % (
+            descriptor_cls_name)
+    return cls
+
+def reorder_field(cls, ordered_field_names, field_by_name):
+    """Reorder descriptor class fields.
+    """
+    log.debug("reorder_fields [%s] %s" % (cls.__name__, ordered_field_names))
+    # validate
+    assert len(cls.fields) == len(ordered_field_names), \
+        "Localization of [%s]: Field list does not match:\nB: %s\nC: %s" % (
+            cls.__name__, [ f.name for f in cls.fields ], ordered_field_names )
+    for f in cls.fields:
+        assert f.name in ordered_field_names, \
+            "Localization of [%s]: Unspecified order for field [%s]" % (
+                cls.__name__, f.name)
+        field_by_name[f.name] = f
+    # reorder (retaining same class atribute list instance)
+    cls.fields[:] = [ field_by_name[name] for name in ordered_field_names ]
+
+
+def localization_errorable(f):
+    """Intercept all errors raised by function f and raise all
+    as LocalizationErrors.
+    """
+    class LocalizationError(Exception):
+        """A Localization Error.
+        """
+    def _errorable(*args, **kw):
+        try: 
+            return f(*args, **kw)
+        except Exception, e: 
+            raise LocalizationError("%s" % (e))
+    return _errorable
+
+@localization_errorable
+def localize_descriptors():
+    """Localizes descriptors from {bungeni_custom}/forms/ui.xml.
+    Called when .localize is imported.
+    """
+    import descriptor as descriptor_module
+    #for d in localizable_model_descriptor_classes(descriptor_module): ...
+    xml = elementtree.ElementTree.fromstring(read_custom())
+    for descriptor_elem in xml.findall("descriptor"):
+        dname = descriptor_elem.get("name")
+        cls = get_localizable_descriptor_class(descriptor_module, dname)
+        field_elems = descriptor_elem.findall("field")
+        field_by_name = {}
+        reorder_field(cls, [f.get("name") for f in field_elems], field_by_name)
+        for f_elem in field_elems:
+            fname = f_elem.get("name")
+            f = field_by_name[fname]
+            reference_localizable_modes = [ bm for bm in f._localizable_modes ]
+            clocs = []
+            for cloc_elem in f_elem.getchildren():
+                modes = cloc_elem.get("modes", None)
+                roles = cloc_elem.get("roles", None) # ROLES_DEFAULT
+                if cloc_elem.tag == "show":
+                    clocs.append(show(modes=modes, roles=roles))
+                elif cloc_elem.tag == "hide":
+                    clocs.append(hide(modes=modes, roles=roles))
+                else:
+                    assert False, "Unknown directive [%s/%s] %s" % (
+                        dname, fname, cloc_elem.tag)
+            if clocs:
+                f.localizable[:] = clocs
+                f.validate_localizable(
+                    reference_localizable_modes=reference_localizable_modes)
+
+
+#####
+# Generation of the default localization file
+
+def process_loc(loc, depth=3, localizable_modes=[]):
+    """(loc:show/hide directive) -> [str]
+    """
+    map = loc._repr_map()
+    localizable_modes.extend(map["modes"])
+    tag = map["tag"]
+    modes = " ".join(map["modes"])
+    roles = " ".join(map["roles"])
+    ind = INDENT * depth
+    acc = []
+    if roles == ROLES_DEFAULT:
+        # ALL targetted roles, no need to write them out, implied by no roles
+        acc.append("""%s<%s modes="%s" />""" % (ind, tag, modes))
+    else:
+        acc.append("""%s<%s modes="%s" roles="%s" />""" % (
+            ind, tag, modes, roles))
+    return acc
+
+def process_field(f, depth=2):
+    """(f:Field) -> [str]
+    """
+    _acc = []
+    field_localizable_modes = []
+    for loc in f.localizable:
+        _acc.extend(process_loc(loc, depth+1, field_localizable_modes))
+    localizable_modes = " ".join(
+        norm_sorted(field_localizable_modes, Field._modes))
+    display_modes = " ".join(f.modes)
+    
+    acc = []
+    ind = INDENT * depth
+    if _acc:
+        if localizable_modes:
+            acc.append('%s<field name="%s" modes="%s" localizable="%s">' % (
+                ind, f.name, display_modes, localizable_modes))
+        else:
+            acc.append('%s<field name="%s" modes="%s">' % (
+                ind, f.name, display_modes))
+        acc.extend(_acc)
+        acc.append('%s</field>' % (ind))
+    else:
+        if localizable_modes:
+            acc.append('%s<field name="%s" modes="%s" localizable="%s" />' % (
+                ind, f.name, display_modes, localizable_modes))
+        else:
+            acc.append('%s<field name="%s" modes="%s" />' % (
+                ind, f.name, display_modes))
+    return acc
+
+def process_cls(cls, depth=1):
+    """(cls:ModelDescriptor) -> [str]
+    
+    Assumption: cls (subclass of ModelDescriptor) is localizable.
+    """
+    assert cls.__name__.endswith(DESCRIPTOR_CLASSNAME_POSTFIX)
+    name = cls.__name__[0:-len(DESCRIPTOR_CLASSNAME_POSTFIX)]
+    
+    _acc = []
+    for f in cls.fields:
+        _acc.extend(process_field(f, depth+1))
+    
+    acc = []
+    ind = INDENT * depth
+    acc.append("")
+    if _acc:
+        acc.append('%s<descriptor name="%s">' % (ind, name))
+        acc.extend(_acc)
+        acc.append('%s</descriptor>' % (ind))
+    else:
+        acc.append('%s<descriptor name="%s" />' % (ind, name))
+    return acc
+
+def process_module(module, depth=0):
+    """(module:bungeni.ui.descriptor.descriptor) -> [str]
+    """
+    _acc = []
+    for dcls in localizable_model_descriptor_classes(module):
+        _acc.extend(process_cls(dcls, depth=1))
+    
+    acc = []
+    ind = INDENT * depth
+    if _acc:
+        acc.append('%s<ui roles="%s">' % (ind, ROLES_DEFAULT))
+        acc.extend(_acc)
+        acc.append("")
+        acc.append("%s</ui>" % (ind))
+    else:
+        acc.append('%s<ui roles="%s" />' % (ind, ROLES_DEFAULT))
+    return acc
+
+
+if __name__ == "__main__":
+    
+    print "Processing localization file: %s" % (CUSTOM_PATH)
+    persisted = read_custom()
+    regenerated = "\n".join(process_module(bungeni.ui.descriptor.descriptor))
+    if persisted != regenerated:
+        print "*** OVERWRITING:"
+        print debug.unified_diff(persisted, regenerated, CUSTOM_PATH, "NEW")
+        write_custom(regenerated)
+    
+
---
+++ bungeni.main/trunk/bungeni/ui/descriptor/localize/__init__.py
@@ -0,0 +1,15 @@
+# Bungeni Parliamentary Information System - http://www.bungeni.org/
+# Copyright (C) 2010 - Africa i-Parliaments - http://www.parliaments.info/
+# Licensed under GNU GPL v2 - http://www.gnu.org/licenses/gpl-2.0.txt
+
+"""Localize Form Schema Descriptors
+
+Import this package to localize descriptors.
+
+$Id$
+"""
+
+from bungeni.ui.descriptor import localization
+
+localization.localize_descriptors()
+

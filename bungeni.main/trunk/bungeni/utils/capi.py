bbdbd81ab5c143cb907f98e4086d6fe8ce868b33
mario.ruggier <mario.ruggier@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2011-03-02 13:23:14 +0000

    Added an initial version of a bungen_custom accessor module, to provide a uniform accessor/validated API, plus any additional utilities, to bungen_custom localization parameters.

---
+++ bungeni.main/trunk/bungeni/utils/capi.py
@@ -0,0 +1,51 @@
+# Bungeni Parliamentary Information System - http://www.bungeni.org/
+# Copyright (C) 2010 - Africa i-Parliaments - http://www.parliaments.info/
+# Licensed under GNU GPL v2 - http://www.gnu.org/licenses/gpl-2.0.txt
+
+"""Accessor API for Bungeni Custom parameters.
+
+Provides uniform access and validation layer, plus related utilities, 
+for using bungeni_custom parameters.
+
+All access to bungeni_custom parameters should go through this module. 
+
+Usage:
+from bungeni.utils.capi import capi
+    ... capi.NAME ...
+
+$Id$
+"""
+__all__ = ["capi"]
+
+log = __import__("logging").getLogger("bungeni.utils.capi")
+
+import os
+import bungeni_custom as bc
+
+
+class CAPI(object):
+    """Accessor class for Bungeni Custom parameters.
+    """
+    
+    # bungeni_custom parameter properties
+    
+    @property
+    def zope_i18n_allowed_languages(self):
+        return tuple(bc.zope_i18n_allowed_languages.split())
+    
+    @property
+    def zope_i18n_compile_mo_files(self):
+        return bool(
+            bc.zope_i18n_compile_mo_files is True or 
+            bc.zope_i18n_compile_mo_files == "1"
+        )
+    
+    # utility methods
+    
+    def get_root_path(self):
+        return os.path.dirname(os.path.abspath(bc.__file__)) 
+
+
+# we access all via the singleton instance
+capi = CAPI()
+

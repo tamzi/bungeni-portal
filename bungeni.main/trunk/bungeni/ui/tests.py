32c8c22e7fe5d210a69685bcb786f6b8033ee746
mario.ruggier <mario.ruggier@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2011-03-07 15:25:58 +0000

    Replacing incorrect fixes, one in r7726 (silences problem of having None descriptor) and other in r7873 (silences newly introduced problem in r7726 of having None jslcache.filter_params). Some source restyling in ui/tests.py.

--- bungeni.main/trunk/bungeni/ui/tests.py
+++ bungeni.main/trunk/bungeni/ui/tests.py
@@ -18,62 +18,61 @@ import forms.test_dates
 
 zcml_slug = """
 <configure xmlns="http://namespaces.zope.org/zope"
-           xmlns:db="http://namespaces.objectrealms.net/rdb"
-           xmlns:bungeni="http://namespaces.bungeni.org/zope"
-           xmlns:i18n="http://namespaces.zope.org/i18n"
-           xmlns:browser="http://namespaces.zope.org/browser"
-           i18n_domain="bungeni">
-
-  <include package="bungeni.alchemist" file="meta.zcml"/>
-  <include package="alchemist.catalyst" file="meta.zcml"/>
-  <include package="zope.component" file="meta.zcml" />
-  <include package="zope.app.component" file="meta.zcml" />
-  <include package="zope.app.publisher" file="meta.zcml" />
-  <!-- Setup Database Connection -->
-  <db:engine
-     name="bungeni-db"
-     url="postgres://localhost/bungeni-test"
-     />
-     
-  <db:bind
-     engine="bungeni-db"
-     metadata="bungeni.models.metadata" />
-
-  <db:bind
-     engine="bungeni-db"
-     metadata="bungeni.alchemist.security.metadata" />
-  <include package="zope.i18n" file="meta.zcml" />
-  <!-- Setup Core Model --> 
-  <include package="bungeni.ui.descriptor" file="catalyst.zcml"/>
-  <include package="bungeni.ui" file="meta.zcml"/>
-  <include package="bungeni_custom" file="openoffice.zcml" />
-  <include package="bungeni.core" file="meta.zcml"/>
-  <bungeni:fs fs_path="fs"/>
+    xmlns:db="http://namespaces.objectrealms.net/rdb"
+    xmlns:bungeni="http://namespaces.bungeni.org/zope"
+    xmlns:i18n="http://namespaces.zope.org/i18n"
+    xmlns:browser="http://namespaces.zope.org/browser"
+    i18n_domain="bungeni">
+    
+    <include package="bungeni.alchemist" file="meta.zcml"/>
+    <include package="alchemist.catalyst" file="meta.zcml"/>
+    <include package="zope.component" file="meta.zcml" />
+    <include package="zope.app.component" file="meta.zcml" />
+    <include package="zope.app.publisher" file="meta.zcml" />
+    <!-- Setup Database Connection -->
+    <db:engine name="bungeni-db"
+        url="postgres://localhost/bungeni-test"
+    />
+    <db:bind engine="bungeni-db"
+        metadata="bungeni.models.metadata"
+    />
+    
+    <db:bind engine="bungeni-db"
+        metadata="bungeni.alchemist.security.metadata"
+    />
+    <include package="zope.i18n" file="meta.zcml" />
+    <!-- Setup Core Model --> 
+    <include package="bungeni.ui.descriptor" />
+    <include package="bungeni.ui" file="meta.zcml" />
+    <include package="bungeni_custom" file="openoffice.zcml" />
+    <include package="bungeni.core" file="meta.zcml" />
+    <bungeni:fs fs_path="fs" />
 </configure>
 """
 
-def setUp( test ):
+def setUp(test):
     placelesssetup.setUp()
-    xmlconfig.string( zcml_slug )
-    metadata.create_all( checkfirst=True )
+    xmlconfig.string(zcml_slug)
+    metadata.create_all(checkfirst=True)
     
-def tearDown( test ):
+def tearDown(test):
     placelesssetup.tearDown()
-    metadata.drop_all( checkfirst=True )
+    metadata.drop_all(checkfirst=True)
 
 def test_suite():
     doctests = (
-                'forms/readme.txt',
-                'report.txt',
-                )
+        "forms/readme.txt",
+        "report.txt",
+    )
 
     docfiles = (
-    "bungeni.ui.forms.forms",
+        "bungeni.ui.forms.forms",
     )
     
     globs = dict(
         interface=interface,
-        component=component)
+        component=component
+    )
     
     test_suites = []
     
@@ -85,7 +84,7 @@ def test_suite():
             globs=globs,
             optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS)
         test_suites.append(test_suite)
-
+    
     for filename in docfiles:
         test_suite = doctestunit.DocTestSuite(
             filename,
@@ -93,11 +92,11 @@ def test_suite():
             tearDown=tearDown,
             optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS)
         test_suites.append(test_suite)
-
+    
     test_suites.append(forms.test_dates.test_suite())
     return unittest.TestSuite( test_suites )
 
-if __name__ == '__main__':
-    unittest.main(defaultTest='test_suite')
 
+if __name__ == "__main__":
+    unittest.main(defaultTest="test_suite")
 

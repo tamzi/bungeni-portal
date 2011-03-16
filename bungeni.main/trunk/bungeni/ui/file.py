f138f4dc2a70ca6064321150ca97109659bd9184
borona.murithi <borona.murithi@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2011-03-16 12:53:02 +0000

    - Adding index view for attached files container - maintains parliamentary item context - Adding menu zcml for attachment editing and downloading - Updating stylesheet with attachment rules - Adding download icon to resources - Updating UI blocking js to disregard download menu action

--- bungeni.main/trunk/bungeni/ui/file.py
+++ bungeni.main/trunk/bungeni/ui/file.py
@@ -1,5 +1,13 @@
-# encoding: utf-8
-# download view to download a file or image
+# Bungeni Parliamentary Information System - http://www.bungeni.org/
+# Copyright (C) 2010 - Africa i-Parliaments - http://www.parliaments.info/
+# Licensed under GNU GPL v2 - http://www.gnu.org/licenses/gpl-2.0.txt
+
+""" Parliamentary object attachment(s) views
+
+$Id$
+$URL$
+"""
+
 from ore.workflow.interfaces import IWorkflowState
 from tempfile import TemporaryFile
 from zope.interface import implements
@@ -7,7 +15,13 @@ from zope.publisher.interfaces import IPublishTraverse
 from zope.publisher.browser import BrowserView
 from zope.security.proxy import removeSecurityProxy
 from zope.security import proxy
-
+from zc.table import column
+import operator
+from bungeni.models.interfaces import IAttachedFile, IAttachedFileVersion, IAttachedFileVersionContainer
+from bungeni.ui.browser import BungeniBrowserView
+from bungeni.ui.i18n import _
+from bungeni.ui.utils import date, url
+from bungeni.ui.table import TableFormatter
 
 class RawView(BrowserView):
     implements(IPublishTraverse)
@@ -63,3 +77,46 @@ class FileDeactivate(BrowserView):
         wf_state_adapter.setState('inactive')
         redirect_url = self.request.getURL().replace('/deactivate', '')
         return self.request.response.redirect(redirect_url)
+
+class FileListingView(BungeniBrowserView):
+
+    _page_title = _(u"Attachments")
+
+    formatter_factory = TableFormatter
+
+    def __init__(self, context, request):
+        super(FileListingView, self).__init__(context.__parent__, request)
+        formatter = date.getLocaleFormatter(self.request, "dateTime", "short")
+        
+        self.columns = [
+            column.GetterColumn(title=_(u"file"),
+                    getter=lambda i,f:'<a href="%s/files/obj-%d">%s</a>' % (
+                            f.url, i.attached_file_id, i.file_title)),
+            column.GetterColumn(title=_(u"status"), 
+                    getter=lambda i,f:i.status),
+            column.GetterColumn(title=_(u"modified"), 
+                    getter=lambda i,f:formatter.format(i.status_date)),
+        ]
+
+    @property
+    def attachments(self):
+        instance = removeSecurityProxy(self.context)
+        attachments = instance.files.values()
+        return list(attachments)
+
+    def hasAttachments(self):
+        return len(self.attachments) > 0
+
+    def listing(self):
+        values = self.attachments
+        values.sort(key=operator.attrgetter("attached_file_id"))
+        values.reverse()
+        formatter = self.formatter_factory(
+            self.context, self.request,
+            values,
+            prefix="results",
+            visible_column_names = [c.name for c in self.columns],
+            columns = self.columns)
+        formatter.url = url.absoluteURL(self.context, self.request)
+        formatter.updateBatching()
+        return formatter()

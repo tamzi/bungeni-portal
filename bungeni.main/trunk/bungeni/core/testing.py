16630420d80afb37e4e21a94316835505eca160e
mario.ruggier <mario.ruggier@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2011-03-18 08:27:36 +0000

    Added core.testing.set_interaction() utility, to facilitate workflow testing code; updated question.txt to use it.

--- bungeni.main/trunk/bungeni/core/testing.py
+++ bungeni.main/trunk/bungeni/core/testing.py
@@ -1,6 +1,7 @@
 import index
 import zope.interface
 import zope.publisher.interfaces
+import zope.security.management
 import zope.security.testing
 from sqlalchemy import create_engine
 from zope import component
@@ -9,28 +10,36 @@ from bungeni.alchemist.interfaces import IDatabaseEngine
 from bungeni import models as model
 from bungeni.models.testing import setup_db
 
+
 def setup_indexer():
     store_dir = index.setupStorageDirectory() + "-test"
-
     # search connection hub
     searcher = index.search.IndexSearch(store_dir)
-
     # async indexer 
     indexer = index.xappy.IndexerConnection(store_dir)
-
     # field definitions
     index.setupFieldDefinitions(indexer)
+
 
 def create_principal(id="manager", title="Manager", groups=()):
     return zope.security.testing.Principal(id, title, groups)
 
+
+#!+ make private to set_interaction
 def create_participation(principal=None):
     if principal is None:
         principal = create_principal()
     participation = zope.security.testing.Participation(principal)
-    
-    zope.interface.alsoProvides(
-        participation, zope.publisher.interfaces.IRequest)
-
+    zope.interface.alsoProvides(participation, 
+        zope.publisher.interfaces.IRequest)
     return participation
+
+def set_interaction(principal):
+    """End current interaction (if any) and, if principal not None, start a 
+    new one with principal participating.
+    """
+    zope.security.management.endInteraction()
+    if principal is not None:
+        zope.security.management.newInteraction(
+            create_participation(principal))
     

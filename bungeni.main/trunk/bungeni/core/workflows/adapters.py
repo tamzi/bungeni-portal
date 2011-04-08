afa01c305bcfee9f3f80380b3a5bd72839c0d8e8
mario.ruggier <mario.ruggier@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2011-04-08 09:51:59 +0000

    Workflow refactoring: renamed adpaters.wf to adapters.get_workflow; some cleanup of adapters source; attempt (failed) to register workflow singletons as named utilities (breaks doctests).

--- bungeni.main/trunk/bungeni/core/workflows/adapters.py
+++ bungeni.main/trunk/bungeni/core/workflows/adapters.py
@@ -1,64 +1,72 @@
 log = __import__("logging").getLogger("bungeni.core.workflows")
 
-from zope.component import provideAdapter
+from zope import component
 from bungeni.models import interfaces
-from bungeni.core import workflow
 from bungeni.core.workflow import xmlimport
+from bungeni.core.workflow.interfaces import IWorkflow, \
+    IStateController, IWorkflowController
+from bungeni.core.workflow.states import StateController, WorkflowController
 import bungeni.core.version
 import bungeni.core.interfaces
-
 from bungeni.utils.capi import capi
-PATH_CUSTOM_WORKLFOWS = capi.get_path_for("workflows")
 
-__all__ = ["WORKFLOWS", "wf"]
+__all__ = ["get_workflow"]
+
+def get_workflow(name):
+    """Get the named workflow utility."""
+    #return component.getUtility(IWorkflow, name) !+BREAKS_DOCTESTS(mr, apr-2011)
+    return _WORKFLOWS[name]
 
 # a global container to easily retrieve workflow instances
-WORKFLOWS = {} # { name: workflow.states.Workflow }
+_WORKFLOWS = {} # { name: workflow.states.Workflow }
 
-def wf(name):
-    """Get the named workflow."""
-    return WORKFLOWS[name]
-
-# provideAdapter(factory, adapts=None, provides=None, name="")
-
+# component.provideAdapter(factory, adapts=None, provides=None, name="")
+def provideUtilityWorkflow(utility, name):
+    #component.provideUtility(utility, IWorkflow, name) !+BREAKS_DOCTESTS
+    _WORKFLOWS[name] = utility
 def provideAdapterWorkflow(factory, adapts_kls):
-    provideAdapter(factory, (adapts_kls,), workflow.interfaces.IWorkflow)
-
+    component.provideAdapter(factory, (adapts_kls,), IWorkflow)
 def provideAdapterStateController(adapts_kls):
-    provideAdapter(workflow.states.StateController, (adapts_kls,), 
-        workflow.interfaces.IStateController)
-
+    component.provideAdapter(StateController, (adapts_kls,), IStateController)
 def provideAdapterWorkflowController(adapts_kls):
-    provideAdapter(workflow.states.WorkflowController, (adapts_kls,), 
-        workflow.interfaces.IWorkflowController)
+    component.provideAdapter(WorkflowController, (adapts_kls,), 
+        IWorkflowController)
+
+PATH_CUSTOM_WORKLFOWS = capi.get_path_for("workflows")
 
 def load_workflow(name, iface):
     """Get the Workflow instance, from XML definition, for named workflow.
     """
-    _log = log.debug
-    global WORKFLOWS
-    if not WORKFLOWS.has_key(name):
-        w = xmlimport.load("%s/%s.xml" % (PATH_CUSTOM_WORKLFOWS, name), name)
-        WORKFLOWS[name] = w
-        _log("Loading WORKFLOW: %s %s" % (name, w))
-        for state_key, state in w.states.items():
-            _log("   STATE: %s %s" % (state_key, state))
+    #
+    # load / register as utility / retrieve
+    #
+    #if not component.queryUtility(IWorkflow, name): !+BREAKS_DOCTESTS
+    if not _WORKFLOWS.has_key(name): 
+        wf = xmlimport.load("%s/%s.xml" % (PATH_CUSTOM_WORKLFOWS, name), name)
+        provideUtilityWorkflow(wf, name)
+        log.debug("Loading WORKFLOW: %s %s" % (name, wf))
+        for state_key, state in wf.states.items():
+            log.debug("   STATE: %s %s" % (state_key, state))
             for p in state.permissions:
-                _log("          %s" % (p,))
+                log.debug("          %s" % (p,))
     else:
-        w = WORKFLOWS[name]
-        _log("Already Loaded WORKFLOW : %s %s" % (name, w))
-    # a Workflow instance is itself the factory of own AdaptedWorkflows
-    provideAdapterWorkflow(w, iface)
+        wf = get_workflow(name)
+        log.debug("Already Loaded WORKFLOW : %s %s" % (name, wf))
+    #
+    # register related adapters
+    #
+    # Workflows are also the factory of own AdaptedWorkflows
+    provideAdapterWorkflow(wf, iface)
+    # StateController adapts all workflow context models
     provideAdapterStateController(iface)
     if name != "version": # !+VERSION_WORKFLOW(mr, apr-2011)
+        # WorkflowController adapts all workflow context models
         provideAdapterWorkflowController(iface)
     else:
-        provideAdapter(bungeni.core.version.ContextVersioned,
+        component.provideAdapter(bungeni.core.version.ContextVersioned,
             (bungeni.core.interfaces.IVersionable,),
             bungeni.core.interfaces.IVersioned)
 
-#
 
 # workflow instances (+ adapter *factories*)
 load_workflow("address", interfaces.IUserAddress)
@@ -78,6 +86,7 @@ load_workflow("report", interfaces.IReport)
 load_workflow("tableddocument", interfaces.ITabledDocument)
 load_workflow("user", interfaces.IBungeniUser)
 load_workflow("version", interfaces.IVersion) # !+VERSION_WORKFLOW(mr, apr-2011)
+
 
 # check/regenerate zcml directives for workflows
 xmlimport.zcml_check_regenerate()

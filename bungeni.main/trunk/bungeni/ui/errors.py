b889647d0b67c463648cfeb4118bc65e6d3a15ae
Borona.Murithi <Borona.Murithi@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2011-05-19 15:39:37 +0000

    Signatories update. See [ issue 703 ]. Unauthorized error view.
    Updated signatories workflow with permissions for Owner.
     => Only owner may see rejected signatures.
    Adding Unauthorized view - in place of blanket "System Error" view.

---
+++ bungeni.main/trunk/bungeni/ui/errors.py
@@ -0,0 +1,31 @@
+# Bungeni Parliamentary Information System - http://www.bungeni.org/
+# Copyright (C) 2010 - Africa i-Parliaments - http://www.parliaments.info/
+# Licensed under GNU GPL v2 - http://www.gnu.org/licenses/gpl-2.0.txt
+
+"""Bungeni Browser Views
+
+$Id:$
+"""
+log = __import__("logging").getLogger("bungeni.ui.errors")
+
+import zope.publisher.browser
+from bungeni.ui.i18n import _
+
+
+
+class BaseErrorView(zope.publisher.browser.BrowserView):
+    
+    @property
+    def page_title(self):
+        return _(u"Bungeni - [Error]")
+    
+    @property
+    def error_message(self):
+        return _(u"An error occured in Bungeni")
+
+
+class Unauthorized(BaseErrorView):
+
+    @property
+    def error_message(self):
+        return _(u"Your account is not authorized to view this item.")

257975e3c5d21ec3dfc1b33643c4169ad1674e8f
mngoka <mngoka@gmail.com>
2011-05-31 22:55:47 +0000

    Updating login and logout links to go via plone, updating workspace links for plone content, adding tab for member and group spaces.

--- bungeni.main/trunk/bungeni/portal/static/themes/rules.py
+++ bungeni.main/trunk/bungeni/portal/static/themes/rules.py
@@ -2,14 +2,17 @@ import urllib2
 from pyquery import PyQuery as pq
 from urlparse import urlsplit
 
+def get_theme_host(log):
+    theme_host = urlsplit(log.theme_url)[1].split("/themes")[0]
+    return theme_host
+
 def add_section_links(content, theme, resource_fetcher, log):
     """
     Add top level class links and logos for the different sections
     (workspace and portal).
     """
     host_url = urlsplit(log.theme_url)
-    if len(theme('#portal-personaltools li:nth-child(4)')) > 0 or \
-           content("#myareaavatarimg") or theme("#user-name"):
+    if "logout" in theme('#portal-personaltools').html():
         theme('body').addClass('template-workspace')
     else:
         theme('body').addClass('template-portal')
@@ -45,23 +48,41 @@ def switch_links_to_cynin(content, theme, resource_fetcher, log):
             obj_id = "/cynin/home/group.political-group." + \
                      link.get("href").split("/", 6)[-1].split("-", 1)[-1]
         else:
-            obj_id = "/cynin/home/" + link.get("href").split("/", 3)[-1]
+            obj_id = "/plone/groups/" + link.get("href").split("/", 3)[-1]
         pq(link).attr('href', obj_id)
         
 def add_member_workspace_links(content, theme, resource_fetcher, log):
     """
-    Add a member's 'private folder' and 'web pages' links to the workspace
-    second level menu items.
+    Add a member's 'private space' and 'web space' links to the workspace
+    second level menu items (if they exist).
     """
-    
     link_items = content("#portal-personaltools span")
     member_id = link_items.pop(0).text
-    content('#portal-personaltools').append("<li class='navigation'>\
-    <a href='/cynin/home/" + member_id + "/private_folder" + "'>private folder\
-    </a></li>")
-    content('#portal-personaltools').append("<li class='navigation'>\
-    <a href='/cynin/home/" + member_id + "/web_pages" + "'>web pages</a></li>")
+    content('#portal-personaltools').append(
+            "<li class='navigation'><a href='/plone/membership/" + member_id +
+            "/private_space/folder_contents" + "'>private space</a></li>")
     
+    host_url = urlsplit(log.theme_url)
+    theme_host = get_theme_host(log).split(":")[0]
+    test_url = "http://" + theme_host + ":8082" + "/plone/membership/" + \
+                member_id + "/web_space"
+
+    try:
+        if urllib2.urlopen(test_url).code == 200:
+            content('#portal-personaltools').append("<li class='navigation'>\
+            <a href='/plone/membership/" + member_id + \
+            "/web_space/folder_contents" + "'>web space</a></li>")
+    except:
+        pass 
+        
+        
+def add_group_workspace_links(content, theme, resource_fetcher, log):
+    """
+    Add a groups 'web space' links to the workspace groups links.
+    """
+    
+    group_table_contents = content("#workspace-table-my_groups")
+
     
 def add_member_public_links(content, theme, resource_fetcher, log):
     """
@@ -69,30 +90,68 @@ def add_member_public_links(content, theme, resource_fetcher, log):
     """
     member_id = content(".MemberOfParliament .User .content-right-column")\
                 .pop(4).text_content()
-    link_value = "/cynin/home/" +  member_id.strip() + "/web_pages"
-
+    link_value = "/plone/membership/" +  member_id.strip() + "/web_space"
+    
+def add_space_tab(content, theme, resource_fetcher, log, space_type):
+    """
+    Add the member's or groups web space as a tab
+    """
+    theme_host = get_theme_host(log)
+    theme_url = "http://" + theme_host
+    if len(theme_url.split(":")) > 1:
+        theme_port = theme_url.split(":")[1]
+        
+    # !+HACK(mn, may-2011) - the plone port is hardcoded. 
+    plone_port = ":8082"    
+    plone_url = theme_url.split(":")[0] + ":" + theme_url.split(":")[1] + \
+                plone_port
+    if space_type == "member":
+        item_id = content('#portal-breadcrumbs a:last-child')
+        space_id = item_id.text().split( )
+        link_val = plone_url + "/plone/membership/" + space_id[1] + "." + \
+                    space_id[2] +"/web_space"        
+    else:
+        item_id = content('#portal-breadcrumbs a:last-child').attr("href")
+        space_id =  item_id.split("-")[-1]
+        if space_type == "committee":
+            link_val = plone_url + "/plone/groups/group.committee." + \
+                        space_id + "web_space"            
+        else:
+            link_val = plone_url + "/plone/groups/group.political-group." + \
+                        space_id + "web_space" 
+    try:
+        if urllib2.urlopen(link_val).code == 200:
+            response = urllib2.urlopen(link_val)
+            html_content = pq(response.read())
+            head_content = html_content("head").html().replace(
+                            plone_port, theme_port)
+            space_tab = html_content("#content-core").html().replace(
+                        plone_port, theme_port)
+            content("head").prepend(head_content)
+            content(".enableFormTabbing").append(
+            "<dt id='fieldsetlegend-web_space'>web space</dt>\
+            <dd id='fieldset-web_space'>" + space_tab + "</dd>")
+            theme(".level0").prepend(content("#portal-globalnav").children())
+    except:
+        pass   
 
 def replace_login_link(content, theme, resource_fetcher, log):
-    """Login and logout will be via cynin to facilitate creation of
-    cynin member spaces.
+    """Login and logout will be via plone to facilitate creation of
+     member  and group spaces.
     """
-    if ("logout" or "Log out") not in theme.html() and \
-           theme("#portal-personaltools").html() != None:
-        login_url = theme("#portal-personaltools li:first-child a").attr("href")
-        if "plone/login_form" in login_url:
-            login_url = login_url.replace("plone", "cynin")
-        else:
-            login_url = login_url.replace("login", "cynin/login_form")
-        pq(theme("#portal-personaltools li:first-child a")).attr("href", login_url)
-    elif ("logout" or "Log out") in theme.html():
-        plone_lnk = theme("#portal-personaltools li a:last-child").attr("href")
-        bungeni_lnk = theme("#portal-personaltools li:nth-child(4) a").attr("href")
-        if plone_lnk:
-            theme("#portal-personaltools li a:last-child").attr(
-                "href", plone_lnk.replace("/plone", "/cynin"))
-        elif bungeni_lnk:
-            pq(theme("#portal-personaltools li:nth-child(4) a")).attr(
-                "href", bungeni_lnk.replace("/logout", "/cynin/logout"))
+    login_link = theme("a[href$='/login']")
+    if len(login_link) > 0:
+        if login_link.attr("href"):
+            login_value = pq(login_link).attr("href").replace(
+                            "login", "plone/login")
+            pq(login_link).attr("href", login_value)
+    
+    logout_link = theme("a[href$='/logout']")
+    if len(logout_link) > 0:
+        if "plone/logout" not in logout_link.attr("href"):
+            logout_value = pq(logout_link).attr("href").replace(
+                            "logout", "plone/logout")
+            pq(logout_link).attr("href", logout_value)
 
 
 def add_menu_items(content, theme, resource_fetcher, log):

4fbc5842698525f7306756715cd507d6e93a98d3
jura.stakhiv <jura.stakhiv@gmail.com>
2011-06-06 13:45:59 +0000

    Branch for alternative routing.

---
+++ bungeni.main/branches/sterch-routing/bungeni/models/resourcebooking.py
@@ -0,0 +1,165 @@
+# Bungeni Parliamentary Information System - http://www.bungeni.org/
+# Copyright (C) 2010 - Africa i-Parliaments - http://www.parliaments.info/
+# Licensed under GNU GPL v2 - http://www.gnu.org/licenses/gpl-2.0.txt
+
+"""Resource Booking
+
+$Id$
+"""
+log = __import__("logging").getLogger("bungeni.models.resourcebooking")
+
+
+import datetime
+
+import sqlalchemy.sql.expression as sql
+import sqlalchemy as rdb
+from sqlalchemy.orm import mapper
+
+from bungeni.alchemist import Session
+from bungeni.models import domain, schema
+
+from i18n import _ 
+
+
+bookedresources = rdb.join(
+    schema.resources, 
+    schema.resourcebookings, 
+    schema.resources.c.resource_id == schema.resourcebookings.c.resource_id
+).join(
+    schema.group_sittings, 
+    schema.resourcebookings.c.group_sitting_id == 
+    schema.group_sittings.c.group_sitting_id
+)
+class BookedResources(object):
+    """Resources booked for a sitting.
+    """
+mapper(BookedResources, bookedresources) 
+
+
+def get_available_resources(start, end):
+    """get all resources that are not booked for a sitting
+    in the given time period 
+    """
+    assert(type(start) == datetime.datetime)
+    assert(type(end) == datetime.datetime)
+    session = Session()
+    #start_end={'start': start, 'end':end}
+    sql_booked_resources =  """
+    SELECT resources.resource_id AS resources_resource_id
+    FROM resources JOIN resourcebookings 
+                   ON resources.resource_id = resourcebookings.resource_id 
+                   JOIN group_sittings 
+                   ON resourcebookings.group_sitting_id = group_sittings.group_sitting_id 
+    WHERE group_sittings.start_date BETWEEN :start AND :end 
+          OR group_sittings.end_date BETWEEN :start AND :end
+          OR :start BETWEEN group_sittings.start_date AND group_sittings.end_date
+          OR :end BETWEEN group_sittings.start_date AND group_sittings.end_date
+    """ 
+    
+    sql_resources = """
+    SELECT resources_1.resource_id AS resources_1_resource_id
+    FROM resources AS resources_1
+    WHERE resources_1.resource_id NOT IN (%s) """ % sql_booked_resources
+    connection = session.connection(domain.Resource)
+    query = connection.execute(rdb.text(sql_resources), start=start, end=end)
+    resources= query.fetchall()
+    return resources
+
+
+def get_unavailable_resources(start, end):
+    """Get all resources that are  booked in the given time period.
+    """
+    assert(type(start) == datetime.datetime)
+    assert(type(end) == datetime.datetime)
+    session = Session()
+    b_filter = sql.or_(
+        sql.between(schema.group_sittings.c.start_date, start, end), 
+        sql.between(schema.group_sittings.c.end_date, start, end),
+        sql.between(start, 
+            schema.group_sittings.c.start_date, schema.group_sittings.c.end_date),
+        sql.between(end, 
+            schema.group_sittings.c.start_date, schema.group_sittings.c.end_date)
+    )
+    query = session.query(BookedResources).filter(b_filter)
+    resources = query.all()
+    return resources
+
+
+def check_bookings(start, end, resource):
+    """Return all group_sittings a resource is booked for in the period.
+    """
+    assert(type(resource) == domain.Resource)
+    assert(type(start) == datetime.datetime)
+    assert(type(end) == datetime.datetime)
+    session = Session()
+    b_filter = sql.and_(
+        schema.resources.c.resource_id == resource.resource_id,
+        sql.or_(
+            sql.between(schema.group_sittings.c.start_date, start, end), 
+            sql.between(schema.group_sittings.c.end_date, start, end),
+            sql.between(start, 
+                schema.group_sittings.c.start_date, schema.group_sittings.c.end_date),
+            sql.between(end, 
+                schema.group_sittings.c.start_date, schema.group_sittings.c.end_date)
+        )
+    )
+    query = session.query(BookedResources).filter(b_filter)
+    bookings = query.all()
+    return bookings
+
+                    
+def check_availability(start, end, resource):
+    """Check if the resource is available in the given period.
+    """
+    assert(type(resource) == domain.Resource)
+    assert(type(start) == datetime.datetime)
+    assert(type(end) == datetime.datetime) 
+    return check_bookings(start, end, resource) == []
+
+
+def book_resource(sitting, resource):
+    """Book a resource for a sitting, check if the resource is available first
+    """
+    assert(type(sitting) == domain.GroupSitting)
+    assert(type(resource) == domain.Resource)
+    session = Session()
+    # check if resource is already boooked for this sitting
+    cq = session.query(domain.ResourceBooking).filter(
+            sql.and_(domain.ResourceBooking.resource_id == 
+                        resource.resource_id,
+                    domain.ResourceBooking.group_sitting_id == sitting.group_sitting_id))
+    results = cq.all()
+    if results:
+        # !+DOCTTEST(mr, sep-2010) a doctest depends on this print !
+        print "already booked"
+        return
+        #nothing to do here it is already booked
+    
+    if check_availability(sitting.start_date, sitting.end_date, resource):
+        booking = domain.ResourceBooking()
+        booking.resource_id = resource.resource_id
+        booking.group_sitting_id = sitting.group_sitting_id 
+        session.add(booking)
+        session.flush()
+    else:
+        # !+DOCTTEST(mr, sep-2010) a doctest depends on this print !
+        print "not available"
+
+
+def unbook_resource(sitting, resource):
+    """Remove a resource from a sitting.
+    """
+    assert(type(sitting) == domain.GroupSitting)
+    assert(type(resource) == domain.Resource)
+    session = Session()
+    cq = session.query(domain.ResourceBooking).filter(
+        sql.and_(
+            domain.ResourceBooking.resource_id == resource.resource_id,
+            domain.ResourceBooking.group_sitting_id == sitting.group_sitting_id
+        )
+    )
+    results = cq.all()
+    for result in results:
+        session.delete(result)
+        session.flush()
+
---
+++ bungeni.main/branches/sterch-routing/bungeni/ui/attendance.py
@@ -0,0 +1,222 @@
+# Bungeni Parliamentary Information System - http://www.bungeni.org/
+# Copyright (C) 2010 - Africa i-Parliaments - http://www.parliaments.info/
+# Licensed under GNU GPL v2 - http://www.gnu.org/licenses/gpl-2.0.txt
+
+"""Sitting attendance UI
+
+$Id:$
+$URL:$
+"""
+
+log = __import__("logging").getLogger("bungeni.ui.attendance")
+
+from operator import itemgetter
+
+import zope.event
+import zope.lifecycleevent
+from zope.interface import Attribute
+from zope.security.proxy import removeSecurityProxy
+from zope import formlib
+from zc.table import column
+from zope.app.pagetemplate import ViewPageTemplateFile
+
+from sqlalchemy import sql
+from bungeni.core.dc import IDCDescriptiveProperties
+from bungeni.alchemist import Session
+from bungeni.models.interfaces import IGroupSittingAttendance
+from bungeni.models.domain import AttendanceType, GroupSittingAttendance
+
+from bungeni.ui.browser import BungeniBrowserView
+from bungeni.ui.i18n import _
+from bungeni.ui.table import TableFormatter
+from bungeni.ui import forms
+from bungeni.ui.utils import url
+
+class AttendanceEditor(BungeniBrowserView, forms.common.BaseForm):
+    """Multiple attendance record editor for sittings"""
+    
+    form_fields = []
+    
+    render = ViewPageTemplateFile("templates/attendance.pt")
+    
+    def __init__(self, context,  request):
+        super(AttendanceEditor, self).__init__(context, request)
+
+    @property
+    def radio_prefix(self):
+        return "sitting_attendance_radio"
+
+    @property
+    def columns(self):
+        listing_columns = [
+            column.GetterColumn(title=_("full names"),
+            getter = lambda i,f: i.get('attendee')
+            )
+        ]
+        r_counter = 0
+        for at_type in self.attendance_types:
+            at_column = column.GetterColumn(
+                title=at_type.attendance_type,
+                getter = lambda i,f,rcounter=r_counter: \
+                    "<input type='radio' name='%s' value='%s' %s>"\
+                    %(i["records"][rcounter]["name"],
+                    i["records"][rcounter]["value"],
+                    (i["records"][rcounter]["checked"] 
+                        and 'checked=\"true\"') or ""
+                    )
+            )
+            listing_columns.append(at_column)
+            r_counter = r_counter + 1
+        return listing_columns
+    
+    
+    def makeId(self, value):
+        return "".join((self.radio_prefix,
+            "".join(str(value).encode('base64').split())
+            )
+        )
+
+    @property
+    def column_titles(self):
+        return [attd.attendance_type for attd in self.attendance_types]
+        
+    @property
+    def attendance_types(self):
+        session = Session()
+        return session.query(AttendanceType).all()
+
+    @property
+    def formatted_listing(self):
+        formatter = TableFormatter(
+            self.context,
+            self.request,
+            self.listing,
+            prefix="attendance",
+            visible_column_names = [c.name for c in self.columns],
+            columns = self.columns
+        )
+        formatter.updateBatching()
+        return formatter()
+    
+    @property
+    def listing(self):
+        list_data = []
+        trusted = removeSecurityProxy(self.context)
+        current_attendance = [
+            attd for attd in trusted.attendance.values()
+        ]
+        for member in trusted.group.members:
+            attd = filter(lambda i:i.member_id==member.user_id,
+                current_attendance
+            )
+            m_data = {}
+            m_data["attendee"] = IDCDescriptiveProperties(member).title
+            m_data["has_record"] = int(bool(attd))
+            m_data["records"] = [
+                {"name" : self.makeId(member.user_id),
+                "checked" : bool(attd) and (
+                    attd[0].attendance_type_id == \
+                        at_type.attendance_type_id
+                    ),
+                "value" : at_type.attendance_type_id
+                }
+                for at_type in self.attendance_types
+            ]
+            list_data.append(m_data)
+        sorted_list = sorted(list_data,
+            key=itemgetter("has_record", "attendee")
+        )
+        return sorted_list
+
+    def hasListing(self, action):
+        return bool(len(self.listing))
+
+    @property
+    def action_url(self):
+        return ""
+
+    @property
+    def action_method(self):
+        return "post"
+    
+    @formlib.form.action(label=_("Save"), condition=hasListing)
+    def handle_save(self, action, data):
+        self.processAttendance()
+
+    @formlib.form.action(label=_("Cancel"))
+    def handle_cancel(self, action, data):
+        session = Session()
+        next_url = url.absoluteURL(self.__parent__, self.request)
+        self.request.response.redirect(next_url)
+        session.close()
+
+    def setUpWidgets(self, ignore_request=False):
+        actions = self.actions
+        self.actions = []
+        for action in actions:
+            if getattr(action, "condition", None):
+                if action.condition(self, self.context):
+                    self.actions.append(action) 
+            else:
+                self.actions.append(action)
+        super(AttendanceEditor, self).setUpWidgets(self)
+
+
+    def getSelected(self):
+        selection = [ 
+            {"user_id": k[len(self.radio_prefix):].decode("base64"),
+             "attendance_type_id": self.request.form.get(k)
+            }
+            for k in self.request.form.keys() \
+            if k.startswith( self.radio_prefix )  \
+                and self.request.form.get(k,'') != '']
+        return selection   
+
+    def processAttendance(self):
+        session = Session()
+        trusted = removeSecurityProxy(self.context)
+        gs_id = trusted.group_sitting_id
+        selected = self.getSelected()
+        for selection in selected:
+            member_id = selection.get("user_id")
+            if not member_id:
+                continue
+            at_id = selection.get("attendance_type_id")
+            if not at_id:
+                continue
+            member_id = int(member_id)
+            at_id = int(at_id)
+            #check existing attendance record
+            query = session.query(GroupSittingAttendance).filter(
+                sql.and_(
+                    GroupSittingAttendance.member_id == member_id,
+                    GroupSittingAttendance.group_sitting_id == gs_id
+                )
+            )
+            result = query.first()
+            if result is not None:
+                result.attendance_type_id = at_id
+                session.flush()
+                zope.event.notify(
+                    zope.lifecycleevent.ObjectModifiedEvent(result,
+                        zope.lifecycleevent.Attributes(
+                            IGroupSittingAttendance, 
+                            "attendance_type_id"
+                        )
+                    )
+                )
+            else:
+                m_attendance = GroupSittingAttendance()
+                m_attendance.group_sitting_id = gs_id
+                m_attendance.attendance_type_id = at_id
+                m_attendance.member_id = member_id
+                session.add(m_attendance)
+                session.flush()
+                zope.event.notify(
+                    zope.lifecycleevent.ObjectCreatedEvent(m_attendance)
+                )
+        self.status = _(u"Updated attendance record")
+
+    def __call__(self):
+        self.update()
+        return self.render()

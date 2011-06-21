0b4f3e50f5606d9bbb62befa9c73da33d413b0af
jura.stakhiv <jura.stakhiv@gmail.com>
2011-06-21 12:22:37 +0000

    Fixed show_url view.

--- bungeni.main/branches/sterch-routing/bungeni/core/uri.py
+++ bungeni.main/branches/sterch-routing/bungeni/core/uri.py
@@ -11,25 +11,27 @@ from bungeni.alchemist import Session
 from bungeni.models.domain import ParliamentaryItem
 from bungeni.alchemist.container import stringKey
 from bungeni.core.language import get_default_language
+import bungeni.ui.utils as ui_utils
 
 def generate_uri(object, event):
     session = Session()
-    object = session.merge(removeSecurityProxy(object))    
-    uri = "/%s/%s/2001-03-01/%s/%s@/main" % (get_default_language(), object.type, object.registry_number, object.language)
+    #object = session.merge(removeSecurityProxy(object))
+    
+    # /<section>/<type>/<date>/<registry id>/<language>@/main/
+    getSite() 
+    if object.type == 'bill':
+        uri = "/bungeni/%s/%s/%s/%s/%s@/main/" % ("ke", object.type, object.publication_date, object.registry_number, object.language)
+    else:
+        uri = "/bungeni/%s/%s/%s/%s/%s@/main/" % ("ke", object.type, object.status_date.date(), object.registry_number, object.language)
+    
     if object.uri is None:
         object.uri = uri
-        print "URI is set to:", uri
     else:
-        print "URI already set"
-    
-
+        object.uri = uri
 
 class ShowURI(AbsoluteURL):
     
     def __str__(self):
-        try:
-            return "URI: " + self.context.uri
-        except:
-            return "URI: no URI"
+            return self.context.uri
 
     __call__ = __str__

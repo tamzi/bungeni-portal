bd7a2fdb0f976fc8a57e24cde9d5c3e12d80a907
jura.stakhiv <jura.stakhiv@gmail.com>
2011-06-22 13:39:02 +0000

    Added custom absolute_url for parliamentary objects in custom traverse section. Fixed standard view.

--- bungeni.main/branches/sterch-routing/bungeni/core/uri.py
+++ bungeni.main/branches/sterch-routing/bungeni/core/uri.py
@@ -1,37 +1,14 @@
-import interfaces
-from zope.security.proxy import removeSecurityProxy
-from zope.traversing.browser import absoluteURL
-from zope.traversing.browser.absoluteurl import AbsoluteURL
-from bungeni.alchemist.container import stringKey
-from zope.app.component.hooks import getSite
-import bungeni.ui.utils as ui_utils
-from zope.security.proxy import removeSecurityProxy
-from zope.interface import providedBy
-from bungeni.alchemist import Session
-from bungeni.models.domain import ParliamentaryItem
-from bungeni.alchemist.container import stringKey
-from bungeni.core.language import get_default_language
 import bungeni.ui.utils as ui_utils
 
 def generate_uri(object, event):
-    session = Session()
-    #object = session.merge(removeSecurityProxy(object))
-    
-    # /<section>/<type>/<date>/<registry id>/<language>@/main/
-    getSite() 
+    uri = ''
+
     if object.type == 'bill':
-        uri = "/bungeni/%s/%s/%s/%s/%s@/main/" % ("ke", object.type, object.publication_date, object.registry_number, object.language)
+        if object.publication_date is not None:
+            uri = "/bungeni/%s/%s/%s/%s/%s@/main" % ("ke", object.type, object.publication_date, object.registry_number, object.language)
     else:
-        uri = "/bungeni/%s/%s/%s/%s/%s@/main/" % ("ke", object.type, object.status_date.date(), object.registry_number, object.language)
-    
-    if object.uri is None:
-        object.uri = uri
-    else:
-        object.uri = uri
+        if object.status_date is not None:
+            uri = "/bungeni/%s/%s/%s/%s/%s@/main" % ("ke", object.type, object.status_date.date(), object.registry_number, object.language)
 
-class ShowURI(AbsoluteURL):
-    
-    def __str__(self):
-            return self.context.uri
-
-    __call__ = __str__
+    if object.uri is None and uri:
+        object.uri = uri

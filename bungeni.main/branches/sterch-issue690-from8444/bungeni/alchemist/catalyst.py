bbc799c13f975ae038efdfc114e224cb013983ca
voyn1991 <voyn1991@gmail.com>
2011-07-15 13:43:32 +0000

    Branch for isssue 690, but revision 8444, because 8447 is broken

---
+++ bungeni.main/branches/sterch-issue690-from8444/bungeni/alchemist/catalyst.py
@@ -0,0 +1,17 @@
+# Bungeni Parliamentary Information System - http://www.bungeni.org/
+# Copyright (C) 2010 - Africa i-Parliaments - http://www.parliaments.info/
+# Licensed under GNU GPL v2 - http://www.gnu.org/licenses/gpl-2.0.txt
+
+"""Bungeni Alchemist catalyst - [
+    alchemist.catalyst.ui
+]
+
+$Id$
+"""
+log = __import__("logging").getLogger("bungeni.alchemist")
+
+
+from alchemist.catalyst.ui import AddForm
+from alchemist.catalyst.ui import DisplayForm
+from alchemist.catalyst.ui import EditForm
+

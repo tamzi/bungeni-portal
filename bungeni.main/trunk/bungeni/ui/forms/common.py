e7c264b338bfb1177ecc85f6a628f2d1f8488864
mario.ruggier <mario.ruggier@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2011-07-27 10:29:25 +0000

    Partial revert of the ui.forms.common changes in r8472, to take into account that these base form classes were in some cases being used as *views* and in others as *viewlets*, each having a different api; this is now made explicit in BaseForm, but other derivative classes just simply make use of the much less telling and explicit *args.

--- bungeni.main/trunk/bungeni/ui/forms/common.py
+++ bungeni.main/trunk/bungeni/ui/forms/common.py
@@ -111,6 +111,13 @@ class BaseForm(formlib.form.FormBase):
 
         If ``next_url`` is provided, a redirect is issued upon
         successful form submission.
+        
+    As a viewlet
+        
+        Two additional init params to the "view" standard init API of 
+        (context, request) are specified for when usage is as as "viewlet"
+        i.e. (context, request, view, manager)
+    
     """
 
     Adapts = None
@@ -121,8 +128,19 @@ class BaseForm(formlib.form.FormBase):
 
     status = None
 
-    def __init__(self, context, request):
-        super(BaseForm, self).__init__(context, request)
+    def __init__(self, context, request,
+            # to support usage as a viewlet
+            view=None, manager=None
+        ):
+        # !+view/viewlet(mr, jul-2011): (self, context, request, view, manager)
+        # here, we make the distinction explicit, for some clarity, but in 
+        # subclasses we simply use the open-ended *args
+        if view is not None:
+            # viewlet api
+            super(BaseForm, self).__init__(context, request, view, manager)
+        else:
+            # view api
+            super(BaseForm, self).__init__(context, request)
         
         if str(self.request.get("headless", "")).lower() in TRUE_VALS:
             self.setPrefix(NO_PREFIX)
@@ -133,7 +151,7 @@ class BaseForm(formlib.form.FormBase):
                 default = DefaultAction(action)
                 self.actions = formlib.form.Actions(default)
                 break
-
+        
         # the ``_next_url`` attribute is used internally by our
         # superclass to implement formlib's ``nextURL`` method
         next_url = self._next_url = self.request.get("next_url", None)
@@ -234,7 +252,7 @@ class AddForm(BaseForm, catalyst.AddForm):
 
     interface.implements(ILocation, IDCDescriptiveProperties)
     description = None
-    
+
     def getDomainModel(self):
         return getattr(self.context, "domain_model", self.context.__class__)
 
@@ -383,8 +401,9 @@ class EditForm(BaseForm, catalyst.EditForm):
     """Custom edit-form for Bungeni content.
     """
 
-    def __init__(self, context, request):
-        super(EditForm, self).__init__(context, request)
+    def __init__(self, *args):
+        # !+view/viewlet(mr, jul-2011)
+        super(EditForm, self).__init__(*args)
         # For bungeni content, mark the request that we are in edit mode e.g. 
         # useful for when editing a question's response, but not wanting to 
         # offer option to submit the response while in response edit mode. 
@@ -517,8 +536,9 @@ class TranslateForm(AddForm):
     def side_by_side(self):
         return True
 
-    def __init__(self, context, request):
-        super(TranslateForm, self).__init__(context, request)
+    def __init__(self, *args):
+        # !+view/viewlet(mr, jul-2011)
+        super(TranslateForm, self).__init__(*args)
         self.language = self.request.get("language", get_default_language())
 
     def translatable_field_names(self):

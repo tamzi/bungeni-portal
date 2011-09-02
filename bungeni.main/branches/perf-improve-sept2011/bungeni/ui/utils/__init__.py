2f627ddedebcb5fdcfc0fe91e473fb3162a2d697
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2011-09-02 09:36:17 +0000

    Branch to test additional indexes

---
+++ bungeni.main/branches/perf-improve-sept2011/bungeni/ui/utils/__init__.py
@@ -0,0 +1,8 @@
+# Bungeni Parliamentary Information System - http://www.bungeni.org/
+# Copyright (C) 2010 - Africa i-Parliaments - http://www.parliaments.info/
+# Licensed under GNU GPL v2 - http://www.gnu.org/licenses/gpl-2.0.txt
+'''Various utilities for the UI
+
+$id$
+'''
+

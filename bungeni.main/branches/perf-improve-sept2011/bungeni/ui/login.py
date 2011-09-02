eb34c4611ceb1c3c28dafd742e30d58ddfd70e6e
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2011-09-02 09:36:17 +0000

    Branch to test additional indexes

---
+++ bungeni.main/branches/perf-improve-sept2011/bungeni/ui/login.py
@@ -0,0 +1,48 @@
+from zope import interface
+from zope import schema
+from zope.formlib import form
+from zope.formlib.namedtemplate import NamedTemplate
+from zope.app.component.hooks import getSite
+from zope.publisher.browser import BrowserView
+from zope.app.security.interfaces import IUnauthenticatedPrincipal
+from zope.app.component.hooks import getSite
+
+import bungeni.ui.utils as ui_utils
+from bungeni.core.i18n import _
+
+
+class ILoginForm(interface.Interface):
+    login = schema.TextLine(title=_(u"Username"))
+    password = schema.Password(title=_(u"Password"))
+
+class Login(form.FormBase):
+    form_fields = form.Fields(ILoginForm)
+    prefix = ""
+    form_name = _(u"Login")
+    
+    # !+ only used here [ bungeni.ui.login.Login ] ?
+    template = NamedTemplate("alchemist.form")
+    
+    def __call__(self):
+        if not IUnauthenticatedPrincipal.providedBy(self.request.principal):
+            app = getSite()
+            workspace = app["workspace"]
+            self.request.response.redirect(
+                        ui_utils.url.absoluteURL(workspace, self.request))
+        return super(Login, self).__call__()
+            
+    @form.action(_(u"Login"))
+    def handle_login(self, action, data):
+        if IUnauthenticatedPrincipal.providedBy(self.request.principal):
+            self.status = _(u"Invalid account credentials")
+        else:
+            site_url = ui_utils.url.absoluteURL(getSite(), self.request)
+            camefrom = self.request.get('camefrom', site_url+'/')
+            self.status = _("You are now logged in")
+            self.request.response.redirect( camefrom )
+
+class Logout(BrowserView):
+    def __call__( self ):
+        self.request.response.expireCookie( "wc.cookiecredentials" )
+        site_url = ui_utils.url.absoluteURL(getSite(), self.request)
+        self.request.response.redirect( site_url )

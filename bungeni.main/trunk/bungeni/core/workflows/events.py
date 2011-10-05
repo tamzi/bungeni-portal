7797cfba800c6d9933fafe986fb784ad4c61d9a8
mario.ruggier <mario.ruggier@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2011-10-05 15:16:01 +0000

    Replacing workflows/configure.zcml with in-place registration (only event handlers in this case). Added a simple decorator utility to register an event handler.

--- bungeni.main/trunk/bungeni/core/workflows/events.py
+++ bungeni.main/trunk/bungeni/core/workflows/events.py
@@ -8,11 +8,13 @@ $Id$
 """
 log = __import__("logging").getLogger("bungeni.core.workflows.events")
 
-from zope import component
+from bungeni.utils import register
 from bungeni.core.workflow import interfaces
+from bungeni.alchemist.interfaces import IAlchemistContent
+from zope.lifecycleevent import IObjectCreatedEvent
 
 
-@component.adapter(interfaces.IWorkflowTransitionEvent)
+@register.handler(adapts=(interfaces.IWorkflowTransitionEvent,))
 def workflowTransitionEventHandler(event):
     log.debug(" ".join(["<%s",
         "source=%s",
@@ -23,24 +25,21 @@ def workflowTransitionEventHandler(event):
                 event.source, event.destination, event.object, event.comment))
 
 
+@register.handler(adapts=(IAlchemistContent, IObjectCreatedEvent))
 def initializeWorkflow(object, event):
     """In response to object created events.
     event:zope.lifecycleevent.ObjectCreatedEvent
     """
-    wfc = interfaces.IWorkflowController(object, None)
-    if wfc is not None:
-        wfc.fireAutomatic()
+    fireAutomaticTransitions(object, event)
 
 
+@register.handler(adapts=(IAlchemistContent, interfaces.IWorkflowTransitionEvent))
 def fireAutomaticTransitions(object, event):
     """Fire automatic transitions for a new state.
     event:bungeni.core.workflow.interfaces.IWorkflowTransitionEvent
     """
-    if interfaces.IWorkflow(object, None) is None:
-        return
-    
-    workflow = interfaces.IWorkflowController(object, None)
-    if workflow is not None:
-        workflow.fireAutomatic()
+    wfc = interfaces.IWorkflowController(object, None)
+    if wfc is not None:
+        wfc.fireAutomatic()
 
 

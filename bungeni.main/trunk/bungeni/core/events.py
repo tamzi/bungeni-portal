85f59cc6bdeee9f625c458e705b5036d074dd0be
mario.ruggier <mario.ruggier@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2011-10-12 16:38:57 +0000

    Import to actually register event subscribers (left out in r8667 and r8677). Switching group principal_id initializer to IObjectCreatedEvent instead of IObjectModifiedEvent.

--- bungeni.main/trunk/bungeni/core/events.py
+++ bungeni.main/trunk/bungeni/core/events.py
@@ -64,7 +64,8 @@ def signatory_modified(ob, event):
         audit.objectContained( ob.item, event)
 
 
-#!+ was zope.app.container.interfaces.IObjectRemovedEvent?
+#!+ was zope.app.container.interfaces.IObjectRemovedEvent that is a different
+# interface than zope.lifecycleevent.IObjectModifiedEvent ?!
 @register.handler(adapts=(ISignatory, IObjectRemovedEvent))
 def signatory_deleted(ob, event):
     """Clear signatory role for a deleted signatory
@@ -97,8 +98,8 @@ def group_member_modified(ob, event):
 
 
 # !+GROUP_PRINCIPAL_ID(ah, sep-2011) event to set group principal id
-@register.handler(adapts=(IBungeniGroup, IObjectModifiedEvent))
-def group_modified(ob, event):
+@register.handler(adapts=(IBungeniGroup, IObjectCreatedEvent))
+def group_created(ob, event):
     """When a group is added, the value in group_principal_id is computed
     out of the group type and group id. This was a computed property in orm.py
     but has been moved here now - so it gets cached in the groups table.

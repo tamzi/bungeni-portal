c611db22c21037974972fdd369fa6aee6d83cc23
borona.murithi <borona.murithi@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2011-11-29 10:14:06 +0000

    Updated message catalog translator tool - bunch translation, dummy mode, translate sets.
    - support for bunch translation of messages (reduce calls to Google translate api)
    - dummy mode (makes no calls to google api nor writes to out file)
    - accepts text parameter to specify which set of messages to translate - all or untranslated
    
    - Other fixes:
      - message string no longer used as source of text to translate
      - captching exceptions from args parsing

--- bungeni-i18n/tools/po-translator/translate.py
+++ bungeni-i18n/tools/po-translator/translate.py
@@ -9,7 +9,18 @@ import re
 import polib
 from apiclient.discovery import build
 
+MESSAGE_SET_OPTIONS = ("untranslated", "all")
+DEFAULT_MESSAGE_SET = "untranslated"
+DEFAULT_BUNCH_SIZE = 50
+DUMMY_OPTIONS = ["Y", "N", "YES", "NO"]
+DUMMY_MAPPING = { "Y": True, "N": False, "YES": True, "NO": False }
 UNTRANSLATABLE_STRINGS = "\$\w+|\${\w+\}|%\w+|%\(\w+\)\w"
+
+def bunch_list(source, size):
+    return [ source[x:(x + size)] for x in xrange(0, len(source), size) ]
+
+def opts(source_list, separator=", "):
+    return separator.join([ str(item) for item in source_list ])
 
 class KeyGenerator(object):
     current = 1
@@ -42,50 +53,100 @@ class CatalogTranslator(object):
             )
         return restored_translation
 
-    def translate(self):
+    def translate(self, bunch_size=DEFAULT_BUNCH_SIZE, 
+        translate=DEFAULT_MESSAGE_SET, dummy=False
+    ):
         service = build("translate", "v2", developerKey=self.api_key)
         keygen = KeyGenerator()
         src_file = polib.pofile(self.source_po)
-        untranslated_messages = src_file.untranslated_entries()
-        print "Translating %d messages" % len(untranslated_messages)
-        for message in untranslated_messages:
-            translate_text = message.msgstr or message.msgid
-            if message.comment:
-                parse_default = eval(
-                    message.comment.lstrip().rstrip().replace(
-                        "Default: ", ""
+        if translate == "all":
+            messages = src_file
+        elif translate == "untranslated":
+            messages = src_file.untranslated_entries()
+        if dummy:
+            print "============ DUMMY MODE =============="
+        print "Translating %d messages" % len(messages)
+        bunched_messages = bunch_list(messages, bunch_size)
+        bunch_length = len(bunched_messages)
+        for bunch_no, bunch in enumerate(bunched_messages):
+            print "Translating bunch %d/%d (%d messages)" %(
+                bunch_no, bunch_length, len(bunch)
+            )
+            messages_to_translate = []
+            for message in bunch:
+                translate_text = message.msgid
+                if message.comment:
+                    parse_default = eval(
+                        message.comment.lstrip().rstrip().replace(
+                            "Default: ", ""
+                        )
                     )
-                )
-                if parse_default:
-                    translate_text = parse_default
-            replace_names = re.findall(UNTRANSLATABLE_STRINGS, translate_text)
-            for name in replace_names:
-                name_key = "DNT%d" % keygen.next
-                self.names_map[name_key] = name
-                translate_text = translate_text.replace(name, name_key, 1)
+                    if parse_default:
+                        translate_text = parse_default
+                replace_names = re.findall(UNTRANSLATABLE_STRINGS, translate_text)
+                for name in replace_names:
+                    name_key = "DNT%d" % keygen.next
+                    self.names_map[name_key] = name
+                    translate_text = translate_text.replace(name, name_key, 1)
+                messages_to_translate.append(translate_text)
+            #translate this bunch
+            if dummy:
+                continue
             result = service.translations().list(
                 source = self.source_lang,
                 target = self.dest_lang,
-                q = translate_text
+                q = messages_to_translate
             ).execute()
-            translation = result.get("translations")[0].get("translatedText")
-            message.msgstr = self.restore_names(translation)
-        src_file.save(self.dest_po)
+            for index, translation in enumerate(result.get("translations")):
+                bunch[index].msgstr = self.restore_names(
+                    translation.get("translatedText")
+                )
+        if not dummy:
+            src_file.save(self.dest_po)
 def usage():
-    print ("USAGE:: python translate.py <source-po-file> <source-language> "
-        "<destination-po-file> <destination-language> <api-key>"
+    options = ("<source-po-file> <source-language> <destination-po-file> " 
+        "<destination-language> <api-key> <bunch-size> <what-messages> "
+        "<dummy>"
+    )
+    print """USAGE:: python translate.py %s\n
+    <bunch-size> options   : %s
+    <what-messages> options: %s
+    <dummy> options        : %s
+    """ %(options, "a number e.g. 10", opts(MESSAGE_SET_OPTIONS), 
+        opts(DUMMY_OPTIONS)
     )
 
 def run_translator():
-    if len(sys.argv[1:]) != 5:
+    #!+ARGS(mb, nov-2011) Should utilize optparse or similar approach here
+    arguments = sys.argv[1:]
+    if len(arguments) not in [5, 6, 7, 8]:
         usage()
         exit(1)
-    source_po, source_lang, dest_po, dest_lang, api_key = sys.argv[1:]
-    assert os.path.exists(source_po)
+    len_arguments = len(arguments)
+    if len_arguments==5:
+        arguments.extend([DEFAULT_BUNCH_SIZE, DEFAULT_MESSAGE_SET, False])
+    elif len_arguments==6:
+        arguments.extend([DEFAULT_MESSAGE_SET, False])
+    elif len_arguments==7:
+        arguments.append(False)
+    source_po, source_lang, dest_po, dest_lang, api_key, bunch_size, what_msgs, dummy = arguments
+    
+    try:
+        assert os.path.exists(source_po)
+        assert what_msgs in MESSAGE_SET_OPTIONS
+        assert dummy in DUMMY_OPTIONS
+        bunch_size = int(bunch_size)
+        assert bunch_size > 0
+    except (AssertionError, ValueError, TypeError):
+        usage()
+        exit(1)
     translator = CatalogTranslator(source_po, source_lang, dest_po, dest_lang,
         api_key
     )
-    translator.translate()
+    dummy_bool = DUMMY_MAPPING[dummy]
+    translator.translate(bunch_size=bunch_size, translate=what_msgs, 
+        dummy=dummy_bool
+    )
 
 if __name__ == "__main__":
     run_translator()

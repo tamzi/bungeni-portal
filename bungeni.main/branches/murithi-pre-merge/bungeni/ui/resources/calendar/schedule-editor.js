a7781e11d5a9895af2a4efd37e2fa10b6e5db9c9
borona.murithi <borona.murithi@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2011-12-06 14:42:45 +0000

    [PrivateBranch] Updates to proposed revision of scheduling UI. Dynamic resources update.
    Scheduling UI:
    - Updated UI composition to split-panel approach
      - One panel with the schedule. Second panel to host available schedulable items.
    - Enabling inline adding/editing of arbitrary text to the schedule
    
    Dynamic Resources:
    - Updating how 'global parameters' are fetched - proper capture of request language
      - Ensures proper I18N/L10N of parameters for use by browser scripts

--- bungeni.main/branches/murithi-pre-merge/bungeni/ui/resources/calendar/schedule-editor.js
+++ bungeni.main/branches/murithi-pre-merge/bungeni/ui/resources/calendar/schedule-editor.js
@@ -4,57 +4,148 @@
  * viewlet
  * 
  */
-YAHOO.util.Event.addListener(window, "load", function(){
-    var scheduledActions = new YAHOO.widget.Panel("scheduled-item-controls",
-        {   
-            underlay: "none"
-        }
-    );
-    scheduledActions.currentItem = null;
-    
-    var commentButton = new YAHOO.util.Element("add-note");
-    commentButton.on("click", function(event){
-        console.log("Processing record", scheduledActions.currentItem);
-    });
-    
-    var datatable_loader = function(){
-        
-        var columnDefinitions = [
-            {key: "item_id", label: scheduler_globals.column_title},
-            {key: "item_type", label: scheduler_globals.column_type},
-        ];
-        
-        var itemsDataSource = new YAHOO.util.DataSource(
-            scheduler_globals.json_listing_url
-        );
-        itemsDataSource.responseType = YAHOO.util.DataSource.TYPE_JSON;
-        itemsDataSource.responseSchema = {
-            resultsList: "nodes",
-            fields: ["item_id", "item_type"],
-        };
-        
-        var itemsDataTable = new YAHOO.widget.DataTable("schedule-table",
-            columnDefinitions, itemsDataSource, { selectionMode:"single" }
-        );
 
-        itemsDataTable.subscribe("rowMouseoverEvent", itemsDataTable.onEventHighlightRow);
-        itemsDataTable.subscribe("rowMouseoutEvent", itemsDataTable.onEventUnhighlightRow);
-        itemsDataTable.subscribe("rowClickEvent", itemsDataTable.onEventSelectRow);
-
-        itemsDataTable.subscribe("rowHighlightEvent", function(args){
-            scheduledActions.currentItem = args.record;
-            console.log(args.record);
-            scheduledActions.cfg.setProperty("context",
-                [args.el.id, "tl", "tr"]
+(function() {
+    var Dom = YAHOO.util.Dom, Event = YAHOO.util.Event, itemsDataTable = null;
+    var onTextRecordAdded = new YAHOO.util.CustomEvent("onTextRecordAdded");
+    
+    Event.onDOMReady(function(){
+        //create datatable controls
+        var scheduledActions = new YAHOO.widget.Panel("scheduled-item-controls",
+            {   
+                underlay: "none"
+            }
+        );
+        scheduledActions.currentItem = null;
+        
+        var scheduleTextButton = new YAHOO.widget.Button(
+            {
+                label: scheduler_globals.text_button_text,
+            }
+        );
+        scheduleTextButton.appendTo(scheduledActions.footer);
+        scheduleTextButton.on("click", function(event){
+            var new_record_index = itemsDataTable.getTrIndex(
+                scheduledActions.currentItem
+            ) + 1;
+            itemsDataTable.addRow(
+                { 
+                    item_id: scheduler_globals.initial_editor_text, 
+                    item_type: "text" 
+                }, 
+                new_record_index
             );
-            scheduledActions.render();
-            scheduledActions.show();
+            var new_record = itemsDataTable.getRecord(
+                itemsDataTable.getTrEl(new_record_index)
+            );
+            itemsDataTable.unselectAllRows();
+            itemsDataTable.selectRow(new_record);
+            /*var firstCell = itemsDataTable.getFirstTdEl(
+                itemsDataTable.getSelectedTrEls()[0]
+            );
+            var editCell = itemsDataTable.getNextTdEl(firstCell);
+            //TODO - fire showCellEditor*/
+            scheduledActions.close();
+        });
+        var removeButton = new YAHOO.widget.Button(
+            {
+                label: scheduler_globals.remove_button_text
+            }
+        );
+        removeButton.appendTo(scheduledActions.footer);
+        
+        //var itemNumberFormatter = function(el, record, column, data){
+        //    el.innerHTML = itemsDataTable.getTrIndex(record) + 1;
+        //}
+        
+        var itemSelectFormatter = function(el, record, column, data){
+            index = itemsDataTable.getTrIndex(record) + 1;
+            el.innerHTML = "<input type='checkbox' name='rec-sel-" + index + "'/>"
+        }
+        
+        //create layout
+        schedulerLayout = new YAHOO.widget.Layout("scheduler-layout",
+            {
+                height:500,
+                units: [
+                    { 
+                        position: "left", 
+                        width: 600, 
+                        body: '',
+                        header: scheduler_globals.current_schedule_title,
+                        gutter: "5 5",
+                        height: 400 
+                    },
+                    { 
+                        position: "center", 
+                        body: '',
+                        header: scheduler_globals.available_items_title,
+                        gutter: "5 5",
+                        height: 400 
+                    },
+                ]
+            }
+        );
+        
+        //load data table
+        schedulerLayout.on("render", function(){
+            var textCellEditor = YAHOO.widget.TextboxCellEditor;
+            var columnDefinitions = [
+                {
+                    key:"item_checked", 
+                    label: "<input type='checkbox' name='rec-sel-all'/>", 
+                    formatter: itemSelectFormatter 
+                },
+                {
+                    key:"item_id", 
+                    label: scheduler_globals.column_title,
+                    editor: new YAHOO.widget.TextboxCellEditor(),
+                },
+                {key:"item_type", label: scheduler_globals.column_type},
+            ];
+            
+            var itemsDataSource = new YAHOO.util.DataSource(
+                scheduler_globals.json_listing_url
+            );
+            itemsDataSource.responseType = YAHOO.util.DataSource.TYPE_JSON;
+            itemsDataSource.responseSchema = {
+                resultsList: "nodes",
+                fields: ["item_id", "item_type"],
+            };
+            
+            layout_centre = schedulerLayout.getUnitByPosition("left");
+            var scheduler_container = document.createElement("div");
+            layout_centre.body.appendChild(scheduler_container);
+
+            itemsDataTable = new YAHOO.widget.DataTable(scheduler_container,
+                columnDefinitions, itemsDataSource, { selectionMode:"single" }
+            );
+            itemsDataTable.subscribe("rowMouseoverEvent", itemsDataTable.onEventHighlightRow);
+            itemsDataTable.subscribe("rowMouseoutEvent", itemsDataTable.onEventUnhighlightRow);
+            itemsDataTable.subscribe("rowClickEvent", itemsDataTable.onEventSelectRow);
+            //itemsDataTable.subscribe("onTextRecordAdded", itemsDataTable.onEventShowCellEditor);
+            itemsDataTable.subscribe("cellDblclickEvent", function(event){
+                var target = Event.getTarget(event);
+                var record = this.getRecord(target);
+                if (record.getData().item_type == "text"){
+                    itemsDataTable.onEventShowCellEditor(event);
+                }
+            });
+
+            itemsDataTable.subscribe("rowSelectEvent", function(args){
+                scheduledActions.currentItem = args.record;
+                scheduledActions.cfg.setProperty("context",
+                    [args.el.id, "tl", "tr"]
+                );
+                scheduledActions.render();
+                scheduledActions.show();
+            });
+            return {
+                oDS: itemsDataSource,
+                oDT: itemsDataTable,
+            }
         });
         
-        return {
-            oDS: itemsDataSource,
-            oDT: itemsDataTable
-        };
-    }();
-    
-});
+        schedulerLayout.render();
+    });
+})();

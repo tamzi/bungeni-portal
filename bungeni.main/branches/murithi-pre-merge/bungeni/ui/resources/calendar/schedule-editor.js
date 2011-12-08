a398f35b936cf736cd97150cfbb131d0f59c77ae
borona.murithi <borona.murithi@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2011-12-08 15:42:54 +0000

    [PrivateBranch] Updates to proposed revision of scheduling UI.
    - Adding reorder columns - move rows up and down the schedule
    - Adding delete column and confirmation dialog to remove items from schedule
    - Adding docstrings and reorganizing schedule editor sources
    - Adding new strings to dynamic JS resources

--- bungeni.main/branches/murithi-pre-merge/bungeni/ui/resources/calendar/schedule-editor.js
+++ bungeni.main/branches/murithi-pre-merge/bungeni/ui/resources/calendar/schedule-editor.js
@@ -1,67 +1,231 @@
 /*
  * This module instatiates a datatable for items in sitting. We also render
- * a panel with items available for scheduling - this is a pre-populated 
- * viewlet
+ * a panel with items available for scheduling.
  * 
  */
 
 (function() {
-    var Dom = YAHOO.util.Dom, Event = YAHOO.util.Event, itemsDataTable = null;
-    var onTextRecordAdded = new YAHOO.util.CustomEvent("onTextRecordAdded");
+    var Dom = YAHOO.util.Dom, Event = YAHOO.util.Event;
+    var itemsDataTable = null;
+    var itemsDataSource = null;
+    var schedulerActions = null;
+    var deleteDialog = null;
+    var ITEM_MOVE_UP_COLUMN = "item_move_up";
+    var ITEM_MOVE_DOWN_COLUMN = "item_move_down";
+    var ITEM_DELETE_COLUMN = "item_delete";
+
+    // custom column formatters
+    /**
+     * @method itemSelectorFormatter
+     * @description renders checkboxes to select items on the schedule
+     */
+    var itemSelectFormatter = function(el, record, column, data){
+        index = this.getTrIndex(record) + 1;
+        el.innerHTML = "<input type='checkbox' name='rec-sel-" + index + "'/>"
+    }
+
+    /**
+     * @method itemMoveFormatter
+     * @description renders controls to move scheduled items up/down the
+     * schedule depending on direction
+     */
+    var itemMoveFormatter = function(el, record, column, data, dir, table){
+        var move_markup = "";
+        var index = table.getTrIndex(record) + 1;
+        var last_row = table.getRecordSet().getLength();
+        var dir_char = (dir=="up")?"&uarr;":"&darr;"
+
+        if (!(((index == 1) && (dir=="up")) || 
+            ((index == last_row) && (dir=="down"))
+        )){
+            move_markup = "<span id='up'>" + dir_char + "</span>";
+        }
+        el.innerHTML = move_markup;
+    }
+    var itemMoveUpFormatter = function(el, record, column, data){
+        itemMoveFormatter(el, record, column, data, "up", this);
+    }
+    var itemMoveDownFormatter = function(el, record, column, data){
+        itemMoveFormatter(el, record, column, data, "down", this);
+    }
+
+    var itemDeleteFormatter = function(el, record, column, data){
+        el.innerHTML = "<span><strong>X</strong></span>";
+    }
+
+    // scheduler handlers for various events
+    /**
+     * @function addTextToSchedule
+     * @description Adds a text record row to the schedule and updates dynamic
+     * cells of the current row.
+     * 
+     */
+    var addTextToSchedule = function(event){
+        var currentItem = schedulerActions.currentItem;
+        var new_record_index = itemsDataTable.getTrIndex(currentItem) + 1;
+        itemsDataTable.addRow(
+            { 
+                item_id: scheduler_globals.initial_editor_text, 
+                item_type: "text" 
+            }, 
+            new_record_index
+        );
+        var new_record = itemsDataTable.getRecord(
+            itemsDataTable.getTrEl(new_record_index)
+        );
+        var target_columns = [
+            itemsDataTable.getColumn(ITEM_MOVE_UP_COLUMN),
+            itemsDataTable.getColumn(ITEM_MOVE_DOWN_COLUMN),
+        ]
+        itemsDataTable.unselectAllRows();
+        itemsDataTable.selectRow(new_record);
+        var updated_record = itemsDataTable.getRecord(
+            (new_record_index - 1 )
+        );
+        for (col_index=0; col_index<=(target_columns.length); col_index++){
+            itemsDataTable.updateCell(updated_record, 
+                target_columns[col_index],
+                updated_record.getData()
+            );
+        }
+        schedulerActions.close();
+    }
     
+    /**
+     * @method showCellEditor
+     * @description displays an editor to modify text records on the schedule
+     */
+    var showCellEditor = function(event){
+        var target = Event.getTarget(event);
+        var record = this.getRecord(target);
+        if (record.getData().item_type == "text"){
+            this.onEventShowCellEditor(event);
+        }
+    }
+
+    /**
+     * @method reorderRow
+     * @description moves an entry up or down the schedule when the up or
+     * down selectors are pushed
+     */
+    var reorderRow = function(args){
+        var target_column = itemsDataTable.getColumn(args.target);
+        if ([ITEM_MOVE_UP_COLUMN, ITEM_MOVE_DOWN_COLUMN].indexOf(
+                target_column.field
+            ) >= 0
+        ){
+            var target_record = this.getRecord(args.target);
+            var target_index = this.getTrIndex(target_record);
+            var record_count = this.getRecordSet().getLength();
+            var swap_rows = [];
+            if (target_column.field == ITEM_MOVE_UP_COLUMN){
+                if (target_index!=0){
+                    swap_rows = [target_index, (target_index - 1)]
+                }
+            }else{
+                if (target_index != (record_count-1)){
+                    swap_rows = [target_index, (target_index + 1)]
+                }
+            }
+            
+            if (swap_rows.length == 2){
+                var data_one = this.getRecord(swap_rows[0]).getData();
+                var data_two = this.getRecord(swap_rows[1]).getData();
+                this.updateRow(swap_rows[0], data_two)
+                this.updateRow(swap_rows[1], data_one)
+            }
+        }
+    }
+    
+    /**
+     * @method deleteRow
+     * @description deletes a row and record from the schedule. Displays
+     * a dialog to confirm intention before deleting item
+     * */
+    var deleteRow = function(args){
+        var target_record = this.getRecord(args.target);
+        var target_index = this.getTrIndex(target_record);
+        this.unselectAllRows();
+        this.selectRow(target_index);
+        schedulerActions.hide();
+        deleteDialog.show();
+    }
+
+    /**
+     * @method showSchedulerControls
+     * @description displays a contextual popup panel when a row is selected.
+     * Controls provide options to modify the schedule within the context of 
+     * the selected row.
+     */
+    var showSchedulerControls = function(args){
+        schedulerActions.currentItem = args.record;
+        schedulerActions.cfg.setProperty("context",
+            [args.el.id, "tl", "tr"]
+        );
+        schedulerActions.render();
+        schedulerActions.show();
+    }
+
+    /**
+     * @method anonymous
+     * @description Renders the panel UI and builds up schedule data table.
+     * Also binds various events to handlers on the data table.
+     * Schedule control actions are also built into the DOM
+     **/
     Event.onDOMReady(function(){
-        //create datatable controls
-        var scheduledActions = new YAHOO.widget.Panel("scheduled-item-controls",
+        //create scheduler actions panel
+        schedulerActions = new YAHOO.widget.Panel("scheduled-item-controls",
             {   
                 underlay: "none"
             }
         );
-        scheduledActions.currentItem = null;
+        schedulerActions.currentItem = null;
         
         var scheduleTextButton = new YAHOO.widget.Button(
             {
                 label: scheduler_globals.text_button_text,
             }
         );
-        scheduleTextButton.appendTo(scheduledActions.footer);
-        scheduleTextButton.on("click", function(event){
-            var new_record_index = itemsDataTable.getTrIndex(
-                scheduledActions.currentItem
-            ) + 1;
-            itemsDataTable.addRow(
-                { 
-                    item_id: scheduler_globals.initial_editor_text, 
-                    item_type: "text" 
-                }, 
-                new_record_index
-            );
-            var new_record = itemsDataTable.getRecord(
-                itemsDataTable.getTrEl(new_record_index)
-            );
-            itemsDataTable.unselectAllRows();
-            itemsDataTable.selectRow(new_record);
-            /*var firstCell = itemsDataTable.getFirstTdEl(
-                itemsDataTable.getSelectedTrEls()[0]
-            );
-            var editCell = itemsDataTable.getNextTdEl(firstCell);
-            //TODO - fire showCellEditor*/
-            scheduledActions.close();
-        });
-        var removeButton = new YAHOO.widget.Button(
+        scheduleTextButton.appendTo(schedulerActions.footer);
+        scheduleTextButton.on("click", addTextToSchedule);
+        
+        //create delete dialog and controls
+        deleteDialog = new YAHOO.widget.SimpleDialog("scheduler-delete-dialog",
             {
-                label: scheduler_globals.remove_button_text
+                width: "200px",
+                fixedcenter: true,
+                model: true,
+                visible: false,
+                draggable: false,
             }
         );
-        removeButton.appendTo(scheduledActions.footer);
+        deleteDialog.setHeader(scheduler_globals.delete_dialog_header);
+        deleteDialog.setBody(scheduler_globals.delete_dialog_text)
         
-        //var itemNumberFormatter = function(el, record, column, data){
-        //    el.innerHTML = itemsDataTable.getTrIndex(record) + 1;
-        //}
-        
-        var itemSelectFormatter = function(el, record, column, data){
-            index = itemsDataTable.getTrIndex(record) + 1;
-            el.innerHTML = "<input type='checkbox' name='rec-sel-" + index + "'/>"
+        var handleDeleteConfirm = function(){
+            selected_record_index = itemsDataTable.getSelectedRows()[0];
+            itemsDataTable.deleteRow(selected_record_index);
+            this.hide();
         }
+        
+        var handleDeleteCancel = function(){
+            this.hide();
+        }
+        
+        var deleteDialogButtons = [
+            {
+                text: scheduler_globals.delete_dialog_confirm, 
+                handler: handleDeleteConfirm
+            },
+            {
+                text: scheduler_globals.delete_dialog_cancel, 
+                handler: handleDeleteCancel,
+                isDefault: true
+            },
+        ] 
+        
+        deleteDialog.cfg.queueProperty("buttons", deleteDialogButtons);
+        deleteDialog.render(document.body);
         
         //create layout
         schedulerLayout = new YAHOO.widget.Layout("scheduler-layout",
@@ -102,9 +266,24 @@
                     editor: new YAHOO.widget.TextboxCellEditor(),
                 },
                 {key:"item_type", label: scheduler_globals.column_type},
+                {
+                    key:ITEM_MOVE_UP_COLUMN, 
+                    label:"", 
+                    formatter:itemMoveUpFormatter 
+                },
+                {
+                    key:ITEM_MOVE_DOWN_COLUMN, 
+                    label:"", 
+                    formatter:itemMoveDownFormatter
+                },
+                {
+                    key:ITEM_DELETE_COLUMN,
+                    label:"",
+                    formatter:itemDeleteFormatter
+                }
             ];
             
-            var itemsDataSource = new YAHOO.util.DataSource(
+            itemsDataSource = new YAHOO.util.DataSource(
                 scheduler_globals.json_listing_url
             );
             itemsDataSource.responseType = YAHOO.util.DataSource.TYPE_JSON;
@@ -118,28 +297,21 @@
             layout_centre.body.appendChild(scheduler_container);
 
             itemsDataTable = new YAHOO.widget.DataTable(scheduler_container,
-                columnDefinitions, itemsDataSource, { selectionMode:"single" }
+                columnDefinitions, itemsDataSource, 
+                { 
+                    selectionMode:"single",
+                    scrollable: true,
+                    width:"100%",
+                }
             );
             itemsDataTable.subscribe("rowMouseoverEvent", itemsDataTable.onEventHighlightRow);
             itemsDataTable.subscribe("rowMouseoutEvent", itemsDataTable.onEventUnhighlightRow);
             itemsDataTable.subscribe("rowClickEvent", itemsDataTable.onEventSelectRow);
-            //itemsDataTable.subscribe("onTextRecordAdded", itemsDataTable.onEventShowCellEditor);
-            itemsDataTable.subscribe("cellDblclickEvent", function(event){
-                var target = Event.getTarget(event);
-                var record = this.getRecord(target);
-                if (record.getData().item_type == "text"){
-                    itemsDataTable.onEventShowCellEditor(event);
-                }
-            });
-
-            itemsDataTable.subscribe("rowSelectEvent", function(args){
-                scheduledActions.currentItem = args.record;
-                scheduledActions.cfg.setProperty("context",
-                    [args.el.id, "tl", "tr"]
-                );
-                scheduledActions.render();
-                scheduledActions.show();
-            });
+            itemsDataTable.subscribe("cellDblclickEvent", showCellEditor);
+            itemsDataTable.subscribe("cellClickEvent", reorderRow);
+            itemsDataTable.subscribe("rowSelectEvent", showSchedulerControls);
+            itemsDataTable.subscribe("cellClickEvent", deleteRow);
+            
             return {
                 oDS: itemsDataSource,
                 oDT: itemsDataTable,

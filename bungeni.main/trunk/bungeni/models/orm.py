71bf8daefbcfd52738b67cc4fb59b495ff045657
mario.ruggier <mario.ruggier@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2012-01-12 16:43:35 +0000

    Correction/workaround of some anomalies in domain model mapping: - Added "item_signatories" attribute to ParliamentaryItem, that now returns a list of Signatory instaqnce; this should replace the current unconventionally named "itemsignatories" that strangely returns a list of User instances (seemingly only used by the reports, please correct). - Switched the (seemingly unused) singular ParliamentaryItem attribute "event_item" to a plural (and list) attribute named "event_items"; used it in code that generates auditing data.
    Streamlined how auditing data is fetched and prepared:
    - the code required per sub-item type is now almost identical (except for events, that are not yet properly audited)
    - no longer using any ad hoc queries to fetch data; all data fetches are via domain model attributes.
    - no longer need to import Session in ui/audit.py.
    
    Other src/comment corrections.

--- bungeni.main/trunk/bungeni/models/orm.py
+++ bungeni.main/trunk/bungeni/models/orm.py
@@ -405,21 +405,23 @@ mapper(domain.ParliamentaryItem, schema.parliamentary_items,
             uselist=False,
             lazy=False),
         # !+NAMING(mr, jul-2011) "itemsignatories" is inconsistent
-        # !+SIGNATORIES(mr, nov-2011) why this PLUS signatories managed container?        
+        # !+ITEMSIGNATORIES(mr, jan-2011) why items here are User and not 
+        # Signatory instances? Cleanout...
         "itemsignatories": relation(domain.User, secondary=schema.signatories),
-        # !+FILES(mr, nov-2011) why this PLUS files managed container?
+        # !+ITEMSIGNATORIES(mr, jan-2011) Adding conventionally named property 
+        # that should replace "itemsignatories"...
+        "item_signatories": relation(domain.Signatory),
         "attached_files": relation(domain.AttachedFile,
             # !+HEAD_DOCUMENT_ITEM(mr, sep-2011) standardize name, "head", 
             # "document", "item"
             backref=backref("item",
                 remote_side=schema.parliamentary_items.c.parliamentary_item_id)
         ),
-        # !+EVENT(mr, nov-2011) why this PLUS event managed container?
-        "event_item": relation(domain.EventItem,
+        "event_items": relation(domain.EventItem,
             primaryjoin=rdb.and_(
                 schema.parliamentary_items.c.parliamentary_item_id ==
                     schema.event_items.c.item_id),
-            uselist=False,
+            uselist=True,
             # !+HEAD_DOCUMENT_ITEM(mr, sep-2011) standardize name
             backref=backref("item",
                 remote_side=schema.parliamentary_items.c.parliamentary_item_id),

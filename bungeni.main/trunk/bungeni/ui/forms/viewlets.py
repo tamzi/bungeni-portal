3e3dcb4b82e4ed2fa26f898297285ffc63f10a6f
mario.ruggier <mario.ruggier@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2012-01-16 09:01:41 +0000

    Reverting changes in r8886 on these two files, as they are (a) not needed at all as it is just exact duplication of code just lines away, and (b) cause double rendering of the addresses viewlet for users (and the rendering duplication in turn fails with a URL resolution typerror. Please always review all modifications before checking/merging anything in.

--- bungeni.main/trunk/bungeni/ui/forms/viewlets.py
+++ bungeni.main/trunk/bungeni/ui/forms/viewlets.py
@@ -250,10 +250,6 @@ class CommitteesViewlet(SubformViewlet):
 class CommitteeStaffViewlet(SubformViewlet):
     sub_attr_name = "committeestaff"
     
-class AddressesViewlet(SubformViewlet):
-    sub_attr_name = "addresses"
-
-
 @register.viewlet(interfaces.IMemberOfParliament, manager=ISubFormViewletManager)
 @register.viewlet(interfaces.IBungeniGroup, manager=ISubFormViewletManager)
 class AddressesViewlet(SubformViewlet):

1cd9d07269c73101c8124e80a0346b1c4e63ee09
mianonjoka <mianonjoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2012-02-28 13:51:53 +0000

    Removed unused Javascript files. Updated advanced search template(part of r9065)

--- bungeni.main/branches/issue-743/bungeni/ui/js/behavior.js
+++ bungeni.main/branches/issue-743/bungeni/ui/js/behavior.js
@@ -1,26 +1,10 @@
-$(document).ready(function() {
-      // wire workflow dropdown menu to use POST actions
-      var menu_links = $('#context_workflow dd.actionMenuContent a');
-      menu_links.bungeniPostWorkflowActionMenuItem();
-
-      // set up parliament date range selection
-      $("select[id=form.parliament]").bungeniTimeRangeSelect(false, false);
-
-      // set up sitting time range form automation
-      var select = $("select[id=form.sitting_type_id]"); // !+group_sitting_type_id
-      var errors = select.parents("form").eq(0).find(".widget.error").length > 0;
-      select.bungeniTimeRangeSelect(true, !errors);
-
-      // set up calendar resizing
-      $("#calendar-table").bungeniSafeResize();
-      $("#scheduling-table tbody.reorder").dragRearrange();
-      $(".scheduling-checkbox").clickScheduling();
-                             
-      // set up calendar ajax
-      $('#weekly-calendar').bungeniCalendarInteractivity(true);
-      $('#daily-calendar').bungeniCalendarInteractivity(false);
-      
-      $('#scheduling-calendar').bungeniInteractiveSchedule();
-      $('#fieldset-mp-items').tablesorter();  
-      $('#fieldset-mp-items').columnFilters({alternateRowClassNames:['odd','even']});
-    });
+$(document).ready(
+  function(){
+    // wire workflow dropdown menu to use POST actions
+    var menu_links = $('#context_workflow dd.actionMenuContent a');
+    menu_links.bungeniPostWorkflowActionMenuItem();
+    
+    // set up parliament date range selection
+    $("select[id=form.parliament]").bungeniTimeRangeSelect(false, false);  
+  }
+);

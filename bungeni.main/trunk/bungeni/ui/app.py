31cb038cc8c692a4cb835645c4c84c7df9e93c57
mario.ruggier <mario.ruggier@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2012-06-21 12:16:16 +0000

    Adding a UI handler for IWSGIApplicationCreatedEvent and moving UI finish-setup calls to it.

---
+++ bungeni.main/trunk/bungeni/ui/app.py
@@ -0,0 +1,28 @@
+# Bungeni Parliamentary Information System - http://www.bungeni.org/
+# Copyright (C) 2010 - Africa i-Parliaments - http://www.parliaments.info/
+# Licensed under GNU GPL v2 - http://www.gnu.org/licenses/gpl-2.0.txt
+
+"""The Bungeni Application - UI Setup
+
+$Id$
+"""
+log = __import__("logging").getLogger("bungeni.ui.app")
+
+
+from ore.wsgiapp.interfaces import IWSGIApplicationCreatedEvent
+from bungeni.models.interfaces import IBungeniApplication
+from bungeni.utils import register
+
+
+@register.handler((IBungeniApplication, IWSGIApplicationCreatedEvent))
+def on_wsgi_application_created_event(application, event):
+    """Additional UI setup UI on IWSGIApplicationCreatedEvent.
+    """
+    
+    # catalyse descriptors
+    from bungeni.ui.descriptor import descriptor
+    descriptor.catalyse_descriptors()
+    
+    # ensure register workflow views
+    import bungeni.ui.workflow
+    

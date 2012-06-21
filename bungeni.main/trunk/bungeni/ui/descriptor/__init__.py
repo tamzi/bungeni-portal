6ceb63a0e09a5acc5d36c8f4c00316e7f78aca5a
mario.ruggier <mario.ruggier@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2012-06-21 12:16:16 +0000

    Adding a UI handler for IWSGIApplicationCreatedEvent and moving UI finish-setup calls to it.

--- bungeni.main/trunk/bungeni/ui/descriptor/__init__.py
+++
@@ -1,8 +0,0 @@
-
-# !+catalyse_descriptors(mr, jul-2011)
-from bungeni.ui.descriptor import descriptor
-descriptor.catalyse_descriptors()
-
-# !+ensure register workflow views
-import bungeni.ui.workflow
-

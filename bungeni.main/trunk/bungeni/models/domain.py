eef171a82cd7626a80b636123e38249ce41b25a6
borona.murithi <borona.murithi@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2012-06-26 07:53:52 +0000

    Fixing political group member container setup. Updated menus.
    - Renaming 'members' container on PoliticalGroup to 'group_members'
      - Fixes conflict with mapped orm property members
      - Fixing typo in setup of container (incorrect container name)
    - Updated PoliticalGroupMember menus
      - Registration against interfaces and container address
    - Adding marker interface IPoliticalGroupMemberContainer
      - Auto-generated contaieners are not initialized by the time menus are loaded

--- bungeni.main/trunk/bungeni/models/domain.py
+++ bungeni.main/trunk/bungeni/models/domain.py
@@ -470,8 +470,8 @@ class PoliticalGroup(Group):
     """A political group in a parliament.
     """
     interface.implements(interfaces.ITranslatable)
-    members = one2many("members",
-        "bungeni.models.domain.PolitcalGroupMemberContainer", "group_id")
+    group_members = one2many("members",
+        "bungeni.models.domain.PoliticalGroupMemberContainer", "group_id")
     title_types = one2many("title_types",
         "bungeni.models.domain.TitleTypeContainer", "group_id")
 class PoliticalGroupMember(GroupMembership):

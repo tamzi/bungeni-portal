f13199781adf1298480b8d7ac875539613347bc2
borona.murithi <borona.murithi@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2012-08-03 13:17:38 +0000

    [workflow] Changed how redirects after workflow transitions work. Updated menus.
    - Adding WorkflowRedirect browser view - default for all redirects after workflow transitions.
      - Updated workflow menu generator
    - Adding customized domain.Sitting workflow redirect view - relocates to schedule editor if current state is a draft state.
      - Falls back to 'view' of sitting for all other states
    
    Other:
    - Cleaning out unused imports.
    - Conforming redirects module to common header style.

--- bungeni.main/trunk/bungeni/ui/menu.py
+++ bungeni.main/trunk/bungeni/ui/menu.py
@@ -303,9 +303,9 @@ class WorkflowMenu(BrowserMenu):
         _url = url.absoluteURL(context, request)
         results = []
         for tid in tids:
-            transit_url = \
-                "%s/change_workflow_state?transition_id=%s&next_url=..." % (
-                    _url, tid)
+            transit_url = ("%s/change_workflow_state?transition_id=%s&"
+                "next_url=./workflow-redirect" % (_url, tid)
+            )
             extra = {"id": "workflow-transition-%s" % tid,
                      "separator": None,
                      "class": ""}

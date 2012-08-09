4073b4a5bc69b1545d24d05a2e320b2ffaa5ad83
aowino <aowino@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2012-08-09 09:45:40 +0000

    Adding placeholder/prompt for search fields on listings

--- bungeni.main/trunk/bungeni/ui/templates/text-input-search-widget.js
+++ bungeni.main/trunk/bungeni/ui/templates/text-input-search-widget.js
@@ -1,13 +1,19 @@
 <script type="text/javascript">(function($) {
+        counter = 0;
         $(document).ready(function() {
                 var input = document.createElement('input');
                 input.setAttribute('type', 'text');
                 input.setAttribute('name', 'filter_%(field_id)s');
                 input.setAttribute('id', 'input_%(field_id)s');
+                if (counter == 0)
+                    input.setAttribute('placeholder', '  type to search...');
+                else
+                    input.setAttribute('placeholder', '  ...')
                 var table_columns = window.%(table_id)s.getColumnSet();
                 var thEl = table_columns.getColumn('%(field_id)s').getThEl();
                 thEl.innerHTML = "";
                 thEl.appendChild(input);
+                counter++;
             });
     })(jQuery);
 </script>

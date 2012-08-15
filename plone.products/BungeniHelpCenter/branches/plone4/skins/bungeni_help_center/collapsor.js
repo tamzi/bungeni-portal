8fd65976f7a33f13723190ee93dc84fe6d114114
fraogongi <fraogongi@gmail.com>
2012-08-15 07:33:48 +0000

    [BungeniHelpCenter] Updating collapsor for empty and non-empty content.

--- plone.products/BungeniHelpCenter/branches/plone4/skins/bungeni_help_center/collapsor.js
+++ plone.products/BungeniHelpCenter/branches/plone4/skins/bungeni_help_center/collapsor.js
@@ -3,7 +3,26 @@ window.onload = function(){
     var siblingDivs = h.siblings('div');
     var siblingHR = h.siblings('hr');
     
-    h.css({cursor:'pointer', background: 'url(plus.png) 2px 2px no-repeat', paddingLeft: '20px'});
+    for (var i = 0; i < descDiv.length; i++) {
+        var currDiv = descDiv[i]; 
+        var siblingH = jq(currDiv).siblings('h1');         
+        var descDivLength = currDiv.innerHTML.length; 
+        
+        if ( descDivLength <= 0 ) {
+            for ( var j = 0; j < siblingH.length; j++ ) {
+                var currH = jq(siblingH[j]);
+                currH.css({cursor:'pointer', paddingLeft: '20px'});
+            }
+                       
+        } else if ( descDivLength > 0 ) {        
+            for ( var j = 0; j < siblingH.length; j++ ) {
+                var currH = jq(siblingH[j]);
+                //currH.css({cursor:'pointer', background: 'url(plus.png) 2px 2px no-repeat', paddingLeft: '20px'});
+                currH.css({cursor:'pointer', paddingLeft: '20px'});
+            }
+        }
+    }
+
     // hide the siblings
     siblingDivs.hide();
     siblingHR.hide();
@@ -16,26 +35,54 @@ window.onload = function(){
         // get the state of the content
         var state = divs.css('display');
         if(state == 'hidden' || state == 'none'){
-                jq(this).css('background-image','url(minus.png)');
+                //jq(this).css('background-image','url(minus.png)');
                 divs.slideDown('fast');
                 hr.show();
         }
         else{
-                jq(this).css('background-image','url(plus.png)');
+                //jq(this).css('background-image','url(plus.png)');
                 divs.slideUp('fast');
                 hr.hide();
         }
     });
     
     
+    
+    
+    
     // table of contents collapsing script
     var toc = jq('td.portlet-toc-item');
     var tocHeading = jq('td.portlet-toc-item div');
     // create the toggle element and add it to the DOM
-    var toggle = jq(document.createElement('span'));
+    /*var toggle = jq(document.createElement('span'));
     toggle.addClass('togglr');
     toggle.css({cursor:'pointer', background: 'url(plus.png) 2px 2px no-repeat', paddingLeft: '1.35em'});
-    tocHeading.prepend(toggle);
+    tocHeading.prepend(toggle);*/
+    
+    
+    // Section has been added to allow the +/- images to be displayed next to 
+    // the P tag elements that have content within them.  
+    // Get the P tags
+    var descP = jq('td.portlet-toc-item p'); 
+    for (var m = 0; m < descP.length; m++) {
+    
+        var currP = descP[m];
+        var siblingHeader = jq(currP).siblings('div');
+        var currPLength = currP.innerHTML.length;
+        
+        if (currPLength <= 0) {
+        } else if (currPLength > 0) {
+            var toggle = jq(document.createElement('span'));
+            toggle.addClass('togglr');
+            toggle.css({cursor:'pointer', background: 'url(plus.png) 2px 2px no-repeat', paddingLeft: '1.35em'});
+            
+            for (var n = 0; n < siblingHeader.length; n++) {
+                var currHeadDiv = jq(siblingHeader[n]);
+                currHeadDiv.prepend(toggle);
+            } 
+        }
+    }
+    
     
     jq('td.portlet-toc-item td.portlet-toc-item span.togglr').remove();
     

5035fb614cfef50acf8def86173f464b4479a794
mario.ruggier <mario.ruggier@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2012-08-15 14:10:50 +0000

    [invariants] Moving schema invariants from descriptor module to constraints module alongside it, to lighten up the large descriptor module.
    Disabling unused DissolutionAfterReinstatement invariant.

--- bungeni.main/trunk/bungeni/ui/descriptor/constraints.py
+++ bungeni.main/trunk/bungeni/ui/descriptor/constraints.py
@@ -2,13 +2,17 @@
 # Copyright (C) 2010 - Africa i-Parliaments - http://www.parliaments.info/
 # Licensed under GNU GPL v2 - http://www.gnu.org/licenses/gpl-2.0.txt
 
-"""Field constraints
+"""Field constraints and schema invariants
 
 $Id$
 """
 import re
 import zope.schema
+from zope.interface import Invalid
 from bungeni.ui.i18n import _
+
+
+# field constraints
 
 EMAIL_RE = ("([0-9a-zA-Z_&.+-]+!)*[0-9a-zA-Z_&.+-]+@"
     "(([0-9a-zA-Z]([0-9a-zA-Z-]*[0-9a-z-A-Z])?\.)"
@@ -43,4 +47,88 @@ class RegexChecker(object):
 
 check_email = RegexChecker(EMAIL_RE, _(u"This is not a valid email address"))
 check_login = RegexChecker(LOGIN_RE, _(u"Invalid login name"))
+
+
+# schema invariants
+
+def ElectionAfterStart(obj):
+    """Start Date must be after Election Date."""
+    if obj.election_date >= obj.start_date:
+        raise Invalid(
+            _("The life of a parliament must start after its election"),
+            "election_date",
+            "start_date"
+        )
+
+def EndAfterStart(obj):
+    """End Date must be after Start Date."""
+    if obj.end_date is None: return
+    if obj.end_date <= obj.start_date:
+        raise Invalid(
+            _("End Date must be after Start Date"),
+            "start_date",
+            "end_date"
+        )
+
+''' !+UNUSED
+def DissolutionAfterReinstatement(obj):
+    """A committee must be disolved before it can be reinstated."""
+    if (obj.dissolution_date is None) or (obj.reinstatement_date is None):
+        return
+    if obj.dissolution_date > obj.reinstatement_date:
+        raise Invalid(
+            _("A committee must be disolved before it can be reinstated"),
+            "dissolution_date",
+            "reinstatement_date"
+        )
+'''
+
+def ActiveAndSubstituted(obj):
+    """A person cannot be active and substituted at the same time."""
+    if obj.active_p and obj.replaced_id:
+        raise Invalid(
+            _("A person cannot be active and substituted at the same time"),
+            "active_p",
+            "replaced_id"
+        )
+
+def SubstitudedEndDate(obj):
+    """If a person is substituted he must have an end date."""
+    if not obj.end_date and obj.replaced_id:
+        raise Invalid(
+            _("If a person is substituted End Date must be set"),
+            "replaced_id",
+            "end_date"
+        )
+
+def InactiveNoEndDate(obj):
+    """If you set a person inactive you must provide an end date."""
+    if not obj.active_p:
+        if not (obj.end_date):
+            raise Invalid(
+                _("If a person is inactive End Date must be set"),
+                "end_date",
+                "active_p"
+            )
+
+def MpStartBeforeElection(obj):
+    """MP start date (when set) must be after election."""
+    if obj.election_nomination_date is None:
+        return
+    if obj.election_nomination_date > obj.start_date:
+        raise Invalid(_("A parliament member has to be "
+                "elected/nominated before she/he can be sworn in"),
+            "election_nomination_date",
+            "start_date"
+        )
+
+def DeathBeforeLife(User):
+    """Check if date of death is after date of birth."""
+    if User.date_of_death is None: return
+    if User.date_of_death < User.date_of_birth:
+        raise Invalid(_("Check dates: death must follow birth"),
+            "date_of_death",
+            "date_of_birth"
+        )
+
 

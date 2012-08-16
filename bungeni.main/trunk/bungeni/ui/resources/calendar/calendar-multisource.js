be04f69452a563b3d3764c23b82275d434070d00
aowino <aowino@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2012-08-16 15:30:56 +0000

    Scheduling, Listing and Calendar updates - Use return key to sort documents and UI updates to scheduler ; Updated vanilla reset

--- bungeni.main/trunk/bungeni/ui/resources/calendar/calendar-multisource.js
+++ bungeni.main/trunk/bungeni/ui/resources/calendar/calendar-multisource.js
@@ -38,4 +38,38 @@ $(document).ready(function(){
             }
         }
     );
+    
+    $("#committees_tab").click(function() {
+            if (!scheduler.config.readonly){
+                scheduler.config.readonly = true;
+                edit_mode_turned_off = true;
+            }
+            if (loaded_calendars.length==0){
+                loaded_calendars.push(scheduler._load_url);
+            }
+            var ev_color = $(this).attr("rel");
+            var cal_url = $(this).val();
+            var is_checked = $(this).attr("checked");
+            cal_url = cal_url + "/dhtmlxcalendar?uid=" + scheduler.uid() + "&amp;color=" + "00FF00";
+            if (!is_checked){
+                if (loaded_calendars.indexOf(cal_url) < 1){
+                    loaded_calendars.push(cal_url);
+                    scheduler._loaded = {};
+                    scheduler.load(cal_url);
+                }
+            }else{
+                loaded_calendars.pop(cal_url);
+                if (loaded_calendars.length > 0){
+                    scheduler.clearAll();
+                    for (cal_id=0; cal_id<loaded_calendars.length; cal_id++){
+                        scheduler._loaded = {};
+                        scheduler.load(loaded_calendars[cal_id]);
+                    }
+                }
+                if ((loaded_calendars.length == 1) && (edit_mode_turned_off)){
+                    scheduler.config.readonly = false;
+                }
+            }
+        }
+    );
 });

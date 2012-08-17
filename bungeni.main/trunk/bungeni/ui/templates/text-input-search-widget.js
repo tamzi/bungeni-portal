bb1ca9ea8cb1b78c879fae388d9274e5294ff0b3
mario.ruggier <mario.ruggier@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2012-08-17 13:46:47 +0000

    [listing filter] Undoing breakage in r9738, that was causing all filter params sent in the request (when any one is set to something) to be all serialised as "undefined", so breaking the listing filtering UI.
    Disabling the added handler (to simulate clicking on column, when ENTER is entered in a filter field, and was the reaosn of the change in the first place) as it does NOT do the equivalent of "clicking" on column heading (it does not set the sort param, should be something like sort_{column_name}).

--- bungeni.main/trunk/bungeni/ui/templates/text-input-search-widget.js
+++ bungeni.main/trunk/bungeni/ui/templates/text-input-search-widget.js
@@ -4,7 +4,7 @@
                 var input = document.createElement('input');
                 input.setAttribute('type', 'text');
                 input.setAttribute('name', 'filter_%(field_id)s');
-                input.setAttribute('id', 'input_%(field_id)s');
+                input.setAttribute('id', 'input_%(table_id)s_%(field_id)s');
                 if (counter == 0)
                     input.setAttribute('placeholder', '  type to search...');
                 else
@@ -13,13 +13,19 @@
                 var thEl = table_columns.getColumn('%(field_id)s').getThEl();
                 thEl.innerHTML = "";
                 thEl.appendChild(input);
-                // add listeners for ENTER key on listing input fields
-                $("#input_%(field_id)s").keyup(function(event){
-                    if(event.keyCode == 13){
-                        $("#yui-dt0-th-sort_%(field_id)s").live("click", table.sortColumn("yui-col"+counter+"", "none"));
-                    }
-                });
+                // add listener for ENTER key on listing input fields
+                // !+SORT_MISSING(mr, aug-2012) disabling the following handler 
+                // as it does NOT the equivalent of "clicking" on column heading
+                // (it does not set the sort param, should be something like:
+                // sort_{column_name}
+                // 
+                //$("#input_%(table_id)s_%(field_id)s").keyup(function(event){
+                //    if(event.keyCode == 13){
+                //        $("#yui-dt0-th-sort_%(field_id)s").live("click", table.sortColumn("yui-col"+counter+"", "none"));
+                //    }
+                //});
                 counter++;
             });
     })(jQuery);
 </script>
+

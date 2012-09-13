dc90eb1d8af0d94ceb3ef8ea64a85b14c27a86b4
borona.murithi <borona.murithi@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2012-09-13 14:48:56 +0000

    [private branch] initial updates to dhtmlx-based scheduler to save records via forms machinery.
    - sittings and recurring sitting instances now saved using base AddForm bound to sittings context
    - updated TextDateTimeWidget to parse combined date/time string (as is the case with dhtmlx data)
    - turning off 'same day validator' for sittings - does not apply to recurring events (initial event span extends to last event)
    - add form instance now has an 'added_object' property - TODO add proper api
    
    - serialization - catching any exception during publish_to_xml call (exceptions kill serialization worker threads) 
      - TODO implement respawning setup or pool

--- bungeni.main/branches/murithi-private/bungeni/ui/forms/common.py
+++ bungeni.main/branches/murithi-private/bungeni/ui/forms/common.py
@@ -148,7 +148,6 @@ class BaseForm(formlib.form.FormBase):
         else:
             # view api
             super(BaseForm, self).__init__(context, request)
-        
         if str(self.request.get("headless", "")).lower() in TRUE_VALS:
             self.setPrefix(NO_PREFIX)
 
@@ -164,17 +163,6 @@ class BaseForm(formlib.form.FormBase):
         next_url = self._next_url = self.request.get("next_url", None)
         if next_url == "...":
             self._next_url = self.request.get("HTTP_REFERER", "")
-
-    def __call__(self):
-        #session = Session()
-        # XXX control the display order of the submit buttons 
-        # the order seems to be determined by the self.actions.actions 
-        # tuple of zope.formlib.form.Action instances
-        print "XXX Order of Form Submit Buttons:", [ (a.name, a.label)
-                                                for a in self.actions.actions ]
-        call = super(BaseForm, self).__call__()
-        #session.close()
-        return call
 
     @property
     def widget_groups(self):
@@ -375,6 +363,7 @@ class AddForm(BaseForm, catalyst.AddForm):
     def createAndAdd(self, data):
         added_obj = super(AddForm, self).createAndAdd(data)
         cascade_modifications(added_obj)
+        self.created_object = added_obj
         return added_obj
     
     @formlib.form.action(

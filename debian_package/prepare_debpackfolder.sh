2b7ce4210b9902466ff736d2fc9bfd40af2bd1d1
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2012-09-28 10:17:21 +0000

    Setting version in control file via the packaging script

--- debian_package/prepare_debpackfolder.sh
+++ debian_package/prepare_debpackfolder.sh
@@ -11,22 +11,30 @@ then
  exit 65
 fi
 
-BUNGENI_REL="bungeni_1.0+$1"
+BUNGENI_VER="1.0"
+BUNGENI_REL="$BUNGENI_VER+$1"
+BUNGENI_REL_FOLDER="bungeni_$BUNGENI_REL"
 BUNGENI_TAR=$2
 echo "found bungeni in $BUNGENI_TAR"
 
-cp -R bungeni_version_revision $BUNGENI_REL
+cp -R bungeni_version_revision $BUNGENI_REL_FOLDER
 
-find ./$BUNGENI_REL -name '.svn' -print0 | xargs -0 rm -rf
+find ./$BUNGENI_REL_FOLDER -name '.svn' -print0 | xargs -0 rm -rf
 
-tar xvf $BUNGENI_TAR --directory=./$BUNGENI_REL/debian
+echo "Setting version in control file"
 
-echo "Now run will attempt to execute run.sh in the $BUNGENI_REL"
+sed -i "s/__BUNGENI_VER__/$BUNGENI_REL/g" ./$BUNGENI_REL_FOLDER/debian/DEBIAN/control
+
+echo "Adding bungeni to debian package"
+
+tar xvf $BUNGENI_TAR --directory=./$BUNGENI_REL_FOLDER/debian
+
+echo "Now run will attempt to execute run.sh in the $BUNGENI_REL_FOLDER"
 
 read -p "Are you sure (Yy) ? " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-   cd $BUNGENI_REL && ./run.sh
+   cd $BUNGENI_REL_FOLDER && ./run.sh
 fi
 
 

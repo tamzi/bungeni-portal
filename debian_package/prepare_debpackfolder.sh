9d865f2d38ff9463cc59bf9ebf2b097c94efd8da
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2012-10-04 08:55:58 +0000

    Parametrized Architecture specification

--- debian_package/prepare_debpackfolder.sh
+++ debian_package/prepare_debpackfolder.sh
@@ -3,11 +3,11 @@
 
 #set -x verbose
 
-EXPECTED_ARGS=2
+EXPECTED_ARGS=3
 
 if [ $# -ne $EXPECTED_ARGS ] 
 then
- echo "Usage: `basename $0` <release name> <pre-built-bungeni.tar.gz>"
+ echo "Usage: `basename $0` <release name> <pre-built-bungeni.tar.gz> <arch>"
  exit 65
 fi
 
@@ -15,6 +15,8 @@ BUNGENI_VER="1.0"
 BUNGENI_REL="$BUNGENI_VER+$1"
 BUNGENI_REL_FOLDER="bungeni_$BUNGENI_REL"
 BUNGENI_TAR=$2
+BUNGENI_ARCH=$3
+
 echo "found bungeni in $BUNGENI_TAR"
 
 cp -R bungeni_version_revision $BUNGENI_REL_FOLDER
@@ -24,6 +26,7 @@ find ./$BUNGENI_REL_FOLDER -name '.svn' -print0 | xargs -0 rm -rf
 echo "Setting version in control file"
 
 sed -i "s/__BUNGENI_VER__/$BUNGENI_REL/g" ./$BUNGENI_REL_FOLDER/debian/DEBIAN/control
+sed -i "s/__ARCH__/$BUNGENI_ARCH/g" ./$BUNGENI_REL_FOLDER/debian/DEBIAN/control
 
 echo "Adding bungeni to debian package"
 

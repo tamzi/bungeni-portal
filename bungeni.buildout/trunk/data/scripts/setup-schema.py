abce49b2e134d26eb81027eb827092c3a360e6e8
mianonjoka <mianonjoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2012-10-04 14:59:49 +0000

    Stamp the current revision of the most recent alembic migration script in the database in order to support movement between out of date databases

--- bungeni.buildout/trunk/data/scripts/setup-schema.py
+++ bungeni.buildout/trunk/data/scripts/setup-schema.py
@@ -16,6 +16,14 @@ from sqlalchemy import create_engine
 schema.metadata.bind = db = create_engine('postgres://localhost/bungeni')
 schema.metadata.drop_all()
 schema.metadata.create_all()
+
+# load the Alembic configuration and generate the
+# version table, "stamping" it with the most recent rev:
+from alembic.config import Config
+from alembic import command
+alembic_cfg = Config("alembic.ini")
+command.stamp(alembic_cfg, "head")
+
 #schema.translation_lookup_index.create(db)
 
 security.metadata.bind = db

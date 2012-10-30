8dd8c57f6308a7cdbc9820eb4f7183053390ba6e
mario.ruggier <mario.ruggier@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2012-10-30 14:51:42 +0000

    [declarative] Refactoring workspace edit and delete menu items to be feature-based and dynamically created and registered (note: using generic layer IBungeniSkin layer (as opposed to the soon-to-be obsoleted IWorkspaceOrAdminSectionLayer). Deleting previously hard-wired menu item registrations in ui/menu.zcml. Refining ui.feature module code.
    Adding a revisit comment for audit timesamp, for when a change in a sub-object causes the timestamp of head doc to be updated.

--- bungeni.main/trunk/bungeni/ui/feature.py
+++ bungeni.main/trunk/bungeni/ui/feature.py
@@ -20,7 +20,7 @@ def apply_customization_ui():
     very late) that need to have been executed prior to this e.g. 
     creation of specific menus such as "context_actions".
     """
-    zcml_slug = """
+    ZCML_SLUG = """
         <configure xmlns="http://namespaces.zope.org/zope"
             xmlns:browser="http://namespaces.zope.org/browser"
             xmlns:i18n="http://namespaces.zope.org/i18n"
@@ -28,43 +28,89 @@ def apply_customization_ui():
             >
             <include package="zope.browsermenu" file="meta.zcml" />
             <include package="zope.browserpage" file="meta.zcml" />
-%s
+{zcml_decls}
         </configure>
         """
-    zcml_decls = []
     
-    add_menu_item_tmpl = """
-            <browser:menuItem menu="%s"
-                for="*"
-                title="%s"
-                action="../../draft/add_%s"
-                permission="bungeni.%s.Add"
-            />"""
-    
-    add_view_tmpl = """
-            <browser:page name="add_%s"
-                for="bungeni.core.interfaces.IWorkspaceDraft"
-                class="bungeni.ui.workspace.WorkspaceAddForm"
+    MENU_ITEM_TMPL = """
+            <browser:menuItem menu="{menu}"
+                for="{for_}"
+                action="{action}"
+                title="{title}"
+                order="{order}"
+                permission="bungeni.{type_key}.{privilege}"
                 layer="bungeni.ui.interfaces.IBungeniSkin"
-                permission="bungeni.%s.Add"
             />"""
+    menu_item_vars_Add = dict(
+        menu="workspace_add_parliamentary_content",
+        privilege="Add",
+        order=7,
+    )
+    # !+ edit/delete used to be on layer="bungeni.ui.interfaces.IWorkspaceOrAdminSectionLayer"
+    menu_item_vars_Edit = dict(
+        menu="context_actions",
+        privilege="Edit",
+        order=10,
+    )
+    menu_item_vars_Delete = dict(
+        menu="context_actions",
+        privilege="Delete",
+        order=99,
+    )
     
+    VIEW_TMPL = """
+            <browser:page name="add_{type_key}"
+                for="{for_}"
+                class="{class_}"
+                permission="bungeni.{type_key}.{privilege}"
+                layer="bungeni.ui.interfaces.IBungeniSkin"
+            />"""
+    view_vars_Add = dict(
+        for_="bungeni.core.interfaces.IWorkspaceDraft",
+        class_="bungeni.ui.workspace.WorkspaceAddForm",
+        privilege="Add",
+    )
+    
+    _decls = []
     # we assume that non-custom types have already been set up as needed
     for type_key, ti in capi.iter_type_info(scope="custom"):
         
+        type_title = naming.split_camel(naming.model_name(type_key))
+        model_interface_qualname = "bungeni.models.interfaces.%s" % (
+                naming.model_interface_name(type_key))
+        
         if ti.workflow.has_feature("workspace"):
             log.debug("Setting up workspace UI for type [%s]" % (type_key))
+            # add menu item
             # !+workspace_feature_add(mr, oct-2012) note that an enabled
             # workspace feature also implies "add" functionality for the type
-            zcml_decls.append(add_menu_item_tmpl % (
-                    "workspace_add_parliamentary_content",
-                    naming.split_camel(naming.model_name(type_key)),
-                    type_key,
-                    type_key))
-            zcml_decls.append(add_view_tmpl % (type_key, type_key))
+            _decls.append(MENU_ITEM_TMPL.format(
+                    type_key=type_key, 
+                    title=type_title,
+                    for_="*",
+                    action="../../draft/add_{k}".format(k=type_key),
+                    **menu_item_vars_Add))
+            # edit menu item
+            _decls.append(MENU_ITEM_TMPL.format(
+                    type_key=type_key, 
+                    title="Edit {t}".format(t=type_title),
+                    for_=model_interface_qualname,
+                    action="edit",
+                    **menu_item_vars_Edit))
+            # delete menu item
+            _decls.append(MENU_ITEM_TMPL.format(
+                    type_key=type_key, 
+                    title="Delete {t}".format(t=type_title),
+                    for_=model_interface_qualname,
+                    action="delete",
+                    **menu_item_vars_Delete))
+            # add view
+            _decls.append(VIEW_TMPL.format(
+                    type_key=type_key, 
+                    **view_vars_Add))
     
     # combine config string and execute it
-    zcml = zcml_slug % ("".join([ zd for zd in zcml_decls ]))
+    zcml = ZCML_SLUG.format(zcml_decls="".join([ zd for zd in _decls ]))
     log.debug("Executing UI feature configuration:\n%s" % (zcml))
     xmlconfig.string(zcml)
 

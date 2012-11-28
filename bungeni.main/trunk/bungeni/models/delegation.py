d8eb7ebffcd1284a9bfe2cdc47b8104a7497e94f
mario.ruggier <mario.ruggier@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2012-11-28 10:37:28 +0000

    [vocabulary doc] Evolved vocabulary doc to distinguish between static and dynamic vocabularies. Added listing of currently available dynamic vocabularies.
    Highlighted issue of the logic of "parliament_member_delegation" vocabulary, namely that delegating to teh Clerk will remove the Clerks

--- bungeni.main/trunk/bungeni/models/delegation.py
+++ bungeni.main/trunk/bungeni/models/delegation.py
@@ -10,14 +10,13 @@ def get_user_delegations(user_id):
     both delegator and delegee must be active
     """
     session = Session()
-    query = session.query(domain.UserDelegation).filter(
-                rdb.and_(
-                domain.UserDelegation.delegation_id == user_id)
-                )
+    query = session.query(domain.UserDelegation
+        ).filter(rdb.and_(domain.UserDelegation.delegation_id == user_id))
     results = query.all()
     for result in results:
-        if ((result.user.active_p == 'A') and
-            (len(result.user.login) > 1) and
-            (result.delegation.active_p == 'A')):
+        if ((result.user.active_p == "A") and
+                (len(result.user.login) > 1) and
+                (result.delegation.active_p == "A")
+            ):
             yield result.user
-    #session.close()
+

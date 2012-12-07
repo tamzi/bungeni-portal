c9ffa10898b22244dfb30194875f7aa491f70307
samweru <samweru@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2012-12-07 09:29:23 +0000

    [Deb-Pack] updates - bungeni kill epmd; bungeni logout user; exist maintain postrm, prerm scripts

---
+++ debian_package_update/trunk/bungeni_force-user-logout/bungeni.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -v
+
+. _bashtasklog.sh
+. _debpackfunctions.sh
+
+new bashtasklog logger
+
+BUNGENI_PACKAGE_DATE=$(getdate)
+BUNGENI_REL_FOLDER="bungeni-update_${BUNGENI_PACKAGE_DATE}"
+BUNGENI_ARCH=$(getarchtype)
+BUNGENI_SIZE=0
+
+logger.printTask "[Bungeni] Setting up debian package folder."
+cp -R bungeni_version_revision $BUNGENI_REL_FOLDER
+rm -rf `find ./${BUNGENI_REL_FOLDER} -type d -name .svn`
+
+logger.printTask "[Bungeni] Setting version in control file."
+sed -i "s/__PACKAGE_NAME__/bungeni-update/g" ./$BUNGENI_REL_FOLDER/debian/DEBIAN/control
+sed -i "s/__BUNGENI_VER__/1.0-${BUNGENI_PACKAGE_DATE}/g" ./$BUNGENI_REL_FOLDER/debian/DEBIAN/control
+sed -i "s/__ARCH__/${BUNGENI_ARCH}/g" ./$BUNGENI_REL_FOLDER/debian/DEBIAN/control
+sed -i "s/__SIZE__/${BUNGENI_SIZE}/g" ./$BUNGENI_REL_FOLDER/debian/DEBIAN/control
+
+cd $BUNGENI_REL_FOLDER && ./run.sh $BUNGENI_ARCH
+
+

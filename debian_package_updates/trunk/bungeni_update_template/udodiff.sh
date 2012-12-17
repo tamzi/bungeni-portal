fbb50274190d29207ce04826298a9bd4e20e2969
samweru <samweru@gmail.com>
2012-12-17 14:53:22 +0000

    [Deb-Pack] created updates folder all updates will be saved and installed from /opt/bungeni/updates

--- debian_package_updates/trunk/bungeni_update_template/udodiff.sh
+++ debian_package_updates/trunk/bungeni_update_template/udodiff.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 #diff
-diff -rq previous/data/ latest/data | grep -vf filter.words | grep "^Files.*differ$" | sed 's/^Files \(.*\) and .* differ$/\1/' | cut -b -14 --complement > common.diff
+diff -rq previous/data/ latest/data | grep -vf filter.words | grep "^Files.*differ$" | sed 's/^Files \(.*\) and .* differ$/\1/' | cut -b -13 --complement > common.diff
 
 #only in previous
-diff -rq previous/data/ latest/data | grep -vf filter.words | grep "Only in previous" | sed 's/Only in //g' | sed 's/: /\//g' > exclude.diff
+diff -rq previous/data/ latest/data | grep -vf filter.words | grep "Only in previous" | sed 's/Only in //g' | sed 's/: /\//g' | cut -b -13 --complement > exclude.diff
 
 #only in latest
-diff -rq previous/data/ latest/data | grep -vf filter.words | grep "Only in latest" | sed 's/Only in //g' | sed 's/: /\//g' | cut -b -12 --complement > include.diff
+diff -rq previous/data/ latest/data | grep -vf filter.words | grep "Only in latest" | sed 's/Only in //g' | sed 's/: /\//g' | cut -b -11 --complement > include.diff
+
---
+++ debian_package_updates/trunk/bungeni_update_template/update/cleanup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -rf bungeni*
+#rm -rf update_version_revision/debian/opt/updates/*

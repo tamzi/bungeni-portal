a46f78f660320569ee03347174b0848b77a40633
mianonjoka <mianonjoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2012-12-21 14:40:37 +0000

    RERUN BUILDOUT. Added a new package, bungeni.capi, that will eventually contain all the configuration parsing code. Within this package there is a test_bungeni_custom package that contains a bungeni_custom that is used in the tests. This ensures that there is always a consistent configuration for the tests separate from the bungeni_custom that contains the customisations of a particular parliament.
    Modified the buildout to use the new test_bungeni_custom

---
+++ bungeni.main/trunk/bungeni/capi/README.txt
@@ -0,0 +1,5 @@
+This package will eventually contain all the configuration parsing code for
+Bungeni.
+
+The test_bungeni_custom contains a sample configuration that is used by the tests
+in Bungeni
---
+++ bungeni.main/trunk/bungeni/capi/test_bungeni_custom/__init__.py
@@ -0,0 +1,49 @@
+# Bungeni Parliamentary Information System - http://www.bungeni.org/
+# Copyright (C) 2010 - Africa i-Parliaments - http://www.parliaments.info/
+# Licensed under GNU GPL v2 - http://www.gnu.org/licenses/gpl-2.0.txt
+
+"""Bungeni Custom
+
+Package to include deployment-specific customizations -- this package should 
+be replicated and modified as necessary. 
+
+To specify a deployment's customizations package simply configure the 
+python path appropriately; a way to do this is to add a ".pth" file in 
+the deployment's python site-packages folder, containing a single line 
+with the path to the parent folder of the "bungeni_custom" package.
+
+For example, create the file at:
+/home/{USER}/cinst/python25/lib/python2.5/site-packages/bungeni_custom.pth
+
+Containing the one line:
+/home/{USER}/cinst/bungeni/src
+
+
+$Id: __init__.py 9809 2012-09-04 15:58:41Z mario.ruggier $
+"""
+
+# language ids, either as a space-separated string or as a list of strings
+zope_i18n_allowed_languages = "ar en es es-do fr it nl pt ru sw tk" 
+
+# boolean: True, False
+zope_i18n_compile_mo_files = True 
+
+# string, language identifier, must be one of zope_i18n_allowed_languages
+default_language = "en"
+
+# language ids for languages written right to left, as a space-separated string
+right_to_left_languages = "ar" 
+
+# integer, minimum number of seconds to wait between checks for whether a 
+# localization file needs reloading; 0 means never check (deployment)
+check_auto_reload_localization = 5
+
+# default number of items in listings
+default_number_of_listing_items = 100
+
+# number of seconds in between workspace tab count refresh
+workspace_tab_count_cache_refresh_time = 120
+
+# when listing text columns, only display first so many characters
+long_text_column_listings_truncate_at = 57
+

479f60b2a47a70942b5716bf9be7ce22939013ad
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2013-01-18 08:54:40 +0000

    Adding @describe annotation to core.workflows._conditions; The describe annotation provides translatable descriptive text which are used to list the conditions in the configuration UI

--- bungeni.main/trunk/bungeni/core/workflows/_conditions.py
+++ bungeni.main/trunk/bungeni/core/workflows/_conditions.py
@@ -21,15 +21,19 @@ from bungeni.core.workflows import utils
 from bungeni.ui.interfaces import IFormEditLayer
 from bungeni.ui.utils import common
 from bungeni.utils import naming
+from bungeni.utils.misc import describe
 
 # common
+
 
 # the condition for the transition from "" (None) to either "draft" or to 
 # "working_draft" seems to need the explicit condition (and negation of 
 # condition) on each of the two transition options 
+@describe("When the user is not the owner in the current context")
 def user_is_not_context_owner(context):
     return not user_is_context_owner(context)
-
+    
+@describe("When the user is the owner in the current context")
 def user_is_context_owner(context, owner_id=None):
     """Test if current user is the context owner e.g. to check if someone 
     manipulating the context object is other than the owner of the object.
@@ -42,6 +46,7 @@ def user_is_context_owner(context, owner_id=None):
         return model_utils.is_current_or_delegated_user(owner_id)
     return model_utils.is_current_or_delegated_user(context.owner_id)
 
+@describe("Can anonymous view in the current context")
 def context_is_public(context):
     """Is the context public i.e. can Anonymous see it?
     """
@@ -51,7 +56,7 @@ def context_is_public(context):
 
 
 # parliamentary items
-
+@describe("Is the document scheduled ?")
 def is_scheduled(doc):
     """Is doc scheduled?
     """
@@ -59,7 +64,7 @@ def is_scheduled(doc):
 
 
 # group
-
+@describe("group: Is the end date set for the group ?") 
 def has_end_date(context):
     """A group can only be dissolved if an end date is set.
     """
@@ -68,28 +73,35 @@ def has_end_date(context):
 
 # sitting
 
+@describe("sitting: Is there a venue set in the current context")
 def has_venue(context):
     return context.venue is not None
 
+@describe("sitting: Is there a agenda in the current context")
 def has_agenda(context):
     return len(context.items)>0
 
+@describe("sitting: Is the agenda finalized")
 def agenda_finalized(context):
     return utils.check_agenda_finalized(context)
 
+@describe("sitting: dummy ?")
 def sitting_dummy(context):
     return context.recurring_type == 'none'
 
 # question
 
+@describe("question: Does the question require a written response ?")
 def is_written_response(context):
     return (context.ministry_id is not None and 
         context.response_type == "written"
     )
-
+    
+@describe("question: Does the question require a oral response ?")
 def is_oral_response(context):
     return context.response_type == "oral"
 
+@describe("question: Allow the response to be submitted in the current context ?")
 def response_allow_submit(context):
     # The "submit_response" workflow transition should NOT be displayed when 
     # the UI is displaying the question in "edit" mode (as this transition
@@ -105,31 +117,40 @@ def response_allow_submit(context):
 
 # user
 
+@describe("user: Is date of death set")
 def has_date_of_death(context):
     return context.date_of_death is not None
 
+@describe("user: Is date of death not set")
 def not_has_date_of_death(context):
     return context.date_of_death is None
 
 
 # child items
 
+@describe("attachments, events etc: Is the parent document in draft state ?")
 def context_parent_is_draft(context):
     """Is parent context in a draft state?
     A doc is a draft iff its current current state is tagged with "draft".
     """
     parent_state = get_object_state(context.head)
     return "draft" in parent_state.tags
+
+@describe("attachments, events etc: Is the parent document not in draft state ?")
 def context_parent_is_not_draft(context):
     return not context_parent_is_draft(context)
 
+@describe("attachments, events etc: Is the parent document visible to Anonymous users ?")
 def context_parent_is_public(context):
     """Is the parent context public i.e. can Anonymous see it?
     """
     return context_is_public(context.head)
+    
+@describe("attachments, events etc: Is the parent document visible not to Anonymous users ?")
 def context_parent_is_not_public(context):
     return not context_is_public(context.head)
 
+@describe("attachments, events etc: Can the user edit the parent document in the current context ?")
 def user_may_edit_context_parent(context):
     """Does user have edit permission on the context's parent?
     For a context that is a workflowed sub-object, such as an Attachment or 
@@ -141,10 +162,11 @@ def user_may_edit_context_parent(context):
 
 
 # signatory
-
+@describe("signatory: Is the user the owner of the parent document ? ")
 def user_is_parent_document_owner(context):
     return context.owner.login == context.head.owner.login
 
+@describe("signatory: Can the document be auto-signed (if it is the owner signing the document) ?")
 def signatory_auto_sign(context):
     """Determines whether signature is automatically signed when a signatory
     is added.
@@ -166,24 +188,29 @@ def signatory_auto_sign(context):
         return True
     return False
 
+@describe("signatory: Does the signatory need to manually sign it ?")
 def signatory_manual_sign(context):
     return not signatory_auto_sign(context)
 
+@describe("signatory: Is the user not the owner of the parent document ? ")
 def user_is_not_parent_document_owner(context):
     return not user_is_parent_document_owner(context)
 
+@describe("signatory: Are there signatories ? ")
 def pi_has_signatories(context):
     manager = ISignatoryManager(context, None)
     if manager is not None:
         return manager.validate_signatories()
     return True
 
+@describe("signatory: Are there consented signatories ? ")
 def pi_signatories_check(context):
     manager = ISignatoryManager(context, None)
     if manager is not None:
         return manager.validate_consented_signatories()
     return True
 
+@describe("signatory: Is the signature period expired ? ")
 def pi_signature_period_expired(context):
     """The document has been submitted"""
     manager = ISignatoryManager(context.head, None)
@@ -191,11 +218,13 @@ def pi_signature_period_expired(context):
         return manager.expire_signatures()
     return False
 
+@describe("signatory: Has the parent document been redrafted ?")
 def pi_document_redrafted(context):
     """Parent document has been redrafted"""
     manager = ISignatoryManager(context.head, None)
     return manager and manager.document_is_draft()
 
+@describe("signatory: Has the signature been withdrawn ?")
 def pi_unsign_signature(context):
     manager = ISignatoryManager(context.head, None)
     if manager:
@@ -204,6 +233,7 @@ def pi_unsign_signature(context):
         )
     return False
 
+@describe("signatory: Is the signatory allowed to sign ?")
 def pi_allow_signature(context):
     manager = ISignatoryManager(context.head, None)
     if manager is not None:
@@ -211,6 +241,7 @@ def pi_allow_signature(context):
             manager.allow_signature())
     return False
 
+@describe("signatory: Is the signatory allowed to withdraw or reject ?")
 def pi_allow_signature_actions(context):
     """allow/disallow other signature actions => such as withdraw and reject
     """

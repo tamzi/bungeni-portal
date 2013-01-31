cc9dc97ae25771d7ae89257172fe8cba27644e4d
mianonjoka <mianonjoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2013-01-31 15:08:51 +0000

    Deleted obsolete bungeni.hansard package

--- bungeni.buildout/trunk/src/bungeni.hansard/bungeni/hansard/__init__.py
+++
@@ -1,6 +0,0 @@
-from models.schema import metadata
-from models.domain import Hansard, Speech, SpeechChange, SpeechVersion, \
-                   Take, Assignment, HansardMediaPaths, HansardItem, \
-                   HansardParliamentaryItem, HansardParliamentaryItemChange, \
-                   HansardParliamentaryItemVersion 
-import models.orm
--- bungeni.buildout/trunk/src/bungeni.hansard/bungeni/hansard/interfaces.py
+++
@@ -1,3 +0,0 @@
-from zope import interface
-class IHansard(interface.Interface):
-    """Hansard items"""

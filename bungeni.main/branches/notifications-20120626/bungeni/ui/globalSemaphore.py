ba081b538c7c993d59b641d6e6e3c9398c0db756
mianonjoka <mianonjoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2013-02-08 07:33:56 +0000

    Removed obsolete notifications branches

--- bungeni.main/branches/notifications-20120626/bungeni/ui/globalSemaphore.py
+++
@@ -1,8 +0,0 @@
-from threading import BoundedSemaphore
-from zope.component import getUtility
-from interfaces import IOpenOfficeConfig
-
-# Singleton semaphore used to limit the number of connections to openoffice
-# to number set in openoffice.zcml
-maxConnections = getUtility(IOpenOfficeConfig).getMaxConnections()
-globalOpenOfficeSemaphore =  BoundedSemaphore(value=maxConnections) 

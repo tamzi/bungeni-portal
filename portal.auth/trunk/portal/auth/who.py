730935e4f9285416642182528de4c12d027399d6
mianonjoka <mianonjoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2013-03-06 15:46:47 +0000

    Added repoze.who identifier, authenticator and challenger plugins to handle OAuth 2.0 access to the api

--- portal.auth/trunk/portal/auth/who.py
+++ portal.auth/trunk/portal/auth/who.py
@@ -2,14 +2,18 @@
 ``repoze.who`` authenticator plugins against a relational database
 """
 log = __import__("logging").getLogger("portal.auth.who")
-
+from datetime import datetime
+from paste.httpheaders import REQUEST_METHOD
+from paste.httpheaders import PATH_INFO
+from paste.httpheaders import CONTENT_TYPE
+from paste.httpheaders import AUTHORIZATION
+from paste.httpexceptions import HTTPUnauthorized
 from zope import interface
 from zope.app.security.principalregistry import principalRegistry
 from zope.app.security.interfaces import PrincipalLookupError
-
-from repoze.who.interfaces import IAuthenticator
-from repoze.who.interfaces import IMetadataProvider
-
+from repoze.who.interfaces import (
+    IAuthenticator, IMetadataProvider, IRequestClassifier, IIdentifier,
+    IChallenger)
 from bungeni.alchemist import Session
 from sqlalchemy.exc import UnboundExecutionError
 import sqlalchemy as rdb
@@ -21,6 +25,7 @@ from bungeni.models import domain, delegation
 # that mappings of domain classes to schema tables is executed.
 from bungeni.models import orm
 
+
 def get_user(login_id):
     session = Session()
     try:
@@ -30,7 +35,7 @@ def get_user(login_id):
         log.warn("No user with login id, %s exists" % login_id)
         return None
     except rdb.orm.exc.MultipleResultsFound:
-        # !+non_unique_login(mr, oct-2011) impossible, login column is UNIQUE!
+        #Should never happen as login column is unique
         log.error("Multiple users found with same login id, %s" % login_id)
         raise
     else:
@@ -54,8 +59,8 @@ def get_user_groups(login_id):
             principal_ids.append(membership.group.group_principal_id)
         return principal_ids
     groups = set()
-    user = get_user(login_id) # user may be None
-    if user: 
+    user = get_user(login_id)  # user may be None
+    if user:
         groups.add(login_id)
         user_id = user.user_id
         for elem in get_groups(user_id):
@@ -63,6 +68,7 @@ def get_user_groups(login_id):
         for user in delegation.get_user_delegations(user):
             for elem in get_groups(user.user_id):
                 groups.add(elem)
+            groups.add(user.login)
     return groups
 
 
@@ -137,3 +143,81 @@ class GlobalAuthWhoPlugin(object):
                 'title': principal.title,
                 'groups': tuple(principal.groups) + (principal.id,),
                 })
+
+
+def bungeni_request_classifier(environ):
+    """ Returns one of the classifiers 'api', 'xmlpost', or 'browser',
+    depending on the imperative logic below"""
+    if PATH_INFO(environ).startswith("/api"):
+        return 'api'
+    request_method = REQUEST_METHOD(environ)
+    if request_method == 'POST':
+        if CONTENT_TYPE(environ) == 'text/xml':
+            return 'xmlpost'
+    return 'browser'
+interface.directlyProvides(bungeni_request_classifier, IRequestClassifier)
+
+
+class OAuthIdentifierPlugin(object):
+    interface.implements(IIdentifier)
+
+    def identify(self, environ):
+        authorization = AUTHORIZATION(environ)
+        tokens = authorization.split(" ")
+        if len(tokens) != 2:
+            return None
+        if tokens[0] != "Bearer":
+            return None
+        return {"access_token": tokens[1]}
+
+    def forget(self, environ, identity):
+        return None
+
+    def remember(self, environ, identity):
+        return None
+
+
+class OAuthChallengerPlugin(object):
+
+    interface.implements(IChallenger)
+
+    # IChallenger
+    def challenge(self, environ, status, app_headers, forget_headers):
+        return HTTPUnauthorized()
+
+
+class OAuthAuthenticatorPlugin(AlchemistWhoPlugin):
+    """OAuth identifier, authenticator"""
+    interface.implements(IAuthenticator, IMetadataProvider)
+
+    def authenticate(self, environ, identity):
+        if not identity:
+            return None
+        if not "access_token" in identity:
+            return None
+        session = Session()
+        try:
+            access_token = session.query(domain.OAuthAccessToken).filter(
+                rdb.and_(
+                    domain.OAuthAccessToken.access_token == identity["access_token"], 
+                    domain.OAuthAccessToken.expiry > datetime.now()
+                )).one()
+        except rdb.orm.exc.NoResultFound:
+            return None
+        except rdb.orm.exc.MultipleResultsFound:
+            # One in a gazillion chance that same access token is generated twice
+            log.error("Multiple records for same access token.")
+            return None
+        if access_token.authorization.active == False:
+            return None
+        return access_token.authorization.user.login
+
+
+
+
+
+
+
+
+
+

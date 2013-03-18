989aec0203e527b61065ef45c5767d09a862a7b8
borona.murithi <borona.murithi@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2013-03-18 16:00:48 +0000

    removing private branch

--- bungeni.main/branches/exist_search/branches/mr/bungeni/ui/forms/interfaces.py
+++
@@ -1,9 +0,0 @@
-from zope.viewlet.interfaces import IViewletManager
-
-class ISubFormViewletManager(IViewletManager):
-    """Viewlet manager for subform viewlets."""
-    
-class IResponeQuestionViewletManager( IViewletManager ):
-    """
-    Manager for question in reponseform
-    """
--- bungeni.main/branches/exist_search/branches/sterch-issue706/bungeni/ui/descriptor/__init__.py
+++
@@ -1,5 +0,0 @@
-
-# !+catalyse_descriptors(mr, jul-2011)
-from bungeni.ui.descriptor import descriptor
-descriptor.catalyse_descriptors()
-

2048ca349c2ed1087f1b3541e690afca4fc6aa9a
borona.murithi <borona.murithi@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2013-03-19 15:13:13 +0000

    [exist search branch] extending processing of results. workspace url generation.

--- bungeni.main/branches/exist_search/bungeni/ui/exist_search/search.py
+++ bungeni.main/branches/exist_search/bungeni/ui/exist_search/search.py
@@ -9,20 +9,59 @@ import json
 import urllib2
 import urllib
 import zope.interface
+import zope.component
 from zope.formlib import form, namedtemplate
 from zope.app.pagetemplate import ViewPageTemplateFile
 from ploned.ui.interfaces import IBelowContentManager
-from bungeni.core.interfaces import ISection
+from bungeni.core.interfaces import ISection, IWorkspaceTabsUtility
 from bungeni.ui import interfaces as ui_ifaces, browser
 import interfaces
-from bungeni.ui.utils.common import get_context_roles
+from bungeni.ui.utils.common import get_context_roles, get_workspace_roles
 from bungeni.ui.i18n import _
+
+from bungeni.models import domain
+
 from bungeni.utils import register
+from bungeni.capi import capi
+
+def make_url(type_id, type_name, status):
+    if type_id and type_name and status:
+        tabs_config = zope.component.getUtility(IWorkspaceTabsUtility)
+        ws_roles = get_workspace_roles()
+        domain_class = getattr(domain, type_name)
+        ti = capi.get_type_info(domain_class)
+        tab = tabs_config.get_tab(ws_roles[0], domain_class, status)
+        if tab:
+            return "./my-documents/%s/%s-%s" %(
+                tab, ti.workflow_key, type_id
+            )
+    return "#"
 
 SEARCH_URL = "http://localhost:8088/exist/restxq/ontology"
-def execute_search(data):
+def execute_search(data, prefix):
+    data = dict([(key.lstrip("%s." % prefix), value) 
+        for key,value in data.iteritems()])
     search_request = urllib2.Request(SEARCH_URL, urllib.urlencode(data))
-    return json.loads(urllib2.urlopen(search_request).read())
+    exist_results = json.loads(urllib2.urlopen(search_request).read())
+    results = {
+        "total": exist_results.get("total"),
+        "items": []
+    }
+    for doc in exist_results.get("doc", []):
+        record = doc.get("ontology")
+        doc = record.get("document") or record.get("group") or record.get("sitting") or {}
+        item = {}
+        item["type"] = doc.get("docType").get("value").get("#text")
+        item["title"] = doc.get("title", {}).get("#text", item["type"])
+        item["author"] = doc.get("author", "Unknown Author")
+        item["status"] = doc.get("status").get("value").get("#text")
+        item["status_date"] = doc.get("statusDate", {}).get("#text", "")
+        item["keys"] = doc.keys()
+        item["id"] = doc.get("id")
+        item["doc_id"] = doc.get("docId", {}).get("#text", "")
+        item["url"] = make_url(item["doc_id"], item["type"], item["status"])
+        results["items"].append(item)
+    return results
 
 @register.view(ISection, ui_ifaces.IBungeniSkin, 
     name="search.exist", protect={ "zope.Public": register.VIEW_DEFAULT_ATTRS })
@@ -34,7 +73,6 @@ class Search(form.PageForm, browser.BungeniBrowserView):
     form_name = _(u"Bungeni Search")
     form_description = _(u"Search Documents in Bungeni")
     show_results = False
-    prefix = ""
 
     def __init__(self, context, request):
         zope.interface.declarations.alsoProvides(
@@ -47,14 +85,17 @@ class Search(form.PageForm, browser.BungeniBrowserView):
         self.show_results = True
         #data["role"] = get_context_roles(self.context, 
         #    self.request.principal)
-        self.search_results = execute_search(data)
-        self.status = _("Searched for ${search_string} and found ${count} "
+        self.search_results = execute_search(data, self.prefix)
+        self.status = _("Searched for '${search_string}' and found ${count} "
             "items", 
             mapping={ 
-                "search_string" : data.get("search", _("everything")), 
+                "search_string" : data.get("search") or _("everything"), 
                 "count": self.search_results.get("total")
             }
         )
+
+    def validate(self, action, data):
+        return form.getWidgetsData(self.widgets, self.prefix, data)
 
 @register.viewlet(interfaces.ISearchResults, layer=ui_ifaces.IBungeniSkin,
     manager=IBelowContentManager, name="bungeni.exist-search",
@@ -71,3 +112,4 @@ class SearchResults(browser.BungeniViewlet):
     @property
     def available(self):
         return self._parent.show_results
+    for_display = available

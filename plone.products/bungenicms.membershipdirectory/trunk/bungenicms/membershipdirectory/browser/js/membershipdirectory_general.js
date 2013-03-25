f173bc40187c4fbea1d10c2d8376d7ca5b67f5a5
fraogongi@gmail.com <fraogongi@gmail.com@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2013-03-25 06:34:19 +0000

    [Membership Directory] Removing constituency search using a blank county drop-down.

--- plone.products/bungenicms.membershipdirectory/trunk/bungenicms/membershipdirectory/browser/js/membershipdirectory_general.js
+++ plone.products/bungenicms.membershipdirectory/trunk/bungenicms/membershipdirectory/browser/js/membershipdirectory_general.js
@@ -10,30 +10,32 @@ jQuery(function($) {
     });
     
     $('#county').change(function(e) {
-        $.ajax({
-            type: 'POST',
-            url:'/fetch_constituencies',
-            data: {
-                'county':$(this).val()
-            },
-            dataType: 'JSON',
-            traditional: true,
-            success:function(data){
-                constituencies_obj = $.parseJSON(data);
-                
-                var selectList = "<span>Constituency :</span>";
-                selectList += "<select id='constituency' name='constituency'>";
-                selectList += "<option value=''>All</option>";
-                for (var x = 0; x < constituencies_obj.length; x++) {
-                    selectList += "<option value='" + constituencies_obj[x] + "'>" + constituencies_obj[x] + "</option>";
+        if ($(this).val() != "") {
+            $.ajax({
+                type: 'POST',
+                url:'/fetch_constituencies',
+                data: {
+                    'county':$(this).val()
+                },
+                dataType: 'JSON',
+                traditional: true,
+                success:function(data){
+                    constituencies_obj = $.parseJSON(data);
+                    
+                    var selectList = "<span>Constituency :</span>";
+                    selectList += "<select id='constituency' name='constituency'>";
+                    selectList += "<option value=''>All</option>";
+                    for (var x = 0; x < constituencies_obj.length; x++) {
+                        selectList += "<option value='" + constituencies_obj[x] + "'>" + constituencies_obj[x] + "</option>";
+                    }
+                    selectList += "</select>";
+                    $('#constituencies-container').html(selectList);              
+                },
+                error:function(errorThrown){
+                    console.log(errorThrown);
                 }
-                selectList += "</select>";
-                $('#constituencies-container').html(selectList);              
-            },
-            error:function(errorThrown){
-                console.log(errorThrown);
-            }
-        });
+            });
+        }
     });
 
 });

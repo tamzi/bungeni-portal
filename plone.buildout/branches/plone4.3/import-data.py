c89ab5bf8d76b8056a26ea466524ae3b71812ca9
mngoka@gmail.com <mngoka@gmail.com@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2013-04-16 14:17:43 +0000

    [Plone] 4.3 buildout branch

---
+++ plone.buildout/branches/plone4.3/import-data.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import urllib
+
+username = 'admin'
+password = 'admin'
+url = 'localhost:8082/plone'
+
+ids_to_transfer = ['front-page','images', 'news', 'plone-bungeni-content-types']
+
+for id in ids_to_transfer:
+
+    urllib.urlopen('http://%s:%s@%s/manage_delObjects?ids=%s' % (username, password, url, id))
+    urllib.urlopen('http://%s:%s@%s/manage_importObject?file=%s.zexp' % (username, password, url,id))
+
+print "Data imported successfully"    

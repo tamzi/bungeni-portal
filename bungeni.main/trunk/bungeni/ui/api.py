849fd2c7ffa0095deca736062c03388863305e55
mianonjoka <mianonjoka@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2013-05-07 07:05:22 +0000

    [Debate] Updated Takes API listing to add url of respective media file.

--- bungeni.main/trunk/bungeni/ui/api.py
+++ bungeni.main/trunk/bungeni/ui/api.py
@@ -6,6 +6,7 @@ from zope.publisher.interfaces import NotFound
 from zope.security.proxy import removeSecurityProxy
 from zope.app.publication.traversers import SimpleComponentTraverser
 from zope.dublincore.interfaces import IDCDescriptiveProperties
+from bungeni.alchemist import container
 from bungeni.core.serialize import obj2dict
 from bungeni.ui.browser import BungeniBrowserView
 from bungeni.ui.utils import url, misc
@@ -48,7 +49,7 @@ class APIDebateRecordView(BrowserPage):
         data["end_date"] = self.context.sitting.end_date
         data["title"] = IDCDescriptiveProperties(self.context).title
         data["url"] = url.absoluteURL(self.context, self.request)
-        data["media"] = obj2dict(self.context.debate_media, 0)
+        data["media"] = [obj2dict(m, 0) for m in self.context.debate_media]
         return simplejson.dumps(data, default=dthandler)
 
 
@@ -80,7 +81,7 @@ class APIUserContainerTraverser(SimpleComponentTraverser):
         raise NotFound(self.context, name)
 
 
-class APITranscriberListing(ContainerJSONListingRaw):
+class APITakeListing(ContainerJSONListingRaw):
 
     def query_add_filters(self, query):
         login = self.request.get("filter_transcriber_login", None)
@@ -88,3 +89,40 @@ class APITranscriberListing(ContainerJSONListingRaw):
             query = query.join(domain.User).filter(domain.User.login == login)
         return query
 
+    def get_batch(self, start, limit):
+        """Get the data instances for this batch.
+        """
+        media_url = ""
+        trusted = removeSecurityProxy(self.context)
+        for media in trusted.__parent__.debate_media:
+            if media.media_type == "transcription":
+                media_url = media.media_path
+                break
+        batch = super(APITakeListing, self).get_batch(start, limit)
+        for item in batch:
+            if media_url == "":
+                item.media_url = ""
+            else:
+                start_time = item.start_date - \
+                    item.debate_record.sitting.start_date
+                end_time = item.end_date - \
+                    item.debate_record.sitting.start_date
+                item.media_url = "{0}?t=npt:{1}/{2}".format(media_url,
+                    str(start_time), str(end_time))
+        return batch
+
+    def _json_values(self, nodes):
+        """Return nodes as JSON"""
+        values = []
+        for node in nodes:
+            d = {}
+            for field in self.fields:
+                fn = field.__name__
+                d[fn] = getattr(node, fn, None)
+                v = d[fn]
+                if type(v) in (date, time, datetime):
+                    d[fn] = v.isoformat()
+            d["object_id"] = url.set_url_context(container.stringKey(node))
+            d["media_url"] = node.media_url
+            values.append(d)
+        return values

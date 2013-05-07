6fbd5b56fc3f9f8e13910fa571eee1bfb601975a
mngoka@gmail.com <mngoka@gmail.com@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2013-05-07 15:53:17 +0000

    [Plone] Updating control panel views for user group membership listing and group details to show correct title and name information.

--- plone.products/bungenicms.plonepas/trunk/bungenicms/plonepas/usergroups.py
+++ plone.products/bungenicms.plonepas/trunk/bungenicms/plonepas/usergroups.py
@@ -26,6 +26,86 @@ from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 from Products.PluggableAuthService.interfaces.plugins import IRolesPlugin
 from plone.app.controlpanel import usergroups
 
+class GroupDetailsControlPanel(usergroups.UsersGroupsControlPanelView):
+
+    def __call__(self):
+        context = aq_inner(self.context)
+        glist = getToolByName(self, 'acl_users').searchGroups()
+         
+        self.gtool = getToolByName(context, 'portal_groups')
+        self.gdtool = getToolByName(context, 'portal_groupdata')
+        self.regtool = getToolByName(context, 'portal_registration')
+        self.groupname = getattr(self.request, 'groupname', None)
+        self.grouproles = self.request.set('grouproles', [])
+        self.group = self.gtool.getGroupById(self.groupname)
+        self.grouptitle = self.groupname
+        
+        if self.group is not None:
+            glist = [item for item in glist if item["groupid"] == self.group.id]
+            if len(glist) == 1:
+                self.grouptitle = glist[0]["title"]
+            else:         
+                self.grouptitle = self.group.getGroupTitleOrName()
+
+        self.request.set('grouproles', self.group.getRoles() if self.group else [])
+
+        submitted = self.request.form.get('form.submitted', False)
+        if submitted:
+            CheckAuthenticator(self.request)
+
+            msg = _(u'No changes made.')
+            self.group = None
+
+            title = self.request.form.get('title', None)
+            description = self.request.form.get('description', None)
+            addname = self.request.form.get('addname', None)
+
+            if addname:
+                if not self.regtool.isMemberIdAllowed(addname):
+                    msg = _(u'The group name you entered is not valid.')
+                    IStatusMessage(self.request).add(msg, 'error')
+                    return self.index()
+
+                success = self.gtool.addGroup(addname, (), (), title=title,
+                                              description=description,
+                                              REQUEST=self.request)
+                if not success:
+                    msg = _(u'Could not add group ${name}, perhaps a user or group with '
+                            u'this name already exists.', mapping={u'name' : addname})
+                    IStatusMessage(self.request).add(msg, 'error')
+                    return self.index()
+
+                self.group = self.gtool.getGroupById(addname)
+                msg = _(u'Group ${name} has been added.',
+                        mapping={u'name' : addname})
+
+            elif self.groupname:
+                self.gtool.editGroup(self.groupname, roles=None, groups=None,
+                                     title=title, description=description,
+                                     REQUEST=context.REQUEST)
+                self.group = self.gtool.getGroupById(self.groupname)
+                msg = _(u'Changes saved.')
+
+            else:
+                msg = _(u'Group name required.')
+
+            processed = {}
+            for id, property in self.gdtool.propertyItems():
+                processed[id] = self.request.get(id, None)
+
+            if self.group:
+                # for what reason ever, the very first group created does not exist
+                self.group.setGroupProperties(processed)
+
+            IStatusMessage(self.request).add(msg, type=self.group and 'info' or 'error')
+            if self.group and not self.groupname:
+                target_url = '%s/%s' % (self.context.absolute_url(), '@@usergroup-groupprefs')
+                self.request.response.redirect(target_url)
+                return ''
+
+        return self.index()
+
+
 class GroupsOverviewControlPanel(usergroups.UsersGroupsControlPanelView): 
 
     def __call__(self):
@@ -154,5 +234,61 @@ class GroupsOverviewControlPanel(usergroups.UsersGroupsControlPanelView):
             groupstool.removeGroups(delete)
             message=_(u'Group(s) deleted.')
 
-        utils.addPortalMessage(message)         
+        utils.addPortalMessage(message)    
+        
+class UserMembershipControlPanel(usergroups.UsersGroupsControlPanelView):
+
+    def update(self):
+        self.userid = getattr(self.request, 'userid')
+        self.gtool = getToolByName(self, 'portal_groups')
+        self.mtool = getToolByName(self, 'portal_membership')
+        self.member = self.mtool.getMemberById(self.userid)
+
+        form = self.request.form
+
+        self.searchResults = []
+        self.searchString = ''
+        self.newSearch = False
+
+        if form.get('form.submitted', False):
+            delete = form.get('delete', [])
+            if delete:
+                for groupname in delete:
+                    self.gtool.removePrincipalFromGroup(self.userid, groupname, self.request)
+                self.context.plone_utils.addPortalMessage(_(u'Changes made.'))
+
+            add = form.get('add', [])
+            if add:
+                for groupname in add:
+                    group = self.gtool.getGroupById(groupname)
+                    if 'Manager' in group.getRoles() and not self.is_zope_manager:
+                        raise Forbidden
+
+                    self.gtool.addPrincipalToGroup(self.userid, groupname, self.request)
+                self.context.plone_utils.addPortalMessage(_(u'Changes made.'))
+
+        search = form.get('form.button.Search', None) is not None
+        findAll = form.get('form.button.FindAll', None) is not None and not self.many_groups
+        self.searchString = not findAll and form.get('searchstring', '') or ''
+
+        if findAll or not self.many_groups or self.searchString != '':
+            self.searchResults = self.getPotentialGroups(self.searchString)
+
+        if search or findAll:
+            self.newSearch = True
+
+        self.groups = self.getGroups()
+
+    def __call__(self):
+        self.update()
+        return self.index()
+
+    def getGroups(self):
+        groupResults = [self.gtool.getGroupById(m) for m in self.gtool.getGroupsForPrincipal(self.member)]
+        groupResults.sort(key=lambda x: x is not None and normalizeString(x.getGroupTitleOrName()))
+        return filter(None, groupResults)
+
+    def getPotentialGroups(self, searchString):
+        ignoredGroups = [x.id for x in self.getGroups() if x is not None]
+        return self.membershipSearch(searchString, searchUsers=False, ignore=ignoredGroups)             
          

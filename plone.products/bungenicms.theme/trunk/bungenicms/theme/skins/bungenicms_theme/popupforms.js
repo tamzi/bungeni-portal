640c647db0cc5560fcc57955f024bf13ec690e57
mngoka <mngoka@gmail.com>
2013-06-07 14:50:06 +0000

    [Plone/Portal] Updating login script to always redirect to the homepage.

--- plone.products/bungenicms.theme/trunk/bungenicms/theme/skins/bungenicms_theme/popupforms.js
+++ plone.products/bungenicms.theme/trunk/bungenicms/theme/skins/bungenicms_theme/popupforms.js
@@ -64,14 +64,15 @@ jQuery(function($){
             filter: common_content_filter,
             formselector: 'form#login_form',
             cssclass: 'overlay-login',
-            noform: function () {
-                return 'redirect';            
+            noform: function () {                                    
+                return 'redirect';
             },
             redirect: function () {
                 var href = location.href;
                 if (href.search(/pwreset_finish$/) >= 0) {
                     return href.slice(0, href.length-14) + 'logged_in';
                 } else {    
+                    href = location.host
                     return location.protocol + "//" +location.host;
                 }
             }

d0dfbeda3de010f1fbc29f10d4d02592ff9c2d0e
ashok.hariharan <ashok.hariharan@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2014-07-30 17:49:59 +0000

    Preparing for release of update package

--- debian_package/trunk/updates/update_version_revision/fabfile.py
+++ debian_package/trunk/updates/update_version_revision/fabfile.py
@@ -4,6 +4,8 @@ from ConfigParser import SafeConfigParser as _scg
 from required import Templates as _tmpl
 import os
 
+__debpack_scripts__ = ["preinst", "postinst", "prerm", "postrm"]
+
 
 class __UpdateConfigParser():
 
@@ -28,9 +30,9 @@ def __platform():
         return "i386" 
 
 
-def _build_archive(archive_name, source_folder):
+def _build_archive(archive_name,  update_config_folder):
     archive = "%s.tar.gz" % archive_name
-    run("tar -czf %s  -T %s/update.include -X %s/update.exclude " % (archive, source_folder, source_folder))
+    run("tar -czf %s  -T %s/update.include -X %s/update.exclude " % (archive, update_config_folder, update_config_folder))
     return os.path.join(env.cwd, archive)
 
 def _get_size():
@@ -38,11 +40,21 @@ def _get_size():
     return size
 
     
-def build_update(in_folder):
-    cfg = __UpdateConfigParser("update.ini")
-    ver = cfg.get_update_version()
+def build_update(in_folder, version):
+    
+    folder_name = "bungeni_update_%s" % version
+    update_config_folder = os.path.join(in_folder, folder_name)
+     
+    if not os.path.exists(update_config_folder):
+        print "Update configuration folder not found, aborting !"
+        return
+    
+    cfg = __UpdateConfigParser(
+        os.path.join(update_config_folder, "update.ini")
+    )
+    ver =  cfg.get_update_version()
+
     depends = cfg.get_update_depends()
-    folder_name = "bungeni_update_%s" % ver
     #in_folder is the source folder
     #target folder is the target folder
     parent_in_folder = os.path.realpath(
@@ -52,25 +64,28 @@ def build_update(in_folder):
         os.path.join(parent_in_folder, folder_name)
     )
     with cd(parent_in_folder):
-         archive_path = _build_archive(folder_name, in_folder)              
-         with cd(in_folder):
-            run("mkdir -p %s" % target_folder)
-            size = _get_size()
-            template_map = {
+         archive_path = _build_archive(folder_name, update_config_folder)              
+         run("mkdir -p %s" % target_folder)
+         size = _get_size()
+         template_map = {
                "size": size,
                "version": ver,
                "arch": __platform(),
                "depends": ",".join(depends)
-            }
-            with cd(target_folder):
-              run("cp -R %s ." % os.path.join(in_folder, "debian"))
-              with cd("debian/DEBIAN"):
-                 t = _tmpl()
-                 t.new_file(
-                     os.path.join(env.cwd, "control.tmpl"),
-                     template_map,
-                     env.cwd
-                 )
+         }
+         with cd(target_folder):
+             run("mkdir -p debian/DEBIAN")
+             for file in __debpack_scripts__:
+                 file_path_in_config = os.path.join(update_config_folder, file)
+                 if os.path.exists(file_path_in_config):
+                    run("cp %s ./debian/DEBIAN" % file_path_in_config)
+             with cd("debian/DEBIAN"):
+                t = _tmpl()
+                t.new_file(
+                    os.path.join(update_config_folderi, "control.tmpl"),
+                    template_map,
+                    env.cwd
+                )
          run("tar xvf %s -C %s" % (archive_path, os.path.join(target_folder, "debian")))
               
 def setup_version():

b738e3906aaaf4e27ccd508ae025c0b0b81dd0b5
mngoka@gmail.com <mngoka@gmail.com@fc5d704a-7d24-0410-8c4a-57ddeba10ffc>
2014-10-26 20:54:10 +0000

    [Plone] Updating tabr and dynatree scripts to use $ notation (jq no longer used) ,reverting back to latest version of jquerytools.

---
+++ plone.products/bungenicms.theme/trunk/bungenicms/theme/skins/bungenicms_theme/dynatree-init.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $('.dynatree-atwidget').each(function () {
+		// get parameters
+		var jqthis = $(this);
+		var valuetarget = $(this)
+		var rawparams = $(this).find('.dynatree_parameters').text().split('/');
+		var params = new Array();
+		for (var idx=0; idx<rawparams.length; idx++) {
+			var pair = rawparams[idx].split(',');
+			var value = pair[1].replace(/^\s+|\s+$/g, "");
+            if (!isNaN(value)) { value = parseInt(value); };
+            if (value=='True') { value = true; };
+            if (value=='False') { value = false; };		
+			params[pair[0].replace(/^\s+|\s+$/g, "")] = value;
+		}
+		// get json url       
+        params['initAjax'] = {
+			url: jqthis.find('.dynatree_ajax_vocabulary').text(),
+            data: {selected: jqthis.find('input').val()}
+		};
+		// activation/ deactivation
+		params['onSelect'] = function(flag, dtnode) {
+			var sel_nodes = $(dtnode.tree.$tree).dynatree('getSelectedNodes');
+			var newvalue = '';
+			for (var idx=0; idx<sel_nodes.length; idx++) { 
+				newvalue = newvalue + sel_nodes[idx].data.key + '|';
+			};  
+			$(dtnode.tree.$tree).siblings('input').val(newvalue);
+		};
+		// init tree
+		jqthis.find('.collective-dynatree-tree').dynatree(params);
+	});
+});
